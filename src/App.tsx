24b973949035fdc1ebaff3ff8bd7fb3b27c05dc7
pcb <per.christian.bjelke@bibsys.no>
2019-09-16 11:07:56 +0200

    set up grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import './App.scss';
+import './styles/App.scss';
 
 const App: React.FC = () => {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <div className="Header">header</div>
+      <div className="Body">body</div>
+      <div className="Footer">footer</div>
+    </div>
+  );
 };
 
 export default App;
