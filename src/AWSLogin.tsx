3f08bed42b4aef8939525732b7a24572a144e14b
pcb <per.christian.bjelke@bibsys.no>
2019-09-18 12:05:46 +0200

    using awsconfig-file

--- src/AWSLogin.tsx
+++ src/AWSLogin.tsx
@@ -2,34 +2,33 @@ import React, { useEffect, useState } from 'react';
 import Amplify, { Auth, Hub } from 'aws-amplify';
 
 import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth/lib/types';
+import awsconfig from './aws-exports';
+// const awsconfig = {
+//   aws_project_region: process.env.REACT_APP_AWS_PROJECT_REGION,
+//   aws_cognito_identity_pool_id: process.env.REACT_APP_AWS_COGNITO_IDENTETY_POOL_ID,
+//   aws_cognito_region: process.env.REACT_APP_AWS_COGNITO_REGION,
+//   aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
+//   aws_user_pools_web_client_id: process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
+//   federationTarget: process.env.REACT_APP_FEDERATIONTARGET,
+// };
+//
+// const oauth = {
+//   domain: 'nva-frontend-auth-test-dev.auth.eu-west-1.amazoncognito.com',
+//   scope: ['phone', 'email', 'openid', 'profile', 'aws.cognito.signin.user.admin'],
+//   redirectSignIn: 'http://localhost:3000/',
+//   redirectSignOut: 'http://localhost:3000/',
+//   responseType: 'code',
+// };
 
-const awsconfig = {
-  aws_project_region: process.env.REACT_APP_AWS_PROJECT_REGION,
-  aws_cognito_identity_pool_id: process.env.REACT_APP_AWS_COGNITO_IDENTETY_POOL_ID,
-  aws_cognito_region: process.env.REACT_APP_AWS_COGNITO_REGION,
-  aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
-  aws_user_pools_web_client_id: process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
-  federationTarget: process.env.REACT_APP_FEDERATIONTARGET,
-};
-
-const oauth = {
-  domain: 'nva-frontend-auth-test-dev.auth.eu-west-1.amazoncognito.com',
-  scope: ['phone', 'email', 'openid', 'profile', 'aws.cognito.signin.user.admin'],
-  redirectSignIn: 'http://localhost:3000/',
-  redirectSignOut: 'http://localhost:3000/',
-  responseType: 'code',
-};
-
-console.log(process.env);
+//console.log(process.env);
 Amplify.configure(awsconfig);
-Auth.configure({ oauth });
+//Auth.configure({ oauth });
 
 const AWSLogin: React.FC = () => {
   const [user, setUser] = useState<any>(null);
 
   useEffect(() => {
     Hub.listen('auth', ({ payload: { event, data } }) => {
-      console.log(event);
       switch (event) {
         case 'signIn':
           setUser(data);
@@ -44,7 +43,6 @@ const AWSLogin: React.FC = () => {
 
     Auth.currentAuthenticatedUser()
       .then(user => {
-        console.log(user);
         setUser(user);
       })
       .catch(err => console.log('Not signed in' + err));
