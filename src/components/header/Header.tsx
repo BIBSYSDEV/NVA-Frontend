5278fd9509cdaa67fe7b42969f3dab617ec69f33
Sarah Serussi <sase@unit.no>
2019-09-23 11:05:34 +0200

    add simple menu to make it easier to navigate

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import '../../styles/header.scss';
 import Login from '../login/Login';
+import Menu from './Menu';
 
 const Header: React.FC = () => (
   <div className="header">
     <div className="logo">NVA</div>
     <Login buttonText="login" />
+    <Menu />
   </div>
 );
 
