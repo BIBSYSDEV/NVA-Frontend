89a23298bf19ecdd014980751a43802127a70f69
Sarah Serussi <sase@unit.no>
2019-10-08 14:49:47 +0200

    test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -6,7 +6,7 @@ describe('A user logs in with Feide', () => {
   });
   it('When they click on the log-in button', () => {
     // Unable to test login via Feide, so mocking login by setting REACY_APP_USE_MOCK=true in .env file
-    cy.contains('login').click();
+    cy.contains('Login').click();
   });
   it('And they are redirected to Feide', () => {
     // mocked during testing
