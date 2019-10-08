9f97b37362e67fb4ccd0008ecd215f239c47f9cd
Sarah Serussi <sase@unit.no>
2019-10-08 12:41:10 +0200

    set baseUrl in cypress

--- cypress/integration/logout.spec.js
+++ cypress/integration/logout.spec.js
@@ -1,14 +1,13 @@
-describe('A user logs out from NVA application', () =>{
-    it('Given that the user is logged in', () => {
-        cy.visit('http://localhost:3000/');
-        cy.contains('Test User');
-    });
-    it('When they click on the log-out button', () => {
-        cy.contains('Logout').click();
-    });
-    it('Then they are logged out of the NVA application', () => {
-        cy.contains('login');
-        cy.contains('Test User').should('not.exist');
-    });
+describe('A user logs out from NVA application', () => {
+  it('Given that the user is logged in', () => {
+    cy.visit('/');
+    cy.contains('Test User');
+  });
+  it('When they click on the log-out button', () => {
+    cy.contains('Logout').click();
+  });
+  it('Then they are logged out of the NVA application', () => {
+    cy.contains('login');
+    cy.contains('Test User').should('not.exist');
+  });
 });
-
