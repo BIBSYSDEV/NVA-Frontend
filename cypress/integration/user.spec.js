2be5d85cb4cdb6ae7558f7a0741702094843d863
pcb <per.christian.bjelke@bibsys.no>
2019-10-14 12:18:43 +0200

    bugfix i spec

--- cypress/integration/user.spec.js
+++ cypress/integration/user.spec.js
@@ -1,4 +1,4 @@
-describe('A user logs in with Feide', () => {
+describe('A user logs in and views their user details', () => {
   it('Given that the user is logged in', () => {
     cy.visit('/');
     cy.get('[data-cy=menu]').contains('Test User');
@@ -6,14 +6,15 @@ describe('A user logs in with Feide', () => {
   it('When they click their user details in the navigation bar', () => {
     cy.get('[data-cy=menu]').click();
   });
-  it(' And they select Profile', () => {
+  it('And they select Profile', () => {
     cy.get('[data-cy=user-profile-button]').click();
   });
-  it(' Then they see their details', () => {
+  it('Then they see their details', () => {
     cy.get('[data-cy=user-name]').contains('Test User');
     cy.get('[data-cy=user-id]').contains('testuser@unit.no');
     cy.get('[data-cy=user-email]').contains('testuser@unit.no');
     cy.get('[data-cy=user-institution]').contains('unit');
+    cy.get('[data-cy=user-applications]').contains('NVA, DLR');
     cy.get('[data-cy=user-role]').contains('Publisher');
   });
 });
