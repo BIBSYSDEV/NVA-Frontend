e6b3bc0506b6d22e8c9d687e68aa3925929bd387
sarahse <sase@unit.no>
2019-10-16 14:38:56 +0200

    Np 97  show previously selected language as selected (#27)
    * set previously selected language as selected when first mounting UserLanguage component
    
    * remove unnecessary update of state

--- src/modules/user/UserLanguage.tsx
+++ src/modules/user/UserLanguage.tsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { FormControl, MenuItem, Select } from '@material-ui/core';
 
-import i18n, { defaultLanguage, languages } from '../../translations/i18n';
+import { defaultLanguage, languages } from '../../translations/i18n';
+import { Language } from '../../types/settings.types';
 import UserCard from './UserCard';
 
 const UserLanguage: React.FC = () => {
-  const [languageSelected, setLanguageSelected] = useState(defaultLanguage);
-  const { t } = useTranslation();
+  const [languageSelected, setLanguageSelected] = useState<Language | string>(defaultLanguage);
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    const previouslySelectedLanguage = i18n.language;
+    setLanguageSelected(previouslySelectedLanguage);
+  }, [i18n.language]);
 
   const handleLanguageChange = (event: React.ChangeEvent<any>) => {
     const language = event.target.value;
-    setLanguageSelected(language);
     i18n.changeLanguage(language);
   };
 
