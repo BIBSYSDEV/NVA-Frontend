1e0b06878c2b7c5cac3c1dfb192fdf9bf5d6479a
placbo <15780177+placbo@users.noreply.github.com>
2019-10-18 09:26:09 +0200

    Np 101 generated json data (#30)
    * added random generated resource-list
    added documentation of how to build new mock-data
    simplified search-test
    
    * added autogenerating of resources using Node
    added script for running jobs (for *nux only)
    
    * ny resource-spec
    
    * trenger ikke template mer
    
    * deletet unused test-files
    
    * fixes after review
    
    * merged

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,4 +1,4 @@
-describe('A user searches for a resource', () => {
+describe('A user searches for resources and browses through the result-pages', () => {
   it('Given that a user is on the start page and is not logged in', () => {
     cy.visit('/');
     cy.get('[data-cy=menu]').click();
@@ -6,62 +6,20 @@ describe('A user searches for a resource', () => {
     cy.get('[data-cy=login-button]').should('be.visible');
     cy.get('[data-cy=menu]').should('not.exist');
   });
-  it('And there are three documents with the word Norway in the title', () => {
-    // mocked during testing. Look in src/utils/testfiles/resources.json
-  });
-  it('When the user enters Norway into the search input', () => {
-    cy.get('[data-cy=search-input] .MuiInputBase-input').type('Norway');
+  it('When the user enters "test" into the search input', () => {
+    cy.get('[data-cy=search-input] .MuiInputBase-input').type('test');
   });
   it('And clicks Search', () => {
     cy.get('[data-cy=search-button]').click({ force: true });
   });
-  it('Then the user sees hits for Norway displayed in the search display', () => {
+  it('And there is 45 results', () => {
     cy.get('[data-cy=search-results]')
       .find('[data-cy=result-list-item] ')
       .should('have.length.greaterThan', 1);
-    cy.get('[data-cy=search-results]').contains('Norway');
-    cy.url().should('include', '/search/Norway');
-  });
-});
-
-describe('A user searches for a resource and gets more than 10 hits', () => {
-  it('Given that a user is on the start page and is not logged in', () => {
-    cy.visit('/');
-    cy.get('[data-cy=menu]').click();
-    cy.get('[data-cy=logout-button]').click();
-    cy.get('[data-cy=login-button]').should('be.visible');
-    cy.get('[data-cy=menu]').should('not.exist');
-  });
-  it('When the user enters Norway into the search input', () => {
-    cy.get('[data-cy=search-input] .MuiInputBase-input').type('Norway');
-  });
-  it('And clicks Search', () => {
-    cy.get('[data-cy=search-button]').click({ force: true });
-  });
-  it('And there is more than 10 results', () => {
-    cy.get('[data-cy=search-results]').contains('25');
+    cy.get('[data-cy=search-results]').contains('45');
   });
   it('Then pagination is shown', () => {
     cy.get('[data-cy=pagination]').contains('2');
-  });
-});
-
-describe('A user searches for a resource and uses pagination', () => {
-  it('Given that a user is on the start page and is not logged in', () => {
-    cy.visit('/');
-    cy.get('[data-cy=menu]').click();
-    cy.get('[data-cy=logout-button]').click();
-    cy.get('[data-cy=login-button]').should('be.visible');
-    cy.get('[data-cy=menu]').should('not.exist');
-  });
-  it('When the user enters Norway into the search input', () => {
-    cy.get('[data-cy=search-input] .MuiInputBase-input').type('Norway');
-  });
-  it('And clicks Search', () => {
-    cy.get('[data-cy=search-button]').click({ force: true });
-  });
-  it('And there is more than 10 results', () => {
-    cy.get('[data-cy=search-results]').contains('25');
   });
   it('And user clicks on ">" in pagination', () => {
     cy.get('[data-cy=pagination]')
--- src/modules/search/SearchResults.tsx
+++ src/modules/search/SearchResults.tsx
@@ -41,18 +41,18 @@ const SearchResults: React.FC<SearchResultsProps> = ({ resources, searchTerm }) 
       <List>
         {resources &&
           resources.map(resource => (
-            <ListItem key={resource.identifier}>
+            <ListItem key={resource.handle}>
               <ListItemIcon>
                 <ImageIcon />
               </ListItemIcon>
               <ListItemText
                 data-cy="result-list-item"
-                primary={resource.title}
+                primary={resource.titles.en}
                 secondary={
                   <React.Fragment>
-                    <Typography component="span">{resource.identifier_handle}</Typography>
+                    <Typography component="span">{resource.creators}</Typography>
                     <br />
-                    {resource.description}
+                    {resource.publisher}
                   </React.Fragment>
                 }>
                 {' '}
