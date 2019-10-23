f65f91640e8a7375ce5d5d5547668be3a43d283f
pcb <per.christian.bjelke@bibsys.no>
2019-10-23 11:50:25 +0200

    added translations

--- src/modules/dashboard/AdminMenu.tsx
+++ src/modules/dashboard/AdminMenu.tsx
@@ -3,16 +3,17 @@ import { Button } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootStore } from '../../reducers/rootReducer';
+import { useTranslation } from 'react-i18next';
 
 const AdminMenu: React.FC = () => {
   const user = useSelector((state: RootStore) => state.user);
-
+  const { t } = useTranslation();
   return (
     <>
       {user.id && (
         <div className="admin-menu">
-          <div className="title">Adminpanel</div>
-          <Button>+ Registrering</Button>
+          <div className="title">{t('Adminpanel')}</div>
+          <Button>{t('New registration')}</Button>
         </div>
       )}
     </>
