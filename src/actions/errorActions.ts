577ce1d37ad247f78091ebdca64dc8382200ffa9
pcb <per.christian.bjelke@bibsys.no>
2019-10-24 08:53:22 +0200

    added actions and reducers

--- src/actions/errorActions.ts
+++ src/actions/errorActions.ts
@@ -1,5 +1,7 @@
 export const LOGIN_FAILURE = 'login failure';
 export const REFRESH_TOKEN_FAILURE = 'refresh token failure';
+export const ORCID_SIGNIN_ERROR = 'orcid sign in failure';
+export const ORCID_REQUEST_ERROR = 'orcid request failure';
 
 export const loginFailureAction = (message: string) => ({
   type: LOGIN_FAILURE,
@@ -8,6 +10,16 @@ export const loginFailureAction = (message: string) => ({
 
 export const refreshTokenFailureAction = (message: string) => ({
   type: REFRESH_TOKEN_FAILURE,
+  message,
+});
+
+export const orcidSignInErrorAction = (message: string) => ({
+  type: ORCID_SIGNIN_ERROR,
+  message,
+});
+
+export const orcidRequestErrorAction = (message: string) => ({
+  type: ORCID_REQUEST_ERROR,
   message,
 });
 
@@ -21,4 +33,13 @@ export interface RefreshTokenFailureAction {
   message: string;
 }
 
-export type ErrorActions = LoginFailureAction | RefreshTokenFailureAction;
+export interface OrcidSignInError {
+  type: typeof ORCID_SIGNIN_ERROR;
+  message: string;
+}
+export interface OrcidRequestError {
+  type: typeof ORCID_REQUEST_ERROR;
+  message: string;
+}
+
+export type ErrorActions = LoginFailureAction | RefreshTokenFailureAction | OrcidRequestError | OrcidSignInError;
--- src/reducers/errorReducer.ts
+++ src/reducers/errorReducer.ts
@@ -1,9 +1,17 @@
-import { ErrorActions, LOGIN_FAILURE, REFRESH_TOKEN_FAILURE } from '../actions/errorActions';
+import {
+  ErrorActions,
+  LOGIN_FAILURE,
+  ORCID_REQUEST_ERROR,
+  ORCID_SIGNIN_ERROR,
+  REFRESH_TOKEN_FAILURE,
+} from '../actions/errorActions';
 import { LOGIN_SUCCESS, LOGOUT_SUCCESS, REFRESH_TOKEN_SUCCESS, SET_USER, UserActions } from '../actions/userActions';
 
 export const errorReducer = (state: string | null = null, action: ErrorActions | UserActions) => {
   switch (action.type) {
     case LOGIN_FAILURE:
+    case ORCID_SIGNIN_ERROR:
+    case ORCID_REQUEST_ERROR:
     case REFRESH_TOKEN_FAILURE:
       return action.message;
     case LOGIN_SUCCESS:
