d59f43e108f76e8d93241df4d012b74e0a40dc8d
pcb <per.christian.bjelke@bibsys.no>
2019-10-25 09:10:39 +0200

    sjekker inn for å teste...

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,32 +1,23 @@
+import mockResources from '../../src/utils/testfiles/resources_2_random_results_generated.json';
+
 describe('A user searches for resources and browses through the result-pages', () => {
-  it('Given that a user is on the start page and is not logged in', () => {
+  it('Search', () => {
     cy.visit('/');
-    cy.get('[data-cy=menu]').click();
-    cy.get('[data-cy=logout-button]').click();
-    cy.get('[data-cy=login-button]').should('be.visible');
-    cy.get('[data-cy=menu]').should('not.exist');
-  });
-  it('When the user enters "test" into the search input', () => {
-    cy.get('[data-cy=search-input] .MuiInputBase-input').type('test');
-  });
-  it('And clicks Search', () => {
+    cy.get('[data-cy=search-input] .MuiInputBase-input').type('XXXXXX');
+
+    cy.server();
+    cy.route('**/resources/**', mockResources); //return mock-data on search
+    cy.log(mockResources);
+
     cy.get('[data-cy=search-button]').click({ force: true });
-  });
-  it('And there is 45 results', () => {
     cy.get('[data-cy=search-results]')
       .find('[data-cy=result-list-item] ')
       .should('have.length.greaterThan', 1);
     cy.get('[data-cy=search-results]').contains('45');
-  });
-  it('Then pagination is shown', () => {
     cy.get('[data-cy=pagination]').contains('2');
-  });
-  it('And user clicks on ">" in pagination', () => {
     cy.get('[data-cy=pagination]')
       .contains('>')
       .click({ force: true });
-  });
-  it('Then the result-title should show "(11 - 20)"', () => {
     cy.get('[data-cy=search-results]').contains('(11 - 20)');
   });
 });
--- src/api/resource.ts
+++ src/api/resource.ts
@@ -1,13 +1,9 @@
 import { Dispatch } from 'redux';
 
-import { useMockData } from '../utils/constants';
-import { mockSearch } from './mock-api';
+import { Search } from './mock-api';
 
 export const search = (searchTerm: string, offset?: number) => {
   return async (dispatch: Dispatch) => {
-    // make api call to search endpoint
-    if (useMockData) {
-      mockSearch(dispatch, searchTerm, offset ? offset : 0);
-    }
+    Search(dispatch, searchTerm, offset ? offset : 0);
   };
 };
