83b1c94b7a4a2c1249cca3f1990db84c620faa71
pcb <per.christian.bjelke@bibsys.no>
2019-10-29 13:58:56 +0100

    travis....

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,5 +1,5 @@
-const mockDataWith2Results = require('../../src/utils/testfiles/resources_2_random_results_generated');
-const mockDataWith45Results = require('../../src/utils/testfiles/resources_45_random_results_generated');
+import mockDataWith2Results from './resources_2_random_results_generated';
+import mockDataWith45Results from './resources_45_random_results_generated';
 
 describe('Searching', () => {
   beforeEach(function() {
