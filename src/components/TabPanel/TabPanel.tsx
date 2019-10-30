77893278206c8d552e29636aa46d207b024a19e7
Sarah Serussi <sase@unit.no>
2019-10-30 10:47:51 +0100

    remove unnecessary props in tab panel

--- src/components/TabPanel/TabPanel.tsx
+++ src/components/TabPanel/TabPanel.tsx
@@ -10,20 +10,15 @@ interface TabPanelProps {
   value: any;
 }
 
-const TabPanel = (props: TabPanelProps) => {
-  const { ariaLabel, children, value, tabNumber, ...other } = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== tabNumber}
-      id={`nav-tabpanel-${ariaLabel}`}
-      aria-labelledby={`nav-tab-${ariaLabel}`}
-      {...other}>
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
-};
+const TabPanel: React.FC<TabPanelProps> = ({ ariaLabel, children, tabNumber, value }) => (
+  <Typography
+    component="div"
+    role="tabpanel"
+    hidden={value !== tabNumber}
+    id={`nav-tabpanel-${ariaLabel}`}
+    aria-labelledby={`nav-tab-${ariaLabel}`}>
+    <Box p={3}>{children}</Box>
+  </Typography>
+);
 
 export default TabPanel;
