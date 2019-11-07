9f5db45f9ce91a54264135813a686148d9b67b66
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2019-11-07 09:01:38 +0100

    Consistent way of declaring styled-components

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -4,12 +4,12 @@ import styled from 'styled-components';
 const StyledFooter = styled.div`
   display: flex;
   min-height: 3rem;
-  color: ${props => props.theme.palette.link.main};
+  color: ${({ theme }) => theme.palette.link.main};
   font-size: 1rem;
   font-weight: bold;
   justify-content: center;
   align-items: center;
-  border-top: 2px solid ${props => props.theme.palette.seperator.main};
+  border-top: 2px solid ${({ theme }) => theme.palette.seperator.main};
 `;
 
 const Footer: React.FC = () => <StyledFooter>footer</StyledFooter>;
