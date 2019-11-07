641268069448dafb783a5a4f26054a15d9b4e661
pcb <per.christian.bjelke@bibsys.no>
2019-11-07 15:01:39 +0100

    NP-163- styles in code again

--- src/pages/user/OrcidModal.tsx
+++ src/pages/user/OrcidModal.tsx
@@ -3,16 +3,38 @@ import { useTranslation } from 'react-i18next';
 import { Button } from '@material-ui/core';
 import { useMockData } from '../../utils/constants';
 import { useHistory } from 'react-router';
-import {
-  StyledFooter,
-  StyledHeading,
-  StyledNavigationHelp,
-  StyledOrcidButtonHolder,
-  StyledSubHeading,
-} from './OricdModal.styles';
+import styled from 'styled-components';
+
+const StyledButtonHolder = styled.div`
+  margin: 3rem 0;
+  text-align: center;
+`;
+
+const StyledNavigationHelp = styled.div`
+  border-bottom: 1px solid black;
+  text-align: right;
+  font-size: 0.8rem;
+  font-weight: bold;
+`;
+
+const StyledHeading = styled.div`
+  font-size: 1.2rem;
+  margin: 1em 0;
+  font-weight: bold;
+`;
+
+const StyledSubHeading = styled.div`
+  margin: 1em 0;
+  font-weight: bold;
+`;
+
+const StyledFooter = styled.div`
+  padding-top: 5rem;
+  text-align: center;
+`;
 
 interface OrcidModalProps {
-  setOpen: any;
+  setOpen: (value: boolean) => void;
 }
 
 const OrcidModal: React.FC<OrcidModalProps> = ({ setOpen }) => {
@@ -36,7 +58,7 @@ const OrcidModal: React.FC<OrcidModalProps> = ({ setOpen }) => {
       <StyledNavigationHelp>{t('Registration')} 2/2</StyledNavigationHelp>
       <StyledHeading>{t('Create or Connect to your ORCID')}</StyledHeading>
       <p>{t('Log in to your ORCID account or create new ORCID account')}</p>
-      <StyledOrcidButtonHolder>
+      <StyledButtonHolder>
         <Button
           data-testid="connect-to-orcid"
           onClick={() => {
@@ -48,7 +70,7 @@ const OrcidModal: React.FC<OrcidModalProps> = ({ setOpen }) => {
           startIcon={<img src="https://orcid.org/sites/default/files/images/orcid_24x24.png" alt="ORCID iD icon" />}>
           {t('Create or Connect to your ORCID')}
         </Button>
-      </StyledOrcidButtonHolder>
+      </StyledButtonHolder>
       <StyledSubHeading>{t('Why should I connect to ORCID?')}</StyledSubHeading>
       <p>{t('description_why_ORCID')}</p>
       <StyledSubHeading>{'What is ORCID?'}</StyledSubHeading>
