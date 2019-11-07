a5d822f0c12f615312b398731dbcb724b8f6363e
pcb <per.christian.bjelke@bibsys.no>
2019-11-07 15:02:40 +0100

    NP-163- styles in code again

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -11,7 +11,30 @@ import UserInfo from './UserInfo';
 import UserLanguage from './UserLanguage';
 import UserOrcid from './UserOrcid';
 import UserRoles from './UserRoles';
-import { StyledPrimaryUserInfo, StyledSecondaryUserInfo, StyledUserPage } from './User.styles';
+import styled from 'styled-components';
+
+const StyledUserPage = styled.div`
+  display: grid;
+  grid-template-areas: 'secondary-info primary-info';
+  grid-template-columns: 1fr 3fr;
+  grid-gap: 3rem;
+  font-size: 1rem;
+  padding: 2rem;
+`;
+
+const StyledSecondaryUserInfo = styled.div`
+  display: grid;
+  grid-area: secondary-info;
+  grid-template-areas: 'profile-image' 'contact-info' 'language' 'author-info';
+  grid-row-gap: 3rem;
+  min-width: 20rem;
+`;
+
+const StyledPrimaryUserInfo = styled.div`
+  display: grid;
+  grid-area: primary-info;
+  grid-gap: 3rem;
+`;
 
 const User: React.FC = () => {
   const { t } = useTranslation();
