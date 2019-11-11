30308155226c9351c7838d1c41f56912597d47aa
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2019-11-11 16:21:01 +0100

    NP-171 Refactor Breadcrumbs to use styled-components

--- src/layout/Breadcrumbs.tsx
+++ src/layout/Breadcrumbs.tsx
@@ -1,5 +1,3 @@
-import '../styles/layout/breadcrumbs.scss';
-
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router';
@@ -9,6 +7,16 @@ import { Breadcrumbs as MuiBreadcrumbs } from '@material-ui/core';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 import { mapBreadcrumbName } from '../utils/mapBreadcrumbName';
+import styled from 'styled-components';
+
+const StyledBreadcrumbs = styled.div`
+  grid-area: breadcrumbs;
+  padding: 0.5rem;
+
+  a {
+    color: ${({ theme }) => theme.palette.text.secondary};
+  }
+`;
 
 const Breadcrumbs: React.FC = () => {
   const location = useLocation();
@@ -16,30 +24,30 @@ const Breadcrumbs: React.FC = () => {
 
   const pathNames = location.pathname.split('/').filter(x => x);
 
+  if (pathNames.length === 0) {
+    return null;
+  }
+
   return (
-    <>
-      {pathNames.length > 0 && (
-        <div className="breadcrumbs">
-          <MuiBreadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
-            <Link to="/">{t('Home')}</Link>
-            {pathNames.map((value, index) => {
-              const translatedValue = mapBreadcrumbName(value);
-              const lastBreadcrumb = index === pathNames.length - 1;
-              const to = `/${pathNames.slice(0, index + 1).join('/')}`;
-              return lastBreadcrumb ? (
-                <Link to={to} key={to}>
-                  <b>{translatedValue}</b>
-                </Link>
-              ) : (
-                <Link to={to} key={to}>
-                  {translatedValue}
-                </Link>
-              );
-            })}
-          </MuiBreadcrumbs>
-        </div>
-      )}
-    </>
+    <StyledBreadcrumbs>
+      <MuiBreadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+        <Link to="/">{t('Home')}</Link>
+        {pathNames.map((value, index) => {
+          const translatedValue = mapBreadcrumbName(value);
+          const lastBreadcrumb = index === pathNames.length - 1;
+          const to = `/${pathNames.slice(0, index + 1).join('/')}`;
+          return lastBreadcrumb ? (
+            <Link to={to} key={to}>
+              <b>{translatedValue}</b>
+            </Link>
+          ) : (
+            <Link to={to} key={to}>
+              {translatedValue}
+            </Link>
+          );
+        })}
+      </MuiBreadcrumbs>
+    </StyledBreadcrumbs>
   );
 };
 
