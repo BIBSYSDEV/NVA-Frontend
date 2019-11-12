e88d9fb1bfea5cff932bc2fc0ea68b8550dfeded
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2019-11-12 09:08:29 +0100

    NP-170 Refactor Dashboard to use styled-components (#65)

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,31 @@
-import '../../styles/pages/dashboard.scss';
-
 import React from 'react';
+import styled from 'styled-components';
 
 import SearchBar from '../../components/SearchBar';
 
+const StyledDashboard = styled.div`
+  width: 100%;
+  height: 85%;
+  display: grid;
+  grid-template-areas: 'search-bar' 'other-content';
+  grid-template-rows: 4rem 15rem auto;
+  justify-items: center;
+  align-items: center;
+`;
+
+const StyledSearchBar = styled(SearchBar)`
+  grid-area: other-content;
+`;
+
+const StyledOtherContent = styled.div`
+  grid-area: search-bar;
+`;
+
 const Dashboard: React.FC = () => (
-  <div className="dashboard">
-    <SearchBar resetSearchInput />
-    <div className="other-content">Annet innhold</div>
-  </div>
+  <StyledDashboard>
+    <StyledSearchBar resetSearchInput />
+    <StyledOtherContent>Annet innhold</StyledOtherContent>
+  </StyledDashboard>
 );
 
 export default Dashboard;
