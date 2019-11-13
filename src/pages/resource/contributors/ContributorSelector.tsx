5586b56bfc921cbb725903dff08bd2ac34fe059d
= <eirik.nilsen@unit.no>
2019-11-13 12:57:36 +0100

    testing

--- src/pages/resource/contributors/ContributorSelector.tsx
+++ src/pages/resource/contributors/ContributorSelector.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+import MenuItem from '@material-ui/core/MenuItem';
+import { useTranslation } from 'react-i18next';
+import StyledContributor from './StyledComponents';
 
-const StyledContributorSelector = styled.div``;
+const StyledContributorSelector = styled(StyledContributor.ContributorContainer)`
+  grid-template-areas: 'icon name institution verify-person verify-person . delete';
+`;
+
+const StyledNameInput = styled(StyledContributor.Name)`
+  height: 2rem;
+  background-color: white;
+  margin-right: 1rem;
+`;
+
+const StyledPersonIcon = styled(StyledContributor.PersonIcon)`
+  color: white;
+`;
 
 const ContributorSelector: React.FC = () => {
-  return <StyledContributorSelector>Select contributor here</StyledContributorSelector>;
+  const { t } = useTranslation();
+
+  return (
+    <StyledContributorSelector>
+      <StyledContributor.AddIcon />
+      <StyledNameInput />
+      <StyledContributor.Select>
+        <MenuItem value=""></MenuItem>
+        text
+      </StyledContributor.Select>
+      <StyledContributor.VerifyPerson color="primary" variant="contained">
+        <StyledPersonIcon />
+        {t('contributors.verify_person')}
+      </StyledContributor.VerifyPerson>
+      <StyledContributor.DeleteIcon />
+    </StyledContributorSelector>
+  );
 };
 
 export default ContributorSelector;
