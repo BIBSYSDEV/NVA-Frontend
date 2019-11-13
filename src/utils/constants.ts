8d41063b79004bbfd3600933fc5ba3e03c71d4f1
Sarah Serussi <sase@unit.no>
2019-11-13 13:38:02 +0100

    prepare connection to orcid sandbox

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,8 @@
 export const USE_MOCK_DATA = process.env.REACT_APP_USE_MOCK === 'true';
 export const SEARCH_RESULTS_PER_PAGE = 10;
-export const ORCID_URL = 'https://sandbox.orcid.org/oauth/token/';
+export const ORCID_BASE_URL = 'https://sandbox.orcid.org';
+export const ORCID_OAUTH_URL = `${ORCID_BASE_URL}/oauth/token/`;
+export const ORCID_SIGN_IN_URL = `${ORCID_BASE_URL}/signin?oauth&client_id=${process.env.REACT_APP_ORCID_CLIENT_ID}&response_type=code&scope=/authenticate&redirect_uri=${process.env.REACT_APP_ORCID_REDIRECT_URI}`;
 
 export enum ApiBaseUrl {
   RESOURCES = 'resources',
