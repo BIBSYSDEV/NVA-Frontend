c589b0c39a9ce3bc009d982c3468abfc935d499e
sarahse <sase@unit.no>
2019-11-26 15:52:18 +0100

    Np 184  search for publishers (#86)
    * NP-184--working autocomplete with formik
    
    * NP-184--remove unnecessary props
    
    * NP-184--cleanup
    
    * use select from formik-material-ui
    
    * cleanup
    
    * more cleanup
    
    * clear options on close
    
    * only search when autosearch is open
    
    * remove id prop which makes chrome remember previous searches
    
    * open autosearch after user has entered 3 chars
    
    * add debounce hook
    
    * add translations and error handling
    
    * minor changes after QA
    
    * create enum ExternalApi
    
    * remove formikFieldName from setFieldValue in AutoSearch

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,16 @@
 export const USE_MOCK_DATA = process.env.REACT_APP_USE_MOCK === 'true';
 
 export const SEARCH_RESULTS_PER_PAGE = 10;
+export const MINIMUM_SEARCH_CHARACTERS = 3;
+export const DEBOUNCE_INTERVAL = 500;
 export const MAX_NOTIFICATIONS = 3;
 
 export const ORCID_OAUTH_URL = `${process.env.REACT_APP_ORCID_BASE_URL}/oauth/token/`;
 export const ORCID_SIGN_IN_URL = `${process.env.REACT_APP_ORCID_BASE_URL}/signin?oauth&client_id=${process.env.REACT_APP_ORCID_CLIENT_ID}&response_type=code&scope=/authenticate&redirect_uri=${process.env.REACT_APP_ORCID_REDIRECT_URI}`;
+
+export enum ExternalApi {
+  PUBLICATION_CHANNEL = 'https://api.nsd.no/dbhapitjener/Tabeller/hentJSONTabellData',
+}
 
 export enum ApiBaseUrl {
   RESOURCES = 'resources',
