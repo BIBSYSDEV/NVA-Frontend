5484366b8a9bc465c00556b0131b7b0fd783fd7f
pcb <per.christian.bjelke@bibsys.no>
2019-11-27 14:10:13 +0100

    NP-194 - fixed notification

--- src/api/cristinProjectApi.ts
+++ src/api/cristinProjectApi.ts
@@ -3,6 +3,14 @@ import { ExternalApi, StatusCode } from '../utils/constants';
 import { addNotification } from '../redux/actions/notificationActions';
 import { Dispatch } from 'redux';
 import i18n from '../translations/i18n';
+import uuid from 'uuid';
+import { Notification } from '../types/notification.types';
+
+const errorNotification: Notification = {
+  key: uuid.v4(),
+  message: i18n.t('feedback:error.get_cristin_project'),
+  variant: 'error',
+};
 
 export const searchCristinProjects = async (query: string, dispatch: Dispatch) => {
   try {
@@ -11,21 +19,9 @@ export const searchCristinProjects = async (query: string, dispatch: Dispatch) =
       response.data.totalCount = response.headers['X-Total-Count'];
       return response.data;
     } else {
-      dispatch(
-        addNotification({
-          key: 'error.get_cristin_project',
-          message: i18n.t('feedback:error.get_cristin_project'),
-          variant: 'error',
-        }) //TODO: vent på Kjetils PR
-      );
+      dispatch(addNotification(errorNotification));
     }
   } catch (err) {
-    dispatch(
-      addNotification({
-        key: 'error.get_cristin_project',
-        message: i18n.t('feedback:error.get_cristin_project'),
-        variant: 'error',
-      }) //TODO: vent på Kjetils PR
-    );
+    dispatch(addNotification(errorNotification));
   }
 };
