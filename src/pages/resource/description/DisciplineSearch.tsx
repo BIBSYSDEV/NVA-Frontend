09c0ad8542db0870fc5533e91264bcc23ba74bd3
= <eirik.nilsen@unit.no>
2019-11-27 09:52:59 +0100

    Fixed type

--- src/pages/resource/description/DisciplineSearch.tsx
+++ src/pages/resource/description/DisciplineSearch.tsx
@@ -3,7 +3,7 @@ import { AutoSearch } from './../../../components/AutoSearch';
 import disciplines from './../../../utils/testfiles/disciplines_en.json';
 import { useTranslation } from 'react-i18next';
 
-interface DisciplineSeachProps {
+interface DisciplineSearchProps {
   setFieldValue: (name: string, value: any) => void;
 }
 
@@ -12,7 +12,7 @@ interface DisciplineType {
   mainDiscipline: string;
 }
 
-const DisciplineSearch: React.FC<DisciplineSeachProps> = ({ setFieldValue }) => {
+const DisciplineSearch: React.FC<DisciplineSearchProps> = ({ setFieldValue }) => {
   const { t } = useTranslation();
 
   const searchResults = Object.values(disciplines)
