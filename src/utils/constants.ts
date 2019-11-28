209e8abf0a148971f2d1695fe26d8881fc68047e
Per Christian Bjelke <pcb@unit.no>
2019-11-28 14:46:00 +0100

    env-check

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -21,3 +21,23 @@ export enum ApiBaseUrl {
 export enum StatusCode {
   OK = 200,
 }
+
+const requiredEnv = [
+  'REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID',
+  'REACT_APP_AWS_REGION',
+  'REACT_APP_AWS_USER_POOLS_ID',
+  'REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID',
+  'REACT_APP_DOMAIN',
+  'REACT_APP_REDIRECT_SIGN_IN',
+  'REACT_APP_REDIRECT_SIGN_OUT',
+  'REACT_APP_ORCID_CLIENT_ID',
+  'REACT_APP_ORCID_CLIENT_SECRET',
+  'REACT_APP_ORCID_REDIRECT_URI',
+  'REACT_APP_ORCID_BASE_URL',
+  'REACT_APP_PUBLICATION_CHANNEL_API_URL',
+  'REACT_APP_AUTHORITY_REGISTER_API_URL',
+];
+const unsetEnv = requiredEnv.filter(env => !(typeof process.env[env] !== 'undefined'));
+if (unsetEnv.length > 0) {
+  throw new Error('Required ENV variables are not set: [' + unsetEnv.join(', ') + ']');
+}
