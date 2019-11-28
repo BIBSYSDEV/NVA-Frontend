1fa944f1aadb0f023b4f92bf19dc7727e53e630a
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2019-11-28 08:39:00 +0100

    NP-204 Use process.env directly

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,10 +8,8 @@ export const MAX_NOTIFICATIONS = 3;
 export const ORCID_OAUTH_URL = `${process.env.REACT_APP_ORCID_BASE_URL}/oauth/token/`;
 export const ORCID_SIGN_IN_URL = `${process.env.REACT_APP_ORCID_BASE_URL}/signin?oauth&client_id=${process.env.REACT_APP_ORCID_CLIENT_ID}&response_type=code&scope=/authenticate&redirect_uri=${process.env.REACT_APP_ORCID_REDIRECT_URI}`;
 
-const { REACT_APP_AUTHORITY_REGISTER_API_URL, REACT_APP_PUBLICATION_CHANNEL_API_URL } = process.env;
-
-export const PUBLICATION_CHANNEL_API_URL = REACT_APP_PUBLICATION_CHANNEL_API_URL;
-export const AUTHORITY_REGISTER_API_URL = REACT_APP_AUTHORITY_REGISTER_API_URL;
+export const PUBLICATION_CHANNEL_API_URL = process.env.REACT_APP_PUBLICATION_CHANNEL_API_URL;
+export const AUTHORITY_REGISTER_API_URL = process.env.REACT_APP_AUTHORITY_REGISTER_API_URL;
 
 export enum ApiBaseUrl {
   RESOURCES = 'resources',
