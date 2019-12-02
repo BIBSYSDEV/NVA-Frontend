a481418f42da19e3df6cc5aee98a279d49d00f4b
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2019-12-02 07:53:24 +0100

    Np 212 alma (#99)
    * NP-212 Preparing fetching publication data from ALMA
    
    * NP-212 Fix constant
    
    * NP-212 Fetch publication data from alma
    
    * NP-212 Extract title and date from publications from alma
    
    * NP-212 Remove forgotten testing value
    
    * NP-212 Use AlmaPublication type

--- src/pages/user/authority/AuthorityCard.tsx
+++ src/pages/user/authority/AuthorityCard.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import Box from '../../../components/Box';
 import { Marc21Codes, Marc21Subcodes, Authority } from '../../../types/authority.types';
+import { AlmaPublication } from '../../../types/resource.types';
+import { useDispatch } from 'react-redux';
+import { getPublications } from '../../../api/external/almaApi';
 
 const StyledBoxContent = styled.div`
   display: grid;
@@ -15,8 +18,19 @@ interface AuthorityCardProps {
 }
 
 const AuthorityCard: React.FC<AuthorityCardProps> = ({ authority }) => {
-  const authorityMarcdata = authority.marcdata || [];
+  const [publications, setPublications] = useState<AlmaPublication[]>([]);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const fetchAuthorities = async () => {
+      const retrievedPublications = await getPublications(authority.systemControlNumber, dispatch);
+      setPublications(retrievedPublications);
+    };
+
+    fetchAuthorities();
+  }, [dispatch, authority.systemControlNumber]);
+
+  const authorityMarcdata = authority.marcdata || [];
   const authorityNameField =
     authorityMarcdata.find(field => Marc21Codes.PERSONAL_NAME === field.tag) ||
     authorityMarcdata.find(field => Marc21Codes.HEADING_PERSONAL_NAME === field.tag);
@@ -27,7 +41,7 @@ const AuthorityCard: React.FC<AuthorityCardProps> = ({ authority }) => {
     <Box>
       <StyledBoxContent>
         <div>{authorityName && authorityName.value}</div>
-        <div>[Siste publikasjon]</div>
+        <div>{publications.length ? publications[0].title : null}</div>
       </StyledBoxContent>
     </Box>
   );
