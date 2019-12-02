4097dca9c26c04a970b5022b57ea6853decdcbd8
Sarah Serussi <sase@unit.no>
2019-12-02 13:48:09 +0100

    fix test - need to set language to english in user profile to test for matching values

--- cypress/integration/user.spec.js
+++ cypress/integration/user.spec.js
@@ -9,6 +9,10 @@ describe('User', () => {
     cy.get('[data-testid=menu]').click({ force: true });
     cy.get('[data-testid=user-profile-button]').click({ force: true });
 
+    // need to set language to english in order to check that the translated values are correct
+    cy.get('[data-testid=language-selector] .MuiSelect-root').click({ force: true });
+    cy.get('[data-testid=user-language-en-US]').click({ force: true });
+
     cy.get('[data-testid=user-name]').contains('Test User');
     cy.get('[data-testid=user-id]').contains('tu@unit.no');
     cy.get('[data-testid=user-email]').contains('testuser@unit.no');
