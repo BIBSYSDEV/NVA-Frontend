e83495433fe4bf65ef72ef01d5eb756547dd2551
= <eirik.nilsen@unit.no>
2019-12-16 13:54:21 +0100

    NP-265 Created component for journal

--- src/pages/publication/references_tab/Journal.tsx
+++ src/pages/publication/references_tab/Journal.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import { Field } from 'formik';
 import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+import { PublicationChannel } from './../../../types/references.types';
 
-interface JournalProps {}
+interface JournalProps {
+  journal: PublicationChannel;
+}
 
-const StyledJournal = styled.div``;
+const StyledJournal = styled.div`
+  display: grid;
+  grid-column-gap: 0.5rem;
+  align-items: center;
+  grid-template-areas:
+    'journal journal journal'
+    'issnLabel levelLabel publisherLabel'
+    'issn level publisher';
+`;
 
-const Journal: React.FC<JournalProps> = ({}) => {
+const StyledJournalLabel = styled.div`
+  font-size: 0.7rem;
+  font-weight: bold;
+`;
+
+const Journal: React.FC<JournalProps> = ({ journal }) => {
+  const { t } = useTranslation();
+
   return (
     <StyledJournal>
-      <Field name="reference.journalPublication.journal" component="TextField" />
+      <Field name="reference.journalPublication.journal" component="TextField" value={journal.title} />
+      <StyledJournalLabel>{t('reference.issn')}</StyledJournalLabel>
+      <StyledJournalLabel>{t('reference.level')}</StyledJournalLabel>
+      <StyledJournalLabel>{t('reference.publisher')}</StyledJournalLabel>
+      <div>{journal.issn}</div>
+      <div>{journal.level}</div>
+      <div>{journal.publisher}</div>
     </StyledJournal>
   );
 };
+
+export default Journal;
