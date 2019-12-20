947dd1a8dd8944d246907df76e1068b86bc72cde
sarahse <sase@unit.no>
2019-12-20 15:31:23 +0100

    Np 272  open orcid modal after author connect (#126)
    * open ORCID modal when clicking on the next button in the AuthorityModal
    
    * open orcid modal if no orcid for user
    
    * cleanup
    
    * simplify logic
    
    * rename authority modal
    
    * changes after QA

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import Dashboard from './pages/dashboard/Dashboard';
 import NotFound from './pages/errorpages/NotFound';
 import PublicationForm from './pages/publication/PublicationForm';
 import Search from './pages/search/Search';
-import AuthorityModal from './pages/user/authority/AuthorityModal';
+import AuthorityOrcidModal from './pages/user/authority/AuthorityOrcidModal';
 import User from './pages/user/User';
 import Workspace from './pages/workspace/Workspace';
 import { setAuthorityData, setUser } from './redux/actions/userActions';
@@ -44,7 +44,7 @@ const StyledPageBody = styled.div`
 const App: React.FC = () => {
   const dispatch = useDispatch();
   const user = useSelector((store: RootStore) => store.user);
-  const [showModal, setShowModal] = useState(false);
+  const [showAuthorityOrcidModal, setShowAuthorityOrcidModal] = useState(false);
 
   useEffect(() => {
     if (USE_MOCK_DATA) {
@@ -72,8 +72,8 @@ const App: React.FC = () => {
   }, [dispatch, user.id]);
 
   useEffect(() => {
-    user.id && !user.authority && setShowModal(true);
-  }, [user.id, user.authority]);
+    user.id && (!user.authority || !user.orcid) && setShowAuthorityOrcidModal(true);
+  }, [user.id, user.authority, user.orcid]);
 
   return (
     <BrowserRouter>
@@ -82,7 +82,7 @@ const App: React.FC = () => {
         <Header />
         {user.isLoggedIn && <AdminMenu />}
         <Breadcrumbs />
-        <AuthorityModal showModal={showModal} />
+        {showAuthorityOrcidModal && <AuthorityOrcidModal />}
         <StyledPageBody>
           <Switch>
             <Route exact path="/" component={Dashboard} />
