26286e43382ce84420df3db048a62d269f8a1736
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-01-09 13:45:07 +0100

    NP-306 Display tab validation

--- src/pages/publication/PublicationForm.tsx
+++ src/pages/publication/PublicationForm.tsx
@@ -31,12 +31,14 @@ const PublicationForm: React.FC = () => {
   );
 
   const validationSchema = Yup.object().shape({
-    title: Yup.string().required(t('publication:feedback.required_field')),
+    title: Yup.object().shape({
+      nb: Yup.string().required(t('publication:feedback.required_field')),
+    }),
 
     reference: Yup.object().shape({
-      referenceType: Yup.string().required(t('publication:feedback.required_field')),
+      type: Yup.string().required(t('publication:feedback.required_field')),
 
-      journalArticle: Yup.object().when('referenceType', {
+      journalArticle: Yup.object().when('type', {
         is: ReferenceType.PUBLICATION_IN_JOURNAL,
         then: Yup.object().shape({
           type: Yup.string(),
@@ -51,7 +53,7 @@ const PublicationForm: React.FC = () => {
         }),
       }),
 
-      book: Yup.object().when('referenceType', {
+      book: Yup.object().when('type', {
         is: ReferenceType.BOOK,
         then: Yup.object().shape({
           type: Yup.string(),
@@ -85,14 +87,9 @@ const PublicationForm: React.FC = () => {
         validationSchema={validationSchema}
         onSubmit={(values: Publication) => savePublication(values)}
         validateOnChange={false}>
-        {({ values, errors, touched }: FormikProps<Publication>) => (
+        {({ values }: FormikProps<Publication>) => (
           <Form onBlur={() => setLocalStorageFormData(values)}>
-            <PublicationFormTabs
-              tabNumber={tabNumber}
-              handleTabChange={handleTabChange}
-              errors={errors}
-              touched={touched}
-            />
+            <PublicationFormTabs tabNumber={tabNumber} handleTabChange={handleTabChange} />
             {tabNumber === 0 && <PublicationPanel goToNextTab={goToNextTab} />}
             {tabNumber === 1 && (
               <DescriptionPanel goToNextTab={goToNextTab} savePublication={() => savePublication(values)} />
