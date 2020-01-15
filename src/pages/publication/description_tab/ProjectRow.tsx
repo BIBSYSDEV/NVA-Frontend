5ee49978b649e2a76c0a0d227763a173fadc6997
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-01-15 10:32:12 +0100

    Np 315 projects selection (#144)
    * NP-315 Allow selection of multiple projects
    
    * NP-315 Show list of selected projects
    
    * NP-315 Minor changes to selection of project
    
    * NP-315 Allow empty value
    
    * NP-315 DeleteIcon and i18n
    
    * NP-315 Fix translation error
    
    * NP-315 Avoid typing same field name twice
    
    * NP-315 QA changes
    
    * NP-315 Fix translation error
    
    * NP-315 Add colors to theme

---
+++ src/pages/publication/description_tab/ProjectRow.tsx
@@ -0,0 +1,85 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { Project } from '../../../types/project.types';
+import { Button } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { useTranslation } from 'react-i18next';
+
+const StyledRow = styled.div`
+  margin-top: 1rem;
+  background: ${({ theme }) => theme.palette.background.default};
+  padding: 1rem;
+  display: grid;
+  grid-column-gap: 0.5rem;
+  grid-template-areas:
+    'titleLabel  grantIdLabel    financedByLabel .'
+    'title       grantId         financedBy      removeButton';
+  grid-template-columns: 4fr 2fr 2fr 1fr;
+`;
+
+const StyledTitleLabel = styled.div`
+  font-weight: bold;
+  grid-area: titleLabel;
+`;
+
+const StyledTitle = styled.div`
+  grid-area: title;
+`;
+
+const StyledGrantLabel = styled.div`
+  font-weight: bold;
+  grid-area: grantIdLabel;
+`;
+
+const StyledGrant = styled.div`
+  grid-area: grantId;
+`;
+
+const StyledFinancedByLabel = styled.div`
+  font-weight: bold;
+  grid-area: financedByLabel;
+`;
+
+const StyledFinancedBy = styled.div`
+  grid-area: financedBy;
+`;
+
+const StyledAction = styled.div`
+  grid-area: removeButton;
+`;
+
+const StyledRemoveButton = styled(Button)`
+  background-color: ${({ theme }) => theme.palette.danger.main};
+  color: ${({ theme }) => theme.palette.background.default};
+`;
+
+interface ProjectRowProps {
+  project: Project;
+  onClickRemove: () => void;
+}
+
+const ProjectRow: FC<ProjectRowProps> = ({ project, onClickRemove }) => {
+  const { t } = useTranslation();
+
+  return (
+    <StyledRow>
+      <StyledTitleLabel>{t('common:title')}</StyledTitleLabel>
+      <StyledTitle>{project.title}</StyledTitle>
+
+      <StyledGrantLabel>{t('publication:description.project_id')}</StyledGrantLabel>
+      <StyledGrant>{project.grantId}</StyledGrant>
+
+      <StyledFinancedByLabel>{t('publication:description.financed_by')}</StyledFinancedByLabel>
+      <StyledFinancedBy>{project.financedBy}</StyledFinancedBy>
+
+      <StyledAction>
+        <StyledRemoveButton onClick={onClickRemove}>
+          <DeleteIcon />
+          {t('common:remove')}
+        </StyledRemoveButton>
+      </StyledAction>
+    </StyledRow>
+  );
+};
+
+export default ProjectRow;
