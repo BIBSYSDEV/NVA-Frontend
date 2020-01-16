5a26caf1c65d8cc618a2ba9dc73d1e26da1edd28
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-01-16 15:52:25 +0100

    Omit already selected project from dropdown (#152)
    * Omit already selected project from dropdown
    
    * Add type

--- src/pages/publication/description_tab/ProjectSearch.tsx
+++ src/pages/publication/description_tab/ProjectSearch.tsx
@@ -5,6 +5,7 @@ import { searchProjectsByTitle } from '../../../api/projectApi';
 import AutoSearch from '../../../components/AutoSearch';
 import { Project } from '../../../types/project.types';
 import { debounce } from '../../../utils/debounce';
+import { useFormikContext } from 'formik';
 
 interface ProjectSearchProps {
   dataTestId: string;
@@ -15,13 +16,21 @@ interface ProjectSearchProps {
 const ProjectSearch: FC<ProjectSearchProps> = ({ dataTestId, setValueFunction, placeholder }) => {
   const [searchResults, setSearchResults] = useState<Project[]>([]);
   const dispatch = useDispatch();
+  const { values } = useFormikContext();
 
   const debouncedSearch = useCallback(
     debounce(async (searchTerm: string) => {
       const response = await searchProjectsByTitle(searchTerm, dispatch);
       if (response) {
+        const unselectedProjects = response.filter(
+          (project: Project) =>
+            !values.projects.some(
+              (selectedProject: any) => selectedProject.cristinProjectId === project.cristinProjectId
+            )
+        );
+
         setSearchResults(
-          response.map((project: Project) => {
+          unselectedProjects.map((project: Project) => {
             return { ...project, title: project.titles?.[0]?.title };
           })
         );
