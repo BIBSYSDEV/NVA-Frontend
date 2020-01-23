a7dd1a391bbbf05dbb4f1c9065ecb035e3dfe3bb
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-01-23 08:22:34 +0100

    NP-378 i18n

--- src/pages/publication/files_and_license_tab/FileUploader.tsx
+++ src/pages/publication/files_and_license_tab/FileUploader.tsx
@@ -4,8 +4,8 @@ import '@uppy/dashboard/dist/style.css';
 
 import Tus from '@uppy/tus';
 import Uppy from '@uppy/core';
-// import AwsS3Multipart from '@uppy/aws-s3-multipart';
 import { Dashboard } from '@uppy/react';
+import { useTranslation } from 'react-i18next';
 
 const uppy = Uppy({
   autoProceed: true,
@@ -15,15 +15,14 @@ const uppy = Uppy({
 }).use(Tus, {
   endpoint: 'https://master.tus.io/files/',
 });
-// .use(AwsS3Multipart, {
-// companionUrl: '',
-// });
 
 interface FileUploaderProps {
   addFile: (file: any) => void;
 }
 
 const FileUploader: React.FC<FileUploaderProps> = ({ addFile }) => {
+  const { t } = useTranslation('publication');
+
   uppy.on('upload-success', uploadedFile => {
     addFile(uploadedFile);
   });
@@ -31,15 +30,15 @@ const FileUploader: React.FC<FileUploaderProps> = ({ addFile }) => {
   return (
     <Dashboard
       uppy={uppy}
-      showProgressDetails={true}
       proudlyDisplayPoweredByUppy={false}
       showSelectedFiles={false}
+      showProgressDetails
       width={10000}
       height={200}
       locale={{
         strings: {
-          dropPaste: 'Dra filer hit eller %{browse}',
-          browse: 'velg filer',
+          dropPaste: `${t('files_and_license.drag_files')} %{browse}`,
+          browse: t('files_and_license.browse'),
         },
       }}
     />
@@ -47,21 +46,3 @@ const FileUploader: React.FC<FileUploaderProps> = ({ addFile }) => {
 };
 
 export default FileUploader;
-
-/* DragDrop + StatusBar:
-import '@uppy/drag-drop/dist/style.css';
-
-<DragDrop
-  uppy={uppy}
-  locale={{
-    strings: {
-      // Text to show on the droppable area.
-      // `%{browse}` is replaced with a link that opens the system file selection dialog.
-      dropHereOr: 'Dra filer hit eller %{browse} for å laste opp',
-      // Used as the label for the link that opens the system file selection dialog.
-      browse: 'trykk',
-    },
-  }}
-/>
-<StatusBar uppy={uppy} showProgressDetails={true}></StatusBar>
-*/
