409d974f2165a8c13cc50cf1c3270221a59d1b73
Per Christian Bjelke <pcb@unit.no>
2020-01-23 11:24:06 +0100

    np-277 - styling for mobile

--- src/components/LabelTextLine.tsx
+++ src/components/LabelTextLine.tsx
@@ -5,18 +5,21 @@ import styled from 'styled-components';
 
 const StyledLine = styled.div`
   padding-bottom: 0.5rem;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 const StyledLabel = styled.div`
   display: inline-block;
+  width: 10rem;
   min-width: 10rem;
 `;
 
 const StyledText = styled.div`
   display: inline-block;
   font-weight: bold;
-  min-width: 10rem;
-  width: 80%;
+  flex: 1;
+  min-width: 80%;
 `;
 
 interface LabelTextLineProps {
