a001dea1d9ea09b0209b06c77e0b1fbe5fbda9d1
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-01-23 14:04:30 +0100

    Np 378 file uploader (#166)
    * NP-378 File uploader
    
    * NP-378 i18n
    
    * Merge develop
    
    * NP-378 QA

--- src/pages/publication/FilesAndLicensePanel.tsx
+++ src/pages/publication/FilesAndLicensePanel.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import Box from '../../components/Box';
 import TabPanel from '../../components/TabPanel/TabPanel';
-import UppyFileUpload from './files_and_license_tab/UppyFileUpload';
+import FileUploader from './files_and_license_tab/FileUploader';
+import FileCard from './files_and_license_tab/FileCard';
 
 interface FilesAndLicensePanelProps {
   goToNextTab: (event: React.MouseEvent<any>) => void;
 }
 
 const FilesAndLicensePanel: React.FC<FilesAndLicensePanelProps> = ({ goToNextTab }) => {
+  const [files, setFiles] = useState<any[]>([]);
   const { t } = useTranslation('publication');
+
+  const addFile = (file: any) => {
+    setFiles([...files, file]);
+  };
 
   return (
     <TabPanel ariaLabel="files and license" goToNextTab={goToNextTab}>
       <h1>{t('files_and_license.upload_files')}</h1>
       <Box>
-        <UppyFileUpload />
+        <FileUploader addFile={addFile} />
       </Box>
+
       <h1>{t('files_and_license.files')}</h1>
-      <Box>TODO</Box>
+      <Box>
+        {files.map(file => (
+          <FileCard key={file.id} file={file} />
+        ))}
+      </Box>
     </TabPanel>
   );
 };
