ca860ebb650f32c4f15951d4c83e7c19b6414e7d
Sarah Serussi <sase@unit.no>
2020-01-28 14:53:22 +0100

    create new authority card

--- src/pages/user/authority/ConnectAuthority.tsx
+++ src/pages/user/authority/ConnectAuthority.tsx
@@ -10,6 +10,7 @@ import { setAuthorityData } from '../../../redux/actions/userActions';
 import { RootStore } from '../../../redux/reducers/rootReducer';
 import { Authority } from '../../../types/authority.types';
 import AuthorityCard from './AuthorityCard';
+import NewAuthorityCard from './NewAuthorityCard';
 
 const StyledAuthorityContainer = styled.div`
   > * {
@@ -63,28 +64,31 @@ export const ConnectAuthority: React.FC = () => {
       </StyledSubHeading>
 
       <StyledAuthorityContainer>
-        {matchingAuthorities?.map(authority => (
-          <StyledClickableDiv
-            data-testid="author-radio-button"
-            key={authority.systemControlNumber}
-            onClick={() => setSelectedSystemControlNumber(authority.systemControlNumber)}>
-            <AuthorityCard
-              authority={authority}
-              isSelected={selectedSystemControlNumber === authority.systemControlNumber}
-            />
-          </StyledClickableDiv>
-        ))}
-
-        {matchingAuthorities?.length > 0 && (
-          <Button
-            data-testid="connect-author-button"
-            color="primary"
-            variant="contained"
-            size="large"
-            onClick={updateAuthorityForUser}
-            disabled={!selectedSystemControlNumber}>
-            {t('authority.connect_authority')}
-          </Button>
+        {matchingAuthorities.length > 0 ? (
+          matchingAuthorities.map(authority => (
+            <>
+              <StyledClickableDiv
+                data-testid="author-radio-button"
+                key={authority.systemControlNumber}
+                onClick={() => setSelectedSystemControlNumber(authority.systemControlNumber)}>
+                <AuthorityCard
+                  authority={authority}
+                  isSelected={selectedSystemControlNumber === authority.systemControlNumber}
+                />
+              </StyledClickableDiv>
+              <Button
+                data-testid="connect-author-button"
+                color="primary"
+                variant="contained"
+                size="large"
+                onClick={updateAuthorityForUser}
+                disabled={!selectedSystemControlNumber}>
+                {t('authority.connect_authority')}
+              </Button>
+            </>
+          ))
+        ) : (
+          <NewAuthorityCard />
         )}
       </StyledAuthorityContainer>
     </>
---
+++ src/pages/user/authority/NewAuthorityCard.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+import { Button, Radio } from '@material-ui/core';
+
+import { RootStore } from '../../../redux/reducers/rootReducer';
+
+const StyledBoxContent = styled.div`
+  display: grid;
+  grid-template-areas: 'authority authority authority' 'description description description' '. . create-button';
+  grid-template-columns: 1fr 3fr 3fr;
+  background-color: ${({ theme }) => theme.palette.box.main};
+  padding: 1rem;
+  align-items: center;
+`;
+
+const StyledAuthority = styled.div`
+  grid-area: authority;
+`;
+
+const StyledDescription = styled.div`
+  grid-area: description;
+  margin-left: 0.7rem;
+`;
+
+const StyledButton = styled(Button)`
+  grid-area: create-button;
+  margin-top: 2rem;
+`;
+
+const NewAuthorityCard: React.FC = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((store: RootStore) => store.user);
+  const { t } = useTranslation('profile');
+
+  return (
+    <StyledBoxContent>
+      <StyledAuthority>
+        <Radio color="primary" checked />
+        {user.name}
+      </StyledAuthority>
+      <StyledDescription>{t('authority.description_no_authority_found')}</StyledDescription>
+      <StyledButton
+        data-testid="create-author-button"
+        color="primary"
+        variant="contained"
+        size="large"
+        onClick={() => {
+          console.log('create authority');
+        }}>
+        {t('authority.create_authority')}
+      </StyledButton>
+    </StyledBoxContent>
+  );
+};
+
+export default NewAuthorityCard;
