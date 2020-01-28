92e003f11cf04075806071f9670da3e96690ea1e
Per Christian Bjelke <pcb@unit.no>
2020-01-28 10:20:49 +0100

    np-277 - added test

---
+++ cypress/integration/publication_submission.spec.js
@@ -0,0 +1,38 @@
+describe('Publication: Description', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.server();
+  });
+
+  it('The user should be able to add and remove projects', () => {
+    cy.mocklogin();
+
+    const title = 'fjksdjf sjf skfj sdkfj';
+    const description = 'fjsdjfksdljk skdjf';
+
+    const isbn = '978-3-16-148410-0';
+
+    cy.get('[data-testid=new-publication-button]').click({ force: true });
+
+    // TODO: Change this when DOI-link is used
+    cy.get('[data-testid=new-schema-button]').click({ force: true });
+
+    // choose fill inn some values on description-tab
+    cy.get('[data-testid=nav-tabpanel-description]').click({ force: true });
+    cy.get('[data-testid=publication-title-input]').type(title);
+    cy.get('[data-testid=publication-description-input]').type(description);
+
+    // choose Book type and fill inn some data
+    cy.get('[data-testid=nav-tabpanel-references]').click({ force: true });
+    cy.get('[data-testid=reference_type]')
+      .click({ force: true })
+      .type(' ');
+    cy.get('[data-testid=reference_type-book]').click({ force: true });
+    cy.get('[data-testid=isbn]').type(isbn);
+
+    cy.get('[data-testid=nav-tabpanel-submission]').click({ force: true });
+    cy.contains(title);
+    cy.contains(isbn);
+    cy.contains(description);
+  });
+});
