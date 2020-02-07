c4a060104e6ed4dffbcb0a97198294dc18df0c99
Per Christian Bjelke <pcb@unit.no>
2020-02-07 11:02:42 +0100

    NP-354 removed tabs

--- src/pages/publication/MyPublications.tsx
+++ src/pages/publication/MyPublications.tsx
@@ -1,9 +1,7 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import FormCard from '../../components/FormCard/FormCard';
 import FormCardHeading from '../../components/FormCard/FormCardHeading';
-import LinkTab from '../../components/TabPanel/LinkTab';
-import { Tabs } from '@material-ui/core';
 import PublicationList from './PublicationList';
 
 export interface DummyPublicationListElement {
@@ -16,20 +14,7 @@ export interface DummyPublicationListElement {
 const MyPublications: FC = () => {
   const { t } = useTranslation();
 
-  const [tabNumber, setTabNumber] = useState(0);
-
-  const handleTabChange = (_: React.ChangeEvent<{}>, newTabNumber: number) => {
-    setTabNumber(newTabNumber);
-  };
-
-  const a11yProps = (tabDescription: string) => {
-    return {
-      id: `my-publication-tab-${tabDescription}`,
-      'aria-controls': `nav-tabpanel-${tabDescription}`,
-      'data-testid': `nav-tabpanel-${tabDescription}`,
-    };
-  };
-
+  //TODO: to be replaced by data from api in NP-471
   let dummyElement: DummyPublicationListElement = {
     id: '5843058934095834905.',
     title: 'Qualitative research practice: A guide for social science students and researchers.',
@@ -41,10 +26,7 @@ const MyPublications: FC = () => {
   return (
     <FormCard>
       <FormCardHeading>{t('workLists:my_publications')}</FormCardHeading>
-      <Tabs variant="fullWidth" value={0} onChange={handleTabChange} aria-label="My publication tabs">
-        <LinkTab label={t('workLists:unpublished_publications')} {...a11yProps('dummyElementList')} />
-      </Tabs>
-      {tabNumber === 0 && <PublicationList elements={dummyElementList} />}
+      <PublicationList elements={dummyElementList} />
     </FormCard>
   );
 };
