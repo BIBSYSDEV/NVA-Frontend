f06f192fc63af5f13650024d0ecfbce806ddb12e
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-02-10 14:51:50 +0100

    Revert "NP-456 Remove redundant title"
    This reverts commit ed53500cb912981e3d0433bff7a3c062cc870b1e.

--- src/pages/publication/DescriptionPanel.tsx
+++ src/pages/publication/DescriptionPanel.tsx
@@ -17,6 +17,7 @@ import { Project } from '../../types/project.types';
 import ChipInput from 'material-ui-chip-input';
 import { getObjectValueByFieldName } from '../../utils/helpers';
 import { orderedLanguages } from '../../types/language.types';
+import FormCardHeading from '../../components/FormCard/FormCardHeading';
 import FormCard from '../../components/FormCard/FormCard';
 
 const MultipleFieldWrapper = styled.div`
@@ -78,6 +79,7 @@ const DescriptionPanel: FC<DescriptionPanelProps> = ({ goToNextTab, savePublicat
     <TabPanel ariaLabel="description" goToNextTab={goToNextTab} onClickSave={validateAndSave}>
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <FormCard>
+          <FormCardHeading>{t('heading.description')}</FormCardHeading>
           <StyledFieldWrapper>
             <Field
               aria-label="title"
