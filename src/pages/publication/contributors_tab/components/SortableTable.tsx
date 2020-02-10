8c9a192a9ae49417d2e5a50ad3af15d03eedca8a
Sarah Serussi <sase@unit.no>
2020-02-10 12:32:39 +0100

    map authority to contributor

--- src/pages/publication/contributors_tab/components/SortableTable.tsx
+++ src/pages/publication/contributors_tab/components/SortableTable.tsx
@@ -2,11 +2,12 @@ import arrayMove from 'array-move';
 import React, { FC } from 'react';
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 
-import { Checkbox, FormControlLabel, Table, TableBody, TableCell, TableRow } from '@material-ui/core';
+import { Checkbox, FormControlLabel, Table, TableBody, TableCell, TableRow, TextField } from '@material-ui/core';
 import { Contributor } from '../../../../types/contributor.types';
-import { FormikProps, useFormikContext } from 'formik';
+import { FormikProps, useFormikContext, Field } from 'formik';
 import { Publication } from '../../../../types/publication.types';
 import AddContributor from '../AddContributor';
+import { useTranslation } from 'react-i18next';
 
 interface SortableItemProps {
   contributor: Contributor;
@@ -14,27 +15,22 @@ interface SortableItemProps {
 }
 
 const SortableItem = SortableElement(({ contributor, placement }: SortableItemProps) => {
-  const { setFieldValue }: FormikProps<Publication> = useFormikContext();
-
-  const handleChange = (event: any) => {
-    setFieldValue(`contributors[${placement - 1}].corresponding`, event.target.checked);
-  };
+  const { t } = useTranslation();
 
   return (
     <TableRow tabIndex={0} key={contributor.name}>
       <TableCell component="th" scope="row">
         <div>{contributor.name}</div>
-        <FormControlLabel
-          control={
-            <Checkbox checked={contributor.corresponding} onChange={handleChange} value={contributor.corresponding} />
-          }
-          label="Korresponderende"
-        />
+        <Field name={`contributors[${placement - 1}].corresponding`}>
+          {({ field }: any) => (
+            <FormControlLabel control={<Checkbox checked={field.value} {...field} />} label="Korresponderende" />
+          )}
+        </Field>
         <div>
           {contributor.corresponding && (
-            <span>
-              Email: <input />
-            </span>
+            <Field name={`contributors[${placement - 1}].email`}>
+              {({ field }: any) => <TextField variant="outlined" label={t('profile:email')} {...field} />}
+            </Field>
           )}
         </div>
       </TableCell>
@@ -78,7 +74,19 @@ const SortableTable: FC<SortableTableProps> = ({ listOfContributors, push }) => 
   return (
     <>
       <SortableList contributors={listOfContributors} onSortEnd={onSortEnd} />
-      <AddContributor onAdd={author => push(author)} />
+      <AddContributor
+        onAdd={authority => {
+          const contributor: Contributor = {
+            name: authority.name,
+            systemControlNumber: authority.systemControlNumber,
+            institutions: [],
+            email: '',
+            type: '',
+            corresponding: false,
+          };
+          push(contributor);
+        }}
+      />
     </>
   );
 };
