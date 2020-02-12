01f50e4090cc7d92e3bba966a08ba4f3f7ca6259
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-02-12 09:18:40 +0100

    NP-462 Move list of disciplines

--- src/pages/publication/description_tab/DisciplineSearch.tsx
+++ src/pages/publication/description_tab/DisciplineSearch.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import AutoLookup from '../../../components/AutoLookup';
-import disciplines from '../../../utils/testfiles/disciplines_en.json';
+import disciplines from '../../../resources/disciplines.json';
 
 interface DisciplineSearchProps {
   dataTestId: string;
@@ -19,10 +19,10 @@ interface DisciplineType {
 const DisciplineSearch: FC<DisciplineSearchProps> = ({ dataTestId, setValueFunction, value, placeholder }) => {
   const { t } = useTranslation();
 
-  const searchResults = Object.values(disciplines)
-    .map(([mainDiscipline, ...disciplineTitles]) =>
-      disciplineTitles.map(disciplineTitle => ({
-        title: t(`disciplines:${disciplineTitle}`),
+  const searchResults = Object.entries(disciplines)
+    .map(([mainDiscipline, subDisciplines]) =>
+      subDisciplines.map(subDiscipline => ({
+        title: t(`disciplines:${subDiscipline}`),
         mainDiscipline: t(`disciplines:${mainDiscipline}`),
       }))
     )
