d4efccb1f46edf0fad99d3a166f59000f775ca36
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-02-17 12:34:33 +0100

    NP-510 Move app routes to a separate file

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react';
+import { Switch, Route } from 'react-router';
+import Dashboard from './pages/dashboard/Dashboard';
+import NewPublication from './pages/publication/NewPublication';
+import MyPublications from './pages/publication/MyPublications';
+import Search from './pages/search/Search';
+import PublicationPage from './pages/publication/PublicationPage';
+import User from './pages/user/User';
+import NotFound from './pages/errorpages/NotFound';
+import { useSelector } from 'react-redux';
+import { RootStore } from './redux/reducers/rootReducer';
+
+const AppRoutes: FC = () => {
+  const user = useSelector((store: RootStore) => store.user);
+
+  return (
+    <Switch>
+      <Route exact path="/" component={Dashboard} />
+      {user.isLoggedIn && <Route exact path="/new-publication" component={NewPublication} />}
+      {user.isLoggedIn && <Route exact path="/my-publications" component={MyPublications} />}
+      <Route exact path="/search" component={Search} />
+      <Route exact path="/publication/:publicationId" component={PublicationPage} />
+      <Route exact path="/search/:searchTerm" component={Search} />
+      <Route exact path="/search/:searchTerm/:offset" component={Search} />
+      {user.isLoggedIn && <Route exact path="/user" component={User} />}
+      <Route path="*" component={NotFound} />
+    </Switch>
+  );
+};
+
+export default AppRoutes;
