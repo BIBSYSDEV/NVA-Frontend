f7cf758430fee566bfebf90d7f9abba83ac92e2d
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-02-20 11:13:51 +0100

    Np 523 worklist tabs (#231)
    * NP-521 Add menu element and route to worklist for curators
    
    * NP-521 Sort i18n keys
    
    * NP-523 Add selectable tabs
    
    * NP-523 Style tab buttons
    
    * NP-523 Minor styling changes
    
    * NP-523 Change DOI icon

--- src/pages/Worklist.tsx
+++ src/pages/Worklist.tsx
@@ -1,7 +1,75 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import LinkIcon from '@material-ui/icons/Link';
+import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
+import styled from 'styled-components';
+
+const StyledTabsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const StyledTabButton = styled.button<{ isSelected: boolean }>`
+  all: unset;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  width: 15rem;
+  font-weight: bold;
+  font-size: 1.2rem;
+
+  ${({ isSelected, theme }) =>
+    isSelected &&
+    `
+      color: ${theme.palette.primary.main};
+      border-bottom: 0.3rem solid;
+    `};
+`;
+
+const StyledContent = styled.div`
+  margin-top: 2rem;
+`;
+
+const StyledPlaylistAddCheckIcon = styled(PlaylistAddCheckIcon)`
+  width: 3rem;
+  height: 3rem;
+`;
+
+const StyledLinkIcon = styled(LinkIcon)`
+  width: 3rem;
+  height: 3rem;
+`;
+
+enum Tab {
+  Doi,
+  Approval,
+}
 
 const Worklist: FC = () => {
-  return <h1>TODO</h1>;
+  const { t } = useTranslation('workLists');
+  const [selectedTab, setSelectedTab] = useState(Tab.Doi);
+
+  return (
+    <>
+      <StyledTabsContainer>
+        <StyledTabButton onClick={() => setSelectedTab(Tab.Approval)} isSelected={selectedTab === Tab.Approval}>
+          <StyledPlaylistAddCheckIcon fontSize="large" />
+          {t('for_approval')}
+        </StyledTabButton>
+        <StyledTabButton onClick={() => setSelectedTab(Tab.Doi)} isSelected={selectedTab === Tab.Doi}>
+          <StyledLinkIcon fontSize="large" />
+          {t('doi_requests')}
+        </StyledTabButton>
+      </StyledTabsContainer>
+
+      <StyledContent>
+        {selectedTab === Tab.Approval && <div>For Approval</div>}
+        {selectedTab === Tab.Doi && <div>DOI</div>}
+      </StyledContent>
+    </>
+  );
 };
 
 export default Worklist;
