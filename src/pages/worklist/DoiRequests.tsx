724801d07d3c6b253d3c0d8df98e738804f06d1d
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-02-21 11:14:04 +0100

    NP-532 Handle backend error

--- src/pages/worklist/DoiRequests.tsx
+++ src/pages/worklist/DoiRequests.tsx
@@ -4,22 +4,27 @@ import WorklistTable from './WorkListTable';
 import { useTranslation } from 'react-i18next';
 import Progress from '../../components/Progress';
 import SubHeading from '../../components/SubHeading';
+import { useDispatch } from 'react-redux';
+import { addNotification } from '../../redux/actions/notificationActions';
 
 const DoiRequests: FC = () => {
   const { t } = useTranslation('workLists');
   const [doiRequests, setDoiRequests] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchDoiRequests = async () => {
       const doiRequestsResponse = await getDoiRequests();
-      if (!doiRequestsResponse.error) {
+      if (doiRequestsResponse.error) {
+        dispatch(addNotification(t('feedback:error.get_doi_requests'), 'error'));
+      } else {
         setDoiRequests(doiRequestsResponse);
-        setIsLoading(false);
       }
+      setIsLoading(false);
     };
     fetchDoiRequests();
-  }, []);
+  }, [dispatch, t]);
 
   return isLoading ? (
     <Progress />
