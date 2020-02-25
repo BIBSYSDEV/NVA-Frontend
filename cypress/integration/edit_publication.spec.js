349d527dec599fe1e5d0e592d1db62ac5d1cbdd8
= <eirik.nilsen@unit.no>
2020-02-25 07:03:12 +0100

    Fixed wrong description in test

--- cypress/integration/edit_publication.spec.js
+++ cypress/integration/edit_publication.spec.js
@@ -1,10 +1,10 @@
-describe('User opens their Public Profile from My Publications Page', () => {
+describe('User opens an item in the My Publication list', () => {
   before('Given that the user is logged in as Creator:', () => {
     cy.visit('/');
     cy.mocklogin();
   });
 
-  it('The User should be able to open their public profile from the page My Publications', () => {
+  it('The User should be able to edit an item in the My Publications list', () => {
     // connect author
     cy.get('[data-testid=connect-author-modal]').click({ force: true });
     cy.get('[data-testid=author-radio-button]')
