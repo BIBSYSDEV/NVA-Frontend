f0239bcdc2f609d97fdd557ceae757a0f130327d
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-02-25 12:46:34 +0100

    NP-540 Minor refactoring

--- cypress/integration/menu.spec.js
+++ cypress/integration/menu.spec.js
@@ -2,24 +2,22 @@ import { setUser } from '../../src/redux/actions/userActions';
 import { mockUser } from '../../src/utils/testfiles/mock_feide_user';
 
 // Weird array format for affiliation is due to current format delivered by FEIDE
-const userWithAffiliations = { ...mockUser, 'custom:affiliation': '[member, employee, staff]' };
-const userWithoutAffiliations = { ...mockUser, 'custom:affiliation': '[]' };
+const authorizedUser = { ...mockUser, 'custom:affiliation': '[member, employee, staff]' };
+const unauthorizedUser = { ...mockUser, 'custom:affiliation': '[]', email: 'ost@loff.org' };
 
 describe('Menu', () => {
   beforeEach(() => {
     cy.server();
     cy.visit('/user');
-    cy.get('[data-testid=menu-login-button]').click({ force: true });
-    cy.get('[data-testid=menu]').click({ force: true });
   });
 
-  it('User with affiliations should see menu options', () => {
+  it('Authorized user with should see protected menu options', () => {
+    cy.get('[data-testid=menu-login-button]').click({ force: true });
     cy.window()
       .its('store')
-      .then(store => {
-        store.dispatch(setUser(userWithAffiliations));
-      });
+      .then(store => store.dispatch(setUser(authorizedUser)));
 
+    cy.get('[data-testid=menu]').click({ force: true });
     cy.get('[data-testid=menu-user-profile-button]').should('be.visible');
     cy.get('[data-testid=menu-my-publications-button]').should('be.visible');
     cy.get('[data-testid=menu-admin-institution-button]').should('be.visible');
@@ -28,38 +26,48 @@ describe('Menu', () => {
     cy.get('[data-testid=new-publication-button]').should('be.visible');
   });
 
-  it('User without affiliations should not see menu options', () => {
+  it('Unauthorized user without should not see protected menu options', () => {
+    cy.get('[data-testid=menu-login-button]').click({ force: true });
     cy.window()
       .its('store')
-      .then(store => {
-        store.dispatch(setUser(userWithoutAffiliations));
-      });
+      .then(store => store.dispatch(setUser(unauthorizedUser)));
 
+    cy.get('[data-testid=menu]').click({ force: true });
     cy.get('[data-testid=menu-user-profile-button]').should('be.visible');
-    cy.get('[data-testid=menu-admin-institution-button]').should('be.visible');
     cy.get('[data-testid=menu-logout-button]').should('be.visible');
+    cy.get('[data-testid=menu-admin-institution-button]').should('not.be.visible');
     cy.get('[data-testid=menu-my-publications-button]').should('not.be.visible');
     cy.get('[data-testid=menu-my-worklist-button]').should('not.be.visible');
     cy.get('[data-testid=new-publication-button]').should('not.be.visible');
   });
 
-  it('User without affiliations should get 404-message when visiting protected URLs', () => {
+  it('Unauthorized user should see 404-message when visiting protected URLs', () => {
+    cy.visit('/publication');
+    cy.get('[data-testid=menu-login-button]').click({ force: true });
     cy.window()
       .its('store')
-      .then(store => {
-        store.dispatch(setUser(userWithoutAffiliations));
-      });
-
-    cy.visit('/publication');
+      .then(store => store.dispatch(setUser(unauthorizedUser)));
     cy.contains('404');
 
     cy.visit('/my-publications');
+    cy.get('[data-testid=menu-login-button]').click({ force: true });
+    cy.window()
+      .its('store')
+      .then(store => store.dispatch(setUser(unauthorizedUser)));
     cy.contains('404');
 
     cy.visit('/admin-institutions');
+    cy.get('[data-testid=menu-login-button]').click({ force: true });
+    cy.window()
+      .its('store')
+      .then(store => store.dispatch(setUser(unauthorizedUser)));
     cy.contains('404');
 
     cy.visit('/worklist');
+    cy.get('[data-testid=menu-login-button]').click({ force: true });
+    cy.window()
+      .its('store')
+      .then(store => store.dispatch(setUser(unauthorizedUser)));
     cy.contains('404');
   });
 });
