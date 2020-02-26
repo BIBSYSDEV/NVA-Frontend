16f1d1b0c14cb853bef7e19515a73ee5326027ba
PCB <46489468+pcb-unit@users.noreply.github.com>
2020-02-26 14:37:50 +0100

    Np 543  link to admin institution page (#272)
    * NP-540-added link, routing, and skeleton page
    
    * NP-540-added test
    
    * NP-540-type
    
    * NP-543--QA-fixes
    
    * NP-543--QA-text-fixes
    
    * NP-543--QA-text-fixes2
    
    Co-authored-by: placbo <15780177+placbo@users.noreply.github.com>

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { Switch, Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import Dashboard from './pages/dashboard/Dashboard';
 import EditPublication from './pages/publication/EditPublication';
 import MyPublications from './pages/publication/MyPublications';
@@ -9,9 +9,10 @@ import User from './pages/user/User';
 import NotFound from './pages/errorpages/NotFound';
 import { useSelector } from 'react-redux';
 import { RootStore } from './redux/reducers/rootReducer';
-import { checkIfAppAdmin, checkIfPublisher, checkIfCurator } from './utils/authorization';
+import { checkIfAppAdmin, checkIfCurator, checkIfPublisher } from './utils/authorization';
 import PublicProfile from './pages/publication/PublicProfile';
-import AdminCustomerInstututionPage from './pages/AdminCustomerInstitutionsPage';
+import AdminCustomerInstitutionPage from './pages/admin/AdminCustomerInstitutionPage';
+import AdminCustomerInstitutionsPage from './pages/admin/AdminCustomerInstitutionsPage';
 import WorklistPage from './pages/worklist/WorklistPage';
 
 const AppRoutes: FC = () => {
@@ -27,7 +28,8 @@ const AppRoutes: FC = () => {
       {isPublisher && <Route exact path="/publication/:id" component={EditPublication} />}
       {isPublisher && <Route exact path="/my-publications" component={MyPublications} />}
       {isCurator && <Route exact path="/worklist" component={WorklistPage} />}
-      {isAppAdmin && <Route exact path="/admin-institutions" component={AdminCustomerInstututionPage} />}
+      {isAppAdmin && <Route exact path="/admin-institutions" component={AdminCustomerInstitutionsPage} />}
+      {isAppAdmin && <Route exact path="/admin-institution" component={AdminCustomerInstitutionPage} />}
       {user.isLoggedIn && <Route exact path="/public-profile/:userName" component={PublicProfile} />}
       <Route exact path="/search" component={Search} />
       <Route exact path="/publication/:publicationId" component={PublicationPage} />
