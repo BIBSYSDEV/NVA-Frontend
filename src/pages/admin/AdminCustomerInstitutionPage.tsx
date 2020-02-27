8f717f8347bc51e5d85587ceb624213da77e6644
Per Christian Bjelke <pcb@unit.no>
2020-02-27 10:31:15 +0100

    NP-544--added formik

--- src/pages/admin/AdminCustomerInstitutionPage.tsx
+++ src/pages/admin/AdminCustomerInstitutionPage.tsx
@@ -1,10 +1,59 @@
 import React, { FC } from 'react';
 import Card from '../../components/Card';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+import { Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import { TextField } from '@material-ui/core';
+
+const StyledFieldWrapper = styled.div`
+  margin: 1rem;
+  flex: 1 0 40%;
+`;
+
+enum FieldNames {
+  NAME = 'name',
+}
 
 const AdminCustomerInstitutionPage: FC = () => {
+  const { t } = useTranslation('publication');
+
   return (
     <Card>
-      <form>instituition-form</form>
+      <Formik
+        initialValues={{
+          name: '',
+          displayName: '',
+          shortName: '',
+          archiveName: '',
+          CNAME: '',
+          institutionDNS: '',
+          AdministrationId: '',
+          FeideOrganizationId: '',
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string().required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}>
+        <Form>
+          <StyledFieldWrapper>
+            <Field
+              aria-label="name"
+              name={FieldNames.NAME}
+              label={t('common:name')}
+              component={TextField}
+              fullWidth
+              variant="outlined"
+              inputProps={{ 'data-testid': 'customer-instituiton-name-input' }}
+            />
+          </StyledFieldWrapper>
+        </Form>
+      </Formik>
     </Card>
   );
 };
