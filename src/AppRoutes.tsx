0009f61f54af9ba6b2be8e98aca87a50bcb020bf
sarahse <sase@unit.no>
2020-03-02 10:29:29 +0100

    use react suspense and lazy loading for code splitting (#271)
    Co-authored-by: Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,19 +1,21 @@
-import React, { FC } from 'react';
-import { Route, Switch } from 'react-router';
-import Dashboard from './pages/dashboard/Dashboard';
-import EditPublication from './pages/publication/EditPublication';
-import MyPublications from './pages/publication/MyPublications';
-import Search from './pages/search/Search';
-import PublicationPage from './pages/publication/PublicationPage';
-import User from './pages/user/User';
-import NotFound from './pages/errorpages/NotFound';
+import React, { FC, Suspense, lazy } from 'react';
+import { Switch, Route } from 'react-router';
 import { useSelector } from 'react-redux';
 import { RootStore } from './redux/reducers/rootReducer';
-import { checkIfAppAdmin, checkIfCurator, checkIfPublisher } from './utils/authorization';
-import PublicProfile from './pages/publication/PublicProfile';
-import AdminCustomerInstitutionPage from './pages/admin/AdminCustomerInstitutionPage';
-import AdminCustomerInstitutionsPage from './pages/admin/AdminCustomerInstitutionsPage';
-import WorklistPage from './pages/worklist/WorklistPage';
+import { checkIfAppAdmin, checkIfPublisher, checkIfCurator } from './utils/authorization';
+import DelayedFallback from './components/DelayedFallback';
+
+const Dashboard = lazy(() => import('./pages/dashboard/Dashboard'));
+const EditPublication = lazy(() => import('./pages/publication/EditPublication'));
+const MyPublications = lazy(() => import('./pages/publication/MyPublications'));
+const Search = lazy(() => import('./pages/search/Search'));
+const PublicationPage = lazy(() => import('./pages/publication/PublicationPage'));
+const User = lazy(() => import('./pages/user/User'));
+const NotFound = lazy(() => import('./pages/errorpages/NotFound'));
+const PublicProfile = lazy(() => import('./pages/publication/PublicProfile'));
+const AdminCustomerInstitutionPage = lazy(() => import('./pages/admin/AdminCustomerInstitutionPage'));
+const AdminCustomerInstitutionsPage = lazy(() => import('./pages/admin/AdminCustomerInstitutionsPage'));
+const WorklistPage = lazy(() => import('./pages/worklist/WorklistPage'));
 
 const AppRoutes: FC = () => {
   const user = useSelector((store: RootStore) => store.user);
@@ -22,22 +24,24 @@ const AppRoutes: FC = () => {
   const isCurator = checkIfCurator(user);
 
   return (
-    <Switch>
-      <Route exact path="/" component={Dashboard} />
-      {isPublisher && <Route exact path="/publication" component={EditPublication} />}
-      {isPublisher && <Route exact path="/publication/:id" component={EditPublication} />}
-      {isPublisher && <Route exact path="/my-publications" component={MyPublications} />}
-      {isCurator && <Route exact path="/worklist" component={WorklistPage} />}
-      {isAppAdmin && <Route exact path="/admin-institutions" component={AdminCustomerInstitutionsPage} />}
-      {isAppAdmin && <Route exact path="/admin-institution" component={AdminCustomerInstitutionPage} />}
-      {user.isLoggedIn && <Route exact path="/public-profile/:userName" component={PublicProfile} />}
-      <Route exact path="/search" component={Search} />
-      <Route exact path="/publication/:publicationId" component={PublicationPage} />
-      <Route exact path="/search/:searchTerm" component={Search} />
-      <Route exact path="/search/:searchTerm/:offset" component={Search} />
-      {user.isLoggedIn && <Route exact path="/user" component={User} />}
-      <Route path="*" component={NotFound} />
-    </Switch>
+    <Suspense fallback={<DelayedFallback />}>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        {isPublisher && <Route exact path="/publication" component={EditPublication} />}
+        {isPublisher && <Route exact path="/publication/:id" component={EditPublication} />}
+        {isPublisher && <Route exact path="/my-publications" component={MyPublications} />}
+        {isCurator && <Route exact path="/worklist" component={WorklistPage} />}
+        {isAppAdmin && <Route exact path="/admin-institutions" component={AdminCustomerInstitutionsPage} />}
+        {isAppAdmin && <Route exact path="/admin-institution" component={AdminCustomerInstitutionPage} />}
+        {user.isLoggedIn && <Route exact path="/public-profile/:userName" component={PublicProfile} />}
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/publication/:publicationId" component={PublicationPage} />
+        <Route exact path="/search/:searchTerm" component={Search} />
+        <Route exact path="/search/:searchTerm/:offset" component={Search} />
+        {user.isLoggedIn && <Route exact path="/user" component={User} />}
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Suspense>
   );
 };
 
