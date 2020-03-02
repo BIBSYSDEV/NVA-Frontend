1f27b4328848d5d430bf3134a5642ff3be1a64a3
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-03-02 13:31:36 +0100

    NP-559 Refactor orcid action notifications

--- src/redux/actions/orcidActions.ts
+++ src/redux/actions/orcidActions.ts
@@ -1,28 +1,13 @@
-import { NotificationVariant } from '../../types/notification.types';
-
-export const ORCID_REQUEST_FAILURE = 'orcid request failure';
 export const SET_EXTERNAL_ORCID = 'set external orcid';
-
-export const orcidRequestFailure = (message: string): OrcidRequestFailureAction => ({
-  type: ORCID_REQUEST_FAILURE,
-  message,
-  variant: NotificationVariant.Error,
-});
 
 export const setExternalOrcid = (orcid: string): SetExternalOrcidAction => ({
   type: SET_EXTERNAL_ORCID,
   orcid,
 });
 
-interface OrcidRequestFailureAction {
-  type: typeof ORCID_REQUEST_FAILURE;
-  message: string;
-  variant: NotificationVariant;
-}
-
 interface SetExternalOrcidAction {
   type: typeof SET_EXTERNAL_ORCID;
   orcid: string;
 }
 
-export type OrcidActions = OrcidRequestFailureAction | SetExternalOrcidAction;
+export type OrcidActions = SetExternalOrcidAction;
