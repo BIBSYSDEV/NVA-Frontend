884ab43296dd1a252aeada902c41dde6d3b919ac
Per Christian Bjelke <pcb@unit.no>
2020-03-03 14:56:04 +0100

    NP-544--QA-fixes++

--- src/pages/admin/AdminCustomerInstitutionPage.tsx
+++ src/pages/admin/AdminCustomerInstitutionPage.tsx
@@ -4,11 +4,12 @@ import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import { Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
-import { Button, TextField } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { TextField } from 'formik-material-ui';
 import { CustomerInstitutionFieldNames, emptyCustomerInstitution } from '../../types/customerInstitution.types';
 import Heading from '../../components/Heading';
 
-const StyledFieldWrapper = styled.div`
+const StyledField = styled(Field)`
   margin: 1rem;
   flex: 1 0 40%;
 `;
@@ -22,15 +23,13 @@ const StyledButtonContainer = styled.div`
 const AdminCustomerInstitutionPage: FC = () => {
   const { t } = useTranslation('admin');
 
-  const onClickSave = () => {};
-
   return (
     <Card>
       <Heading>{t('add_institution')}</Heading>
       <Formik
         initialValues={emptyCustomerInstitution}
         validationSchema={Yup.object({
-          name: Yup.string().required('Required'),
+          name: Yup.string().required(t('feedback.required_field')),
         })}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
@@ -39,100 +38,80 @@ const AdminCustomerInstitutionPage: FC = () => {
           }, 400);
         }}>
         <Form>
-          <StyledFieldWrapper>
-            <Field
-              aria-label={CustomerInstitutionFieldNames.NAME}
-              name={CustomerInstitutionFieldNames.NAME}
-              label={t('organizationRegisterName')}
-              component={TextField}
-              fullWidth
-              variant="outlined"
-              inputProps={{ 'data-testid': 'customer-instituiton-name-input' }}
-            />
-          </StyledFieldWrapper>
-          <StyledFieldWrapper>
-            <Field
-              aria-label={CustomerInstitutionFieldNames.DISPLAY_NAME}
-              name={CustomerInstitutionFieldNames.DISPLAY_NAME}
-              label={t('displayName')}
-              component={TextField}
-              fullWidth
-              variant="outlined"
-              inputProps={{ 'data-testid': 'customer-instituiton-display-name-input' }}
-            />
-          </StyledFieldWrapper>
-          <StyledFieldWrapper>
-            <Field
-              aria-label={CustomerInstitutionFieldNames.SHORT_NAME}
-              name={CustomerInstitutionFieldNames.SHORT_NAME}
-              label={t('shortName')}
-              component={TextField}
-              fullWidth
-              variant="outlined"
-              inputProps={{ 'data-testid': 'customer-instituiton-short-name-input' }}
-            />
-          </StyledFieldWrapper>
-          <StyledFieldWrapper>
-            <Field
-              aria-label={CustomerInstitutionFieldNames.ARCHIVE_NAME}
-              name={CustomerInstitutionFieldNames.ARCHIVE_NAME}
-              label={t('archiveName')}
-              component={TextField}
-              fullWidth
-              variant="outlined"
-              inputProps={{ 'data-testid': 'customer-instituiton-archive-name-input' }}
-            />
-          </StyledFieldWrapper>
-          <StyledFieldWrapper>
-            <Field
-              aria-label={CustomerInstitutionFieldNames.CNAME}
-              name={CustomerInstitutionFieldNames.CNAME}
-              label={t('cname')}
-              component={TextField}
-              fullWidth
-              variant="outlined"
-              inputProps={{ 'data-testid': 'customer-instituiton-cname-input' }}
-            />
-          </StyledFieldWrapper>
-          <StyledFieldWrapper>
-            <Field
-              aria-label={CustomerInstitutionFieldNames.INSTITUTION_DNS}
-              name={CustomerInstitutionFieldNames.INSTITUTION_DNS}
-              label={t('institutionDns')}
-              component={TextField}
-              fullWidth
-              variant="outlined"
-              inputProps={{ 'data-testid': 'customer-instituiton-institution-dns-input' }}
-            />
-          </StyledFieldWrapper>
-          <StyledFieldWrapper>
-            <Field
-              aria-label={CustomerInstitutionFieldNames.ADMINISTRATION_ID}
-              name={CustomerInstitutionFieldNames.ADMINISTRATION_ID}
-              label={t('administrationId')}
-              component={TextField}
-              fullWidth
-              variant="outlined"
-              inputProps={{ 'data-testid': 'customer-instituiton-administrator-id-input' }}
-            />
-          </StyledFieldWrapper>
-          <StyledFieldWrapper>
-            <Field
-              aria-label={CustomerInstitutionFieldNames.FEIDE_ORGANIZATION_ID}
-              name={CustomerInstitutionFieldNames.FEIDE_ORGANIZATION_ID}
-              label={t('feideOrganizationId')}
-              component={TextField}
-              fullWidth
-              variant="outlined"
-              inputProps={{ 'data-testid': 'customer-instituiton-feide-organization-id-input' }}
-            />
-          </StyledFieldWrapper>
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.NAME}
+            name={CustomerInstitutionFieldNames.NAME}
+            label={t('organization_register_name')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-name-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.DISPLAY_NAME}
+            name={CustomerInstitutionFieldNames.DISPLAY_NAME}
+            label={t('displayName')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-display-name-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.SHORT_NAME}
+            name={CustomerInstitutionFieldNames.SHORT_NAME}
+            label={t('shortName')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-short-name-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.ARCHIVE_NAME}
+            name={CustomerInstitutionFieldNames.ARCHIVE_NAME}
+            label={t('archiveName')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-archive-name-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.CNAME}
+            name={CustomerInstitutionFieldNames.CNAME}
+            label={t('cname')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-cname-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.INSTITUTION_DNS}
+            name={CustomerInstitutionFieldNames.INSTITUTION_DNS}
+            label={t('institutionDns')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-institution-dns-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.ADMINISTRATION_ID}
+            name={CustomerInstitutionFieldNames.ADMINISTRATION_ID}
+            label={t('administrationId')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-administrator-id-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.FEIDE_ORGANIZATION_ID}
+            name={CustomerInstitutionFieldNames.FEIDE_ORGANIZATION_ID}
+            label={t('feideOrganizationId')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-feide-organization-id-input' }}
+          />
           <StyledButtonContainer>
-            <Button
-              color="primary"
-              data-testid="customer-instituiton-save-button"
-              variant="contained"
-              onClick={onClickSave}>
+            <Button color="primary" data-testid="customer-instituiton-save-button" variant="contained" type="submit">
               {t('common:save')}
             </Button>
           </StyledButtonContainer>
