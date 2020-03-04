1b6ed8473860e6389d07337a49ee099d5dfb6563
PCB <46489468+pcb-unit@users.noreply.github.com>
2020-03-04 12:26:27 +0100

    Np 544  create customer inst form (#298)
    * NP-544--added formik
    
    * NP-544--defined types
    
    * NP-544--defined types
    
    * NP-544--added rest of the fields and translations
    
    * NP-544--text-changes
    
    * NP-544--added test for form
    
    * NP-544--added test for form part 2
    
    * NP-544--QA-fixes++
    
    * NP-544--QA-dato
    
    * NP-544--QA-translations snakecase
    
    * NP-544--Fixed tests
    
    * NP-544--camelcase

--- src/pages/admin/AdminCustomerInstitutionPage.tsx
+++ src/pages/admin/AdminCustomerInstitutionPage.tsx
@@ -1,10 +1,122 @@
 import React, { FC } from 'react';
 import Card from '../../components/Card';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+import { Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import { Button } from '@material-ui/core';
+import { TextField } from 'formik-material-ui';
+import { CustomerInstitutionFieldNames, emptyCustomerInstitution } from '../../types/customerInstitution.types';
+import Heading from '../../components/Heading';
 
+const StyledField = styled(Field)`
+  margin: 1rem;
+  flex: 1 0 40%;
+`;
+
+const StyledButtonContainer = styled.div`
+  margin-top: 2rem;
+  margin-right: 1rem;
+  display: flex;
+  justify-content: flex-end;
+`;
 const AdminCustomerInstitutionPage: FC = () => {
+  const { t } = useTranslation('admin');
+
   return (
     <Card>
-      <form>instituition-form</form>
+      <Heading>{t('add_institution')}</Heading>
+      <Formik
+        initialValues={emptyCustomerInstitution}
+        validationSchema={Yup.object({
+          name: Yup.string().required(t('feedback.required_field')),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}>
+        <Form>
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.NAME}
+            name={CustomerInstitutionFieldNames.NAME}
+            label={t('organization_register_name')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-name-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.DISPLAY_NAME}
+            name={CustomerInstitutionFieldNames.DISPLAY_NAME}
+            label={t('display_name')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-display-name-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.SHORT_NAME}
+            name={CustomerInstitutionFieldNames.SHORT_NAME}
+            label={t('short_name')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-short-name-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.ARCHIVE_NAME}
+            name={CustomerInstitutionFieldNames.ARCHIVE_NAME}
+            label={t('archive_name')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-archive-name-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.CNAME}
+            name={CustomerInstitutionFieldNames.CNAME}
+            label={t('cname')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-cname-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.INSTITUTION_DNS}
+            name={CustomerInstitutionFieldNames.INSTITUTION_DNS}
+            label={t('institution_dns')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-institution-dns-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.ADMINISTRATION_ID}
+            name={CustomerInstitutionFieldNames.ADMINISTRATION_ID}
+            label={t('administration_id')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-administrator-id-input' }}
+          />
+          <StyledField
+            aria-label={CustomerInstitutionFieldNames.FEIDE_ORGANIZATION_ID}
+            name={CustomerInstitutionFieldNames.FEIDE_ORGANIZATION_ID}
+            label={t('feide_organization_id')}
+            component={TextField}
+            fullWidth
+            variant="outlined"
+            inputProps={{ 'data-testid': 'customer-instituiton-feide-organization-id-input' }}
+          />
+          <StyledButtonContainer>
+            <Button color="primary" data-testid="customer-instituiton-save-button" variant="contained" type="submit">
+              {t('common:save')}
+            </Button>
+          </StyledButtonContainer>
+        </Form>
+      </Formik>
     </Card>
   );
 };
