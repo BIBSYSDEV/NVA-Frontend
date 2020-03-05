8d7184a98a4c7de0f2593777c60f1ce44f14640c
Per Christian Bjelke <pcb@unit.no>
2020-03-05 15:06:17 +0100

    NP-555--added test

--- cypress/integration/admin_institution.js
+++ cypress/integration/admin_institution.js
@@ -3,8 +3,9 @@ import { mockUser } from '../../src/utils/testfiles/mock_feide_user';
 const authorizedUser = { ...mockUser, 'custom:affiliation': '[member, employee, staff]', email: 'ost@unit.no' }; //@unit.no-address resolves to app admin
 
 describe('User administers institutions ', () => {
-  before('Given that the user is logged in as Application administrator:', () => {
+  beforeEach('Given that the user is logged in as Application administrator:', () => {
     cy.visit('/');
+    cy.server();
     cy.get('[data-testid=menu-login-button]').click({ force: true });
     cy.setUserInRedux(authorizedUser);
   });
@@ -37,5 +38,36 @@ describe('User administers institutions ', () => {
     cy.get('[data-testid=customer-instituiton-feide-organization-id-input]').type('2345667');
 
     cy.get('[data-testid=customer-instituiton-save-button]').click({ force: true });
+  });
+
+  it('The user should be able to upload a file for an institution', () => {
+    // Open administer institutions page
+    cy.get('[data-testid=menu]').click({ force: true });
+    cy.get('[data-testid=menu-admin-institution-button]').click({ force: true });
+
+    // Open new institution page
+    cy.get('[data-testid=add-institution-button]').click({ force: true });
+
+    // Mock Uppys upload requests to S3 Bucket
+    cy.route({
+      method: 'PUT',
+      url: 'https://file-upload.com/files/', // Must match URL set in mock-interceptor, which cannot be imported into a test
+      response: '',
+      headers: { ETag: 'etag' },
+    });
+
+    // Inspired by: https://github.com/cypress-io/cypress/issues/170#issuecomment-533519550
+    Cypress.Commands.add('uploadFile', { prevSubject: true }, (subject, fileName) => {
+      cy.fixture(fileName).then(content => {
+        const el = subject[0];
+        const testFile = new File([content], fileName);
+        const dataTransfer = new DataTransfer();
+        dataTransfer.items.add(testFile);
+        el.files = dataTransfer.files;
+        cy.wrap(subject).trigger('change', { force: true });
+      });
+    });
+    cy.get('input[type=file]').uploadFile('img.jpg');
+    cy.get('[data-testid=uploaded-file-card]').should('be.visible');
   });
 });
