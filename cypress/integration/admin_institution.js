77048e15ce6a851dd7d9605291fd501c42c88df9
Per Christian Bjelke <pcb@unit.no>
2020-03-09 12:01:35 +0100

    np.NP-555--qa-fixes. Moved out cypress-commmand

--- cypress/integration/admin_institution.js
+++ cypress/integration/admin_institution.js
@@ -56,17 +56,6 @@ describe('User administers institutions ', () => {
       headers: { ETag: 'etag' },
     });
 
-    // Inspired by: https://github.com/cypress-io/cypress/issues/170#issuecomment-533519550
-    Cypress.Commands.add('uploadFile', { prevSubject: true }, (subject, fileName) => {
-      cy.fixture(fileName).then(content => {
-        const el = subject[0];
-        const testFile = new File([content], fileName);
-        const dataTransfer = new DataTransfer();
-        dataTransfer.items.add(testFile);
-        el.files = dataTransfer.files;
-        cy.wrap(subject).trigger('change', { force: true });
-      });
-    });
     cy.get('input[type=file]').uploadFile('img.jpg');
     cy.get('[data-testid=uploaded-file-card]').should('be.visible');
   });
