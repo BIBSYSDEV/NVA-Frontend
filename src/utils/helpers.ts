dec0cf86a914d4ddb457ff38206e2111330d700d
Sarah Serussi <sase@unit.no>
2020-03-18 10:05:22 +0100

    remove removeDuplicates function

--- src/utils/helpers.ts
+++
@@ -1,4 +0,0 @@
-// Remove duplicates filtered by id
-export const removeContributorDuplicatesById = (list: any[]) => {
-  return [...new Map(list.map(item => [item.identity.id, item])).values()];
-};
