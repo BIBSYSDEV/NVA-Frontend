6be077cca2e2f2f32e469154e96e602103f000ef
Sarah Serussi <sase@unit.no>
2020-03-23 15:25:34 +0100

    cleanup

--- src/api/mock-interceptor.ts
+++ src/api/mock-interceptor.ts
@@ -118,8 +118,6 @@ export const interceptRequestsOnMock = () => {
   mock
     .onPut(new RegExp(`${API_URL}${AuthorityApiPaths.PERSON}/*`))
     .replyOnce(200, mockSingleAuthorityResponseWithFeide);
-  // mock.onPut(new RegExp(`${API_URL}${AuthorityApiPaths.PERSON}/*`)).replyOnce(200, mockSingleAuthorityResponse);
-  // mock.onPut(new RegExp(`${API_URL}${AuthorityApiPaths.PERSON}/*`)).reply(200, mockSingleAuthorityResponseWithOrcid);
   mock
     .onPost(new RegExp(`${API_URL}${AuthorityApiPaths.PERSON}/901790000000/identifiers/orgunitid/*`))
     .replyOnce(200, mockSingleAuthorityResponse);
