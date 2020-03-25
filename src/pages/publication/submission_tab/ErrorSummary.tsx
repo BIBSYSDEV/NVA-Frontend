b2385b1dfcc37490c61401ba2b97028c654aad3a
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-03-25 10:20:31 +0100

    NP-667 Simplify summary of errors

--- src/pages/publication/submission_tab/ErrorSummary.tsx
+++ src/pages/publication/submission_tab/ErrorSummary.tsx
@@ -12,31 +12,32 @@ const StyledCard = styled(Card)`
   background-color: ${({ theme }) => theme.palette.danger.light};
 `;
 
-interface ErrorSummaryProps {}
-
-const ErrorSummary: FC<ErrorSummaryProps> = () => {
+const ErrorSummary: FC = () => {
   const { t } = useTranslation('publication');
   const { errors }: FormikProps<FormikPublication> = useFormikContext();
-  const validationErrors = errors.entityDescription;
+  const validationErrors = errors.entityDescription || {};
 
-  return validationErrors ? (
+  const flattendErrors = Object.entries(validationErrors)
+    .map(([fieldName, errorMessage]) => {
+      if (typeof errorMessage === 'object') {
+        return Object.entries(errorMessage).map(([fieldName2, errorMessage2]) => ({
+          fieldName: fieldName2,
+          errorMessage: errorMessage2,
+        }));
+      }
+      return { fieldName, errorMessage };
+    })
+    .flat();
+
+  return flattendErrors.length > 0 ? (
     <StyledCard>
       <Heading>{t('heading.validation_errors')}</Heading>
-      {Object.entries(validationErrors).map(([key, value]) =>
-        typeof value === 'string' ? (
-          <NormalText key={key}>
-            <b>{t(`formikValues:entityDescription.${key}`)}: </b>
-            {value}
-          </NormalText>
-        ) : (
-          Object.entries(value || {}).map(([key2, value2]) => (
-            <NormalText key={key2}>
-              <b>{t(`formikValues:entityDescription.${key2}`)}: </b>
-              {value2}
-            </NormalText>
-          ))
-        )
-      )}
+      {flattendErrors.map(({ fieldName, errorMessage }) => (
+        <NormalText key={fieldName}>
+          <b>{t(`formikValues:entityDescription.${fieldName}`)}: </b>
+          {errorMessage}
+        </NormalText>
+      ))}
     </StyledCard>
   ) : null;
 };
