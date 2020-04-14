6fe78915f3a8d46315f830f5537aef7bf878a6e1
sarahse <sase@unit.no>
2020-04-14 14:28:52 +0200

    Np 753  styling fonts submission page (#503)
    * same font on submission page
    
    * NP-764--rename publisher field

--- src/components/LabelContentRow.tsx
+++ src/components/LabelContentRow.tsx
@@ -12,12 +12,17 @@ const StyledRow = styled.div`
 const StyledLabel = styled(NormalText)`
   display: inline-block;
   min-width: 17rem;
-  font-weight: normal;
+  font-weight: bold;
 `;
 
-const StyledTextContainer = styled.div`
+const StyledSingleRowContainer = styled(NormalText)`
   display: inline-block;
-  font-weight: bold;
+  flex: 1;
+  min-width: 60%;
+`;
+
+const StyledMultipleRowContainer = styled.div`
+  display: inline-block;
   flex: 1;
   min-width: 60%;
 `;
@@ -26,13 +31,18 @@ interface LabelContentRowProps {
   label: string;
   children: any;
   dataTestId?: string;
+  multiple?: boolean;
 }
 
-const LabelContentRow: React.FC<LabelContentRowProps> = ({ label, children, dataTestId }) => {
+const LabelContentRow: React.FC<LabelContentRowProps> = ({ label, children, dataTestId, multiple }) => {
   return (
     <StyledRow data-testid={dataTestId}>
       <StyledLabel>{label}</StyledLabel>
-      <StyledTextContainer>{children}</StyledTextContainer>
+      {multiple ? (
+        <StyledMultipleRowContainer>{children}</StyledMultipleRowContainer>
+      ) : (
+        <StyledSingleRowContainer>{children}</StyledSingleRowContainer>
+      )}
     </StyledRow>
   );
 };
