bb26f993b63b95a73978133ef4337ee67f122555
sarahse <sase@unit.no>
2020-04-28 09:16:22 +0200

    Np 772 navigate from publication (#582)
    * NP-772 Initial work with warning when closing publication form
    
    * block navigation if form is dirty
    
    * cleanup
    
    * fix tests
    
    * more cleanup
    
    * remove unnecessary prop
    
    * more descriptive propNames
    
    Co-authored-by: Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>

---
+++ src/components/RouteLeavingGuard.tsx
@@ -0,0 +1,51 @@
+import { Location } from 'history';
+import React, { useEffect, useState, FC } from 'react';
+import { Prompt, useHistory } from 'react-router-dom';
+import ConfirmDialog from './ConfirmDialog';
+
+interface RouteLeavingGuardProps {
+  modalDescription: string;
+  modalHeading: string;
+  shouldBlockNavigation: boolean;
+}
+const RouteLeavingGuard: FC<RouteLeavingGuardProps> = ({ modalDescription, modalHeading, shouldBlockNavigation }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [nextLocation, setNextLocation] = useState<Location | null>(null);
+  const [confirmedNavigation, setConfirmedNavigation] = useState(false);
+  const history = useHistory();
+
+  const handleBlockedNavigation = (nextLocation: Location): boolean => {
+    if (!confirmedNavigation && shouldBlockNavigation) {
+      setShowModal(true);
+      setNextLocation(nextLocation);
+      return false;
+    }
+    return true;
+  };
+
+  const handleConfirmNavigationClick = () => {
+    setShowModal(false);
+    setConfirmedNavigation(true);
+  };
+
+  useEffect(() => {
+    if (confirmedNavigation && nextLocation) {
+      history.push(nextLocation.pathname);
+    }
+  }, [confirmedNavigation, nextLocation, history]);
+
+  return (
+    <>
+      <Prompt when={shouldBlockNavigation} message={handleBlockedNavigation} />
+      <ConfirmDialog
+        open={showModal}
+        title={modalHeading}
+        text={modalDescription}
+        onAccept={handleConfirmNavigationClick}
+        onCancel={() => setShowModal(false)}
+      />
+    </>
+  );
+};
+
+export default RouteLeavingGuard;
