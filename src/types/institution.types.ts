5e484f4bc57250d6a11adc7a07e7d1a27b091d3a
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-04-29 11:29:33 +0200

    NP-572 Remove unused code

--- src/types/institution.types.ts
+++ src/types/institution.types.ts
@@ -7,30 +7,12 @@ export interface RecursiveInstitutionUnit extends InstitutionUnitBase {
   subunits?: RecursiveInstitutionUnit[];
 }
 
-export interface FormikInstitutionUnit extends Partial<InstitutionUnitBase> {
-  subunits?: InstitutionUnitBase[];
+export interface FormikInstitutionUnit extends Partial<RecursiveInstitutionUnit> {
   subunit?: InstitutionUnitBase;
   unit?: RecursiveInstitutionUnit;
-  editId?: string;
 }
-
-export const emptyRecursiveUnit: RecursiveInstitutionUnit = {
-  name: '',
-  id: '',
-  subunits: [],
-};
-
-export const emptyFormikUnit: FormikInstitutionUnit = {
-  subunits: [],
-  unit: emptyRecursiveUnit,
-};
 
 export enum FormikInstitutionUnitFieldNames {
   SUB_UNIT = 'subunit',
   UNIT = 'unit',
-  // TODO: Remove these:
-  NAME = 'name',
-  ID = 'id',
-  SUBUNITS = 'subunits',
-  EDIT_ID = 'editId',
 }
