e6c12e1de19003dddfff47fe3a98ca89e5249bc3
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-05-05 13:35:22 +0200

    Np 834 app admin institutions (#615)
    * NP-834 Move fetch institution to a custom hook
    
    * NP-834 Use formik props instead of adding own state
    
    * NP-834 Add instition selection to customer form
    
    * NP-834 Comment out file related code
    
    * NP-834 Allow filtering on acronym
    
    * NP-834 Test acronyms
    
    * NP-834 Update tests
    
    * NP-834 Remove redundant tag
    
    * NP-834 Add type
    
    * NP-834 Remove commented file upload code
    
    * NP-834 Generalize institutino autocomplete
    
    * NP-834 Move general institution components to same folder
    
    * NP-834 Update values when changing institution
    
    * NP-834 Set aria-label
    
    * NP-834 Update comments
    
    * NP-834 Avoid console warnings
    
    * NP-834 Update tests
    
    * NP-834 Use default prop

---
+++ src/components/institution/InstitutionAutocomplete.tsx
@@ -0,0 +1,68 @@
+import { FC, ChangeEvent } from 'react';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import React from 'react';
+import { InstitutionUnitBase } from '../../types/institution.types';
+import { TextField, CircularProgress } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+import { FilterOptionsState } from '@material-ui/lab/useAutocomplete';
+
+interface InstitutionAutocompleteProps {
+  institutions: InstitutionUnitBase[];
+  onChange: (value: InstitutionUnitBase | null) => void;
+  value: InstitutionUnitBase | null;
+  isLoading?: boolean;
+  disabled?: boolean;
+}
+
+const InstitutionAutocomplete: FC<InstitutionAutocompleteProps> = ({
+  institutions,
+  onChange,
+  value = null,
+  isLoading = false,
+  disabled = false,
+}) => {
+  const { t } = useTranslation('common');
+
+  return (
+    <Autocomplete
+      disabled={disabled}
+      options={institutions}
+      getOptionLabel={(option: InstitutionUnitBase) => option.name}
+      getOptionSelected={(option: InstitutionUnitBase, value: InstitutionUnitBase) => option.id === value.id}
+      value={value}
+      filterOptions={(options: InstitutionUnitBase[], state: FilterOptionsState<InstitutionUnitBase>) => {
+        const inputValue = state.inputValue.toLowerCase();
+        return options.filter(
+          (option) =>
+            option.name.toLowerCase().includes(inputValue) || option.acronym?.toLowerCase().includes(inputValue)
+        );
+      }}
+      noOptionsText={t('no_hits')}
+      onChange={(_: ChangeEvent<{}>, value: InstitutionUnitBase | null) => onChange(value)}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={t('institution')}
+          placeholder={t('institution:search_institution')}
+          variant="outlined"
+          inputProps={{
+            ...params.inputProps,
+            'data-testid': 'autocomplete-institution',
+            'aria-label': t('institution'),
+          }}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <>
+                {isLoading && <CircularProgress size={20} />}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+};
+
+export default InstitutionAutocomplete;
