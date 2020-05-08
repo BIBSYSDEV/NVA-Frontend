964f10a07c98d45445df7d2095b9f3d92c7f0c25
Sarah Serussi <sase@unit.no>
2020-05-08 11:10:25 +0200

    add icon and translation

--- src/pages/publication/publication_page/PublicationPageFiles.tsx
+++ src/pages/publication/publication_page/PublicationPageFiles.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 import DescriptionIcon from '@material-ui/icons/DescriptionOutlined';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
 import { File } from '../../../types/file.types';
 import NormalText from '../../../components/NormalText';
 import { Button } from '@material-ui/core';
 import { downloadFile } from '../../../api/fileApi';
 import { useParams } from 'react-router';
+import { useTranslation } from 'react-i18next';
 
 const StyledFileIcon = styled(DescriptionIcon)`
   width: 100px;
@@ -18,12 +20,17 @@ const StyledFileIconWrapper = styled.div`
   text-align: center;
 `;
 
+const StyledButton = styled(Button)`
+  margin: 1rem;
+`;
+
 interface PublicationPageFilesProps {
   files: File[];
 }
 
 const PublicationPageFiles: FC<PublicationPageFilesProps> = ({ files }) => {
   const { identifier } = useParams();
+  const { t } = useTranslation('common');
   const handleDownload = async (fileId: string) => {
     const file = await downloadFile(identifier, fileId);
     console.log('file', file);
@@ -34,7 +41,13 @@ const PublicationPageFiles: FC<PublicationPageFilesProps> = ({ files }) => {
         <StyledFileIconWrapper key={file.identifier}>
           <StyledFileIcon />
           <NormalText>{file.name}</NormalText>
-          <Button onClick={() => handleDownload(file.identifier)}>Download</Button>
+          <StyledButton
+            color="primary"
+            variant="contained"
+            endIcon={<CloudDownloadIcon />}
+            onClick={() => handleDownload(file.identifier)}>
+            {t('download')}
+          </StyledButton>
         </StyledFileIconWrapper>
       ))}
     </>
