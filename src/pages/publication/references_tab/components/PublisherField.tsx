013565a1bcbe2de1daa9c301e94a4f2f1adeb4d5
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-05-08 08:46:33 +0200

    NP-883 Update comment

--- src/pages/publication/references_tab/components/PublisherField.tsx
+++ src/pages/publication/references_tab/components/PublisherField.tsx
@@ -39,8 +39,8 @@ const PublisherField: FC<PublisherFieldProps> = ({
             placeholder={placeholder}
             errorMessage={
               error && touched.entityDescription?.reference?.publicationContext?.title ? (
-                // Must use getIn of global touched variable instead of what is in meta of field, since meta.touched
-                // always will evalute to true if it is a object (as in this case). Even though this field will update
+                // Must use global touched variable instead of what is in meta, since meta.touched always will
+                // evaluate to true if it is a object (as in this case). Even though this field will update
                 // the whole object, we only want to show error massage if we are missing the title property.
                 <ErrorMessage name={`${fieldName}.title`} />
               ) : (
