a4bc4a5b1d8cf8a5472ba0d14fd7bf004e296ce2
Sarah Serussi <sase@unit.no>
2020-05-18 17:08:43 +0200

    create pagination and update mockdata

--- src/pages/worklist/WorkListTable.tsx
+++ src/pages/worklist/WorkListTable.tsx
@@ -1,15 +1,24 @@
-import React, { FC } from 'react';
+import React, { FC, useState, ChangeEvent } from 'react';
 import styled from 'styled-components';
-import { TableRow, Table, TableHead, TableCell, TableBody, Button } from '@material-ui/core';
+import {
+  TableRow,
+  Table,
+  TableHead,
+  TableCell,
+  TableBody,
+  Button,
+  TableContainer,
+  TablePagination,
+} from '@material-ui/core';
 import { PublicationPreview } from '../../types/publication.types';
 import { useTranslation } from 'react-i18next';
 import Label from '../../components/Label';
 import NormalText from '../../components/NormalText';
 
 const StyledTableRow = styled(TableRow)`
-  background-color: ${props => props.theme.palette.box.main};
+  background-color: ${(props) => props.theme.palette.box.main};
   :nth-child(odd) {
-    background-color: ${props => props.theme.palette.background.default};
+    background-color: ${(props) => props.theme.palette.background.default};
   }
 `;
 
@@ -24,45 +33,75 @@ interface WorklistTableProps {
 }
 
 const WorklistTable: FC<WorklistTableProps> = ({ publications }) => {
-  const { t } = useTranslation();
+  const { t } = useTranslation('workLists');
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (_: any, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <StyledCell>
-            <Label>{t('workLists:publication_name')}</Label>
-          </StyledCell>
-          <StyledCell>
-            <Label>{t('workLists:submitter')}</Label>
-          </StyledCell>
-          <StyledCell>
-            <Label>{t('common:date')}</Label>
-          </StyledCell>
-          <TableCell />
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {publications.map(publication => (
-          <StyledTableRow key={publication.identifier}>
-            <StyledCell component="th" scope="row">
-              <NormalText>{publication.mainTitle}</NormalText>
-            </StyledCell>
-            <StyledCell>
-              <NormalText>{publication.owner}</NormalText>
-            </StyledCell>
-            <StyledCell>
-              <NormalText>{publication.createdDate}</NormalText>
-            </StyledCell>
-            <TableCell>
-              <Button color="primary" variant="contained">
-                <NormalText>{t('common:open')}</NormalText>
-              </Button>
-            </TableCell>
-          </StyledTableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <StyledCell>
+                <Label>{t('publication_name')}</Label>
+              </StyledCell>
+              <StyledCell>
+                <Label>{t('submitter')}</Label>
+              </StyledCell>
+              <StyledCell>
+                <Label>{t('common:date')}</Label>
+              </StyledCell>
+              <TableCell />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {publications.map((publication) => (
+              <StyledTableRow key={publication.identifier}>
+                <StyledCell component="th" scope="row">
+                  <NormalText>{publication.mainTitle}</NormalText>
+                </StyledCell>
+                <StyledCell>
+                  <NormalText>{publication.owner}</NormalText>
+                </StyledCell>
+                <StyledCell>
+                  <NormalText>{publication.createdDate}</NormalText>
+                </StyledCell>
+                <TableCell>
+                  <Button color="primary" variant="contained">
+                    <NormalText>{t('common:open')}</NormalText>
+                  </Button>
+                </TableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, { value: -1, label: t('common:all') }]}
+        component="div"
+        count={publications.length}
+        labelRowsPerPage={t('common:rows_per_page')}
+        labelDisplayedRows={({ from, to, count }) =>
+          `${from}-${to === -1 ? count : to} ${t('common:of')} ${
+            count !== -1 ? count : `${t('common:more_than')} ${to}`
+          }`
+        }
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </>
   );
 };
 
