7d5d7a0e152400ee541954a2b336feddd5740daf
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-05-18 13:45:27 +0200

    NP-921 Display affiliations for every contributor

--- src/pages/publication/publication_page/PublicationPageAuthors.tsx
+++ src/pages/publication/publication_page/PublicationPageAuthors.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
+import { Link, CircularProgress } from '@material-ui/core';
+
 import { Contributor } from '../../../types/contributor.types';
 import NormalText from '../../../components/NormalText';
-import styled from 'styled-components';
-import { Link } from '@material-ui/core';
+import useFetchUnitHierarchy from '../../../utils/hooks/useFetchUnitHierarchy';
+import { getCommaSeparatedUnitString, getDistinctContributorUnits } from '../../../utils/institutions-helpers';
 
 const StyledAuthor = styled.span`
   margin-right: 1rem;
+`;
+
+const StyledAffiliationsContainer = styled.div`
+  margin-top: 0.5rem;
+  padding-left: 1rem;
 `;
 
 interface PublicationPageProps {
@@ -13,15 +21,7 @@ interface PublicationPageProps {
 }
 
 const PublicationPageAuthors: FC<PublicationPageProps> = ({ contributors }) => {
-  const distinctUnits = [
-    ...new Set(
-      contributors
-        .map((contributor) => contributor.affiliations)
-        .flat()
-        .filter((unit) => unit)
-        .map((unit) => unit.id)
-    ),
-  ];
+  const distinctUnits = getDistinctContributorUnits(contributors);
 
   return (
     <>
@@ -34,19 +34,37 @@ const PublicationPageAuthors: FC<PublicationPageProps> = ({ contributors }) => {
 
           return (
             <StyledAuthor key={index}>
-              {arpId ? <Link href={`/profile/${arpId}`}>{name}</Link> : name}{' '}
+              {arpId ? <Link href={`/profile/${arpId}`}>{name}</Link> : name}
               {affiliationIndexes?.length > 0 && <sup>{affiliationIndexes.join(',')}</sup>}
             </StyledAuthor>
           );
         })}
       </NormalText>
-      {distinctUnits.map((unit, index) => (
-        <p key={unit}>
-          <sup>{index + 1}</sup> {unit}
-        </p>
-      ))}
+      <StyledAffiliationsContainer>
+        {distinctUnits.map((unitUri, index) => (
+          <PublicationPageAffiliation key={unitUri} unitUri={unitUri} index={index + 1} />
+        ))}
+      </StyledAffiliationsContainer>
     </>
   );
+};
+
+interface PublicationPageAffiliationProps {
+  unitUri: string;
+  index: number;
+}
+
+const PublicationPageAffiliation: FC<PublicationPageAffiliationProps> = ({ unitUri, index }) => {
+  const [unit, isLoadingUnit] = useFetchUnitHierarchy(unitUri);
+
+  return isLoadingUnit ? (
+    <CircularProgress size={20} />
+  ) : unit ? (
+    <NormalText>
+      <sup>{index}</sup>
+      <i>{getCommaSeparatedUnitString(unit)}</i>
+    </NormalText>
+  ) : null;
 };
 
 export default PublicationPageAuthors;
