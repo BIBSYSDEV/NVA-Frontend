e0f556f8bd3f9c3a293c110faa3a13034adcf1df
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-05-20 09:38:14 +0200

    NP-969 Add mailto and link to orcid profile

--- src/pages/publication/publication_page/PublicationPageAuthors.tsx
+++ src/pages/publication/publication_page/PublicationPageAuthors.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import { Link, CircularProgress } from '@material-ui/core';
+import { Link, CircularProgress, IconButton } from '@material-ui/core';
 
 import { Contributor } from '../../../types/contributor.types';
 import NormalText from '../../../components/NormalText';
 import useFetchUnitHierarchy from '../../../utils/hooks/useFetchUnitHierarchy';
 import { getCommaSeparatedUnitString, getDistinctContributorUnits } from '../../../utils/institutions-helpers';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import OrcidLogo from '../../../resources/images/orcid_logo.svg';
+import { ORCID_BASE_URL } from '../../../utils/constants';
 
 const StyledAuthor = styled.span`
   margin-right: 1rem;
@@ -27,15 +30,29 @@ const PublicationPageAuthors: FC<PublicationPageProps> = ({ contributors }) => {
     <>
       <NormalText>
         {contributors.map((contributor, index) => {
-          const { arpId, name } = contributor.identity;
+          const {
+            identity: { arpId, name, orcId },
+            email,
+          } = contributor;
           const affiliationIndexes = contributor.affiliations?.map(
             (affiliation) => distinctUnits.indexOf(affiliation.id) + 1
           );
-
           return (
             <StyledAuthor key={index}>
               {arpId ? <Link href={`/profile/${arpId}`}>{name}</Link> : name}
-              {affiliationIndexes?.length > 0 && <sup>{affiliationIndexes.join(',')}</sup>}
+              <sup>
+                {affiliationIndexes?.length > 0 && affiliationIndexes.join(',')}
+                {email && (
+                  <IconButton size="small" href={`mailto:${email}`}>
+                    <MailOutlineIcon fontSize="small" />
+                  </IconButton>
+                )}
+                {orcId && (
+                  <IconButton size="small" href={`${ORCID_BASE_URL}/${orcId}`}>
+                    <img src={OrcidLogo} height="20" alt="orcid" />
+                  </IconButton>
+                )}
+              </sup>
             </StyledAuthor>
           );
         })}
