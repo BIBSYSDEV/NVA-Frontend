c7d4d1a3572abdbc8614291dfff8b52d1dddc0d9
sarahse <sase@unit.no>
2020-06-02 11:21:54 +0200

    add useFetchMyPublications hook (#733)
    * add useFetchMyPublications hook
    
    * remove error handling since we always return data or error
    
    * remove error handling
    
    * QA - remove unnecessary return value

--- src/pages/publication/my_publications/MyPublications.tsx
+++ src/pages/publication/my_publications/MyPublications.tsx
@@ -1,16 +1,14 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Button, CircularProgress, Card } from '@material-ui/core';
 import styled from 'styled-components';
 import { RootStore } from '../../../redux/reducers/rootReducer';
 import { Link as RouterLink } from 'react-router-dom';
-import { getMyPublications } from '../../../api/publicationApi';
-import { setNotification } from '../../../redux/actions/notificationActions';
-import { NotificationVariant } from '../../../types/notification.types';
-import { PublicationPreview, PublicationStatus } from '../../../types/publication.types';
+import { PublicationStatus } from '../../../types/publication.types';
 import PublicationList from './PublicationList';
 import TabButton from '../../../components/TabButton';
+import useFetchMyPublications from '../../../utils/hooks/useFetchMyPublications';
 
 const StyledContainer = styled.div`
   display: block;
@@ -51,25 +49,9 @@ enum Tab {
 
 const MyPublications: FC = () => {
   const { t } = useTranslation('workLists');
-  const dispatch = useDispatch();
   const user = useSelector((store: RootStore) => store.user);
   const [selectedTab, setSelectedTab] = useState(Tab.Unpublished);
-  const [isLoading, setIsLoading] = useState(true);
-  const [publications, setPublications] = useState<PublicationPreview[]>([]);
-
-  useEffect(() => {
-    const loadData = async () => {
-      setIsLoading(true);
-      const publications = await getMyPublications();
-      if (publications?.error) {
-        dispatch(setNotification(publications.error, NotificationVariant.Error));
-      } else {
-        setPublications(publications);
-      }
-      setIsLoading(false);
-    };
-    loadData();
-  }, [dispatch]);
+  const [publications, isLoading] = useFetchMyPublications();
 
   const unpublishedPublications = publications
     .filter((publication) => publication.status !== PublicationStatus.PUBLISHED)
