4cd4e32d6fe5b8a6775d54a83ad373265d01726f
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-06-05 09:13:21 +0200

    NP-1053 Add forbidden page

---
+++ src/layout/Forbidden.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { Link as MuiLink } from '@material-ui/core';
+import styled from 'styled-components';
+
+import Heading from '../components/Heading';
+import NormalText from '../components/NormalText';
+
+const StyledLinkText = styled(NormalText)`
+  margin-top: 1rem;
+`;
+
+const Forbidden: React.FC = () => {
+  const { t } = useTranslation('authorization');
+
+  return (
+    <div>
+      <Heading>{t('forbidden')}</Heading>
+      <NormalText>{t('forbidden_description')}</NormalText>
+      <MuiLink component={Link} to="/">
+        <StyledLinkText>{t('back_to_home')}</StyledLinkText>
+      </MuiLink>
+    </div>
+  );
+};
+
+export default Forbidden;
