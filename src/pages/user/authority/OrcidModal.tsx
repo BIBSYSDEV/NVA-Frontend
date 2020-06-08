9f335ffe2cc473f1081604ce14fa0bd5fa84901a
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-06-08 11:20:28 +0200

    NP-1062 Split modals into separate components (#763)
    * NP-1062 Split modals into separate components
    
    * NP-1062 Use enum for modal types
    
    * NP-1062 Set previouslyLoggedIn independent of button click
    
    * NP-1062 Set has logged in only if authority is set

---
+++ src/pages/user/authority/OrcidModal.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+import { Button } from '@material-ui/core';
+
+import OrcidModalContent from '../OrcidModalContent';
+import Modal from '../../../components/Modal';
+import orcidLogo from '../../../resources/images/orcid_logo.svg';
+
+const StyledSkipButtonContainer = styled.div`
+  text-align: center;
+`;
+
+const StyledButton = styled(Button)`
+  max-width: 22rem;
+`;
+
+interface OrcidModalProps {
+  closeModal: () => void;
+}
+
+const OrcidModal: FC<OrcidModalProps> = ({ closeModal }) => {
+  const { t } = useTranslation('common');
+
+  return (
+    <Modal
+      dataTestId="open-orcid-modal"
+      ariaLabelledBy="orcid-modal"
+      openModal={true}
+      onClose={closeModal}
+      maxWidth="lg"
+      headingIcon={{ src: orcidLogo, alt: 'ORCID iD icon' }}
+      headingText={t('profile:orcid.create_or_connect')}>
+      <OrcidModalContent />
+      <StyledSkipButtonContainer>
+        <StyledButton data-testid="skip-connect-to-orcid" color="default" variant="outlined" onClick={closeModal}>
+          {t('profile:orcid.skip_this_step')}
+        </StyledButton>
+      </StyledSkipButtonContainer>
+    </Modal>
+  );
+};
+
+export default OrcidModal;
