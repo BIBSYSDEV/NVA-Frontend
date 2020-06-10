674c32869268c05bf0c83fd550244aec5abee0a1
sarahse <sase@unit.no>
2020-06-10 12:43:25 +0200

    Np 1075  bugfix delete publication modal (#780)
    * fix delete publication modal
    
    * set delete buttons to disabled on MyPublications
    
    * comment out unused import

--- src/components/ConfirmDialog.tsx
+++ src/components/ConfirmDialog.tsx
@@ -2,6 +2,14 @@ import React, { FC } from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import ButtonWithProgress from './ButtonWithProgress';
+import styled from 'styled-components';
+
+const StyledDialogContentText = styled(DialogContentText)`
+  min-width: 35rem;
+  @media (max-width: ${({ theme }) => theme.breakpoints.values.md + 'px'}) {
+    min-width: auto;
+  }
+`;
 
 interface ConfirmDialogProps {
   open: boolean;
@@ -19,7 +27,7 @@ const ConfirmDialog: FC<ConfirmDialogProps> = ({ open, title, text, onAccept, on
     <Dialog open={open} onClose={onCancel}>
       <DialogTitle>{title}</DialogTitle>
       <DialogContent>
-        <DialogContentText>{text}</DialogContentText>
+        <StyledDialogContentText>{text}</StyledDialogContentText>
       </DialogContent>
       <DialogActions>
         <Button data-testid="cancel-button" variant="contained" onClick={onCancel}>
