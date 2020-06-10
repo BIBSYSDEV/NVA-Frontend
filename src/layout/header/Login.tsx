8309ebb8a6c2cecb4ca0f52c8f78f1503d15d6b5
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-06-10 14:17:12 +0200

    NP-1078 Show progress when logging in

--- src/layout/header/Login.tsx
+++ src/layout/header/Login.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
@@ -11,6 +11,7 @@ import { USE_MOCK_DATA } from '../../utils/constants';
 import { logoutSuccess } from '../../redux/actions/authActions';
 import { setUser } from '../../redux/actions/userActions';
 import { mockUser } from '../../utils/testfiles/mock_feide_user';
+import ButtonWithProgress from '../../components/ButtonWithProgress';
 
 const StyledLoginComponent = styled.div`
   grid-area: auth;
@@ -18,15 +19,28 @@ const StyledLoginComponent = styled.div`
   align-items: center;
 `;
 
+const redirectedToFeideKey = 'redirectedToFeide';
+
 const Login: FC = () => {
   const user = useSelector((state: RootStore) => state.user);
   const dispatch = useDispatch();
   const { t } = useTranslation('authorization');
 
+  // Use localStorage to track if user comes from a sign in or sign out process (FEIDE)
+  const redirectedToFeide = !!localStorage.getItem(redirectedToFeideKey);
+
+  useEffect(() => {
+    // Clear possible redirect state in localStorage on mount
+    if (redirectedToFeide) {
+      localStorage.removeItem(redirectedToFeideKey);
+    }
+  }, [redirectedToFeide]);
+
   const handleLogin = () => {
     if (USE_MOCK_DATA) {
       dispatch(setUser(mockUser));
     } else {
+      localStorage.setItem(redirectedToFeideKey, 'true');
       Auth.federatedSignIn();
     }
   };
@@ -35,6 +49,7 @@ const Login: FC = () => {
     if (USE_MOCK_DATA) {
       dispatch(logoutSuccess());
     } else {
+      localStorage.setItem(redirectedToFeideKey, 'true');
       Auth.signOut();
     }
   };
@@ -43,6 +58,8 @@ const Login: FC = () => {
     <StyledLoginComponent>
       {user ? (
         <Menu menuButtonLabel={user.name} handleLogout={handleLogout} />
+      ) : redirectedToFeide ? (
+        <ButtonWithProgress isLoading>{t('common:loading')}</ButtonWithProgress>
       ) : (
         <Button color="primary" variant="contained" onClick={handleLogin} data-testid="menu-login-button">
           {t('login')}
