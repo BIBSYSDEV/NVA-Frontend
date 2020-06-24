d29b3c173d2a10e8c8b22f652c23f081ee9bf608
Sarah Serussi <sase@unit.no>
2020-06-24 10:16:22 +0200

    cannot check for !user.authority because then the next-button (and orcid modal) will never show

--- src/App.tsx
+++ src/App.tsx
@@ -144,10 +144,9 @@ const App: FC = () => {
         </StyledContent>
         <Footer />
       </StyledApp>
-      {!localStorage.getItem('previouslyLoggedIn') &&
-        !isLoadingAuthorities &&
-        authorityDataUpdated &&
-        !user.authority && <AuthorityOrcidModal />}
+      {!localStorage.getItem('previouslyLoggedIn') && !isLoadingAuthorities && authorityDataUpdated && (
+        <AuthorityOrcidModal />
+      )}
     </BrowserRouter>
   );
 };
