0d1358d2a9971fb432b4409d566c9b1b11199733
Sarah Serussi <sase@unit.no>
2020-06-29 10:30:09 +0200

    show text when no publications

--- src/pages/dashboard/LatestPublications.tsx
+++ src/pages/dashboard/LatestPublications.tsx
@@ -11,6 +11,7 @@ import Heading from '../../components/Heading';
 import { useTranslation } from 'react-i18next';
 import useFetchLatestPublications from '../../utils/hooks/useFetchLatestPublications';
 import { StyledProgressWrapper } from '../../components/styled/Wrappers';
+import NormalText from '../../components/NormalText';
 
 const StyledListContainer = styled.div`
   padding-bottom: 1rem;
@@ -22,49 +23,53 @@ const StyledListItem = styled.li`
   padding-top: 0.5rem;
 `;
 
+const StyledNormalText = styled(NormalText)`
+  padding-top: 1rem;
+`;
+
 const LatestPublications: FC = () => {
   const { t } = useTranslation('publication');
   const [publications, isLoadingPublications] = useFetchLatestPublications();
 
   return (
-    <StyledListContainer data-testid="search-results">
+    <StyledListContainer data-testid="newest-publications">
+      <Heading>{t('publication.newest_publications')}</Heading>
+      <Divider />
       {isLoadingPublications ? (
         <StyledProgressWrapper>
           <CircularProgress />
         </StyledProgressWrapper>
-      ) : (
-        publications.length > 0 && (
-          <>
-            <Heading>{t('publication.newest_publications')}</Heading>
-            <Divider />
-            <List>
-              {publications.map((publication: PublicationListItem) => (
-                <StyledListItem key={publication.identifier}>
-                  <ListItemAvatar>
-                    <Avatar>
-                      <ImageIcon />
-                    </Avatar>
-                  </ListItemAvatar>
+      ) : publications.length > 0 ? (
+        <>
+          <List>
+            {publications.map((publication: PublicationListItem) => (
+              <StyledListItem key={publication.identifier}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <ImageIcon />
+                  </Avatar>
+                </ListItemAvatar>
 
-                  <ListItemText
-                    data-testid="result-list-item"
-                    primary={
-                      <MuiLink component={Link} to={`/publication/${publication.identifier}/public`}>
-                        {publication.mainTitle}
-                      </MuiLink>
-                    }
-                    secondary={
-                      <Typography component="span">
-                        {new Date(publication.modifiedDate).toLocaleDateString()} - {publication.owner}
-                      </Typography>
-                    }
-                  />
-                </StyledListItem>
-              ))}
-            </List>
-            <Divider />
-          </>
-        )
+                <ListItemText
+                  data-testid="result-list-item"
+                  primary={
+                    <MuiLink component={Link} to={`/publication/${publication.identifier}/public`}>
+                      {publication.mainTitle}
+                    </MuiLink>
+                  }
+                  secondary={
+                    <Typography component="span">
+                      {new Date(publication.modifiedDate).toLocaleDateString()} - {publication.owner}
+                    </Typography>
+                  }
+                />
+              </StyledListItem>
+            ))}
+          </List>
+          <Divider />
+        </>
+      ) : (
+        <StyledNormalText>{t('publication.no_published_publications_yet')}</StyledNormalText>
       )}
     </StyledListContainer>
   );
