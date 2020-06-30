a102205067d15b0e98715adc1ad5e58c324534d4
Sarah Serussi <sase@unit.no>
2020-06-30 15:07:58 +0200

    check if user is logged in before redirecting to forbidden page

--- src/utils/routes/PrivateRoute.tsx
+++ src/utils/routes/PrivateRoute.tsx
@@ -1,14 +1,23 @@
 import React, { FC, ComponentType } from 'react';
 import { Route, RouteProps } from 'react-router-dom';
 import Forbidden from '../../pages/errorpages/Forbidden';
+import { useSelector } from 'react-redux';
+import { RootStore } from '../../redux/reducers/rootReducer';
 
 export interface PrivateRouteProps extends RouteProps {
   component: ComponentType<any>;
   isAuthorized: boolean;
 }
 
-const PrivateRoute: FC<PrivateRouteProps> = ({ component: Component, isAuthorized, ...rest }) => (
-  <Route {...rest} render={(props) => (isAuthorized ? <Component {...props} {...rest} /> : <Forbidden />)} />
-);
+const PrivateRoute: FC<PrivateRouteProps> = ({ component: Component, isAuthorized, ...rest }) => {
+  const user = useSelector((store: RootStore) => store.user);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => (isAuthorized ? <Component {...props} {...rest} /> : !!user ? <Forbidden /> : null)}
+    />
+  );
+};
 
 export default PrivateRoute;
