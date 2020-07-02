405cb5a974e980e522ea2043a4fd129d9baffdff
Sarah Serussi <sase@unit.no>
2020-07-02 14:55:17 +0200

    set 'FeideIdentityProvider' as customProvider when using federatedSignIn

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,6 +10,7 @@ export const NAVIGATE_TO_PUBLIC_PUBLICATION_DURATION = 3000;
 export const ORCID_BASE_URL = process.env.REACT_APP_ORCID_BASE_URL;
 export const ORCID_USER_INFO_URL = `${ORCID_BASE_URL}/oauth/userinfo`;
 export const ORCID_SIGN_IN_URL = `${ORCID_BASE_URL}/signin?oauth&client_id=${process.env.REACT_APP_ORCID_CLIENT_ID}&response_type=token&scope=openid&redirect_uri=${process.env.REACT_APP_ORCID_REDIRECT_URI}`;
+export const FEIDE_IDENTITY_PROVIDER = 'FeideIdentityProvider';
 
 export const API_URL = process.env.REACT_APP_API_URL;
 
