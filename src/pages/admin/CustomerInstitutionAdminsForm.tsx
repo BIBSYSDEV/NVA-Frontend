f7cb6ad2d99a4e1e788389e2447c3c6abffcd207
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-07-09 13:40:23 +0200

    NP-1241 Add row for adding admin

--- src/pages/admin/CustomerInstitutionAdminsForm.tsx
+++ src/pages/admin/CustomerInstitutionAdminsForm.tsx
@@ -1,9 +1,32 @@
 import React, { FC } from 'react';
+import Card from '../../components/Card';
+import { TextField, Button } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+const StyledNewAdminRow = styled.div`
+  display: flex;
+`;
+
+const StyledTextField = styled(TextField)`
+  width: 20rem;
+  margin-right: 2rem;
+`;
 
 const CustomerInstitutionAdminsForm: FC = () => {
+  const { t } = useTranslation();
   // TODO: Fetch existing admins
 
-  return <>TODO: Admins form</>;
+  return (
+    <Card>
+      <StyledNewAdminRow>
+        <StyledTextField variant="outlined" label={t('TODO.label')} />
+        <Button color="primary" variant="contained">
+          Legg til
+        </Button>
+      </StyledNewAdminRow>
+    </Card>
+  );
 };
 
 export default CustomerInstitutionAdminsForm;
