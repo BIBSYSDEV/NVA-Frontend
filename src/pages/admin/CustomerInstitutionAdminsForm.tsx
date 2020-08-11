854425e58decadbff31f306d77bf7740c82a366f
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-08-11 11:06:07 +0200

    Np 1287 search for user (#914)
    * NP-1287 Search from existing users when adding admins
    
    * NP-1287 Update admin form model
    
    * NP-1287 Add translations
    
    * NP-1287 Refactor and simplify adding role to user
    
    * NP-1287 Avoid fetching users twice
    
    * NP-1287 Allow refetching institution users

--- src/pages/admin/CustomerInstitutionAdminsForm.tsx
+++ src/pages/admin/CustomerInstitutionAdminsForm.tsx
@@ -1,33 +1,35 @@
-import React, { FC, useState } from 'react';
-import { TextField, Button, Table, TableBody, TableCell, TableRow } from '@material-ui/core';
+import React, { FC } from 'react';
+import { Button, Table, TableBody, TableCell, TableRow, TextField } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
-import { Formik, Field, FieldProps, ErrorMessage, Form, FormikHelpers } from 'formik';
+import { Formik, Field, FieldProps, Form, FormikHelpers } from 'formik';
 import styled from 'styled-components';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddIcon from '@material-ui/icons/Add';
 import * as Yup from 'yup';
 import { useDispatch } from 'react-redux';
+import { Autocomplete } from '@material-ui/lab';
 
 import Card from '../../components/Card';
 import Heading from '../../components/Heading';
 import { ErrorMessage as ErrorMessageString } from '../publication/PublicationFormValidationSchema';
 import { InstitutionUser, RoleName } from '../../types/user.types';
 import NormalText from '../../components/NormalText';
-import { assignUserRole } from '../../api/roleApi';
+import { addRoleToUser } from '../../api/roleApi';
 import { setNotification } from '../../redux/actions/notificationActions';
 import { NotificationVariant } from '../../types/notification.types';
 import ButtonWithProgress from '../../components/ButtonWithProgress';
+import { filterUsersByRole } from '../../utils/role-helpers';
 
 const StyledTextField = styled(TextField)`
   width: 20rem;
 `;
 
 interface AdminValues {
-  userId: string;
+  user: InstitutionUser | null;
 }
 
 const adminInitialValues: AdminValues = {
-  userId: '',
+  user: null,
 };
 
 const adminValidationSchema = Yup.object().shape({
@@ -35,31 +37,31 @@ const adminValidationSchema = Yup.object().shape({
 });
 
 interface CustomerInstitutionAdminsFormProps {
-  admins: InstitutionUser[];
-  customerInstitutionId: string;
+  users: InstitutionUser[];
+  refetchInstitutionUsers: () => void;
 }
 
-const CustomerInstitutionAdminsForm: FC<CustomerInstitutionAdminsFormProps> = ({ admins, customerInstitutionId }) => {
+const CustomerInstitutionAdminsForm: FC<CustomerInstitutionAdminsFormProps> = ({ users, refetchInstitutionUsers }) => {
   const { t } = useTranslation('admin');
   const dispatch = useDispatch();
-  const [currentAdmins, setCurrentAdmins] = useState(admins);
+
+  const institutionAdmins = filterUsersByRole(users, RoleName.INSTITUTION_ADMIN);
+  const nonInstitutionAdmins = users.filter((user) => !institutionAdmins.includes(user));
 
   const addAdmin = async (adminValues: AdminValues, { resetForm }: FormikHelpers<AdminValues>) => {
-    // Cast values according to validation schema to ensure userId is trimmed
-    const trimmedValues = adminValidationSchema.cast(adminValues);
-    const userId = trimmedValues?.userId as string;
-    if (currentAdmins.some((admin) => admin.username === userId)) {
-      dispatch(setNotification(t('feedback:info.user_already_has_role'), NotificationVariant.Info));
+    const { user } = adminValues;
+    if (!user) {
+      dispatch(setNotification(t('feedback:error.missing.user'), NotificationVariant.Info));
       return;
     }
 
-    const createdUserResponse = await assignUserRole(customerInstitutionId, userId, RoleName.INSTITUTION_ADMIN);
-    if (createdUserResponse) {
-      if (createdUserResponse.error) {
-        dispatch(setNotification(createdUserResponse.error, NotificationVariant.Error));
+    const addRoleResponse = await addRoleToUser(user.username, RoleName.INSTITUTION_ADMIN);
+    if (addRoleResponse) {
+      if (addRoleResponse.error) {
+        dispatch(setNotification(addRoleResponse.error, NotificationVariant.Error));
       } else {
         dispatch(setNotification(t('feedback:success.added_role')));
-        setCurrentAdmins((state) => [...state, createdUserResponse]);
+        refetchInstitutionUsers();
         resetForm();
       }
     }
@@ -73,11 +75,11 @@ const CustomerInstitutionAdminsForm: FC<CustomerInstitutionAdminsFormProps> = ({
         initialValues={adminInitialValues}
         validationSchema={adminValidationSchema}
         validateOnChange={false}>
-        {({ isSubmitting, isValid, dirty }) => (
+        {({ isSubmitting, isValid, dirty, setFieldValue }) => (
           <Form>
             <Table>
               <TableBody>
-                {currentAdmins.map((admin) => (
+                {institutionAdmins.map((admin) => (
                   <TableRow key={admin.username}>
                     <TableCell>
                       <NormalText>{admin.username}</NormalText>
@@ -90,17 +92,26 @@ const CustomerInstitutionAdminsForm: FC<CustomerInstitutionAdminsFormProps> = ({
                     </TableCell>
                   </TableRow>
                 ))}
-
                 <TableRow>
-                  <Field name="userId">
-                    {({ field, meta: { touched, error } }: FieldProps) => (
+                  <Field name="user">
+                    {({ field }: FieldProps) => (
                       <TableCell>
-                        <StyledTextField
-                          {...field}
-                          label={t('users.new_institution_admin')}
-                          variant="outlined"
-                          error={touched && !!error}
-                          helperText={<ErrorMessage name={field.name} />}
+                        <Autocomplete
+                          options={nonInstitutionAdmins}
+                          getOptionLabel={(option) => option.username}
+                          value={field.value}
+                          getOptionSelected={(option: InstitutionUser, value: InstitutionUser | null) =>
+                            value?.username === option.username
+                          }
+                          onChange={(_, value: InstitutionUser | null) => setFieldValue(field.name, value)}
+                          renderInput={(params) => (
+                            <StyledTextField
+                              {...params}
+                              label={t('users.new_institution_admin')}
+                              variant="outlined"
+                              helperText={t('search_for_user')}
+                            />
+                          )}
                         />
                       </TableCell>
                     )}
