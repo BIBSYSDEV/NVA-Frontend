57af699d14403fd5a841e763e1f553f986488bcc
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-08-17 13:54:11 +0200

    NP-1346 Add status bar for public publication

---
+++ src/pages/publication/public_publication/PublicPublicationStatusBar.tsx
@@ -0,0 +1,68 @@
+import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { Button } from '@material-ui/core';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+
+import { RootStore } from '../../../redux/reducers/rootReducer';
+import Card from '../../../components/Card';
+import NormalText from '../../../components/NormalText';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { PublicPublicationContentProps } from './PublicPublicationContent';
+
+const StyledCard = styled(Card)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  button {
+    margin-left: 0.5rem;
+  }
+`;
+
+const StyledDescription = styled.div`
+  display: flex;
+  align-items: center;
+  svg {
+    margin-right: 0.5rem;
+    color: green;
+  }
+`;
+
+export const PublicPublicationStatusBar: FC<PublicPublicationContentProps> = ({ publication }) => {
+  const { t } = useTranslation('publication');
+  const { id, isPublisher } = useSelector((store: RootStore) => store.user);
+  const {
+    identifier,
+    owner,
+    status,
+    entityDescription: {
+      reference: { doi },
+    },
+  } = publication;
+  const isOwner = owner === id;
+  const hasDoi = !!doi;
+
+  return isPublisher && isOwner ? (
+    <StyledCard>
+      <StyledDescription>
+        <CheckCircleOutlineIcon fontSize="large" />
+        <NormalText>
+          {t('common:status')}: {t(`status:${status}`)}
+        </NormalText>
+      </StyledDescription>
+      <div>
+        {!hasDoi && (
+          <Button variant="contained" color="primary">
+            {t('public_page.request_doi')}
+          </Button>
+        )}
+        <Link to={`/publication/${identifier}`}>
+          <Button variant="contained" color="primary">
+            {t('public_page.edit_publication')}
+          </Button>
+        </Link>
+      </div>
+    </StyledCard>
+  ) : null;
+};
