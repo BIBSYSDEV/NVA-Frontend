ca3b14bbd613c4607e8cc296a06b5446d1bf2a50
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-08-24 13:18:55 +0200

    Add link to public publication

--- src/pages/publication/my_publications/PublicationList.tsx
+++ src/pages/publication/my_publications/PublicationList.tsx
@@ -12,10 +12,10 @@ import {
   TablePagination,
 } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
-import { PublicationPreview } from '../../../types/publication.types';
-// import { PublicationPreview, PublicationStatus } from '../../../types/publication.types';
+import { PublicationPreview, PublicationStatus } from '../../../types/publication.types';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 import Label from '../../../components/Label';
 import NormalText from '../../../components/NormalText';
 import { Link as RouterLink } from 'react-router-dom';
@@ -82,6 +82,7 @@ const PublicationList: FC<PublicationListProps> = ({ publications }) => {
               </TableCell>
               <TableCell />
               <TableCell />
+              <TableCell />
             </TableRow>
           </TableHead>
           <TableBody>
@@ -97,8 +98,22 @@ const PublicationList: FC<PublicationListProps> = ({ publications }) => {
                   <NormalText>{new Date(publication.createdDate).toLocaleString()}</NormalText>
                 </TableCell>
                 <TableCell>
+                  {publication.status === PublicationStatus.PUBLISHED && (
+                    <Button
+                      color="primary"
+                      variant="outlined"
+                      component={RouterLink}
+                      to={`/publication/${publication.identifier}/public`}
+                      data-testid={`open-publication-${publication.identifier}`}>
+                      <MenuBookIcon />
+                      <StyledNormalTextWithIcon>{t('common:show')}</StyledNormalTextWithIcon>
+                    </Button>
+                  )}
+                </TableCell>
+                <TableCell>
                   <Button
                     color="primary"
+                    variant="outlined"
                     component={RouterLink}
                     to={`/publication/${publication.identifier}`}
                     data-testid={`edit-publication-${publication.identifier}`}>
