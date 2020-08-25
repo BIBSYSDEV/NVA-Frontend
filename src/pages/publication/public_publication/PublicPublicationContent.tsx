cf1bb4fa9b36bb5c936f26c7b142cac3e6ce19db
sarahse <sase@unit.no>
2020-08-25 15:43:29 +0200

    Np 1371  refactor public publication page (#959)
    * show publication context in public publication based on type
    
    * show publication instance info
    
    * check if publisher is present before showing label for publisher
    
    * cleanup
    
    * remove unnecessary check for publicationContext and publicationInstance
    
    * check for publication types once
    
    * pull series and pages out into separate components

--- src/pages/publication/public_publication/PublicPublicationContent.tsx
+++ src/pages/publication/public_publication/PublicPublicationContent.tsx
@@ -15,9 +15,30 @@ import { licenses } from '../../../types/file.types';
 import { getNpiDiscipline } from '../../../utils/npiDisciplines';
 import { StyledNormalTextPreWrapped } from '../../../components/styled/Wrappers';
 import { displayDate } from '../../../utils/date-helpers';
-import OpenInNewIcon from '@material-ui/icons/OpenInNew';
-import { JournalEntityDescription } from '../../../types/publication_types/journalPublication.types';
+import {
+  JournalPublicationContext,
+  JournalPublicationInstance,
+} from '../../../types/publication_types/journalPublication.types';
 import { PublicPublicationStatusBar } from './PublicPublicationStatusBar';
+import { isJournal, isDegree, isReport } from '../../../utils/publication-helpers';
+import {
+  PublicPublicationInstanceJournal,
+  PublicPublicationInstanceDegree,
+  PublicPublicationInstanceReport,
+} from './PublicPublicationInstance';
+import {
+  PublicPublicationContextJournal,
+  PublicPublicationContextDegree,
+  PublicPublicationContextReport,
+} from './PublicPublicationContext';
+import {
+  DegreePublicationContext,
+  DegreePublicationInstance,
+} from '../../../types/publication_types/degreePublication.types';
+import {
+  ReportPublicationContext,
+  ReportPublicationInstance,
+} from '../../../types/publication_types/reportPublication.types';
 
 const StyledContentWrapper = styled.div`
   display: flex;
@@ -79,14 +100,6 @@ const StyledTag = styled.div`
   margin-right: 1rem;
 `;
 
-const StyledContainer = styled.div`
-  display: flex;
-`;
-
-const StyledOpenInNewIcon = styled(OpenInNewIcon)`
-  margin-left: 0.25rem;
-`;
-
 export interface PublicPublicationContentProps {
   publication: Publication;
 }
@@ -103,7 +116,7 @@ const PublicPublicationContent: FC<PublicPublicationContentProps> = ({ publicati
     npiSubjectHeading,
     reference: { doi, publicationContext, publicationInstance },
     tags,
-  } = publication.entityDescription as JournalEntityDescription;
+  } = publication.entityDescription;
 
   // Show only the license for the first file for now
   const currentLicense = publication.fileSet?.files[0]?.license ?? null;
@@ -148,30 +161,26 @@ const PublicPublicationContent: FC<PublicPublicationContentProps> = ({ publicati
               ))}
             </LabelContentRow>
           )}
-          {publicationContext && (
-            <LabelContentRow minimal multiple label={`${t('references.journal')}:`}>
-              <StyledContainer>
-                <NormalText>{publicationContext.title}</NormalText>
-                {publicationContext.url && (
-                  <Link href={publicationContext.url} target="_blank" rel="noopener noreferrer">
-                    <StyledOpenInNewIcon aria-label={publicationContext.url} />
-                  </Link>
-                )}
-              </StyledContainer>
-              {publicationContext.onlineIssn && `${t('references.issn')} ${publicationContext.onlineIssn}`}
-            </LabelContentRow>
-          )}
-          {publicationInstance && (
-            <LabelContentRow minimal label={`${t('common:details')}:`}>
-              {publicationInstance.volume && `${t('references.volume')} ${publicationInstance.volume}`}
-              {publicationInstance.issue && `, ${t('references.issue')} ${publicationInstance.issue}`}
-              {publicationInstance.pages?.begin &&
-                publicationInstance.pages?.end &&
-                `, ${t('references.pages')} ${publicationInstance.pages.begin}-${publicationInstance.pages.end}`}
-              {publicationInstance.articleNumber &&
-                `, ${t('references.article_number')} ${publicationInstance.articleNumber}`}
-            </LabelContentRow>
-          )}
+
+          {isJournal(publication) ? (
+            <>
+              <PublicPublicationContextJournal publicationContext={publicationContext as JournalPublicationContext} />
+              <PublicPublicationInstanceJournal
+                publicationInstance={publicationInstance as JournalPublicationInstance}
+              />
+            </>
+          ) : isDegree(publication) ? (
+            <>
+              <PublicPublicationContextDegree publicationContext={publicationContext as DegreePublicationContext} />
+              <PublicPublicationInstanceDegree publicationInstance={publicationInstance as DegreePublicationInstance} />
+            </>
+          ) : isReport(publication) ? (
+            <>
+              <PublicPublicationContextReport publicationContext={publicationContext as ReportPublicationContext} />
+              <PublicPublicationInstanceReport publicationInstance={publicationInstance as ReportPublicationInstance} />
+            </>
+          ) : null}
+
           {date?.year && (
             <LabelContentRow minimal label={`${t('description.date_published')}:`}>
               {displayDate(date)}
