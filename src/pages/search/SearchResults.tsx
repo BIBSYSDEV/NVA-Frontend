e3eb68dc1302c187a7f658c2d9693ec2fd2d50b1
Sarah Serussi <sase@unit.no>
2020-09-03 13:39:46 +0200

    add pagination

--- src/pages/search/SearchResults.tsx
+++ src/pages/search/SearchResults.tsx
@@ -1,11 +1,12 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC, Fragment, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { Link as MuiLink } from '@material-ui/core';
+import { Link as MuiLink, TablePagination } from '@material-ui/core';
 import { List, Typography } from '@material-ui/core';
 import PublicationListItemComponent from '../dashboard/PublicationListItemComponent';
 import { SearchResult } from '../../types/search.types';
+import { ROWS_PER_PAGE_OPTIONS } from '../../utils/constants';
 
 const StyledSearchResults = styled.div`
   padding-bottom: 1rem;
@@ -18,13 +19,18 @@ interface SearchResultsProps {
 
 const SearchResults: FC<SearchResultsProps> = ({ publications, searchTerm }) => {
   const { t } = useTranslation();
+  const [rowsPerPage, setRowsPerPage] = useState(ROWS_PER_PAGE_OPTIONS[0]);
+  const [page, setPage] = useState(0);
+
+  // Ensure selected page is not out of bounds due to manipulated userList
+  const validPage = publications.length <= page * rowsPerPage ? 0 : page;
 
   return (
     <StyledSearchResults data-testid="search-results">
       {searchTerm && t('results', { count: publications.length, term: searchTerm })}
       <List>
         {publications &&
-          publications.map((publication) => (
+          publications.slice(validPage * rowsPerPage, validPage * rowsPerPage + rowsPerPage).map((publication) => (
             <PublicationListItemComponent
               key={publication.identifier}
               primaryComponent={
@@ -48,8 +54,20 @@ const SearchResults: FC<SearchResultsProps> = ({ publications, searchTerm }) => 
             />
           ))}
       </List>
-      {/* TODO: Pagination has no function atm */}
-      {/* <Pagination data-testid="pagination" count={5} onClick={() => updateSearch(offset)} /> */}
+      {publications.length > ROWS_PER_PAGE_OPTIONS[0] && (
+        <TablePagination
+          rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
+          component="div"
+          count={publications.length}
+          rowsPerPage={rowsPerPage}
+          page={validPage}
+          onChangePage={(_, newPage) => setPage(newPage)}
+          onChangeRowsPerPage={(event) => {
+            setRowsPerPage(parseInt(event.target.value));
+            setPage(0);
+          }}
+        />
+      )}
     </StyledSearchResults>
   );
 };
