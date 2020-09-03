23e1339f0177bc5ba29835d7c586ef7c0737b95f
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-09-03 16:07:10 +0200

    NP-1452 Show skeleton loading for doi requests

--- src/pages/worklist/DoiRequests.tsx
+++ src/pages/worklist/DoiRequests.tsx
@@ -1,22 +1,19 @@
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { CircularProgress } from '@material-ui/core';
 
 import SubHeading from '../../components/SubHeading';
 import useFetchDoiRequests from '../../utils/hooks/useFetchDoiRequests';
 import { RoleName } from '../../types/user.types';
 import { DoiRequestAccordion } from './DoiRequestAccordion';
 import Card from '../../components/Card';
-import { StyledProgressWrapper } from '../../components/styled/Wrappers';
+import { PublicationListSkeleton } from '../publication/my_publications/PublicationListSkeleton';
 
 const DoiRequests: FC = () => {
   const { t } = useTranslation('workLists');
   const [doiRequests, isLoadingDoiRequests] = useFetchDoiRequests(RoleName.CURATOR);
 
   return isLoadingDoiRequests ? (
-    <StyledProgressWrapper>
-      <CircularProgress />
-    </StyledProgressWrapper>
+    <PublicationListSkeleton />
   ) : doiRequests.length === 0 ? (
     <Card>
       <SubHeading>{t('doi_requests.no_pending_doi_requests')}</SubHeading>
