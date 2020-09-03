5d57cad913f68c58b306c48d2c1ded777e551cad
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-09-03 16:21:09 +0200

    NP-1452 Simplify layout

--- src/pages/publication/PublicProfile.tsx
+++ src/pages/publication/PublicProfile.tsx
@@ -2,6 +2,7 @@ import React, { FC, useEffect } from 'react';
 import { CircularProgress, IconButton, Link as MuiLink } from '@material-ui/core';
 import styled from 'styled-components';
 import { useParams, useHistory } from 'react-router-dom';
+import WorkIcon from '@material-ui/icons/Work';
 import orcidIcon from '../../resources/images/orcid_logo.svg';
 import Card from '../../components/Card';
 import Heading from '../../components/Heading';
@@ -9,27 +10,15 @@ import { AffiliationHierarchy } from '../../components/institution/AffiliationHi
 import useFetchAuthority from '../../utils/hooks/useFetchAuthority';
 import { ORCID_BASE_URL } from '../../utils/constants';
 import NormalText from '../../components/NormalText';
-import WorkIcon from '@material-ui/icons/Work';
-
-const StyledUserInfo = styled.div`
-  display: flex;
-  background-color: ${(props) => props.theme.palette.background.default};
-  align-content: flex-start;
-  width: 100%;
-  padding: 0.5rem;
-`;
 
 const StyledLine = styled.div`
-  display: grid;
-  grid-template-areas: 'icon text';
-  grid-template-columns: auto 90%;
-  gap: 1rem;
-  justify-content: start;
+  display: flex;
   margin-top: 1rem;
 `;
 
 const StyledTextContainer = styled.div`
-  grid-area: text;
+  width: 100%;
+  margin-left: 1rem;
 `;
 
 const PublicProfile: FC = () => {
@@ -47,34 +36,34 @@ const PublicProfile: FC = () => {
         <CircularProgress />
       ) : (
         authority && (
-          <StyledUserInfo>
-            <Card>
-              <Heading>{authority.name}</Heading>
-              {authority.orgunitids.length > 0 && (
-                <StyledLine>
-                  <WorkIcon />
+          <Card>
+            <Heading>{authority.name}</Heading>
+            {authority.orgunitids.length > 0 && (
+              <StyledLine>
+                <WorkIcon />
+                <StyledTextContainer>
+                  {authority.orgunitids.map((unitId) => (
+                    <AffiliationHierarchy key={unitId} unitUri={unitId} commaSeparated />
+                  ))}
+                </StyledTextContainer>
+              </StyledLine>
+            )}
+            {authority.orcids.map((orcid: string) => {
+              const orcidLink = `${ORCID_BASE_URL}/${orcid}`;
+              return (
+                <StyledLine key={orcid}>
+                  <IconButton size="small" href={orcidLink} key={orcid}>
+                    <img src={orcidIcon} height="20" alt="orcid" />
+                  </IconButton>
                   <StyledTextContainer>
-                    {authority.orgunitids.map((unitId) => (
-                      <AffiliationHierarchy key={unitId} unitUri={unitId} commaSeparated />
-                    ))}
-                  </StyledTextContainer>
-                </StyledLine>
-              )}
-              {authority.orcids.map((orcid: string) => {
-                const orcidLink = `${ORCID_BASE_URL}/${orcid}`;
-                return (
-                  <StyledLine key={orcid}>
-                    <IconButton size="small" href={orcidLink} key={orcid}>
-                      <img src={orcidIcon} height="20" alt="orcid" />
-                    </IconButton>
                     <MuiLink href={orcidLink} target="_blank" rel="noopener noreferrer">
                       <NormalText>{orcidLink}</NormalText>
                     </MuiLink>
-                  </StyledLine>
-                );
-              })}
-            </Card>
-          </StyledUserInfo>
+                  </StyledTextContainer>
+                </StyledLine>
+              );
+            })}
+          </Card>
         )
       )}
     </>
