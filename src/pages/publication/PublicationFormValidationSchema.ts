088696a47dbd71d6e40e53cb35c5521312f63bb3
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-09-18 15:44:36 +0200

    NP-1533 Add inheritance for reusing validation

--- src/pages/publication/PublicationFormValidationSchema.ts
+++ src/pages/publication/PublicationFormValidationSchema.ts
@@ -16,6 +16,27 @@ export const ErrorMessage = {
 
 export const isbnRegex = /^(97(8|9))?\d{9}(\d|X)$/g; // ISBN without hyphens
 
+// Common Fields
+const isbnListField = Yup.array().of(Yup.string().matches(isbnRegex, ErrorMessage.INVALID_FORMAT));
+const doiField = Yup.string().trim().url(ErrorMessage.INVALID_FORMAT);
+const peerReviewedField = Yup.boolean().required(ErrorMessage.REQUIRED);
+const pagesMonographField = Yup.object()
+  .nullable()
+  .shape({
+    pages: Yup.number().typeError(ErrorMessage.INVALID_FORMAT).min(1, ErrorMessage.MUST_BE_MIN_1),
+  });
+const pagesRangeField = Yup.object()
+  .nullable()
+  .shape({
+    begin: Yup.number()
+      .typeError(ErrorMessage.INVALID_FORMAT)
+      .min(0, ErrorMessage.MUST_BE_POSITIVE)
+      .max(Yup.ref('end'), ErrorMessage.INVALID_PAGE_INTERVAL),
+    end: Yup.number().typeError(ErrorMessage.INVALID_FORMAT).min(Yup.ref('begin'), ErrorMessage.INVALID_PAGE_INTERVAL),
+  });
+const publisherField = Yup.string().required(ErrorMessage.REQUIRED);
+
+// Contributor
 const contributorValidationSchema = Yup.object().shape({
   correspondingAuthor: Yup.boolean(),
   email: Yup.string().when('correspondingAuthor', {
@@ -25,6 +46,7 @@ const contributorValidationSchema = Yup.object().shape({
   sequence: Yup.number().min(0),
 });
 
+// File
 const fileValidationSchema = Yup.object().shape({
   administrativeAgreement: Yup.boolean(),
   embargoDate: Yup.date()
@@ -47,32 +69,26 @@ const fileValidationSchema = Yup.object().shape({
     }),
 });
 
+// Reference
+const baseReference = Yup.object().shape({
+  doi: doiField,
+});
+
 // Journal
 const journalPublicationInstance = Yup.object().shape({
   type: Yup.string().oneOf(Object.values(JournalType)).required(ErrorMessage.REQUIRED),
-  peerReviewed: Yup.boolean().required(ErrorMessage.REQUIRED),
+  peerReviewed: peerReviewedField,
   articleNumber: Yup.number().typeError(ErrorMessage.INVALID_FORMAT).min(0, ErrorMessage.MUST_BE_POSITIVE),
   volume: Yup.number().typeError(ErrorMessage.INVALID_FORMAT).min(0, ErrorMessage.MUST_BE_POSITIVE),
   issue: Yup.number().typeError(ErrorMessage.INVALID_FORMAT).min(0, ErrorMessage.MUST_BE_POSITIVE),
-  pages: Yup.object()
-    .nullable()
-    .shape({
-      begin: Yup.number()
-        .typeError(ErrorMessage.INVALID_FORMAT)
-        .min(0, ErrorMessage.MUST_BE_POSITIVE)
-        .max(Yup.ref('end'), ErrorMessage.INVALID_PAGE_INTERVAL),
-      end: Yup.number()
-        .typeError(ErrorMessage.INVALID_FORMAT)
-        .min(Yup.ref('begin'), ErrorMessage.INVALID_PAGE_INTERVAL),
-    }),
+  pages: pagesRangeField,
 });
 
 const journalPublicationContext = Yup.object().shape({
   title: Yup.string().required(ErrorMessage.REQUIRED),
 });
 
-const journalReference = Yup.object().shape({
-  doi: Yup.string().url(),
+const journalReference = baseReference.shape({
   publicationInstance: journalPublicationInstance,
   publicationContext: journalPublicationContext,
 });
@@ -80,21 +96,16 @@ const journalReference = Yup.object().shape({
 // Book
 const bookPublicationInstance = Yup.object().shape({
   type: Yup.string().oneOf(Object.values(BookType)).required(ErrorMessage.REQUIRED),
-  pages: Yup.object()
-    .nullable()
-    .shape({
-      pages: Yup.number().typeError(ErrorMessage.INVALID_FORMAT).min(1, ErrorMessage.MUST_BE_MIN_1),
-    }),
-  peerReviewed: Yup.boolean().required(ErrorMessage.REQUIRED),
+  pages: pagesMonographField,
+  peerReviewed: peerReviewedField,
 });
 
 const bookPublicationContext = Yup.object().shape({
-  publisher: Yup.string().required(ErrorMessage.REQUIRED),
-  isbnList: Yup.array().of(Yup.string().matches(isbnRegex, ErrorMessage.INVALID_FORMAT)),
+  publisher: publisherField,
+  isbnList: isbnListField,
 });
 
-const bookReference = Yup.object().shape({
-  doi: Yup.string().url(),
+const bookReference = baseReference.shape({
   publicationInstance: bookPublicationInstance,
   publicationContext: bookPublicationContext,
 });
@@ -102,20 +113,15 @@ const bookReference = Yup.object().shape({
 // Report
 const reportPublicationInstance = Yup.object().shape({
   type: Yup.string().oneOf(Object.values(ReportType)).required(ErrorMessage.REQUIRED),
-  pages: Yup.object()
-    .nullable()
-    .shape({
-      pages: Yup.number().typeError(ErrorMessage.INVALID_FORMAT).min(1, ErrorMessage.MUST_BE_MIN_1),
-    }),
+  pages: pagesMonographField,
 });
 
 const reportPublicationContext = Yup.object().shape({
-  publisher: Yup.string().required(ErrorMessage.REQUIRED),
-  isbnList: Yup.array().of(Yup.string().matches(isbnRegex, ErrorMessage.INVALID_FORMAT)),
+  publisher: publisherField,
+  isbnList: isbnListField,
 });
 
-const reportReference = Yup.object().shape({
-  doi: Yup.string().url(),
+const reportReference = baseReference.shape({
   publicationInstance: reportPublicationInstance,
   publicationContext: reportPublicationContext,
 });
@@ -126,11 +132,10 @@ const degreePublicationInstance = Yup.object().shape({
 });
 
 const degreePublicationContext = Yup.object().shape({
-  publisher: Yup.string().required(ErrorMessage.REQUIRED),
+  publisher: publisherField,
 });
 
-const degreeReference = Yup.object().shape({
-  doi: Yup.string().url(),
+const degreeReference = baseReference.shape({
   publicationInstance: degreePublicationInstance,
   publicationContext: degreePublicationContext,
 });
@@ -179,7 +184,7 @@ export const publicationValidationSchema = Yup.object().shape({
 });
 
 export const doiValidationSchema = Yup.object().shape({
-  doiUrl: Yup.string().trim().url(ErrorMessage.INVALID_FORMAT).required(ErrorMessage.REQUIRED),
+  doiUrl: doiField.required(ErrorMessage.REQUIRED),
 });
 
 export const newContributorValidationSchema = Yup.object().shape({
