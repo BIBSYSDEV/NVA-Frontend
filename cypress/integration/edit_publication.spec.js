22acf8d3545d7d0f79c8dba79bd42d74c8a77935
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-10-01 14:06:03 +0200

    NP-1540 Fix capitalization

--- cypress/integration/edit_publication.spec.js
+++ cypress/integration/edit_publication.spec.js
@@ -18,7 +18,7 @@ describe('User opens an item in the My Publication list', () => {
 
     // Reference tab
     cy.get('[data-testid=nav-tabpanel-reference]').click({ force: true });
-    cy.contains('Contribution to Journal');
+    cy.contains('Contribution to journal');
 
     // Contributors tab
     cy.get('[data-testid=nav-tabpanel-contributors]').click({ force: true });
