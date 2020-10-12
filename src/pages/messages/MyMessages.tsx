e5b17327031d9227acf9d4efb8ba4b249257a589
Sarah Serussi <sase@unit.no>
2020-10-12 13:57:04 +0200

    add doi request list

--- src/pages/messages/MyMessages.tsx
+++ src/pages/messages/MyMessages.tsx
@@ -2,13 +2,35 @@ import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { PageHeader } from '../../components/PageHeader';
+import { RoleName } from '../../types/user.types';
+import ListSkeleton from '../../components/ListSkeleton';
+import Card from '../../components/Card';
+import SubHeading from '../../components/SubHeading';
+import useFetchPublicationsWithPendingDoiRequest from '../../utils/hooks/useFetchPublicationsWithPendingDoiRequest';
+import { DoiRequestAccordion } from '../worklist/DoiRequestAccordion';
 
 const MyMessages: FC = () => {
   const { t } = useTranslation('workLists');
+  const [publicationsWithPendingDoiRequest, isLoadingPendingDoiRequests] = useFetchPublicationsWithPendingDoiRequest(
+    RoleName.CREATOR
+  );
 
   return (
     <>
       <PageHeader>{t('my_messages')}</PageHeader>
+      {isLoadingPendingDoiRequests ? (
+        <ListSkeleton minWidth={100} maxWidth={100} height={100} />
+      ) : publicationsWithPendingDoiRequest.length === 0 ? (
+        <Card>
+          <SubHeading>{t('doi_requests.no_pending_doi_requests')}</SubHeading>
+        </Card>
+      ) : (
+        <>
+          {publicationsWithPendingDoiRequest.map((publication) => (
+            <DoiRequestAccordion key={publication.identifier} publication={publication} />
+          ))}
+        </>
+      )}
     </>
   );
 };
