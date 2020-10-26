839c77b6a503c48ab644121a0609412404a6ad2c
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-10-26 15:32:06 +0100

    NP-1751 Use language code when requesting institutions

--- src/api/institutionApi.ts
+++ src/api/institutionApi.ts
@@ -3,21 +3,31 @@ import { StatusCode } from '../utils/constants';
 import i18n from '../translations/i18n';
 import { apiRequest } from './apiRequest';
 import { InstitutionUnitBase } from '../types/institution.types';
+import { LanguageCodes } from '../types/language.types';
 
 export enum InstitutionApiPaths {
   INSTITUTIONS = '/institution/institutions',
   DEPARTMENTS = '/institution/departments',
 }
 
+const getLanguageCode = () => {
+  const currentLanguage = localStorage.getItem('i18nextLng');
+  if (currentLanguage === LanguageCodes.NORWEGIAN_BOKMAL || currentLanguage === LanguageCodes.NORWEGIAN_NYNORSK) {
+    return 'nb';
+  } else {
+    return 'en';
+  }
+};
+
 export const getInstitutions = async (cancelToken?: CancelToken) =>
   await apiRequest<InstitutionUnitBase[]>({
-    url: InstitutionApiPaths.INSTITUTIONS,
+    url: `${InstitutionApiPaths.INSTITUTIONS}?language=${getLanguageCode()}`,
     method: 'GET',
     cancelToken,
   });
 
 export const getDepartment = async (departmentUri: string, cancelToken?: CancelToken) => {
-  const url = `${InstitutionApiPaths.DEPARTMENTS}?uri=${departmentUri}`;
+  const url = `${InstitutionApiPaths.DEPARTMENTS}?uri=${departmentUri}&language=${getLanguageCode()}`;
   try {
     const response = await Axios.get(url, { cancelToken });
     if (response.status === StatusCode.OK) {
