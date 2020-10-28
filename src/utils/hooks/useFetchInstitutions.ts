4f5d2e3afa4afef6395fdc064528cb9b29218015
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-10-28 11:02:53 +0100

    NP-1751 Update institutions when user changes language

--- src/utils/hooks/useFetchInstitutions.ts
+++ src/utils/hooks/useFetchInstitutions.ts
@@ -1,7 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import Axios from 'axios';
 
 import { InstitutionUnitBase } from '../../types/institution.types';
 import { getInstitutions } from '../../api/institutionApi';
@@ -9,35 +8,39 @@ import { setNotification } from '../../redux/actions/notificationActions';
 import { NotificationVariant } from '../../types/notification.types';
 import { RootStore } from '../../redux/reducers/rootReducer';
 import { setInstitutions } from '../../redux/actions/institutionActions';
+import useCancelToken from './useCancelToken';
 
 // This hook is used to fetch all top-level institutions
 const useFetchInstitutions = (): [InstitutionUnitBase[], boolean] => {
   const dispatch = useDispatch();
-  const { t } = useTranslation('feedback');
+  const { t, i18n } = useTranslation('feedback');
   const institutions = useSelector((store: RootStore) => store.institutions);
   const [isLoading, setIsLoading] = useState(true);
+  const cancelToken = useCancelToken();
+
+  const fetchInstitutions = useCallback(async () => {
+    const response = await getInstitutions(cancelToken);
+    if (response) {
+      if (response.error) {
+        dispatch(setNotification(t('error.get_institutions'), NotificationVariant.Error));
+      } else if (response.data) {
+        dispatch(setInstitutions(response.data));
+      }
+      setIsLoading(false);
+    }
+  }, [dispatch, t, cancelToken]);
 
   useEffect(() => {
-    const cancelSource = Axios.CancelToken.source();
-
-    const fetchInstitutions = async () => {
-      const response = await getInstitutions(cancelSource.token);
-      if (response) {
-        if (response.error) {
-          dispatch(setNotification(t('error.get_institutions'), NotificationVariant.Error));
-        } else if (response.data) {
-          dispatch(setInstitutions(response.data));
-        }
-        setIsLoading(false);
-      }
-    };
     // Institutions should not change, so ensure we fetch only once
     if (!institutions || institutions.length === 0) {
       fetchInstitutions();
     }
+  }, [fetchInstitutions, institutions]);
 
-    return () => cancelSource.cancel();
-  }, [t, dispatch, institutions]);
+  useEffect(() => {
+    // Update institutions if user updates language
+    fetchInstitutions();
+  }, [fetchInstitutions, i18n.language]);
 
   return [institutions, isLoading];
 };
