6cdb13e167761d7b52c5ed28b7fb4df705bd43ed
Sarah Serussi <sase@unit.no>
2020-10-28 14:45:55 +0100

    set return type on doiRequestApi

--- src/api/doiRequestApi.ts
+++ src/api/doiRequestApi.ts
@@ -1,5 +1,5 @@
 import { CancelToken } from 'axios';
-import { authenticatedApiRequest } from './apiRequest';
+import { ApiResponse, authenticatedApiRequest } from './apiRequest';
 import { RoleName } from '../types/user.types';
 import { Registration } from '../types/registration.types';
 
@@ -7,13 +7,20 @@ export enum DoiRequestApiPaths {
   DOI_REQUEST = '/doi-request',
 }
 
-export const getRegistrationsWithPendingDoiRequest = async (role: RoleName, cancelToken?: CancelToken) =>
+export const getRegistrationsWithPendingDoiRequest = async (
+  role: RoleName,
+  cancelToken?: CancelToken
+): Promise<ApiResponse<Registration[]>> =>
   await authenticatedApiRequest<Registration[]>({
     url: `${DoiRequestApiPaths.DOI_REQUEST}?role=${role}`,
     cancelToken,
   });
 
-export const createDoiRequest = async (registrationId: string, message?: string, cancelToken?: CancelToken) =>
+export const createDoiRequest = async (
+  registrationId: string,
+  message?: string,
+  cancelToken?: CancelToken
+): Promise<ApiResponse<unknown>> =>
   await authenticatedApiRequest({
     url: DoiRequestApiPaths.DOI_REQUEST,
     method: 'POST',
