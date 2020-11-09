0c5bbd70857bee6548d11ffd81975875a8317fbd
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-11-09 14:05:32 +0100

    NP-1775 Add link to My Messages

--- src/pages/registration/SupportModalContent.tsx
+++ src/pages/registration/SupportModalContent.tsx
@@ -1,21 +1,21 @@
 import React, { FC } from 'react';
-import { useTranslation } from 'react-i18next';
-import { Typography } from '@material-ui/core';
-// import { useDispatch } from 'react-redux';
+import { Trans } from 'react-i18next';
+import { Link, Typography } from '@material-ui/core';
 import { MessageForm } from '../../components/MessageForm';
-// import { setNotification } from '../../redux/actions/notificationActions';
 
 interface SupportModalContentProps {
   closeModal: () => void;
 }
 
 export const SupportModalContent: FC<SupportModalContentProps> = ({ closeModal }) => {
-  const { t } = useTranslation('registration');
-  // const dispatch = useDispatch();
-
   return (
     <>
-      <Typography>{t('support_description')}</Typography>
+      <Typography>
+        <Trans i18nKey="registration:support_description">
+          <Link href="/my-messages" />
+        </Trans>
+      </Typography>
+
       <MessageForm
         confirmAction={(message) => {
           // TODO: Send message to backend
