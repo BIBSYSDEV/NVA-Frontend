9980919812e0ffac96498f9508dea70ee1f68582
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-11-09 13:27:54 +0100

    NP-1775 Comment fake confirm message

--- src/pages/registration/SupportModalContent.tsx
+++ src/pages/registration/SupportModalContent.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Typography } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 import { MessageForm } from '../../components/MessageForm';
-import { setNotification } from '../../redux/actions/notificationActions';
+// import { setNotification } from '../../redux/actions/notificationActions';
 
 interface SupportModalContentProps {
   closeModal: () => void;
@@ -11,7 +11,7 @@ interface SupportModalContentProps {
 
 export const SupportModalContent: FC<SupportModalContentProps> = ({ closeModal }) => {
   const { t } = useTranslation('registration');
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   return (
     <>
@@ -22,7 +22,7 @@ export const SupportModalContent: FC<SupportModalContentProps> = ({ closeModal }
           // eslint-disable-next-line no-console
           console.log('Support Message:', message);
           closeModal();
-          dispatch(setNotification(t('feedback:success.support_message_sent')));
+          // dispatch(setNotification(t('feedback:success.support_message_sent')));
         }}
         cancelAction={closeModal}
       />
