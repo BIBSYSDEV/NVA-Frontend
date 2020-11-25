d1c637cc3b64946133c4b6cde476589ad0f1b603
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-11-25 09:29:51 +0100

    NP-1858 Refactor action buttons

--- src/pages/registration/RegistrationFormActions.tsx
+++ src/pages/registration/RegistrationFormActions.tsx
@@ -4,21 +4,21 @@ import { setNestedObjectValues, useFormikContext } from 'formik';
 import styled from 'styled-components';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import SaveIcon from '@material-ui/icons/Save';
 import { useTranslation } from 'react-i18next';
 import ButtonWithProgress from '../../components/ButtonWithProgress';
 import { Registration, RegistrationTab } from '../../types/registration.types';
 import Modal from '../../components/Modal';
 import { SupportModalContent } from './SupportModalContent';
 
-const StyledButtonGroupContainer = styled.div`
+const StyledActionsContainer = styled.div`
   margin-bottom: 1rem;
   display: flex;
   justify-content: space-between;
-`;
 
-const StyledButtonContainer = styled.div`
-  display: inline-block;
-  margin-left: 0.5rem;
+  > div > button:not(:last-child) {
+    margin-right: 0.5rem;
+  }
 `;
 
 interface RegistrationFormActionsProps {
@@ -41,47 +41,53 @@ export const RegistrationFormActions: FC<RegistrationFormActionsProps> = ({
 
   return (
     <>
-      {tabNumber !== RegistrationTab.Submission ? (
-        <StyledButtonGroupContainer>
+      <StyledActionsContainer>
+        <div>
+          {/* Left aligned buttons */}
           {tabNumber !== RegistrationTab.Description && (
             <Button
               color="primary"
-              variant="contained"
+              variant="outlined"
               data-testid="button-previous-tab"
               startIcon={<ArrowBackIcon />}
               onClick={() => setTabNumber(tabNumber - 1)}>
               {t('common:previous')}
             </Button>
           )}
+        </div>
+        <div>
+          {/* Right aligned buttons */}
           <Button data-testid="open-support-button" variant="text" color="primary" onClick={toggleSupportModal}>
             {t('common:support')}
           </Button>
-          <StyledButtonContainer>
-            <ButtonWithProgress
-              type="submit"
-              variant="outlined"
-              isLoading={isSaving}
-              data-testid="button-save-registration"
-              onClick={async () => {
-                await saveRegistration();
-                // Set all fields with error to touched to ensure error messages are shown
-                setTouched(setNestedObjectValues(errors, true));
-              }}>
-              {t('save_draft')}
-            </ButtonWithProgress>
-          </StyledButtonContainer>
-          <StyledButtonContainer>
-            <Button
-              color="primary"
-              variant="contained"
-              data-testid="button-next-tab"
-              endIcon={<ArrowForwardIcon />}
-              onClick={() => setTabNumber(tabNumber + 1)}>
-              {t('common:next')}
-            </Button>
-          </StyledButtonContainer>
-        </StyledButtonGroupContainer>
-      ) : null}
+          {tabNumber !== RegistrationTab.Submission ? (
+            <>
+              <ButtonWithProgress
+                type="submit"
+                variant="outlined"
+                isLoading={isSaving}
+                data-testid="button-save-registration"
+                endIcon={<SaveIcon />}
+                onClick={async () => {
+                  await saveRegistration();
+                  // Set all fields with error to touched to ensure error messages are shown
+                  setTouched(setNestedObjectValues(errors, true));
+                }}>
+                {t('save_draft')}
+              </ButtonWithProgress>
+              <Button
+                color="primary"
+                variant="contained"
+                data-testid="button-next-tab"
+                endIcon={<ArrowForwardIcon />}
+                onClick={() => setTabNumber(tabNumber + 1)}>
+                {t('common:next')}
+              </Button>
+            </>
+          ) : null}
+        </div>
+      </StyledActionsContainer>
+
       <Modal open={openSupportModal} onClose={toggleSupportModal} headingText={t('common:support')}>
         <SupportModalContent closeModal={toggleSupportModal} />
       </Modal>
