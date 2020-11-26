db55f4cf9d229466465e764584448f33cb6d6d1e
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-11-26 15:29:23 +0100

    NP-1956 Remove debugging

--- src/pages/registration/references_tab/components/PublisherField.tsx
+++ src/pages/registration/references_tab/components/PublisherField.tsx
@@ -14,26 +14,23 @@ const PublisherField: FC = () => {
   return (
     <Field name={contextTypeBaseFieldName}>
       {({ field: { name, value } }: FieldProps) => (
-        <>
-          {JSON.stringify(value)}
-          <PublicationChannelSearch
-            dataTestId="publisher-search-input"
-            publicationTable={PublicationTableNumber.PUBLISHERS}
-            label={t('common:publisher')}
-            placeholder={t('references.search_for_publisher')}
-            errorFieldName={ReferenceFieldNames.PUBLICATION_CONTEXT_PUBLISHER}
-            setValue={(newValue) => {
-              const contextValues: any = formatPublicationContextWithPublisher(value.type, newValue);
-              if (newValue && value.seriesTitle) {
-                // Keep data from selected series, since this has precedence
-                contextValues.seriesTitle = value.seriesTitle;
-                contextValues.level = value.level;
-              }
-              setFieldValue(name, contextValues);
-            }}
-            value={publicationContextToPublisher(value)}
-          />
-        </>
+        <PublicationChannelSearch
+          dataTestId="publisher-search-input"
+          publicationTable={PublicationTableNumber.PUBLISHERS}
+          label={t('common:publisher')}
+          placeholder={t('references.search_for_publisher')}
+          errorFieldName={ReferenceFieldNames.PUBLICATION_CONTEXT_PUBLISHER}
+          setValue={(newValue) => {
+            const contextValues: any = formatPublicationContextWithPublisher(value.type, newValue);
+            if (newValue && value.seriesTitle) {
+              // Keep data from selected series, since this has precedence
+              contextValues.seriesTitle = value.seriesTitle;
+              contextValues.level = value.level;
+            }
+            setFieldValue(name, contextValues);
+          }}
+          value={publicationContextToPublisher(value)}
+        />
       )}
     </Field>
   );
