e254dbbe44e4a90fc27bfa61def5a1913474bb3b
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-12-02 13:14:26 +0100

    NP-1683 Remove deprecated stuff

--- src/types/publicationFieldNames.ts
+++ src/types/publicationFieldNames.ts
@@ -1,10 +1,8 @@
-// For valid values, see https://github.com/BIBSYSDEV/nva-datamodel-java/blob/develop/src/main/java/no/unit/nva/model/PublicationType.java
 export enum PublicationType {
   PUBLICATION_IN_JOURNAL = 'Journal',
   BOOK = 'Book',
   REPORT = 'Report',
   DEGREE = 'Degree',
-  // CHAPTER = 'Chapter',
 }
 
 export enum JournalType {
