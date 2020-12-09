be49d51e87ddd6ea07415738cd617e70f0898089
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-12-09 15:04:49 +0100

    NP-1683 Show original article on landing page

--- src/pages/public_registration/PublicPublicationInstance.tsx
+++ src/pages/public_registration/PublicPublicationInstance.tsx
@@ -1,23 +1,44 @@
 import React, { FC } from 'react';
-import LabelContentRow from '../../components/LabelContentRow';
-import { JournalPublicationInstance } from '../../types/publication_types/journalRegistration.types';
+import { Skeleton } from '@material-ui/lab';
 import { useTranslation } from 'react-i18next';
+import { JournalPublicationInstance } from '../../types/publication_types/journalRegistration.types';
+import LabelContentRow from '../../components/LabelContentRow';
 import { DegreePublicationInstance } from '../../types/publication_types/degreeRegistration.types';
 import { ReportPublicationInstance } from '../../types/publication_types/reportRegistration.types';
 import { PagesMonograph } from '../../types/registration.types';
+import { JournalType } from '../../types/publicationFieldNames';
+import useSearchRegistrations from '../../utils/hooks/useSearchRegistrations';
 
 export const PublicPublicationInstanceJournal: FC<{ publicationInstance: JournalPublicationInstance }> = ({
   publicationInstance,
 }) => {
   const { t } = useTranslation('registration');
-  const { articleNumber, issue, pages, volume } = publicationInstance;
+  const { type, articleNumber, issue, pages, volume, corrigendumFor } = publicationInstance;
 
   return (
-    <LabelContentRow minimal label={`${t('common:details')}:`}>
-      {volume && `${t('references.volume')} ${volume}`}
-      {issue && `, ${t('references.issue')} ${issue}`}
-      {pages?.begin && pages?.end && `, ${t('references.pages')} ${pages.begin}-${pages.end}`}
-      {articleNumber && `, ${t('references.article_number')} ${articleNumber}`}
+    <>
+      <LabelContentRow minimal label={`${t('common:details')}:`}>
+        {volume && `${t('references.volume')} ${volume}`}
+        {issue && `, ${t('references.issue')} ${issue}`}
+        {pages?.begin && pages?.end && `, ${t('references.pages')} ${pages.begin}-${pages.end}`}
+        {articleNumber && `, ${t('references.article_number')} ${articleNumber}`}
+      </LabelContentRow>
+      {type === JournalType.CORRIGENDUM && <OriginalArticleInfo originalArticleId={corrigendumFor} />}
+    </>
+  );
+};
+
+export const OriginalArticleInfo: FC<{ originalArticleId: string }> = ({ originalArticleId }) => {
+  const { t } = useTranslation('registration');
+  const [originalArticleSearch, isLoadingOriginalArticleSearch] = useSearchRegistrations(
+    `identifier="${originalArticleId.split('/').pop()}"`
+  );
+  const originalArticleTitle =
+    originalArticleSearch && originalArticleSearch.hits.length === 1 ? originalArticleSearch.hits[0].title : '';
+
+  return (
+    <LabelContentRow minimal label={`${t('references.original_article')}:`}>
+      {isLoadingOriginalArticleSearch ? <Skeleton width={200} /> : originalArticleTitle}
     </LabelContentRow>
   );
 };
