8129cf547c0abb502aa80d0746180eb53a0b5e28
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2020-12-17 11:50:25 +0100

    NP-1998 Minor simplifications

--- src/pages/registration/EditRegistration.tsx
+++ src/pages/registration/EditRegistration.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
@@ -12,33 +12,38 @@ const StyledEditRegistration = styled.div`
   max-width: 55rem;
 `;
 
+enum PanelName {
+  Link = 'link-panel',
+  File = 'file-panel',
+}
+
 const EditRegistration: FC = () => {
   const { identifier } = useParams<{ identifier: string }>();
-  const [expanded, setExpanded] = useState<string | false>(false);
+  const [expanded, setExpanded] = useState<PanelName | false>(false);
   const [showForm, setShowForm] = useState(!!identifier);
   const { t } = useTranslation('registration');
 
-  const handleChange = (panel: string) => (_: React.ChangeEvent<unknown>, isExpanded: boolean) => {
+  const handleChange = (panel: PanelName) => (_: ChangeEvent<unknown>, isExpanded: boolean) => {
     setExpanded(isExpanded ? panel : false);
   };
 
-  const handleClick = () => {
+  const handleOpenForm = () => {
     setShowForm(true);
   };
 
-  return !showForm || !identifier ? (
+  return !showForm ? (
     <>
       <PageHeader>{t('new_registration')}</PageHeader>
       <StyledEditRegistration>
         <LinkRegistration
-          expanded={expanded === 'link-panel'}
-          onChange={handleChange('link-panel')}
-          openForm={handleClick}
+          expanded={expanded === PanelName.Link}
+          onChange={handleChange(PanelName.Link)}
+          openForm={handleOpenForm}
         />
         <UploadRegistration
-          expanded={expanded === 'load-panel'}
-          onChange={handleChange('load-panel')}
-          openForm={() => setShowForm(true)}
+          expanded={expanded === PanelName.File}
+          onChange={handleChange(PanelName.File)}
+          openForm={handleOpenForm}
         />
       </StyledEditRegistration>
     </>
