2db0ef084c948168346a4af97caaf4ec0ba645b2
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-01-06 10:36:34 +0100

    NP-2011 Add paths file

---
+++ src/utils/urlPaths.ts
@@ -0,0 +1,35 @@
+export enum UrlPathTemplate {
+  AdminInstitutions = '/admin-institutions',
+  Description = '/description',
+  Home = '/',
+  Logout = '/logout',
+  MyInstitution = '/my-institution',
+  MyInstitutionUsers = '/my-institution-users',
+  MyMessages = '/my-messages',
+  MyProfile = '/my-profile',
+  MyRegistrations = '/my-registrations',
+  OrderInformation = '/order-information',
+  PrivacyPolicy = '/privacy-policy',
+  Registration = '/registration/:identifier?',
+  RegistrationLandingPage = '/registration/:identifier/public',
+  Search = '/search',
+  User = '/user',
+  Wildcard = '*',
+  Worklist = '/worklist',
+}
+
+export const getRegistrationLandingPagePath = (identifier: string) =>
+  UrlPathTemplate.RegistrationLandingPage.replace(':identifier', encodeURIComponent(identifier));
+
+export const getRegistrationPath = (identifier?: string) =>
+  identifier
+    ? UrlPathTemplate.Registration.replace(':identifier?', encodeURIComponent(identifier))
+    : UrlPathTemplate.Registration.replace('/:identifier?', '');
+
+export const getSearchPath = (searchTerm: string) =>
+  `${UrlPathTemplate.Search}?query=${encodeURIComponent(searchTerm)}`;
+
+export const getUserPath = (authorityId: string) => `${UrlPathTemplate.User}/id=${encodeURIComponent(authorityId)}`;
+
+export const getAdminInstitutionPath = (id: string) =>
+  `${UrlPathTemplate.AdminInstitutions}?id=${encodeURIComponent(id)}`;
