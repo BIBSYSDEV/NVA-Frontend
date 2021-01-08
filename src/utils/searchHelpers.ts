dc09da26f461042b3c8caba57b3aa90e1a20c7ce
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-01-08 12:22:39 +0100

    NP-2020 Refactor to avoid having to supply default parameters

--- src/utils/searchHelpers.ts
+++ src/utils/searchHelpers.ts
@@ -5,30 +5,25 @@ interface PropertySearch {
 interface SearchConfig {
   searchTerm?: string;
   properties?: PropertySearch[];
-  mustMatchAllProperties?: boolean; // Whether to use "AND" or "OR" operator for each property check
-  mustMatchAllSubqueries?: boolean; // Whether to use "AND" or "OR" operator for each subquery
+  canMatchAnyProperty?: boolean; // Whether to use "OR" or "AND" operator for each property check
+  canMatchAnySubquery?: boolean; // Whether to use "OR" or "AND" operator for each subquery
 }
 
 const createSearchTermFilter = (searchTerm?: string) => (searchTerm ? `*${searchTerm}*` : '');
 
-const createPropertyFilter = (properties?: PropertySearch[], mustMatchAllProperties = true) =>
+const createPropertyFilter = (properties?: PropertySearch[], canMatchAnyProperty?: boolean) =>
   properties && properties.length > 0
     ? `(${properties
         .map(({ key, value }) => `${key}="${Array.isArray(value) ? value.join('" OR "') : value}"`)
-        .join(mustMatchAllProperties ? ' AND ' : ' OR ')})`
+        .join(canMatchAnyProperty ? ' OR ' : ' AND ')})`
     : '';
 
-export const createSearchQuery = ({
-  searchTerm,
-  properties,
-  mustMatchAllProperties = true,
-  mustMatchAllSubqueries = true,
-}: SearchConfig) => {
-  const textSearch = createSearchTermFilter(searchTerm);
-  const propertySearch = createPropertyFilter(properties, mustMatchAllProperties);
+export const createSearchQuery = (searchConfig: SearchConfig) => {
+  const textSearch = createSearchTermFilter(searchConfig.searchTerm);
+  const propertySearch = createPropertyFilter(searchConfig.properties, searchConfig.canMatchAnyProperty);
 
   const searchQuery = [textSearch, propertySearch]
     .filter((search) => !!search)
-    .join(mustMatchAllSubqueries ? ' AND ' : ' OR ');
+    .join(searchConfig.canMatchAnySubquery ? ' OR ' : ' AND ');
   return searchQuery;
 };
