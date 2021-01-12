d0f3953dfcf27f2b43468d0588f819f446336336
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-01-12 15:40:57 +0100

    NP-2028 Add more padding to header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -19,14 +19,15 @@ const StyledAppBar = styled(AppBar)`
   grid-template-areas: 'logo shortcuts auth';
   grid-template-columns: 5rem auto auto;
   align-items: center;
-  padding-left: 1rem;
-  padding-right: 1rem;
+  padding-left: 5%;
+  padding-right: 5%;
   min-height: 4rem;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.values.md + 'px'}) {
     grid-template-areas: 'menu logo auth';
-    grid-template-columns: 1fr 1fr 1fr;
+    padding-left: 1rem;
     padding-right: 1rem;
+    grid-template-columns: 1fr 1fr 1fr;
   }
 `;
 
