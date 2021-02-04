a4e879896b70cd835ed7c7ba8d14f96331a258cb
Sarah Serussi <sase@unit.no>
2021-02-04 11:26:43 +0100

    add collapse with ellipsis and button

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,11 +1,13 @@
 import React, { ReactNode, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
-import Truncate from 'react-truncate';
 import styled from 'styled-components';
-import { Button, Typography, TypographyProps } from '@material-ui/core';
+import { Button, Collapse, Typography, TypographyProps } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import { UrlPathTemplate } from '../utils/urlPaths';
+import { StyledRightAlignedWrapper } from './styled/Wrappers';
 
 const StyledHeader = styled.div`
   width: 100%;
@@ -50,8 +52,30 @@ export const PageHeader = ({ backPath, children, ...props }: PageHeaderProps) =>
 };
 
 const StyledRegistrationPageHeader = styled(PageHeader)`
+  display: grid;
+  grid-template-areas: 'title ellipsis button';
   font-weight: 700;
   font-style: italic;
+  cursor: pointer;
+`;
+
+const RightAlignedWrapper = styled(StyledRightAlignedWrapper)`
+  align-items: flex-end;
+  margin: 0 1rem;
+`;
+
+const StyledEllipsis = styled(Typography)`
+  font-weight: 700;
+  font-style: italic;
+  cursor: pointer;
+  align-self: end;
+`;
+
+const StyledButton = styled(Button)`
+  background-color: ${({ theme }) => theme.palette.section.light};
+  color: ${({ theme }) => theme.palette.section.megaDark};
+  padding: 0.2rem;
+  min-width: 0;
 `;
 
 export const RegistrationPageHeader = (props: PageHeaderProps) => {
@@ -61,13 +85,19 @@ export const RegistrationPageHeader = (props: PageHeaderProps) => {
 
   return (
     <StyledRegistrationPageHeader variant="h2" variantMapping={{ h2: 'h1' }} onClick={toggleFullText} {...props}>
-      {showFullText ? (
-        props.children
-      ) : (
-        <Truncate lines={2} ellipsis="...">
-          {props.children}
-        </Truncate>
+      <Collapse in={showFullText} collapsedHeight="5.3rem">
+        {props.children}
+      </Collapse>
+      {!showFullText && (
+        <StyledEllipsis variant="h2" variantMapping={{ h2: 'span' }}>
+          ...
+        </StyledEllipsis>
       )}
+      <RightAlignedWrapper>
+        <StyledButton variant="contained">
+          {showFullText ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+        </StyledButton>
+      </RightAlignedWrapper>
     </StyledRegistrationPageHeader>
   );
 };
