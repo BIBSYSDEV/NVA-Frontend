db99945837f2870066a61259a8c465e999cb762a
Sarah Serussi <sase@unit.no>
2021-02-10 13:39:56 +0100

    add front page button links

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from 'react-i18next';
 import { Link, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { Link as MuiLink, Typography } from '@material-ui/core';
+import PostAddIcon from '@material-ui/icons/PostAdd';
+import SearchIcon from '@material-ui/icons/Search';
 import BackgroundDiv from '../../components/BackgroundDiv';
 import SearchBar from '../../components/SearchBar';
 import lightTheme from '../../themes/lightTheme';
@@ -27,6 +29,35 @@ const StyledBackgroundDiv = styled(BackgroundDiv)`
   margin: 0;
 `;
 
+const StyledLinksContainer = styled.div`
+  display: grid;
+  grid-template-areas: 'search new-registration';
+  gap: 2rem;
+  margin-top: 2rem;
+`;
+
+const StyledLink = styled(Link)`
+  border: 2px solid;
+  border-radius: 4px;
+  padding: 3rem 6rem;
+`;
+
+const StyledSearchLink = styled(StyledLink)`
+  border-color: ${({ theme }) => theme.palette.secondary.main};
+`;
+
+const StyledNewRegistrationLink = styled(StyledLink)`
+  border-color: ${({ theme }) => theme.palette.primary.main};
+`;
+
+const StyledLinkContent = styled.div`
+  display: grid;
+  grid-template-areas: 'icon' 'text';
+  justify-items: center;
+  gap: 2rem;
+  width: 12rem;
+`;
+
 const Dashboard = () => {
   const { t } = useTranslation();
   const history = useHistory();
@@ -45,10 +76,28 @@ const Dashboard = () => {
           {t('common:read_more')}
         </MuiLink>
       </StyledBackgroundDiv>
-      <StyledSearchBarContainer>
+      <StyledLinksContainer>
+        <StyledSearchLink to={UrlPathTemplate.Search}>
+          <StyledLinkContent>
+            <SearchIcon fontSize="large" />
+            <Typography variant="h4" variantMapping={{ h4: 'a' }}>
+              Søk etter forskning
+            </Typography>
+          </StyledLinkContent>
+        </StyledSearchLink>
+        <StyledNewRegistrationLink to={UrlPathTemplate.NewRegistration}>
+          <StyledLinkContent>
+            <PostAddIcon fontSize="large" />
+            <Typography variant="h4" variantMapping={{ h4: 'a' }}>
+              Ny registrering
+            </Typography>
+          </StyledLinkContent>
+        </StyledNewRegistrationLink>
+      </StyledLinksContainer>
+      {/* <StyledSearchBarContainer>
         <SearchBar handleSearch={handleSearch} initialSearchTerm="" />
         <LatestRegistrations />
-      </StyledSearchBarContainer>
+      </StyledSearchBarContainer> */}
     </StyledDashboard>
   );
 };
