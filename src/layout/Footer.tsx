ad870a2bd99991ca87eeca6de1cad394545e7f8a
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-02-19 10:59:12 +0100

    NP-2195 Update footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,51 +1,31 @@
-import React, { FC } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import logo from '../resources/images/unit_logo.png';
-import { Link } from 'react-router-dom';
-import { Link as MuiLink } from '@material-ui/core';
-import NormalText from '../components/NormalText';
+import { Button } from '@material-ui/core';
 import { UrlPathTemplate } from '../utils/urlPaths';
 
 const StyledFooter = styled.div`
   display: grid;
-  grid-template-areas: '. logo privacy';
-  grid-template-columns: 1fr 1fr 1fr;
-  min-height: 3rem;
+  grid-template-areas: '. privacy';
+  grid-template-columns: 4fr 1fr;
   align-items: center;
-  border-top: 2px solid ${({ theme }) => theme.palette.divider};
+  min-height: 4rem;
+  background: rgba(210, 210, 210, 0.25);
 `;
 
-const StyledLogoContainer = styled.div`
-  grid-area: logo;
-  justify-self: center;
-`;
-
-const StyledPrivacyPolicyContainer = styled.div`
+const StyledPrivacyButton = styled(Button)`
   grid-area: privacy;
-  justify-self: end;
-  padding-right: 0.5rem;
+  width: min-content;
 `;
 
-const Footer: FC = () => {
-  const { t } = useTranslation('infopages');
+const Footer = () => {
+  const { t } = useTranslation('privacy');
 
   return (
     <StyledFooter>
-      <StyledLogoContainer>
-        <NormalText>{t('common:delivered_by')}</NormalText>
-        <img src={logo} alt="UNIT logo" />
-      </StyledLogoContainer>
-      <StyledPrivacyPolicyContainer>
-        <MuiLink
-          aria-label={t('privacy:privacy_statement')}
-          color="primary"
-          component={Link}
-          to={UrlPathTemplate.PrivacyPolicy}
-          data-testid="privacy_statement_link">
-          {t('privacy:privacy_statement')}
-        </MuiLink>
-      </StyledPrivacyPolicyContainer>
+      <StyledPrivacyButton data-testid="privacy_statement_link" color="primary" href={UrlPathTemplate.PrivacyPolicy}>
+        {t('privacy_statement')}
+      </StyledPrivacyButton>
     </StyledFooter>
   );
 };
