19532c233eba278b83bf9626142a08da5ecc4d0e
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-02-19 15:30:23 +0100

    NP-2219 Reimplement delivered by unit in footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { Button } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 import { UrlPathTemplate } from '../utils/urlPaths';
+import logo from '../resources/images/unit_logo.png';
 
 const StyledFooter = styled.div`
   display: grid;
-  grid-template-areas: '. privacy';
-  grid-template-columns: 4fr 1fr;
+  grid-template-areas: '. logo privacy';
+  grid-template-columns: 1fr 1fr 1fr;
   align-items: center;
   min-height: 4rem;
   background: ${({ theme }) => theme.palette.background.footer};
 `;
 
+const StyledLogoContainer = styled.div`
+  grid-area: logo;
+  justify-self: center;
+`;
+
 const StyledPrivacyButton = styled(Button)`
   grid-area: privacy;
-  width: min-content;
+  justify-self: center;
 `;
 
 const Footer = () => {
@@ -23,6 +29,10 @@ const Footer = () => {
 
   return (
     <StyledFooter>
+      <StyledLogoContainer>
+        <Typography>{t('common:delivered_by')}</Typography>
+        <img src={logo} alt="UNIT logo" />
+      </StyledLogoContainer>
       <StyledPrivacyButton data-testid="privacy_statement_link" color="primary" href={UrlPathTemplate.PrivacyPolicy}>
         {t('privacy_statement')}
       </StyledPrivacyButton>
