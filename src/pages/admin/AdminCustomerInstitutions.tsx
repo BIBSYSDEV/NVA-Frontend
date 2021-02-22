02ac446515f372a0414103bc39ccbe93eda91546
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-02-22 10:14:50 +0100

    NP-2217 Add loading indicator for page

--- src/pages/admin/AdminCustomerInstitutions.tsx
+++ src/pages/admin/AdminCustomerInstitutions.tsx
@@ -2,20 +2,17 @@ import React, { FC, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
-import { Button, CircularProgress } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import { getAllCustomerInstitutions } from '../../api/customerInstitutionsApi';
 import Card from '../../components/Card';
 import { PageHeader } from '../../components/PageHeader';
-import {
-  StyledPageWrapperWithMaxWidth,
-  StyledProgressWrapper,
-  StyledRightAlignedWrapper,
-} from '../../components/styled/Wrappers';
+import { StyledPageWrapperWithMaxWidth, StyledRightAlignedWrapper } from '../../components/styled/Wrappers';
 import { setNotification } from '../../redux/actions/notificationActions';
 import { CustomerInstitution } from '../../types/customerInstitution.types';
 import { NotificationVariant } from '../../types/notification.types';
 import { getAdminInstitutionPath } from '../../utils/urlPaths';
 import InstitutionList from './InstitutionList';
+import { PageSpinner } from '../../components/PageSpinner';
 
 const AdminCustomerInstitutions: FC = () => {
   const { t } = useTranslation('admin');
@@ -50,13 +47,7 @@ const AdminCustomerInstitutions: FC = () => {
             {t('add_institution')}
           </Button>
         </StyledRightAlignedWrapper>
-        {isLoading ? (
-          <StyledProgressWrapper>
-            <CircularProgress />
-          </StyledProgressWrapper>
-        ) : (
-          <InstitutionList institutions={institutions} />
-        )}
+        {isLoading ? <PageSpinner /> : <InstitutionList institutions={institutions} />}
       </Card>
     </StyledPageWrapperWithMaxWidth>
   );
