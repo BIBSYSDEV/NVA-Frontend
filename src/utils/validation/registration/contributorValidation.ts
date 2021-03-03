4b008c49f9186eab49fcb2ee55057ec53eac68de
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-03-03 08:46:34 +0100

    NP-2212 Validate contributors based on role

--- src/utils/validation/registration/contributorValidation.ts
+++ src/utils/validation/registration/contributorValidation.ts
@@ -1,10 +1,41 @@
 import * as Yup from 'yup';
+import { Contributor, ContributorRole } from '../../../types/contributor.types';
+import { BookType, PublicationType } from '../../../types/publicationFieldNames';
 import { ErrorMessage } from '../errorMessage';
 
-export const contributorValidationSchema = Yup.object().shape({
+const contributorValidationSchema = Yup.object().shape({
   correspondingAuthor: Yup.boolean(),
   email: Yup.string().when('correspondingAuthor', {
     is: true,
     then: Yup.string().email(ErrorMessage.INVALID_FORMAT).required(ErrorMessage.REQUIRED),
   }),
 });
+
+export const contributorsValidationSchema = Yup.array()
+  .when('$publicationInstanceType', {
+    is: BookType.ANTHOLOGY,
+    then: Yup.array()
+      .of(contributorValidationSchema)
+      .test('editor-test', ErrorMessage.MISSING_EDITOR, (contributors) =>
+        hasRole(contributors, ContributorRole.EDITOR)
+      ),
+    otherwise: Yup.array()
+      .of(contributorValidationSchema)
+      .test('author-test', ErrorMessage.MISSING_AUTHOR, (contributors) =>
+        hasRole(contributors, ContributorRole.CREATOR)
+      ),
+  })
+  .when('$publicationContextType', {
+    is: PublicationType.DEGREE,
+    then: Yup.array()
+      .of(contributorValidationSchema)
+      .test('supervisor-test', ErrorMessage.MISSING_SUPERVISOR, (contributors) =>
+        hasRole(contributors, ContributorRole.SUPERVISOR)
+      )
+      .test('author-test', ErrorMessage.MISSING_AUTHOR, (contributors) =>
+        hasRole(contributors, ContributorRole.CREATOR)
+      ),
+  });
+
+const hasRole = (contributors: any, role: ContributorRole) =>
+  !!contributors && contributors.some((contributor: Contributor) => contributor.role === role);
