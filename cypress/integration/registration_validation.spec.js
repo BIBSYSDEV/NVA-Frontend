79dbd9260525cf248d772ea52df400150ef6f127
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-03-08 07:52:09 +0100

    a

--- cypress/integration/registration_validation.spec.js
+++ cypress/integration/registration_validation.spec.js
@@ -174,9 +174,9 @@ describe('User opens registration form and can see validation errors', () => {
 
     // Add author
     cy.get('[data-testid=add-contributor-Creator]').first().click({ force: true });
-    cy.get('[data-testid=search-input]').click({ force: true }).type('test');
+    cy.get('[data-testid=search-input]').first().click({ force: true }).type('test');
     cy.get('[data-testid=author-radio-button]').first().click({ force: true });
-    cy.get('[data-testid=connect-author-button]').click({ force: true });
+    cy.get('[data-testid=connect-author-button]').first().click({ force: true });
     cy.contains(ErrorMessage.MISSING_AUTHOR).should('not.exist');
     cy.contains(ErrorMessage.MISSING_SUPERVISOR).should('be.visible');
 
@@ -184,7 +184,7 @@ describe('User opens registration form and can see validation errors', () => {
     cy.get('[data-testid=add-contributor-Supervisor]').first().click({ force: true });
     cy.get('[data-testid=search-input]').click({ force: true }).type('test');
     cy.get('[data-testid=author-radio-button]').last().click({ force: true });
-    cy.get('[data-testid=connect-author-button]').click({ force: true });
+    cy.get('[data-testid=connect-author-button]').first().click({ force: true });
     cy.contains(ErrorMessage.MISSING_AUTHOR).should('not.exist');
     cy.contains(ErrorMessage.MISSING_SUPERVISOR).should('not.exist');
 
