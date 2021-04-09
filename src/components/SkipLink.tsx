76f59ec55e9d167e431049b7877a7754d1773ade
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-04-09 16:54:50 +0200

    NP-2438 Add some styling

--- src/components/SkipLink.tsx
+++ src/components/SkipLink.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-import { LinkProps, Link } from '@material-ui/core';
+import { LinkProps, Link, Typography } from '@material-ui/core';
 
 const StyledSkipLink = styled(Link)`
-  text-align: center;
   position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 3rem;
+  background-color: #1a1a18;
 
-  &.Mui-focusVisible {
+  :focus {
     position: static;
   }
 `;
 
-export const SkipLink = (props: LinkProps) => <StyledSkipLink {...props} />;
+const StyledTypography = styled(Typography)`
+  color: #fff;
+  outline: 3px solid #e24c5e;
+  outline-offset: 3px;
+`;
+
+export const SkipLink = ({ children, ...props }: LinkProps) => (
+  <StyledSkipLink underline="none" {...props}>
+    <StyledTypography>{children}</StyledTypography>
+  </StyledSkipLink>
+);
