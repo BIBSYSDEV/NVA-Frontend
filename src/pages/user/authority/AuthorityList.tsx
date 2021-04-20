d07a2c25e2ed637c1e0340444be4daba7831eb46
Sarah Serussi <sase@unit.no>
2021-04-20 14:40:20 +0200

    add background to search results

--- src/pages/user/authority/AuthorityList.tsx
+++ src/pages/user/authority/AuthorityList.tsx
@@ -3,7 +3,9 @@ import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { Radio, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
+import BackgroundDiv from '../../../components/BackgroundDiv';
 import AffiliationHierarchy from '../../../components/institution/AffiliationHierarchy';
+import lightTheme from '../../../themes/lightTheme';
 import { Authority } from '../../../types/authority.types';
 import useFetchLastRegistrationFromAlma from '../../../utils/hooks/useFetchLastRegistration';
 
@@ -29,57 +31,59 @@ const AuthorityList = ({ authorities, searchTerm, onSelectAuthority, selectedArp
         </Typography>
       )}
 
-      <TableContainer>
-        <Table size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell id="selected-heading" padding="checkbox">
-                {t('common:selected')}
-              </TableCell>
-              <TableCell data-testid="author-name-column">{t('name')}</TableCell>
-              <TableCell data-testid="author-last-registration-column">
-                {t('profile:authority.example_publication')}
-              </TableCell>
-              <TableCell data-testid="author-organizations-column">{t('profile:heading.affiliations')}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {authorities.map((authority) => (
-              <StyledTableRow
-                data-testid="author-radio-button"
-                key={authority.id}
-                hover
-                onClick={() => onSelectAuthority?.(authority)}
-                selected={authority.id === selectedArpId}>
-                <TableCell padding="checkbox">
-                  <Radio
-                    inputProps={{ 'aria-labelledby': 'selected-heading' }}
-                    checked={authority.id === selectedArpId}
-                  />
+      <BackgroundDiv backgroundColor={lightTheme.palette.section.megaLight}>
+        <TableContainer>
+          <Table size="medium">
+            <TableHead>
+              <TableRow>
+                <TableCell id="selected-heading" padding="checkbox">
+                  {t('common:selected')}
                 </TableCell>
-                <TableCell>
-                  <Typography>{authority.name}</Typography>
+                <TableCell data-testid="author-name-column">{t('name')}</TableCell>
+                <TableCell data-testid="author-last-registration-column">
+                  {t('profile:authority.example_publication')}
                 </TableCell>
-                <TableCell>
-                  <LastAlmaRegistrationCell authority={authority} />
-                </TableCell>
-                <TableCell>
-                  {authority.orgunitids.length > 0 ? (
-                    <>
-                      <AffiliationHierarchy unitUri={authority.orgunitids[0]} boldTopLevel={false} />
-                      {authority.orgunitids.length > 1 && (
-                        <i>{t('profile:authority.other_affiliations', { count: authority.orgunitids.length - 1 })}</i>
-                      )}
-                    </>
-                  ) : (
-                    <i>{t('profile:authority.no_affiliations_found')}</i>
-                  )}
-                </TableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                <TableCell data-testid="author-organizations-column">{t('profile:heading.affiliations')}</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {authorities.map((authority) => (
+                <StyledTableRow
+                  data-testid="author-radio-button"
+                  key={authority.id}
+                  hover
+                  onClick={() => onSelectAuthority?.(authority)}
+                  selected={authority.id === selectedArpId}>
+                  <TableCell padding="checkbox">
+                    <Radio
+                      inputProps={{ 'aria-labelledby': 'selected-heading' }}
+                      checked={authority.id === selectedArpId}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Typography>{authority.name}</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <LastAlmaRegistrationCell authority={authority} />
+                  </TableCell>
+                  <TableCell>
+                    {authority.orgunitids.length > 0 ? (
+                      <>
+                        <AffiliationHierarchy unitUri={authority.orgunitids[0]} boldTopLevel={false} />
+                        {authority.orgunitids.length > 1 && (
+                          <i>{t('profile:authority.other_affiliations', { count: authority.orgunitids.length - 1 })}</i>
+                        )}
+                      </>
+                    ) : (
+                      <i>{t('profile:authority.no_affiliations_found')}</i>
+                    )}
+                  </TableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </BackgroundDiv>
     </>
   );
 };
