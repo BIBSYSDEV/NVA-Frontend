155f40f2b3b7e85b32f125f8fb51452edeb2958d
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-05-05 11:48:13 +0200

    NP-2542 Remove redundant div

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import styled from 'styled-components';
 import { PageHeader } from '../../components/PageHeader';
 import { SearchBar } from '../../components/SearchBar';
 import { StyledPageWrapperWithMaxWidth } from '../../components/styled/Wrappers';
 import RegistrationSearch from './RegistrationSearch';
-
-const StyledSearch = styled.div`
-  width: 85%;
-  justify-items: center;
-`;
 
 const SearchPage = () => {
   const { t } = useTranslation('common');
@@ -17,10 +11,8 @@ const SearchPage = () => {
   return (
     <StyledPageWrapperWithMaxWidth>
       <PageHeader backPath="/">{t('registrations')}</PageHeader>
-      <StyledSearch>
-        <SearchBar />
-        <RegistrationSearch />
-      </StyledSearch>
+      <SearchBar />
+      <RegistrationSearch />
     </StyledPageWrapperWithMaxWidth>
   );
 };
