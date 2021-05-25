3cd467cf766c8801569b91a8ed5ceeee09b185fd
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-05-25 13:36:02 +0200

    NP-2601 Show icons for roles

--- src/components/IconLabelTextLine.tsx
+++ src/components/IconLabelTextLine.tsx
@@ -1,52 +1,48 @@
-import React from 'react';
+import { Typography } from '@material-ui/core';
+import React, { ReactNode } from 'react';
 import styled from 'styled-components';
 
-import { Icon } from '@material-ui/core';
-
 const StyledLine = styled.div`
-  padding-bottom: 0.8rem;
   padding-top: 0.8rem;
   display: grid;
+  grid-template-areas: 'icon label .' 'text text text';
+  grid-template-columns: auto 3fr;
+  column-gap: 0.5rem;
+
   @media (min-width: ${({ theme }) => theme.breakpoints.values.md + 'px'}) {
     grid-template-areas: 'icon label' 'text text';
     grid-template-columns: auto 1fr;
   }
-  grid-template-areas: 'icon label .' 'text text text';
-  grid-template-columns: auto 3fr;
   border-bottom: 1px solid ${({ theme }) => theme.palette.divider};
   &:first-of-type {
     border-top: 1px solid ${({ theme }) => theme.palette.divider};
   }
 `;
 
-const StyledLabel = styled.div`
+const StyledLabel = styled(Typography)`
   grid-area: label;
   font-weight: bold;
 `;
 
-const StyledText = styled.div`
+const StyledText = styled(Typography)`
   grid-area: text;
 `;
 
-const StyledIcon = styled(Icon)`
+const StyledIcon = styled.div`
   grid-area: icon;
-  padding-right: 2rem;
 `;
 
 interface IconLabelTextLineProps {
   dataTestId?: string;
-  icon: string;
+  icon: ReactNode;
   label: string;
   text: string;
 }
 
-const IconLabelTextLine: React.FC<IconLabelTextLineProps> = ({ dataTestId, icon, label, text }) => {
-  return (
-    <StyledLine data-testid={dataTestId}>
-      <StyledIcon>{icon}</StyledIcon>
-      <StyledLabel>{label}</StyledLabel>
-      <StyledText>{text}</StyledText>
-    </StyledLine>
-  );
-};
-export default IconLabelTextLine;
+export const IconLabelTextLine = ({ dataTestId, icon, label, text }: IconLabelTextLineProps) => (
+  <StyledLine data-testid={dataTestId}>
+    <StyledIcon>{icon}</StyledIcon>
+    <StyledLabel variantMapping={{ body1: 'h3' }}>{label}</StyledLabel>
+    <StyledText gutterBottom>{text}</StyledText>
+  </StyledLine>
+);
