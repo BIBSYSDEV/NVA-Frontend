ecdd6c3a9eb0f33da0ecb2ea470232e186e2bcff
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-05-31 09:58:16 +0200

    NP-2621 Fix layout

--- src/pages/projects/ProjectLandingPage.tsx
+++ src/pages/projects/ProjectLandingPage.tsx
@@ -1,6 +1,7 @@
-import { Typography } from '@material-ui/core';
+import { Accordion, AccordionSummary, Typography } from '@material-ui/core';
 import React from 'react';
 import styled from 'styled-components';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { PageHeader } from '../../components/PageHeader';
 import { CristinProject } from '../../types/project.types';
 import {
@@ -20,6 +21,16 @@ const StyledGeneralInfoBox = styled.div`
   }
 `;
 
+const StyledAccordion = styled(Accordion)`
+  border-top: 3px solid;
+  padding: 1rem;
+  background: white;
+
+  :last-child {
+    border-bottom: 3px solid;
+  }
+`;
+
 interface ProjectLandingPageProps {
   project: CristinProject;
 }
@@ -28,32 +39,57 @@ export const ProjectLandingPage = ({ project }: ProjectLandingPageProps) => {
   return (
     <>
       <PageHeader>{project.title}</PageHeader>
+
       <StyledGeneralInfoBox>
         <div>
           <Typography variant="overline" component="h2">
             Prosjekteier
           </Typography>
-          <Typography>{getProjectName(project)}</Typography>
+          <Typography>{getProjectName(project) ?? '-'}</Typography>
           <Typography variant="overline" component="h2">
             Prosjektleder
           </Typography>
-          <Typography>{getProjectManagerName(project)}</Typography>
+          <Typography>{getProjectManagerName(project) ?? '-'}</Typography>
           <Typography variant="overline" component="h2">
             Prosjektperiode
           </Typography>
-          <Typography>{getProjectPeriod(project)}</Typography>
+          <Typography>{getProjectPeriod(project) ?? '-'}</Typography>
         </div>
         <div>
           <Typography variant="overline" component="h2">
             Kategori og disiplin
           </Typography>
-          <Typography>asdda</Typography>
+          <Typography>-</Typography>
           <Typography variant="overline" component="h2">
             Finansiering
           </Typography>
-          <Typography>asdads</Typography>
+          <Typography>-</Typography>
         </div>
       </StyledGeneralInfoBox>
+
+      <StyledAccordion square elevation={0}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon color="primary" />}>
+          <Typography variant="h3" color="primary">
+            Vitenskapelig sammendrag
+          </Typography>
+        </AccordionSummary>
+      </StyledAccordion>
+
+      <StyledAccordion square elevation={0}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon color="primary" />}>
+          <Typography variant="h3" color="primary">
+            Prosjektdeltakere
+          </Typography>
+        </AccordionSummary>
+      </StyledAccordion>
+
+      <StyledAccordion square elevation={0}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon color="primary" />}>
+          <Typography variant="h3" color="primary">
+            Resultater
+          </Typography>
+        </AccordionSummary>
+      </StyledAccordion>
     </>
   );
 };
