cd0c8e7537a46ed5482fd62d8fe833eed846179d
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-06-01 12:33:54 +0200

    NP-2665 Use constants as testid

--- cypress/integration/registration_landing_page.spec.js
+++ cypress/integration/registration_landing_page.spec.js
@@ -1,3 +1,10 @@
+import { dataTestId } from '../../src/utils/dataTestIds';
+
+const {
+  registrationLandingPage: { projectTitle, status },
+  projectLandingPage: { generalInfoBox, participantsAccordion, resultsAccordion, scientificSummaryAccordion },
+} = dataTestId;
+
 describe('User opens Landing Page for Registration', () => {
   beforeEach('Given that the user is logged in as Creator:', () => {
     cy.visit('/');
@@ -9,25 +16,24 @@ describe('User opens Landing Page for Registration', () => {
     cy.get('[data-testid^=open-registration]').eq(0).click({ force: true });
 
     cy.url().should('include', '/public');
-    cy.get('[data-testid=public-registration-status]').should('be.visible');
+    cy.get(`[data-testid=${status}]`).should('be.visible');
   });
 
   it('Anonymous user should be able to open Landing Page for Registration', () => {
     cy.visit('/registration/123/public');
     cy.get('[data-testid=my-registrations]').should('not.exist');
-
-    cy.get('[data-testid=public-registration-status]').should('not.exist');
+    cy.get(`[data-testid=${status}]`).should('not.exist');
   });
 
   it('Project should have a link to Landing Page for Project', () => {
     cy.visit('/registration/123/public');
-    cy.get('[data-testid=project-title]').should('be.visible');
-    cy.get('[data-testid=project-title] > a').click({ force: true });
+    cy.get(`[data-testid=${projectTitle}]`).should('be.visible');
+    cy.get(`[data-testid=${projectTitle}] > a`).click({ force: true });
 
     cy.url().should('include', '/project');
-    cy.get('[data-testid=general-info]').should('be.visible');
-    cy.get('[data-testid=participants-accordion]').should('be.visible');
-    cy.get('[data-testid=results-accordion]').should('be.visible');
-    cy.get('[data-testid=scientific-summary-accordion]').should('be.visible');
+    cy.get(`[data-testid=${generalInfoBox}]`).should('be.visible');
+    cy.get(`[data-testid=${participantsAccordion}]`).should('be.visible');
+    cy.get(`[data-testid=${resultsAccordion}]`).should('be.visible');
+    cy.get(`[data-testid=${scientificSummaryAccordion}]`).should('be.visible');
   });
 });
