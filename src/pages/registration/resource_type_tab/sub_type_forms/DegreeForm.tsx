ec25540f98b0462978d0eef384d83f746ea34a84
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-06-21 15:27:58 +0200

    NP-2780 Show series number only for PHD

--- src/pages/registration/resource_type_tab/sub_type_forms/DegreeForm.tsx
+++ src/pages/registration/resource_type_tab/sub_type_forms/DegreeForm.tsx
@@ -6,8 +6,13 @@ import lightTheme from '../../../../themes/lightTheme';
 import { DoiField } from '../components/DoiField';
 import PublisherField from '../components/PublisherField';
 import { SeriesFields } from '../components/SeriesFields';
+import { DegreeType } from '../../../../types/publicationFieldNames';
 
-const DegreeForm = () => {
+interface DegreeFormProps {
+  subType: DegreeType;
+}
+
+const DegreeForm = ({ subType }: DegreeFormProps) => {
   const { t } = useTranslation('registration');
 
   return (
@@ -17,11 +22,13 @@ const DegreeForm = () => {
         <PublisherField />
       </BackgroundDiv>
 
-      <BackgroundDiv backgroundColor={lightTheme.palette.section.dark}>
-        <Typography variant="h5">{t('resource_type.series')}</Typography>
-        <Typography>{t('resource_type.series_info')}</Typography>
-        <SeriesFields />
-      </BackgroundDiv>
+      {subType === DegreeType.PHD && (
+        <BackgroundDiv backgroundColor={lightTheme.palette.section.dark}>
+          <Typography variant="h5">{t('resource_type.series')}</Typography>
+          <Typography>{t('resource_type.series_info')}</Typography>
+          <SeriesFields />
+        </BackgroundDiv>
+      )}
     </>
   );
 };
