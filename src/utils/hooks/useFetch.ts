5eab1b5b5dd2d67a342bc200a9f53b5de85968e5
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-06-24 11:55:20 +0200

    NP-2540 Use custom config for data fetching

--- src/utils/hooks/useFetch.ts
+++ src/utils/hooks/useFetch.ts
@@ -6,42 +6,49 @@ import { setNotification } from '../../redux/actions/notificationActions';
 import { NotificationVariant } from '../../types/notification.types';
 import { apiRequest2, authenticatedApiRequest2 } from '../../api/apiRequest';
 
-export const useFetch = <T>(requestUrl: string, withAuthentication = false): [T | undefined, boolean] => {
+interface UseFetchConfig {
+  url: string;
+  errorMessage?: string;
+  withAuthentication?: boolean;
+}
+
+export const useFetch = <T>({
+  url,
+  errorMessage,
+  withAuthentication = false,
+}: UseFetchConfig): [T | undefined, boolean] => {
   const dispatch = useDispatch();
   const { t } = useTranslation('feedback');
   const [data, setData] = useState<T>();
   const [isLoading, setIsLoading] = useState(false);
   const cancelToken = useCancelToken();
 
-  const fetchData = useCallback(
-    async (url: string) => {
-      setIsLoading(true);
-      const fetchedData = withAuthentication
-        ? await authenticatedApiRequest2<T>({ url, cancelToken })
-        : await apiRequest2<T>({ url, cancelToken });
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+    const fetchedData = withAuthentication
+      ? await authenticatedApiRequest2<T>({ url, cancelToken })
+      : await apiRequest2<T>({ url, cancelToken });
 
-      if (fetchedData) {
-        if (fetchedData.status >= 400 && fetchedData.status <= 599) {
-          dispatch(
-            setNotification(
-              t('error.fetch', { resource: url, interpolation: { escapeValue: false } }),
-              NotificationVariant.Error
-            )
-          );
-        } else if (fetchedData.data) {
-          setData(fetchedData.data);
-        }
+    if (fetchedData) {
+      if (fetchedData.status >= 400 && fetchedData.status <= 599) {
+        dispatch(
+          setNotification(
+            errorMessage ?? t('error.fetch', { resource: url, interpolation: { escapeValue: false } }),
+            NotificationVariant.Error
+          )
+        );
+      } else if (fetchedData.data) {
+        setData(fetchedData.data);
       }
-      setIsLoading(false);
-    },
-    [dispatch, t, cancelToken, withAuthentication]
-  );
+    }
+    setIsLoading(false);
+  }, [dispatch, t, cancelToken, withAuthentication, errorMessage, url]);
 
   useEffect(() => {
-    if (requestUrl) {
-      fetchData(requestUrl);
+    if (url) {
+      fetchData();
     }
-  }, [dispatch, t, fetchData, requestUrl]);
+  }, [dispatch, t, fetchData, url]);
 
   return [data, isLoading];
 };
