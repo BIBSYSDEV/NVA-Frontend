6cf9e558ecd60dcc489648ed896f507336798406
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-06-30 15:22:30 +0200

    NP-2876 Use one method for all updates of user

--- src/api/roleApi.ts
+++ src/api/roleApi.ts
@@ -2,7 +2,7 @@ import Axios, { CancelToken } from 'axios';
 import i18n from '../translations/i18n';
 import { StatusCode } from '../utils/constants';
 import { getIdToken } from './userApi';
-import { RoleName, InstitutionUser, UserRole } from '../types/user.types';
+import { InstitutionUser } from '../types/user.types';
 import { RoleApiPath } from './apiPaths';
 import { authenticatedApiRequest } from './apiRequest';
 
@@ -29,88 +29,9 @@ export const getInstitutionUser = async (username: string, cancelToken?: CancelT
   // return await authenticatedApiRequest<InstitutionUser>({ url: `${RoleApiPath.Users}/${username}` }); // error: t('feedback:error.get_roles')
 };
 
-export const updateUser = async (username: string, newUser: InstitutionUser) => {
-  return await authenticatedApiRequest<InstitutionUser>({
-    url: `${RoleApiPath.Users}/${username}`,
+export const updateUser = async (username: string, newUser: InstitutionUser) =>
+  await authenticatedApiRequest<InstitutionUser>({
+    url: `${RoleApiPath.Users}/${encodeURIComponent(username)}`,
     method: 'PUT',
     data: newUser,
   });
-};
-
-export const addRoleToUser = async (username: string, rolename: RoleName, cancelToken?: CancelToken) => {
-  try {
-    const existingUser = await getInstitutionUser(username, cancelToken);
-    if (existingUser) {
-      if (existingUser.error) {
-        // Forward error message returned from fetching existing user request
-        return existingUser;
-      } else if (existingUser.roles.some((role: UserRole) => role.rolename === rolename)) {
-        // Skip update if user already has role we want to add
-        return existingUser;
-      } else {
-        // Update user
-        const roleElement = { rolename, type: 'Role' };
-        const newInstitutionUser: InstitutionUser = {
-          ...existingUser,
-          roles: [...existingUser.roles, roleElement],
-        };
-        return await updateUserRoles(newInstitutionUser, cancelToken);
-      }
-    } else {
-      return { error: i18n.t('feedback:error.add_role') };
-    }
-  } catch (error) {
-    if (!Axios.isCancel(error)) {
-      return { error: i18n.t('feedback:error.add_role') };
-    }
-  }
-};
-
-export const removeRoleFromUser = async (username: string, rolename: RoleName, cancelToken?: CancelToken) => {
-  try {
-    const existingUser = await getInstitutionUser(username, cancelToken);
-    if (existingUser) {
-      if (existingUser.error) {
-        // Forward error message returned from fetching existing user request
-        return existingUser;
-      } else {
-        // Update user
-        const newInstitutionUser: InstitutionUser = {
-          ...existingUser,
-          roles: existingUser.roles.filter((role: UserRole) => role.rolename !== rolename),
-        };
-        return await updateUserRoles(newInstitutionUser, cancelToken);
-      }
-    } else {
-      return { error: i18n.t('feedback:error.remove_role') };
-    }
-  } catch (error) {
-    if (!Axios.isCancel(error)) {
-      return { error: i18n.t('feedback:error.remove_role') };
-    }
-  }
-};
-
-const updateUserRoles = async (institutionUser: InstitutionUser, cancelToken?: CancelToken) => {
-  const url = `${RoleApiPath.Users}/${encodeURIComponent(institutionUser.username)}`;
-  try {
-    const idToken = await getIdToken();
-    const headers = {
-      Authorization: `Bearer ${idToken}`,
-    };
-
-    const response = await Axios.put(url, institutionUser, { headers, cancelToken });
-
-    if (response.status === StatusCode.OK) {
-      return response.data;
-    } else if (response.status === StatusCode.ACCEPTED) {
-      return institutionUser;
-    } else {
-      return { error: i18n.t('feedback:error.update_institution_user') };
-    }
-  } catch (error) {
-    if (!Axios.isCancel(error)) {
-      return { error: i18n.t('feedback:error.update_institution_user') };
-    }
-  }
-};
--- src/pages/admin/UserList.tsx
+++ src/pages/admin/UserList.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { Table, TableBody, TableCell, TableHead, TablePagination, TableRow, Typography } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { removeRoleFromUser, updateUser } from '../../api/roleApi';
+import { updateUser } from '../../api/roleApi';
 import { ButtonWithProgress } from '../../components/ButtonWithProgress';
 import { ConfirmDialog } from '../../components/ConfirmDialog';
 import { DangerButton } from '../../components/DangerButton';
@@ -74,15 +74,22 @@ export const UserList = ({
   const handleRemoveRoleFromUser = async () => {
     if (roleToRemove && removeRoleForUser) {
       setUpdatedRoleForUsers((state) => [...state, removeRoleForUser]);
-      const response = await removeRoleFromUser(removeRoleForUser, roleToRemove);
-      if (response) {
-        if (response.error) {
-          setUpdatedRoleForUsers((state) => state.filter((user) => user !== removeRoleForUser));
-          dispatch(setNotification(t('feedback:error.remove_role'), NotificationVariant.Error));
-        } else {
-          dispatch(setNotification(t('feedback:success.removed_role')));
-          refetchUsers?.();
-        }
+
+      const existingUser = userList.find((user) => user.username === removeRoleForUser);
+      if (!existingUser) {
+        return;
+      }
+      const newUser: InstitutionUser = {
+        ...existingUser,
+        roles: existingUser.roles.filter((role) => role.rolename !== roleToRemove),
+      };
+      const updateUserResponse = await updateUser(removeRoleForUser, newUser);
+      if (isErrorStatus(updateUserResponse.status)) {
+        setUpdatedRoleForUsers((state) => state.filter((user) => user !== removeRoleForUser));
+        dispatch(setNotification(t('feedback:error.remove_role'), NotificationVariant.Error));
+      } else if (isSuccessStatus(updateUserResponse.status)) {
+        dispatch(setNotification(t('feedback:success.removed_role')));
+        refetchUsers?.();
       }
     }
     setRemoveRoleForUser('');
