c1c156e8bce00c6b6bcd7520276e62f6d198e98f
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-07-08 10:53:03 +0200

    NP-2919 Add all types

--- src/pages/search/filters/RegistrationTypeFilter.tsx
+++ src/pages/search/filters/RegistrationTypeFilter.tsx
@@ -1,12 +1,11 @@
 import { ListItem, ListSubheader } from '@material-ui/core';
-
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import { JournalType } from '../../../types/publicationFieldNames';
+import { BookType, ChapterType, DegreeType, JournalType, ReportType } from '../../../types/publicationFieldNames';
 import { BaseFilterItem } from './BaseFilterItem';
 
 const StyledIndentedListItem = styled(ListItem)`
-  padding-left: 2rem;
+  padding-left: 1.5rem;
 `;
 
 export const RegistrationTypeFilter = () => {
@@ -16,6 +15,30 @@ export const RegistrationTypeFilter = () => {
     <BaseFilterItem title="Registration Type">
       <ListSubheader disableSticky>{t('Journal')}</ListSubheader>
       {Object.values(JournalType).map((type) => (
+        <StyledIndentedListItem key={type} button>
+          {t(type)}
+        </StyledIndentedListItem>
+      ))}
+      <ListSubheader disableSticky>{t('Book')}</ListSubheader>
+      {Object.values(BookType).map((type) => (
+        <StyledIndentedListItem key={type} button>
+          {t(type)}
+        </StyledIndentedListItem>
+      ))}
+      <ListSubheader disableSticky>{t('Report')}</ListSubheader>
+      {Object.values(ReportType).map((type) => (
+        <StyledIndentedListItem key={type} button>
+          {t(type)}
+        </StyledIndentedListItem>
+      ))}
+      <ListSubheader disableSticky>{t('Degree')}</ListSubheader>
+      {Object.values(DegreeType).map((type) => (
+        <StyledIndentedListItem key={type} button>
+          {t(type)}
+        </StyledIndentedListItem>
+      ))}
+      <ListSubheader disableSticky>{t('Chapter')}</ListSubheader>
+      {Object.values(ChapterType).map((type) => (
         <StyledIndentedListItem key={type} button>
           {t(type)}
         </StyledIndentedListItem>
