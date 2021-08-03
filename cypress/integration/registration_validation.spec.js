91ad1abac595320d867ed13cd5ac9ea798a4f3f4
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-08-03 16:21:41 +0200

    NP-2691 Update tests

--- cypress/integration/registration_validation.spec.js
+++ cypress/integration/registration_validation.spec.js
@@ -87,10 +87,12 @@ describe('User opens registration form and can see validation errors', () => {
 
     cy.get(`[data-testid=${dataTestId.registrationWizard.resourceType.contentField}] p.Mui-error`).should('be.visible');
     cy.get(`[data-testid=${dataTestId.registrationWizard.resourceType.peerReviewed}] input`).should('not.exist');
+    cy.get(`[data-testid=${dataTestId.registrationWizard.resourceType.originalResearchField}]`).should('not.exist');
     cy.get(`[data-testid=${dataTestId.registrationWizard.resourceType.contentField}]`).click();
     cy.get(`[data-value="${JournalArticleContentType.ResearchArticle}"]`).click();
-    cy.get(`[data-testid=${dataTestId.registrationWizard.resourceType.contentField}] p.Mui-error`).should('not.exist');
     cy.get(`[data-testid=${dataTestId.registrationWizard.resourceType.peerReviewed}] input`).eq(0).click();
+    cy.get(`[data-testid=${dataTestId.registrationWizard.resourceType.originalResearchField}] input`).eq(1).click();
+    cy.get(`[data-testid=${dataTestId.registrationWizard.resourceType.contentField}] p.Mui-error`).should('not.exist');
 
     cy.get('[data-testid=nav-tabpanel-resource-type]').within(() =>
       cy.get('[data-testid=error-tab]').should('not.exist')
