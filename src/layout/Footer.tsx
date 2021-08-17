0b0f095588e6dddf5c5d7f0e04bf2b5db7cb239f
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-08-17 12:41:32 +0200

    NP-3007 Add about page

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -7,16 +7,16 @@ import logo from '../resources/images/unit_logo.png';
 
 const StyledFooter = styled.footer`
   display: grid;
-  grid-template-areas: '. logo privacy';
+  grid-template-areas: 'about  logo  privacy';
   grid-template-columns: 1fr 1fr 1fr;
   align-items: center;
   min-height: 4rem;
   background: ${({ theme }) => theme.palette.background.footer};
+`;
 
-  @media (max-width: ${({ theme }) => `${theme.breakpoints.values.md}px`}) {
-    grid-template-areas: 'logo privacy';
-    grid-template-columns: 1fr 1fr;
-  }
+const StyledAboutButton = styled(Button)`
+  grid-area: about;
+  justify-self: center;
 `;
 
 const StyledLogoContainer = styled.div`
@@ -27,6 +27,7 @@ const StyledLogoContainer = styled.div`
 const StyledPrivacyButton = styled(Button)`
   grid-area: privacy;
   justify-self: center;
+  word-break: break-all;
 `;
 
 export const Footer = () => {
@@ -38,6 +39,9 @@ export const Footer = () => {
         <Typography>{t('common:delivered_by')}</Typography>
         <img src={logo} alt="UNIT logo" />
       </StyledLogoContainer>
+      <StyledAboutButton data-testid="about_link" color="primary" href={UrlPathTemplate.About}>
+        {t('common:about_nva')}
+      </StyledAboutButton>
       <StyledPrivacyButton data-testid="privacy_statement_link" color="primary" href={UrlPathTemplate.PrivacyPolicy}>
         {t('privacy_statement')}
       </StyledPrivacyButton>
