8bf20e6294480ae36f94bc608a5eabe0e1a8dc32
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-08-17 13:00:51 +0200

    NP-3007 Avoid refreshing whole page when using links

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import { Button, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import { UrlPathTemplate } from '../utils/urlPaths';
 import logo from '../resources/images/unit_logo.png';
 
 const StyledFooter = styled.footer`
   display: grid;
-  grid-template-areas: 'about  logo  privacy';
+  grid-template-areas: 'about logo privacy';
   grid-template-columns: 1fr 1fr 1fr;
   align-items: center;
   min-height: 4rem;
   background: ${({ theme }) => theme.palette.background.footer};
 `;
 
-const StyledAboutButton = styled(Button)`
+const StyledAboutButtonContainer = styled.div`
   grid-area: about;
   justify-self: center;
 `;
@@ -24,7 +25,7 @@ const StyledLogoContainer = styled.div`
   justify-self: center;
 `;
 
-const StyledPrivacyButton = styled(Button)`
+const StyledPrivacyButtonContainer = styled.div`
   grid-area: privacy;
   justify-self: center;
   word-break: break-all;
@@ -35,16 +36,24 @@ export const Footer = () => {
 
   return (
     <StyledFooter>
+      <StyledAboutButtonContainer>
+        <Button data-testid="about_link" color="primary" component={Link} to={UrlPathTemplate.About}>
+          {t('common:about_nva')}
+        </Button>
+      </StyledAboutButtonContainer>
       <StyledLogoContainer>
         <Typography>{t('common:delivered_by')}</Typography>
         <img src={logo} alt="UNIT logo" />
       </StyledLogoContainer>
-      <StyledAboutButton data-testid="about_link" color="primary" href={UrlPathTemplate.About}>
-        {t('common:about_nva')}
-      </StyledAboutButton>
-      <StyledPrivacyButton data-testid="privacy_statement_link" color="primary" href={UrlPathTemplate.PrivacyPolicy}>
-        {t('privacy_statement')}
-      </StyledPrivacyButton>
+      <StyledPrivacyButtonContainer>
+        <Button
+          data-testid="privacy_statement_link"
+          color="primary"
+          component={Link}
+          to={UrlPathTemplate.PrivacyPolicy}>
+          {t('privacy_statement')}
+        </Button>
+      </StyledPrivacyButtonContainer>
     </StyledFooter>
   );
 };
