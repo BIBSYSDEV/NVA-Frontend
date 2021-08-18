ffa0e09391dc4b6ef46e70e5247eb1879f947ecd
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-08-18 11:04:49 +0200

    NP-3011 Update path after logging out

--- src/layout/Logout.tsx
+++ src/layout/Logout.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import { Link as MuiLink, Typography } from '@material-ui/core';
-import { StyledCenteredContent } from '../components/styled/Wrappers';
+import { Redirect } from 'react-router-dom';
 import { UrlPathTemplate } from '../utils/urlPaths';
+import { LOGIN_REDIRECT_PATH_KEY } from '../utils/constants';
+
+const isPublicPage = (path: string) => {
+  if (
+    path === UrlPathTemplate.Home ||
+    path === UrlPathTemplate.About ||
+    path === UrlPathTemplate.PrivacyPolicy ||
+    path.startsWith(UrlPathTemplate.User) ||
+    (path.startsWith(UrlPathTemplate.Registration) && path.endsWith('/public'))
+  ) {
+    return true;
+  }
+  return false;
+};
 
 const Logout = () => {
-  const { t } = useTranslation('authorization');
+  const previousPath = localStorage.getItem(LOGIN_REDIRECT_PATH_KEY);
+  const redirectPath = previousPath && isPublicPage(previousPath) ? previousPath : UrlPathTemplate.Home;
+  localStorage.removeItem(LOGIN_REDIRECT_PATH_KEY);
 
-  return (
-    <StyledCenteredContent>
-      <Typography variant="h2" component="h1" paragraph>
-        {t('logged_out')}
-      </Typography>
-      <MuiLink component={Link} to={UrlPathTemplate.Home}>
-        <Typography>{t('back_to_home')}</Typography>
-      </MuiLink>
-    </StyledCenteredContent>
-  );
+  return <Redirect to={redirectPath} />;
 };
 
 export default Logout;
--- src/utils/hooks/useAuthentication.ts
+++ src/utils/hooks/useAuthentication.ts
@@ -1,6 +1,5 @@
 import { useDispatch } from 'react-redux';
 import { Auth } from 'aws-amplify';
-
 import { USE_MOCK_DATA, FEIDE_IDENTITY_PROVIDER, LOGIN_REDIRECT_PATH_KEY } from '../constants';
 import { setUser } from '../../redux/actions/userActions';
 import { mockUser } from '../testfiles/mock_feide_user';
@@ -15,7 +14,7 @@ export const useAuthentication = (): UseAuthentication => {
   const dispatch = useDispatch();
 
   const handleLogin = () => {
-    localStorage.setItem(LOGIN_REDIRECT_PATH_KEY, window.location.pathname);
+    localStorage.setItem(LOGIN_REDIRECT_PATH_KEY, `${window.location.pathname}${window.location.search}`);
     if (USE_MOCK_DATA) {
       dispatch(setUser(mockUser));
     } else {
@@ -24,9 +23,9 @@ export const useAuthentication = (): UseAuthentication => {
   };
 
   const handleLogout = () => {
+    localStorage.setItem(LOGIN_REDIRECT_PATH_KEY, `${window.location.pathname}${window.location.search}`);
     if (USE_MOCK_DATA) {
       dispatch(logoutSuccess());
-      window.location.pathname = '/logout';
     } else {
       Auth.signOut();
     }
