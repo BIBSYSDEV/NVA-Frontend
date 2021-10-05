d9452817eca0b91053463b146a4fcc8195ae0787
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-10-05 14:47:07 +0200

    NP-3215 Update NVI rules for journal article

--- src/pages/registration/resource_type_tab/components/NviValidation.tsx
+++ src/pages/registration/resource_type_tab/components/NviValidation.tsx
@@ -1,53 +1,49 @@
 import { Typography } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { BackgroundDiv } from '../../../../components/BackgroundDiv';
+import { RootStore } from '../../../../redux/reducers/rootReducer';
 import { lightTheme } from '../../../../themes/lightTheme';
 import { JournalType } from '../../../../types/publicationFieldNames';
 import { JournalArticleContentType } from '../../../../types/publication_types/content.types';
-import { JournalPublicationInstance } from '../../../../types/publication_types/journalRegistration.types';
-import { Journal, Publisher, Registration } from '../../../../types/registration.types';
+import { JournalEntityDescription } from '../../../../types/publication_types/journalRegistration.types';
+import { Journal, Registration } from '../../../../types/registration.types';
 
 interface NviValidationProps {
   registration: Registration;
-  journal?: Journal;
-  publisher?: Publisher;
-  series?: Journal;
 }
 
-export const NviValidation = ({ registration, journal, publisher, series }: NviValidationProps) => {
-  const { type } = registration.entityDescription.reference.publicationInstance;
+export const NviValidation = ({ registration }: NviValidationProps) => {
+  const { publicationInstance } = registration.entityDescription.reference;
 
-  const isNviApplicableType = type === JournalType.Article;
+  const isNviApplicableJournalArticle =
+    publicationInstance.type === JournalType.Article &&
+    'contentType' in publicationInstance &&
+    (publicationInstance.contentType === JournalArticleContentType.ResearchArticle ||
+      publicationInstance.contentType === JournalArticleContentType.ReviewArticle);
 
-  return isNviApplicableType ? (
+  return isNviApplicableJournalArticle ? (
     <BackgroundDiv backgroundColor={lightTheme.palette.section.black}>
-      {type === JournalType.Article && <NviValidationJournalArticle registration={registration} journal={journal} />}
+      {publicationInstance.type === JournalType.Article && <NviValidationJournalArticle registration={registration} />}
     </BackgroundDiv>
   ) : null;
 };
 
-const NviValidationJournalArticle = ({
-  registration,
-  journal,
-}: Pick<NviValidationProps, 'registration' | 'journal'>) => {
+const NviValidationJournalArticle = ({ registration }: Pick<NviValidationProps, 'registration'>) => {
   const { t } = useTranslation('registration');
-  const publicationInstance = registration.entityDescription.reference
-    .publicationInstance as JournalPublicationInstance;
+  const entityDescription = registration.entityDescription as JournalEntityDescription;
+  const { publicationContext, publicationInstance } = entityDescription.reference;
 
-  if (
-    publicationInstance.contentType !== JournalArticleContentType.ResearchArticle &&
-    publicationInstance.contentType !== JournalArticleContentType.ReviewArticle
-  ) {
-    return null;
-  }
+  const publicationChannelState = useSelector((store: RootStore) => store.publicationChannel);
+  const journal = publicationContext.id ? (publicationChannelState[publicationContext.id] as Journal) : null;
+  const isRatedJournal = parseInt(journal?.level ?? '0') > 0;
 
-  const isRated = parseInt(journal?.level ?? '0') > 0;
   const isPeerReviewed = !!publicationInstance.peerReviewed;
 
   return (
     <Typography>
-      {isRated
+      {isRatedJournal
         ? isPeerReviewed
           ? t('resource_type.nvi.applicable')
           : t('resource_type.nvi.not_peer_reviewed')
