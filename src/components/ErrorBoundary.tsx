c079a4a0b4bc4b8697f29f4b34dd809e46641c34
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-10-12 15:56:24 +0200

    NP-3244 Add error boundary on top level

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { WithTranslation, withTranslation } from 'react-i18next';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+import commonNb from '../translations/nb/common.json';
 
 type ErrorBoundaryClassProps = RouteComponentProps & WithTranslation;
 
@@ -21,8 +22,29 @@ class ErrorBoundaryClass extends Component<ErrorBoundaryClassProps> {
     const { t, children } = this.props;
     const { hasError } = this.state;
 
-    return hasError ? <h1>{t('common:error_occurred')} &#129301;</h1> : children;
+    return hasError ? <ErrorMessage errorMessage={t('common:error_occurred')} /> : children;
   }
 }
 
 export const ErrorBoundary = withTranslation()(withRouter(ErrorBoundaryClass));
+
+export class BasicErrorBoundary extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
+
+    return hasError ? <ErrorMessage errorMessage={commonNb.error_occurred} /> : children;
+  }
+}
+
+interface ErrorMessageProps {
+  errorMessage: string;
+}
+
+const ErrorMessage = ({ errorMessage }: ErrorMessageProps) => <h1>{errorMessage} &#129301;</h1>;
