e5303704904ccadc4745f63fd7f16ed02adcc1fb
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-10-21 09:48:07 +0200

    NP-3309 Remove unused license images

--- src/resources/images/licenses/index.ts
+++ src/resources/images/licenses/index.ts
@@ -1,13 +1,3 @@
-export { default as ccByImage } from './cc-by.svg';
-export { default as ccBySaImage } from './cc-by-sa.svg';
-export { default as ccByNdImage } from './cc-by-nd.svg';
-export { default as ccByNcImage } from './cc-by-nc.svg';
-export { default as ccByNcSaImage } from './cc-by-nc-sa.svg';
-export { default as ccByNcNdImage } from './cc-by-nc-nd.svg';
-export { default as cc0Image } from './cc0.svg';
-export { default as ccImage } from './cc.svg';
-
-// button images - used by license selector
 export { default as ccByButton } from './button-cc-by.svg';
 export { default as ccBySaButton } from './button-cc-by-sa.svg';
 export { default as ccByNdButton } from './button-cc-by-nd.svg';
