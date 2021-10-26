f43860477995b0cec671bb2abae935728ad7e31d
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-10-26 16:30:24 +0200

    NP-2664 Show project participants on landing page

--- src/pages/projects/ProjectLandingPage.tsx
+++ src/pages/projects/ProjectLandingPage.tsx
@@ -3,7 +3,7 @@ import { Link, Typography } from '@mui/material';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import { useTranslation } from 'react-i18next';
 import { ItalicPageHeader } from '../../components/PageHeader';
-import { CristinProject } from '../../types/project.types';
+import { CristinProject, ProjectContributor } from '../../types/project.types';
 import {
   getProjectName,
   getProjectManagerName,
@@ -13,6 +13,11 @@ import { dataTestId } from '../../utils/dataTestIds';
 import { LandingPageAccordion } from '../../components/landing_page/LandingPageAccordion';
 import { StyledGeneralInfo } from '../../components/landing_page/SyledGeneralInfo';
 import { getLanguageString } from '../../utils/translation-helpers';
+import styled from 'styled-components';
+
+const StyledContributorElement = styled.div`
+  margin-bottom: 0.5rem;
+`;
 
 interface ProjectLandingPageProps {
   project: CristinProject;
@@ -20,6 +25,9 @@ interface ProjectLandingPageProps {
 
 export const ProjectLandingPage = ({ project }: ProjectLandingPageProps) => {
   const { t } = useTranslation('project');
+
+  const projectManagers = project.contributors.filter((contributor) => contributor.type === 'ProjectManager');
+  const projectParticipants = project.contributors.filter((contributor) => contributor.type === 'ProjectParticipant');
 
   const popularScienceSummary = getLanguageString(project.popularScientificSummary);
   const academicSummary = getLanguageString(project.academicSummary);
@@ -91,11 +99,47 @@ export const ProjectLandingPage = ({ project }: ProjectLandingPageProps) => {
 
       <LandingPageAccordion
         data-testid={dataTestId.projectLandingPage.participantsAccordion}
-        heading={t('project_participants')}></LandingPageAccordion>
+        heading={t('project_participants')}>
+        {projectManagers.length > 0 && (
+          <>
+            <Typography variant="overline" component="h3">
+              {t('project_manager')}
+            </Typography>
+            {projectManagers.map((manager) => (
+              <ContributorElement key={manager.identity.id} contributor={manager} />
+            ))}
+          </>
+        )}
+        {projectParticipants.length > 0 && (
+          <>
+            <Typography variant="overline" component="h3">
+              {t('project_participants')}
+            </Typography>
+            {projectParticipants.map((participant) => (
+              <ContributorElement key={participant.identity.id} contributor={participant} />
+            ))}
+          </>
+        )}
+      </LandingPageAccordion>
 
       <LandingPageAccordion
         data-testid={dataTestId.projectLandingPage.resultsAccordion}
         heading={t('results')}></LandingPageAccordion>
     </>
+  );
+};
+
+interface ContributorElementProps {
+  contributor: ProjectContributor;
+}
+
+const ContributorElement = ({ contributor }: ContributorElementProps) => {
+  return (
+    <StyledContributorElement>
+      <Typography variant="subtitle2" component="p">
+        {contributor.identity.firstName} {contributor.identity.lastName}
+      </Typography>
+      <Typography variant="body2">{getLanguageString(contributor.affiliation.name)}</Typography>
+    </StyledContributorElement>
   );
 };
