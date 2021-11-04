a47612abb5c852c4e5efe812e6fb961fc041808b
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-11-04 15:55:17 +0100

    NP-3386 Remove unused export

--- src/types/publication_types/presentationRegistration.types.ts
+++ src/types/publication_types/presentationRegistration.types.ts
@@ -6,7 +6,7 @@ export interface PresentationRegistration extends BaseRegistration {
   entityDescription: PresentationEntityDescription;
 }
 
-export interface PresentationPublicationInstance {
+interface PresentationPublicationInstance {
   type: PresentationType | '';
 }
 
