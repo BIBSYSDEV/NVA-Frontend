a3bc5f7589ee0c6483c58a5c8b0a6c62ab94380f
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-11-11 11:06:10 +0100

    NP-3399 Simplify logic for uppy config

--- src/api/fileApi.ts
+++ src/api/fileApi.ts
@@ -1,5 +1,3 @@
-import { AwsS3Part } from '@uppy/aws-s3-multipart';
-import { UppyFile } from '@uppy/core';
 import { isSuccessStatus } from '../utils/constants';
 import { FileApiPath } from './apiPaths';
 import { apiRequest, authenticatedApiRequest } from './apiRequest';
@@ -26,77 +24,4 @@ export const downloadFile = async (registrationIdentifier: string, fileId: strin
   }
 
   return null;
-};
-
-export const abortMultipartUpload = async (uploadId: string, key: string) => {
-  const payload = {
-    uploadId,
-    key,
-  };
-
-  const completeResponse = await authenticatedApiRequest<any>({
-    url: FileApiPath.Abort,
-    method: 'POST',
-    data: payload,
-  });
-  return completeResponse.data;
-};
-
-export const completeMultipartUpload = async (uploadId: string, key: string, parts: AwsS3Part[]) => {
-  const payload = {
-    uploadId,
-    key,
-    parts,
-  };
-
-  const completeResponse = await authenticatedApiRequest<any>({
-    url: FileApiPath.Complete,
-    method: 'POST',
-    data: payload,
-  });
-  return completeResponse.data;
-};
-export const createMultipartUpload = async (file: UppyFile) => {
-  const payload = {
-    filename: file.name,
-    size: file.data.size,
-    lastmodified: (file.data as File).lastModified,
-    mimetype: file.data.type,
-  };
-
-  const createResponse = await authenticatedApiRequest<any>({
-    url: FileApiPath.Create,
-    method: 'POST',
-    data: payload,
-  });
-  return createResponse.data;
-};
-
-export const listParts = async (uploadId: string, key: string) => {
-  const payload = {
-    uploadId,
-    key,
-  };
-
-  const listPartsResponse = await authenticatedApiRequest<any>({
-    url: FileApiPath.ListParts,
-    method: 'POST',
-    data: payload,
-  });
-  return listPartsResponse.data;
-};
-
-export const prepareUploadPart = async (uploadId: string, key: string, partNumbers: number[]) => {
-  const payload = {
-    uploadId,
-    key,
-    partNumbers,
-  };
-
-  const prepareResponse = await authenticatedApiRequest<any>({
-    url: FileApiPath.Prepare,
-    method: 'POST',
-    data: payload,
-  });
-  return prepareResponse.data;
 };
--- src/utils/uppy/uppy-config.ts
+++ src/utils/uppy/uppy-config.ts
@@ -2,15 +2,10 @@ import Uppy from '@uppy/core';
 import AwsS3Multipart from '@uppy/aws-s3-multipart';
 import norwegianLocale from '@uppy/locales/lib/nb_NO';
 import englishLocale from '@uppy/locales/lib/en_US';
-import {
-  createMultipartUpload,
-  listParts,
-  prepareUploadPart,
-  abortMultipartUpload,
-  completeMultipartUpload,
-} from '../../api/fileApi';
 import i18n from '../../translations/i18n';
 import { LanguageCodes } from '../../types/language.types';
+import { FileApiPath } from '../../api/apiPaths';
+import { authenticatedApiRequest } from '../../api/apiRequest';
 
 const uppyLocale =
   i18n.language === LanguageCodes.NORWEGIAN_BOKMAL || i18n.language === LanguageCodes.NORWEGIAN_NYNORSK
@@ -25,10 +20,50 @@ export const createUppy =
       autoProceed: true,
       restrictions: { maxNumberOfFiles: shouldAllowMultipleFiles ? null : 1 },
     }).use(AwsS3Multipart, {
-      abortMultipartUpload: async (file, opts) => await abortMultipartUpload(opts.uploadId, opts.key),
-      completeMultipartUpload: async (file, opts) => await completeMultipartUpload(opts.uploadId, opts.key, opts.parts),
-      createMultipartUpload: async (file) => await createMultipartUpload(file),
-      listParts: async (file, opts) => await listParts(opts.uploadId, opts.key),
-      prepareUploadParts: async (file, partData) =>
-        await prepareUploadPart(partData.uploadId, partData.key, partData.partNumbers),
+      abortMultipartUpload: async (file, opts) => {
+        const abortResponse = await authenticatedApiRequest<any>({
+          url: FileApiPath.Abort,
+          method: 'POST',
+          data: opts,
+        });
+        return abortResponse.data;
+      },
+      completeMultipartUpload: async (file, opts) => {
+        const completeResponse = await authenticatedApiRequest<any>({
+          url: FileApiPath.Complete,
+          method: 'POST',
+          data: opts,
+        });
+        return completeResponse.data;
+      },
+      createMultipartUpload: async (file) => {
+        const createResponse = await authenticatedApiRequest<any>({
+          url: FileApiPath.Create,
+          method: 'POST',
+          data: {
+            // TODO: Use whole file as data?
+            filename: file.name,
+            size: file.data.size,
+            lastmodified: (file.data as File).lastModified,
+            mimetype: file.data.type,
+          },
+        });
+        return createResponse.data;
+      },
+      listParts: async (file, opts) => {
+        const listPartsResponse = await authenticatedApiRequest<any>({
+          url: FileApiPath.ListParts,
+          method: 'POST',
+          data: opts,
+        });
+        return listPartsResponse.data;
+      },
+      prepareUploadParts: async (file, partData) => {
+        const prepareResponse = await authenticatedApiRequest<any>({
+          url: FileApiPath.Prepare,
+          method: 'POST',
+          data: partData,
+        });
+        return prepareResponse.data;
+      },
     });
