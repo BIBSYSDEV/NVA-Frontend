937892eb28a07d95879ff3c6642e280899f8f0fd
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2021-11-16 08:09:42 +0100

    NP-3404 Simplify props

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -69,7 +69,11 @@ export const SearchBar = () => {
           {({ push, remove }: FieldArrayRenderProps) => (
             <Box gridArea="advanced" sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
               {properties.map((_, index) => (
-                <AdvancedSearchRow key={index} index={index} remove={remove} />
+                <AdvancedSearchRow
+                  key={index}
+                  removeFilter={() => remove(index)}
+                  baseFieldName={`properties[${index}]`}
+                />
               ))}
               <Box sx={{ display: 'flex', gap: '1rem' }}>
                 <Button
@@ -99,18 +103,24 @@ export const SearchBar = () => {
   );
 };
 
-interface AdvancedSearchRowProps extends Pick<FieldArrayRenderProps, 'remove'> {
-  index: number;
+interface AdvancedSearchRowProps {
+  baseFieldName: string;
+  removeFilter: () => void;
 }
 
-const AdvancedSearchRow = ({ index, remove }: AdvancedSearchRowProps) => {
+const AdvancedSearchRow = ({ removeFilter, baseFieldName }: AdvancedSearchRowProps) => {
   const { t } = useTranslation('search');
 
   return (
     <Box sx={{ display: 'grid', gridTemplateColumns: '3fr 3fr 6fr 2fr', gap: '1rem' }}>
-      <Field name={`properties[${index}].fieldName`}>
+      <Field name={`${baseFieldName}.fieldName`}>
         {({ field }: FieldProps<string>) => (
-          <StyledTextField {...field} select variant="outlined" label={t('field_label')}>
+          <StyledTextField
+            {...field}
+            select
+            variant="outlined"
+            label={t('field_label')}
+            InputLabelProps={{ shrink: true }}>
             <MenuItem value={DescriptionFieldNames.Title}>{t('common:title')}</MenuItem>
             <MenuItem value={DescriptionFieldNames.Abstract}>{t('registration:description.abstract')}</MenuItem>
             <MenuItem value={ResourceFieldNames.SubType}>{t('registration_type')}</MenuItem>
@@ -121,20 +131,15 @@ const AdvancedSearchRow = ({ index, remove }: AdvancedSearchRowProps) => {
           </StyledTextField>
         )}
       </Field>
-      <Field name={`properties[${index}].operator`}>
+      <Field name={`${baseFieldName}.operator`}>
         {({ field }: FieldProps<string>) => (
-          <StyledTextField
-            {...field}
-            select
-            SelectProps={{ displayEmpty: true }}
-            variant="outlined"
-            label={t('operator')}>
+          <StyledTextField {...field} select variant="outlined" label={t('operator')}>
             <MenuItem value={ExpressionStatement.Contains}>{t('contains')}</MenuItem>
             <MenuItem value={ExpressionStatement.NotContaining}>{t('not_containing')}</MenuItem>
           </StyledTextField>
         )}
       </Field>
-      <Field name={`properties[${index}].value`}>
+      <Field name={`${baseFieldName}.value`}>
         {({ field }: FieldProps<string>) => (
           <StyledTextField
             {...field}
@@ -144,7 +149,7 @@ const AdvancedSearchRow = ({ index, remove }: AdvancedSearchRowProps) => {
           />
         )}
       </Field>
-      <Button onClick={() => remove(index)} color="error">
+      <Button onClick={removeFilter} color="error">
         {t('remove_filter')}
       </Button>
     </Box>
