1cac8d3bc0d041612e0027acfb587b6b0db9c7d1
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-01-20 15:48:15 +0100

    NP-3561 Allow anonymous user to preview files on landing page

--- src/api/fileApi.ts
+++ src/api/fileApi.ts
@@ -10,22 +10,12 @@ interface DownloadFileResponse {
 }
 
 export const downloadFile = async (registrationIdentifier: string, fileId: string) => {
-  try {
-    const authenticatedResponse = await authenticatedApiRequest<DownloadFileResponse>({
-      url: `${FileApiPath.Download}/${registrationIdentifier}/files/${fileId}`,
-    });
-    if (isSuccessStatus(authenticatedResponse.status)) {
-      return authenticatedResponse.data;
-    }
-  } catch {
-    const publicDownloadResponse = await apiRequest<DownloadFileResponse>({
-      url: `${FileApiPath.PublicDownload}/${registrationIdentifier}/files/${fileId}`,
-    });
-    if (isSuccessStatus(publicDownloadResponse.status)) {
-      return publicDownloadResponse.data;
-    }
+  const authenticatedResponse = await apiRequest<DownloadFileResponse>({
+    url: `${FileApiPath.PublicDownload}/${registrationIdentifier}/files/${fileId}`,
+  });
+  if (isSuccessStatus(authenticatedResponse.status)) {
+    return authenticatedResponse.data;
   }
-
   return null;
 };
 
