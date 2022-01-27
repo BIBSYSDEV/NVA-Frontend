6ead81b8139ebc552a348c9f4339bcfe3ceaadfb
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-01-27 12:01:50 +0100

    NP-3569 Show tooltip for expanding title

--- src/components/TruncatableTypography.tsx
+++ src/components/TruncatableTypography.tsx
@@ -1,5 +1,6 @@
-import { TypographyProps, Typography, styled } from '@mui/material';
-import { useRef, useState } from 'react';
+import { TypographyProps, Typography, styled, Tooltip } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 interface StyledTruncatableTypographyProps {
   lineClamp: number | undefined;
@@ -23,19 +24,28 @@ interface TruncatableTypographyProps extends TypographyProps {
 }
 
 export const TruncatableTypography = ({ lines = 3, ...props }: TruncatableTypographyProps) => {
-  const [lineClamp, setLineClamp] = useState<number | undefined>(lines);
+  const { t } = useTranslation('common');
   const typographyRef = useRef<HTMLElement | null>(null);
-  const isTruncated = isOverflown(typographyRef.current);
+  const [lineClamp, setLineClamp] = useState<number | undefined>(lines);
+  const [isTruncated, setIsTruncated] = useState(isOverflown(typographyRef.current));
+
+  useEffect(() => {
+    // Avoid bug where typographyRef did not get any value
+    if (typographyRef.current) {
+      setIsTruncated(isOverflown(typographyRef.current));
+    }
+  }, []);
 
   return (
-    // TODO: tooltip om man kan vise hele?
-    <StyledTruncatableTypography
-      ref={typographyRef}
-      lineClamp={lineClamp}
-      isTruncated={isTruncated}
-      onClick={() => isTruncated && setLineClamp(undefined)}
-      {...props}
-    />
+    <Tooltip followCursor title={isTruncated && lineClamp !== undefined ? t<string>('click_to_show_all') : ''}>
+      <StyledTruncatableTypography
+        ref={typographyRef}
+        lineClamp={lineClamp}
+        isTruncated={isTruncated}
+        onClick={() => isTruncated && setLineClamp(undefined)}
+        {...props}
+      />
+    </Tooltip>
   );
 };
 
