9fe4c23dba52ed87b79c3f5658656e29934e2d7a
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-02-03 17:25:54 +0100

    NP-3585 Remove redundant indentation

--- src/pages/registration/description_tab/projects_field/CreateProjectDialog.tsx
+++ src/pages/registration/description_tab/projects_field/CreateProjectDialog.tsx
@@ -12,7 +12,7 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
-import { ErrorMessage, Field, FieldProps, Form, Formik, FormikProps } from 'formik';
+import { ErrorMessage, Field, FieldProps, Form, Formik } from 'formik';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { apiRequest } from '../../../../api/apiRequest';
@@ -68,184 +68,178 @@ export const CreateProjectDialog = (props: DialogProps) => {
         initialValues={initialValues}
         validationSchema={basicProjectValidationSchema}
         onSubmit={(values) => console.log('Values', values)}>
-        {({ values }: FormikProps<BasicCristinProject>) => (
-          <Form noValidate>
-            <DialogContent>
-              <InputContainerBox>
-                <Field name={'title'}>
-                  {({ field, meta: { touched, error } }: FieldProps) => (
-                    <TextField
-                      {...field}
-                      label={'Tittel'}
-                      required
-                      variant="filled"
-                      fullWidth
-                      error={touched && !!error}
-                      helperText={<ErrorMessage name={field.name} />}
+        <Form noValidate>
+          <DialogContent>
+            <InputContainerBox>
+              <Field name={'title'}>
+                {({ field, meta: { touched, error } }: FieldProps) => (
+                  <TextField
+                    {...field}
+                    label={'Tittel'}
+                    required
+                    variant="filled"
+                    fullWidth
+                    error={touched && !!error}
+                    helperText={<ErrorMessage name={field.name} />}
+                  />
+                )}
+              </Field>
+              <Box sx={{ display: 'grid', gridTemplateColumns: '2fr 1fr', gap: '1rem', alignItems: 'start' }}>
+                <Field name={'coordinatingInstitution.id'}>
+                  {({ field, form: { setFieldValue }, meta: { touched, error } }: FieldProps) => (
+                    <OrganizationSearchField
+                      onChange={(selectedInstitution) => {
+                        const newOrg: CoordinatingInstitution = {
+                          type: 'Organization',
+                          id: selectedInstitution?.id ?? '',
+                          name: selectedInstitution?.name ?? {},
+                        };
+                        setFieldValue('coordinatingInstitution', newOrg);
+                      }}
+                      errorMessage={touched && !!error ? error : undefined}
+                      fieldInputProps={field}
                     />
                   )}
                 </Field>
-                <Box sx={{ display: 'grid', gridTemplateColumns: '2fr 1fr', gap: '1rem', alignItems: 'start' }}>
-                  <Field name={'coordinatingInstitution.id'}>
-                    {({ field, form: { setFieldValue }, meta: { touched, error } }: FieldProps) => (
-                      <OrganizationSearchField
-                        onChange={(selectedInstitution) => {
-                          const newOrg: CoordinatingInstitution = {
-                            type: 'Organization',
-                            id: selectedInstitution?.id ?? '',
-                            name: selectedInstitution?.name ?? {},
-                          };
-                          setFieldValue('coordinatingInstitution', newOrg);
+
+                <Field name={'startDate'}>
+                  {({ field, form: { setFieldValue }, meta: { touched, error } }: FieldProps) => (
+                    <LocalizationProvider dateAdapter={AdapterDateFns} locale={getDateFnsLocale(i18n.language)}>
+                      <DatePicker
+                        {...datePickerTranslationProps}
+                        label={t('Startdato')}
+                        onChange={(date: Date | null) => {
+                          if (date instanceof Date && !isNaN(date.getTime())) {
+                            setFieldValue(field.name, date.toISOString());
+                          }
                         }}
-                        errorMessage={touched && !!error ? error : undefined}
-                        fieldInputProps={field}
+                        value={field.value ? new Date(field.value) : null}
+                        inputFormat="dd.MM.yyyy"
+                        mask="__.__.____"
+                        renderInput={(params) => (
+                          <TextField
+                            {...field}
+                            {...params}
+                            variant="filled"
+                            required
+                            error={touched && !!error}
+                            helperText={<ErrorMessage name={field.name} />}
+                          />
+                        )}
+                      />
+                    </LocalizationProvider>
+                  )}
+                </Field>
+              </Box>
+              <Typography variant="h3" gutterBottom>
+                Prosjektleder
+              </Typography>
+            </InputContainerBox>
+
+            <Field name={'contributors'}>
+              {({ field, form: { setFieldValue }, meta: { touched, error } }: FieldProps<ProjectContributor[]>) =>
+                field.value.length === 0 ? (
+                  <Autocomplete
+                    options={searchByNameResults?.hits ?? []}
+                    inputMode="search"
+                    getOptionLabel={(option) =>
+                      `${getValueByKey('FirstName', option.names)} ${getValueByKey('LastName', option.names)}`
+                    }
+                    filterOptions={(options) => options}
+                    onInputChange={(_, value, reason) => {
+                      if (reason !== 'reset') {
+                        setSearchTerm(value);
+                      }
+                    }}
+                    onChange={async (_, selectedUser) => {
+                      if (!selectedUser) {
+                        if (field.value.length > 0) {
+                          setFieldValue(field.name, []);
+                        }
+                      } else {
+                        const orgId =
+                          selectedUser.affiliations.length > 0 ? selectedUser.affiliations[0].organization ?? '' : '';
+
+                        const newUser: Partial<ProjectContributor> = {
+                          type: 'ProjectManager',
+                          identity: {
+                            type: 'Person',
+                            id: selectedUser.id ?? '',
+                            firstName: getValueByKey('FirstName', selectedUser?.names),
+                            lastName: getValueByKey('LastName', selectedUser?.names),
+                          },
+                        };
+
+                        if (orgId) {
+                          const institutionResponse = await apiRequest<any>({ url: orgId }); // todo: type
+                          if (isSuccessStatus(institutionResponse.status)) {
+                            newUser.affiliation = {
+                              type: 'Organization',
+                              id: orgId,
+                              name: institutionResponse.data.unit_name,
+                            };
+                          }
+                        }
+                        setFieldValue(field.name, [newUser]);
+                        setSearchTerm('');
+                      }
+                    }}
+                    loading={isLoadingSearchByName}
+                    renderOption={(props, option) => (
+                      <li {...props} key={option.id}>
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                          <Typography variant="subtitle1">
+                            {getValueByKey('FirstName', option.names)} {getValueByKey('LastName', option.names)}
+                          </Typography>
+                          {option.affiliations.length > 0 && (
+                            <Typography variant="body2" color="textSecondary">
+                              {getLanguageString(option.affiliations[0].role.labels ?? {})}
+                            </Typography>
+                          )}
+                        </Box>
+                      </li>
+                    )}
+                    renderInput={(params) => (
+                      <TextField
+                        onBlur={field.onBlur}
+                        value={field.value}
+                        name={field.name}
+                        {...params}
+                        required
+                        label={t('Cristin-person')}
+                        placeholder="Søk etter person"
+                        variant="filled"
+                        error={touched && !!error}
+                        helperText={<ErrorMessage name={field.name} />}
                       />
                     )}
-                  </Field>
+                  />
+                ) : (
+                  <Box sx={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
+                    <Typography>
+                      {[
+                        `${field.value[0].identity.firstName} ${field.value[0].identity.lastName}`,
+                        getLanguageString(field.value[0].affiliation?.name),
+                      ]
+                        .filter((item) => !!item)
+                        .join(' - ')}
+                    </Typography>
+                    <Button size="small" variant="outlined" color="error" onClick={() => setFieldValue(field.name, [])}>
+                      Fjern
+                    </Button>
+                  </Box>
+                )
+              }
+            </Field>
+          </DialogContent>
 
-                  <Field name={'startDate'}>
-                    {({ field, form: { setFieldValue }, meta: { touched, error } }: FieldProps) => (
-                      <LocalizationProvider dateAdapter={AdapterDateFns} locale={getDateFnsLocale(i18n.language)}>
-                        <DatePicker
-                          {...datePickerTranslationProps}
-                          label={t('Startdato')}
-                          onChange={(date: Date | null) => {
-                            if (date instanceof Date && !isNaN(date.getTime())) {
-                              setFieldValue(field.name, date.toISOString());
-                            }
-                          }}
-                          value={field.value ? new Date(field.value) : null}
-                          inputFormat="dd.MM.yyyy"
-                          mask="__.__.____"
-                          renderInput={(params) => (
-                            <TextField
-                              {...field}
-                              {...params}
-                              variant="filled"
-                              required
-                              error={touched && !!error}
-                              helperText={<ErrorMessage name={field.name} />}
-                            />
-                          )}
-                        />
-                      </LocalizationProvider>
-                    )}
-                  </Field>
-                </Box>
-                <Typography variant="h3" gutterBottom>
-                  Prosjektleder
-                </Typography>
-              </InputContainerBox>
-
-              <Field name={'contributors'}>
-                {({ field, form: { setFieldValue }, meta: { touched, error } }: FieldProps<ProjectContributor[]>) =>
-                  field.value.length === 0 ? (
-                    <Autocomplete
-                      options={searchByNameResults?.hits ?? []}
-                      inputMode="search"
-                      getOptionLabel={(option) =>
-                        `${getValueByKey('FirstName', option.names)} ${getValueByKey('LastName', option.names)}`
-                      }
-                      filterOptions={(options) => options}
-                      onInputChange={(_, value, reason) => {
-                        if (reason !== 'reset') {
-                          setSearchTerm(value);
-                        }
-                      }}
-                      onChange={async (_, selectedUser) => {
-                        if (!selectedUser) {
-                          if (field.value.length > 0) {
-                            setFieldValue(field.name, []);
-                          }
-                        } else {
-                          const orgId =
-                            selectedUser.affiliations.length > 0 ? selectedUser.affiliations[0].organization ?? '' : '';
-
-                          const newUser: Partial<ProjectContributor> = {
-                            type: 'ProjectManager',
-                            identity: {
-                              type: 'Person',
-                              id: selectedUser.id ?? '',
-                              firstName: getValueByKey('FirstName', selectedUser?.names),
-                              lastName: getValueByKey('LastName', selectedUser?.names),
-                            },
-                          };
-
-                          if (orgId) {
-                            const institutionResponse = await apiRequest<any>({ url: orgId }); // todo: type
-                            if (isSuccessStatus(institutionResponse.status)) {
-                              newUser.affiliation = {
-                                type: 'Organization',
-                                id: orgId,
-                                name: institutionResponse.data.unit_name,
-                              };
-                            }
-                          }
-                          setFieldValue(field.name, [newUser]);
-                          setSearchTerm('');
-                        }
-                      }}
-                      loading={isLoadingSearchByName}
-                      renderOption={(props, option) => (
-                        <li {...props} key={option.id}>
-                          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                            <Typography variant="subtitle1">
-                              {getValueByKey('FirstName', option.names)} {getValueByKey('LastName', option.names)}
-                            </Typography>
-                            {option.affiliations.length > 0 && (
-                              <Typography variant="body2" color="textSecondary">
-                                {getLanguageString(option.affiliations[0].role.labels ?? {})}
-                              </Typography>
-                            )}
-                          </Box>
-                        </li>
-                      )}
-                      renderInput={(params) => (
-                        <TextField
-                          onBlur={field.onBlur}
-                          value={field.value}
-                          name={field.name}
-                          {...params}
-                          required
-                          label={t('Cristin-person')}
-                          placeholder="Søk etter person"
-                          variant="filled"
-                          error={touched && !!error}
-                          helperText={<ErrorMessage name={field.name} />}
-                        />
-                      )}
-                    />
-                  ) : (
-                    <Box sx={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
-                      <Typography>
-                        {[
-                          `${field.value[0].identity.firstName} ${field.value[0].identity.lastName}`,
-                          getLanguageString(field.value[0].affiliation?.name),
-                        ]
-                          .filter((item) => !!item)
-                          .join(' - ')}
-                      </Typography>
-                      <Button
-                        size="small"
-                        variant="outlined"
-                        color="error"
-                        onClick={() => setFieldValue(field.name, [])}>
-                        Fjern
-                      </Button>
-                    </Box>
-                  )
-                }
-              </Field>
-            </DialogContent>
-
-            <DialogActions>
-              <Button>Avbryt</Button>
-              <Button variant="contained" type="submit">
-                Lagre
-              </Button>
-            </DialogActions>
-          </Form>
-        )}
+          <DialogActions>
+            <Button>Avbryt</Button>
+            <Button variant="contained" type="submit">
+              Lagre
+            </Button>
+          </DialogActions>
+        </Form>
       </Formik>
     </Dialog>
   );
