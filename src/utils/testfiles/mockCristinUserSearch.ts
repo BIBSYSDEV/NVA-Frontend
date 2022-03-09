f19bb0e7292e1677555d82cac688893413fcb24b
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-03-09 11:02:29 +0100

    NP-5929 Add mocking of user search

---
+++ src/utils/testfiles/mockCristinUserSearch.ts
@@ -0,0 +1,56 @@
+import { SearchResponse } from '../../types/common.types';
+import { CristinUser } from '../../types/user.types';
+import { mockOrganizationSearch } from './mockOrganizationSearch';
+
+export const mockCristinUserSearch: SearchResponse<CristinUser> = {
+  processingTime: 10,
+  size: 2,
+  hits: [
+    {
+      id: 'https://api.dev.nva.aws.unit.no/cristin/person/1',
+      identifiers: [
+        {
+          type: 'CristinIdentifier',
+          value: '1',
+        },
+      ],
+      names: [
+        {
+          type: 'LastName',
+          value: 'Etternavn',
+        },
+        {
+          type: 'FirstName',
+          value: 'Fornavn',
+        },
+      ],
+      affiliations: [],
+    },
+    {
+      id: 'https://api.dev.nva.aws.unit.no/cristin/person/2',
+      identifiers: [
+        {
+          type: 'CristinIdentifier',
+          value: '2',
+        },
+      ],
+      names: [
+        {
+          type: 'LastName',
+          value: 'Doofenshmirtz',
+        },
+        {
+          type: 'FirstName',
+          value: 'Heinz',
+        },
+      ],
+      affiliations: [
+        {
+          organization: mockOrganizationSearch.hits[0].id,
+          active: true,
+          role: { labels: { nob: 'Manager' } },
+        },
+      ],
+    },
+  ],
+};
