ca5303c2beda9405623eb2dcb4fd3625c0017f97
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-03-24 16:38:04 +0100

    NP-7222 Add roles panel

--- src/pages/basic_data/AddEmployee.tsx
+++ src/pages/basic_data/AddEmployee.tsx
@@ -1,12 +1,26 @@
-import { TextField, CircularProgress, IconButton, Typography, Box, Divider, styled, Button } from '@mui/material';
-import { useCallback, useEffect, useState } from 'react';
+import {
+  TextField,
+  CircularProgress,
+  IconButton,
+  Typography,
+  Box,
+  Divider,
+  styled,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+} from '@mui/material';
+import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 import LooksOneIcon from '@mui/icons-material/LooksOne';
 import LooksTwoIcon from '@mui/icons-material/LooksTwo';
 import LooksThreeIcon from '@mui/icons-material/Looks3';
 import SearchIcon from '@mui/icons-material/Search';
 import { useTranslation } from 'react-i18next';
 import { authenticatedApiRequest } from '../../api/apiRequest';
-import { CristinUser } from '../../types/user.types';
+import { CristinUser, RoleName } from '../../types/user.types';
 import { isSuccessStatus } from '../../utils/constants';
 import { getValueByKey } from '../../utils/user-helpers';
 import { CristinApiPath } from '../../api/apiPaths';
@@ -31,6 +45,16 @@ export const AddEmployee = () => {
     errorMessage: 'todo',
     withAuthentication: true,
   });
+
+  const [selectedRoles, setSelectedRoles] = useState<RoleName[]>([]);
+  const onChangeRoles = (event: ChangeEvent<HTMLInputElement>) => {
+    const roleName = event.target.value as RoleName;
+    if (selectedRoles.includes(roleName)) {
+      setSelectedRoles((state) => state.filter((role) => role !== roleName));
+    } else {
+      setSelectedRoles((state) => [...state, roleName]);
+    }
+  };
 
   const searchByNationalId = useCallback(async () => {
     setIsLoading(true);
@@ -118,6 +142,79 @@ export const AddEmployee = () => {
           <StyledCenterContainer>
             <LooksThreeIcon color="primary" fontSize="large" />
           </StyledCenterContainer>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">{t('profile:heading.roles')}</FormLabel>
+            <FormGroup sx={{ gap: '0.5rem' }}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={selectedRoles.includes(RoleName.CURATOR)}
+                    onChange={onChangeRoles}
+                    value={RoleName.CURATOR}
+                  />
+                }
+                label={
+                  <>
+                    <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
+                      {t('profile:roles.curator')}
+                    </Typography>
+                    <Typography>{t('profile:roles.curator_description')}</Typography>
+                  </>
+                }
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={selectedRoles.includes(RoleName.CREATOR)}
+                    onChange={onChangeRoles}
+                    value={RoleName.CREATOR}
+                  />
+                }
+                label={
+                  <>
+                    <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
+                      {t('profile:roles.creator')}
+                    </Typography>
+                    <Typography>{t('profile:roles.creator_description')}</Typography>
+                  </>
+                }
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={selectedRoles.includes(RoleName.EDITOR)}
+                    onChange={onChangeRoles}
+                    value={RoleName.EDITOR}
+                  />
+                }
+                label={
+                  <>
+                    <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
+                      {t('profile:roles.editor')}
+                    </Typography>
+                    <Typography>{t('profile:roles.editor_description')}</Typography>
+                  </>
+                }
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={selectedRoles.includes(RoleName.INSTITUTION_ADMIN)}
+                    onChange={onChangeRoles}
+                    value={RoleName.INSTITUTION_ADMIN}
+                  />
+                }
+                label={
+                  <>
+                    <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
+                      {t('profile:roles.institution_admin')}
+                    </Typography>
+                    <Typography>{t('profile:roles.institution_admin_description')}</Typography>
+                  </>
+                }
+              />
+            </FormGroup>
+          </FormControl>
         </Box>
       </Box>
       <StyledCenterContainer>
