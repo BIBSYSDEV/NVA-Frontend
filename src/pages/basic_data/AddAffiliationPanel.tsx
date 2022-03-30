0b1fd0ff425724c25eef1bce74550d55e6d3412f
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-03-30 10:55:56 +0200

    NP-7222 Add formik fields

--- src/pages/basic_data/AddAffiliationPanel.tsx
+++ src/pages/basic_data/AddAffiliationPanel.tsx
@@ -1,25 +1,62 @@
-import { Box, TextField } from '@mui/material';
+import { Box, Button, Dialog, DialogContent, DialogTitle, TextField } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import LooksTwoIcon from '@mui/icons-material/LooksTwo';
+import { useState } from 'react';
+import { Field, FieldProps } from 'formik';
 import { SelectInstitutionForm } from '../../components/institution/SelectInstitutionForm';
 import { StyledCenterContainer } from '../../components/styled/Wrappers';
 
 export const AddAffiliationPanel = () => {
   const { t } = useTranslation('basicData');
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <StyledCenterContainer>
         <LooksTwoIcon color="primary" fontSize="large" />
       </StyledCenterContainer>
-      <SelectInstitutionForm onSubmit={(id) => console.log(id)} />
-
+      <Field name="affiliation.organization">
+        {({ field, form }: FieldProps<string>) => (
+          <>
+            <Button variant="outlined" onClick={() => setOpen(true)}>
+              Velg institusjon
+            </Button>
+            <Dialog open={open} fullWidth onClose={() => setOpen(false)}>
+              <DialogTitle>Velg institusjon</DialogTitle>
+              <DialogContent>
+                <SelectInstitutionForm
+                  onSubmit={(id) => {
+                    console.log(id);
+                    form.setFieldValue(field.name, id);
+                    setOpen(false);
+                  }}
+                />
+              </DialogContent>
+            </Dialog>
+          </>
+        )}
+      </Field>
       <Box display={{ display: 'flex', gap: '1rem' }}>
-        <TextField fullWidth variant="filled" label={t('position')} />
-        <TextField fullWidth variant="filled" label={t('position_percent')} />
+        <Field name="affiliation.position">
+          {({ field }: FieldProps<string>) => <TextField {...field} fullWidth variant="filled" label={t('position')} />}
+        </Field>
+        <Field name="affiliation.percent">
+          {({ field }: FieldProps<string>) => (
+            <TextField {...field} fullWidth variant="filled" label={t('position_percent')} />
+          )}
+        </Field>
       </Box>
       <Box display={{ display: 'flex', gap: '1rem' }}>
-        <TextField fullWidth variant="filled" label={t('common:start_date')} />
-        <TextField fullWidth variant="filled" label={t('common:end_date')} />
+        <Field name="affiliation.startDate">
+          {({ field }: FieldProps<string>) => (
+            <TextField {...field} fullWidth variant="filled" label={t('common:start_date')} />
+          )}
+        </Field>
+        <Field name="affiliation.endDate">
+          {({ field }: FieldProps<string>) => (
+            <TextField {...field} fullWidth variant="filled" label={t('common:end_date')} />
+          )}
+        </Field>
       </Box>
     </>
   );
