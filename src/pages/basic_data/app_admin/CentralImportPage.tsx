8fee57c520bb214190c0f6a44ceac8cc5707d1ba
PCB <46489468+pcb-unit@users.noreply.github.com>
2022-04-21 12:14:37 +0200

    Smile 1522 implement central import wireframe (#2726)
    * test
    
    * SMILE-1522

--- src/pages/basic_data/app_admin/CentralImportPage.tsx
+++ src/pages/basic_data/app_admin/CentralImportPage.tsx
@@ -1,7 +1,38 @@
-import { Typography } from '@mui/material';
+import { List, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { ResultItem } from './CentralImportResultItem';
+
+const mockPublication = {
+  title: 'The Title',
+  description: 'The Description',
+  type: 'Article',
+  doi: 'https://doi.org/10.1038/s41467-021-25342423',
+  contributors: ['Peder Botten', 'Gregor Garbrielsen', 'Oddny Osteloff'],
+  institution: 'The Institution',
+  confirmedContributors: '0 of 2',
+};
+
+const mockPublicationList = [
+  mockPublication,
+  mockPublication,
+  mockPublication,
+  mockPublication,
+  mockPublication,
+  mockPublication,
+];
 
 export const CentralImportPage = () => {
   const { t } = useTranslation('basicData');
-  return <Typography variant="h2">{t('central_import')}</Typography>;
+  return (
+    <>
+      <Typography variant="h3" component="h2" paragraph>
+        {t('publications')}
+      </Typography>
+      <List>
+        {mockPublicationList.map((publication, index) => (
+          <ResultItem publication={publication} key={index} />
+        ))}
+      </List>
+    </>
+  );
 };
