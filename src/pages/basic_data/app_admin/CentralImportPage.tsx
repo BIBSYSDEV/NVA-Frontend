93c87fe5c9f441780ec97a602fe03f85f549d5e1
pcb <pcb@unit.no>
2022-04-21 09:46:30 +0200

    SMILE-1522

--- src/pages/basic_data/app_admin/CentralImportPage.tsx
+++ src/pages/basic_data/app_admin/CentralImportPage.tsx
@@ -1,5 +1,6 @@
-import { Divider, Grid, Link, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { List, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { ResultItem } from './CentralImportResultItem';
 
 const mockPublication = {
   title: 'The Title',
@@ -20,55 +21,12 @@ const mockPublicationList = [
   mockPublication,
 ];
 
-interface ResultItemProps {
-  publication: any;
-}
-
-const ResultItem = ({ publication }: ResultItemProps) => {
-  return (
-    <>
-      <ListItem>
-        <ListItemText disableTypography>
-          <Grid container spacing={2} justifyContent="space-between" alignItems="baseline">
-            <Grid item md={2} xs={12}>
-              <Typography variant="body1">{publication.type}</Typography>
-            </Grid>
-            <Grid item md={5} xs={12}>
-              <Link underline="hover" href={publication.doi} target="_blank" rel="noopener noreferrer">
-                <Typography variant="body2" sx={{ color: 'primary.dark' }}>
-                  {publication.doi}
-                </Typography>
-              </Link>
-              <Typography gutterBottom sx={{ fontSize: '1rem', fontWeight: '600', fontStyle: 'italic' }}>
-                {publication.title}
-              </Typography>
-              {publication.contributors.map((contributor: any, index: number) => (
-                <Typography display="inline" variant="body2" key={index}>
-                  {contributor}
-                  {'; '}
-                </Typography>
-              ))}
-            </Grid>
-            <Grid item md={2} xs={12}>
-              <Typography variant="body1">({publication.confirmedContributors})</Typography>
-            </Grid>
-            <Grid item md={3} xs={12}>
-              <Typography variant="body1">{publication.institution}</Typography>
-            </Grid>
-          </Grid>
-        </ListItemText>
-      </ListItem>
-      <Divider sx={{ my: '1rem', borderWidth: 1 }} />
-    </>
-  );
-};
-
 export const CentralImportPage = () => {
   const { t } = useTranslation('basicData');
   return (
     <>
       <Typography variant="h3" component="h2" paragraph>
-        {t('Publikasjoner')}
+        {t('publications')}
       </Typography>
       <List>
         {mockPublicationList.map((publication, index) => (
