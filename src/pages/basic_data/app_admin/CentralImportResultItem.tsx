26e6819377d57320f65f3e05d55cbaa069d71d26
pcb <pcb@unit.no>
2022-04-26 11:27:54 +0200

    SMILE-1523-fetch-import-publications - code review

--- src/pages/basic_data/app_admin/CentralImportResultItem.tsx
+++ src/pages/basic_data/app_admin/CentralImportResultItem.tsx
@@ -1,4 +1,4 @@
-import { Divider, Grid, Link, ListItem, ListItemText, Typography } from '@mui/material';
+import { Grid, Link, ListItem, ListItemText, Typography } from '@mui/material';
 import { Registration } from '../../../types/registration.types';
 import { useTranslation } from 'react-i18next';
 import { getLanguageString } from '../../../utils/translation-helpers';
@@ -10,64 +10,57 @@ interface CentralImportResultItemProps {
 export const CentralImportResultItem = ({ publication }: CentralImportResultItemProps) => {
   const { t } = useTranslation('publicationTypes');
 
-  const verifiedContributorCount =
-    publication.entityDescription?.contributors.filter((contributor) => !!contributor.identity.id).length ?? 0;
-  const contributorsCount = publication.entityDescription?.contributors.length ?? 0;
+  const contributors = publication.entityDescription?.contributors ?? [];
+  const verifiedContributorCount = contributors.filter((contributor) => !!contributor.identity.id).length;
+  const contributorsCount = contributors.length;
 
   const allContributorInstitutions = publication.entityDescription?.contributors.map((contributor) =>
     contributor.affiliations?.map((affiliation) => affiliation.labels && getLanguageString(affiliation.labels))
   );
   const institutions = new Set(allContributorInstitutions?.flat() ?? []);
 
+  const publicationInstanceType = publication.entityDescription?.reference?.publicationInstance.type ?? '';
+
   return (
-    <>
-      <ListItem>
-        <ListItemText disableTypography>
-          <Grid container spacing={2} justifyContent="space-between" alignItems="baseline">
-            <Grid item md={2} xs={12}>
-              {publication.entityDescription?.reference?.publicationInstance.type && (
-                <Typography variant="body1">
-                  {t(publication.entityDescription?.reference?.publicationInstance.type ?? '')}
-                </Typography>
-              )}
-            </Grid>
-            <Grid item md={5} xs={12}>
-              {publication.entityDescription?.reference?.doi && (
-                <Link
-                  underline="hover"
-                  href={publication.entityDescription.reference.doi}
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <Typography gutterBottom variant="body2" sx={{ color: 'primary.dark', wordBreak: 'break-word' }}>
-                    {publication.entityDescription.reference.doi}
-                  </Typography>
-                </Link>
-              )}
-              {publication.entityDescription?.mainTitle && (
-                <Typography gutterBottom sx={{ fontSize: '1rem', fontWeight: '600', fontStyle: 'italic' }}>
-                  {publication.entityDescription.mainTitle}
-                </Typography>
-              )}
-              {publication.entityDescription?.contributors && (
-                <Typography display="inline" variant="body2">
-                  {publication.entityDescription.contributors
-                    .map((contributor) => contributor.identity.name)
-                    .join('; ')}
-                </Typography>
-              )}
-            </Grid>
-            <Grid item md={2} xs={12}>
-              <Typography variant="body1">
-                {t('basicData:verifiedContributorCount', { verifiedContributorCount, contributorsCount })}
-              </Typography>
-            </Grid>
-            <Grid item md={3} xs={12}>
-              {institutions && <Typography variant="body1">{Array.from(institutions).join(', ')}</Typography>}
-            </Grid>
+    <ListItem divider>
+      <ListItemText disableTypography>
+        <Grid container spacing={2} justifyContent="space-between" alignItems="baseline">
+          <Grid item md={2} xs={12}>
+            {publicationInstanceType && <Typography variant="body1">{t(publicationInstanceType)}</Typography>}
           </Grid>
-        </ListItemText>
-      </ListItem>
-      <Divider sx={{ my: '1rem', borderWidth: 1 }} />
-    </>
+          <Grid item md={5} xs={12}>
+            {publication.entityDescription?.reference?.doi && (
+              <Link
+                underline="hover"
+                href={publication.entityDescription.reference.doi}
+                target="_blank"
+                rel="noopener noreferrer">
+                <Typography gutterBottom variant="body2" sx={{ color: 'primary.dark', wordBreak: 'break-word' }}>
+                  {publication.entityDescription.reference.doi}
+                </Typography>
+              </Link>
+            )}
+            {publication.entityDescription?.mainTitle && (
+              <Typography gutterBottom sx={{ fontSize: '1rem', fontWeight: '600', fontStyle: 'italic' }}>
+                {publication.entityDescription.mainTitle}
+              </Typography>
+            )}
+            {publication.entityDescription?.contributors && (
+              <Typography display="inline" variant="body2">
+                {publication.entityDescription.contributors.map((contributor) => contributor.identity.name).join('; ')}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item md={2} xs={12}>
+            <Typography variant="body1">
+              {t('basicData:verifiedContributorCount', { verifiedContributorCount, contributorsCount })}
+            </Typography>
+          </Grid>
+          <Grid item md={3} xs={12}>
+            {institutions && <Typography variant="body1">{Array.from(institutions).join(', ')}</Typography>}
+          </Grid>
+        </Grid>
+      </ListItemText>
+    </ListItem>
   );
 };
