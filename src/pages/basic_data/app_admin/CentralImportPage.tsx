4c3e5e56d0367ae9c8e2c67509ce8526d0eb0e49
pcb <pcb@unit.no>
2022-04-27 15:20:38 +0200

    SMILE-1536-pagination

--- src/pages/basic_data/app_admin/CentralImportPage.tsx
+++ src/pages/basic_data/app_admin/CentralImportPage.tsx
@@ -1,4 +1,4 @@
-import { List, Typography } from '@mui/material';
+import { Divider, List, TablePagination, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useFetch } from '../../../utils/hooks/useFetch';
 import { Registration } from '../../../types/registration.types';
@@ -6,29 +6,75 @@ import { SearchApiPath } from '../../../api/apiPaths';
 import { SearchResponse } from '../../../types/common.types';
 import { ListSkeleton } from '../../../components/ListSkeleton';
 import { CentralImportResultItem } from './CentralImportResultItem';
+import { dataTestId } from '../../../utils/dataTestIds';
+import { ROWS_PER_PAGE_OPTIONS } from '../../../utils/constants';
+import { SearchParam } from '../../../utils/searchHelpers';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { stringIncludesMathJax, typesetMathJax } from '../../../utils/mathJaxHelpers';
 
 export const CentralImportPage = () => {
   const { t } = useTranslation('basicData');
+  const history = useHistory();
+  const params = new URLSearchParams(history.location.search);
+  const resultsParam = params.get(SearchParam.Results);
+  const fromParam = params.get(SearchParam.From);
+  const rowsPerPage = (resultsParam && +resultsParam) || ROWS_PER_PAGE_OPTIONS[1];
+  const page = (fromParam && resultsParam && Math.floor(+fromParam / rowsPerPage)) || 0;
 
-  const [results, isLoadingResults] = useFetch<SearchResponse<Registration>>({
-    url: SearchApiPath.Registrations,
+  const [searchResults, isLoadingSearchResults] = useFetch<SearchResponse<Registration>>({
+    url: `${SearchApiPath.Registrations}?${params.toString()}`,
     errorMessage: t('feedback:error.search'),
   });
-  const publications = results?.hits ?? [];
+
+  const updatePath = (from: string, results: string) => {
+    params.set(SearchParam.From, from);
+    params.set(SearchParam.Results, results);
+    history.push({ search: params.toString() });
+  };
+
+  const publications = searchResults?.hits ?? [];
+
+  useEffect(() => {
+    if (
+      searchResults?.hits.some(
+        ({ entityDescription }) =>
+          stringIncludesMathJax(entityDescription?.mainTitle) || stringIncludesMathJax(entityDescription?.abstract)
+      )
+    ) {
+      typesetMathJax();
+    }
+  }, [searchResults]);
+
   return (
     <>
       <Typography variant="h3" component="h2" paragraph>
         {t('publications')}
       </Typography>
-      {isLoadingResults ? (
+      {isLoadingSearchResults && !searchResults ? (
         <ListSkeleton minWidth={100} maxWidth={100} height={100} />
       ) : (
+        searchResults &&
         publications.length > 0 && (
-          <List>
-            {publications.map((publication) => (
-              <CentralImportResultItem publication={publication} key={publication.identifier} />
-            ))}
-          </List>
+          <>
+            <Typography variant="subtitle1">{t('search:hits', { count: searchResults.size })}:</Typography>
+            <Divider />
+            <List>
+              {publications.map((publication, index) => (
+                <CentralImportResultItem publication={publication} key={publication.identifier + index} />
+              ))}
+            </List>
+            <TablePagination
+              data-testid={dataTestId.basicData.centralImport.searchPagination}
+              rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
+              component="div"
+              count={searchResults.size}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={(_, newPage) => updatePath((newPage * rowsPerPage).toString(), rowsPerPage.toString())}
+              onRowsPerPageChange={(event) => updatePath('0', event.target.value)}
+            />
+          </>
         )
       )}
     </>
