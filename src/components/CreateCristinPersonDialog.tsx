736e3598df96e1ebba397e2131bb7f6cc137773e
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-05-10 14:17:25 +0200

    NP-9082 Add confirm dialog and update translations

--- src/components/CreateCristinPersonDialog.tsx
+++ src/components/CreateCristinPersonDialog.tsx
@@ -23,16 +23,18 @@ import { isErrorStatus, isSuccessStatus } from '../utils/constants';
 import { useAuthentication } from '../utils/hooks/useAuthentication';
 import { convertToCristinUser } from '../utils/user-helpers';
 import { userValidationSchema } from '../utils/validation/basic_data/addEmployeeValidation';
+import { ConfirmDialog } from './ConfirmDialog';
 
 interface CreateCristinPersonDialogProps {
   user: User;
 }
 
 export const CreateCristinPersonDialog = ({ user }: CreateCristinPersonDialogProps) => {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation('authorization');
   const dispatch = useDispatch();
   const { handleLogout } = useAuthentication();
   const [acceptedTermsValue, setAcceptedTermsValue] = useState(false);
+  const [showConfirmCancelDialog, setShowConfirmCancelDialog] = useState(false);
 
   const createPerson = async (values: FlatCristinUser) => {
     const cristinUser: CreateCristinUser = convertToCristinUser(values);
@@ -48,7 +50,7 @@ export const CreateCristinPersonDialog = ({ user }: CreateCristinPersonDialogPro
 
   return (
     <Dialog open={true} fullWidth maxWidth="xs">
-      <DialogTitle>{t('authorization:your_user_profile')}</DialogTitle>
+      <DialogTitle>{t('your_user_profile')}</DialogTitle>
       <Formik
         initialValues={{
           ...emptyUser,
@@ -61,13 +63,13 @@ export const CreateCristinPersonDialog = ({ user }: CreateCristinPersonDialogPro
         {({ isSubmitting }: FormikProps<FlatCristinUser>) => (
           <Form noValidate>
             <DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-              <Typography>{t('authorization:create_user_info')}</Typography>
+              <Typography>{t('create_user_info')}</Typography>
               <Field name="firstName">
                 {({ field, meta: { error, touched } }: FieldProps<string>) => (
                   <TextField
                     {...field}
                     variant="filled"
-                    label={t('first_name')}
+                    label={t('common:first_name')}
                     required
                     error={!!error && touched}
                     helperText={<ErrorMessage name={field.name} />}
@@ -79,7 +81,7 @@ export const CreateCristinPersonDialog = ({ user }: CreateCristinPersonDialogPro
                   <TextField
                     {...field}
                     variant="filled"
-                    label={t('last_name')}
+                    label={t('common:last_name')}
                     required
                     error={!!error && touched}
                     helperText={<ErrorMessage name={field.name} />}
@@ -94,7 +96,7 @@ export const CreateCristinPersonDialog = ({ user }: CreateCristinPersonDialogPro
                 value={user.nationalIdNumber}
               />
               <FormControlLabel
-                label={t('authorization:accept_terms_to_create_user')}
+                label={t('accept_terms_to_create_user')}
                 control={
                   <Checkbox
                     required
@@ -105,7 +107,7 @@ export const CreateCristinPersonDialog = ({ user }: CreateCristinPersonDialogPro
               />
             </DialogContent>
             <DialogActions>
-              <Button onClick={() => handleLogout()}>{t('common:cancel')}</Button>
+              <Button onClick={() => setShowConfirmCancelDialog(true)}>{t('common:cancel')}</Button>
               <LoadingButton type="submit" variant="contained" disabled={!acceptedTermsValue} loading={isSubmitting}>
                 {t('common:create')}
               </LoadingButton>
@@ -113,6 +115,13 @@ export const CreateCristinPersonDialog = ({ user }: CreateCristinPersonDialogPro
           </Form>
         )}
       </Formik>
+      <ConfirmDialog
+        open={showConfirmCancelDialog}
+        title={t('cancel_create_user_title')}
+        onAccept={() => handleLogout()}
+        onCancel={() => setShowConfirmCancelDialog(false)}>
+        <Typography>{t('cancel_create_user_text')}</Typography>
+      </ConfirmDialog>
     </Dialog>
   );
 };
