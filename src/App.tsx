20f867fb74a48d989a396a8351d5474d8f93b2b8
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-05-11 13:23:53 +0200

    Allow relative urls in redux

--- src/App.tsx
+++ src/App.tsx
@@ -22,12 +22,11 @@ import { mockUser } from './utils/testfiles/mock_feide_user';
 import { PageSpinner } from './components/PageSpinner';
 import { SkipLink } from './components/SkipLink';
 import { useFetch } from './utils/hooks/useFetch';
-import { CristinApiPath, RoleApiPath } from './api/apiPaths';
-import { InstitutionUser, PositionResponse } from './types/user.types';
+import { RoleApiPath } from './api/apiPaths';
+import { InstitutionUser } from './types/user.types';
 import { UrlPathTemplate } from './utils/urlPaths';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { SelectCustomerInstitutionDialog } from './components/SelectCustomerInstitutionDialog';
-import { useFetchResource } from './utils/hooks/useFetchResource';
 
 const getLanguageTagValue = (language: string) => {
   if (language === 'eng') {
@@ -53,7 +52,6 @@ export const App = () => {
     errorMessage: t('feedback:error.get_roles'),
     withAuthentication: true,
   });
-  useFetchResource<PositionResponse>(CristinApiPath.Position);
 
   useEffect(() => {
     // Setup aws-amplify
