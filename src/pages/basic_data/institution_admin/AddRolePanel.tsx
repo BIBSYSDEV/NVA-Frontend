4dc3bf22e704d7605f3c3a1ebbd28a30d9b363e8
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-05-12 11:07:39 +0200

    NP-7222 Add roles when creating new employees

--- src/pages/basic_data/institution_admin/AddRolePanel.tsx
+++ src/pages/basic_data/institution_admin/AddRolePanel.tsx
@@ -5,91 +5,87 @@ import { useTranslation } from 'react-i18next';
 import { RoleName } from '../../../types/user.types';
 import { StyledCenterContainer } from '../../../components/styled/Wrappers';
 import { FieldArray, FieldArrayRenderProps } from 'formik';
-import { BetaFunctionality } from '../../../components/BetaFunctionality';
 
 export const AddRolePanel = () => {
   const { t } = useTranslation('basicData');
 
   return (
-    <BetaFunctionality>
-      <FieldArray name="roles">
-        {({
-          push,
-          remove,
-          form: {
-            values: { roles },
-          },
-        }: FieldArrayRenderProps) => (
-          <>
-            <StyledCenterContainer>
-              <LooksThreeIcon color="primary" fontSize="large" />
-            </StyledCenterContainer>
-            <FormControl
-              disabled
-              component="fieldset"
-              onChange={(event: ChangeEvent<any>) => {
-                const role = event.target.value as RoleName;
-                const index = roles.indexOf(role);
-                if (index > -1) {
-                  remove(index);
-                } else {
-                  push(role);
+    <FieldArray name="roles">
+      {({
+        push,
+        remove,
+        form: {
+          values: { roles },
+        },
+      }: FieldArrayRenderProps) => (
+        <>
+          <StyledCenterContainer>
+            <LooksThreeIcon color="primary" fontSize="large" />
+          </StyledCenterContainer>
+          <FormControl
+            component="fieldset"
+            onChange={(event: ChangeEvent<any>) => {
+              const role = event.target.value as RoleName;
+              const index = roles.indexOf(role);
+              if (index > -1) {
+                remove(index);
+              } else {
+                push(role);
+              }
+            }}>
+            <FormLabel component="legend">{t('profile:heading.roles')}</FormLabel>
+            <FormGroup sx={{ gap: '0.5rem' }}>
+              <FormControlLabel
+                control={<Checkbox checked={roles.includes(RoleName.Curator)} value={RoleName.Curator} />}
+                label={
+                  <>
+                    <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
+                      {t('profile:roles.curator')}
+                    </Typography>
+                    <Typography>{t('profile:roles.curator_description')}</Typography>
+                  </>
                 }
-              }}>
-              <FormLabel component="legend">{t('profile:heading.roles')}</FormLabel>
-              <FormGroup sx={{ gap: '0.5rem' }}>
-                <FormControlLabel
-                  control={<Checkbox checked={roles.includes(RoleName.Curator)} value={RoleName.Curator} />}
-                  label={
-                    <>
-                      <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
-                        {t('profile:roles.curator')}
-                      </Typography>
-                      <Typography>{t('profile:roles.curator_description')}</Typography>
-                    </>
-                  }
-                />
-                <FormControlLabel
-                  disabled
-                  control={<Checkbox checked={roles.includes(RoleName.Creator)} value={RoleName.Creator} />}
-                  label={
-                    <>
-                      <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
-                        {t('profile:roles.creator')}
-                      </Typography>
-                      <Typography>{t('profile:roles.creator_description')}</Typography>
-                    </>
-                  }
-                />
-                <FormControlLabel
-                  control={<Checkbox checked={roles.includes(RoleName.Editor)} value={RoleName.Editor} />}
-                  label={
-                    <>
-                      <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
-                        {t('profile:roles.editor')}
-                      </Typography>
-                      <Typography>{t('profile:roles.editor_description')}</Typography>
-                    </>
-                  }
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox checked={roles.includes(RoleName.InstitutionAdmin)} value={RoleName.InstitutionAdmin} />
-                  }
-                  label={
-                    <>
-                      <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
-                        {t('profile:roles.institution_admin')}
-                      </Typography>
-                      <Typography>{t('profile:roles.institution_admin_description')}</Typography>
-                    </>
-                  }
-                />
-              </FormGroup>
-            </FormControl>
-          </>
-        )}
-      </FieldArray>
-    </BetaFunctionality>
+              />
+              <FormControlLabel
+                disabled
+                control={<Checkbox checked={roles.includes(RoleName.Creator)} value={RoleName.Creator} />}
+                label={
+                  <>
+                    <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
+                      {t('profile:roles.creator')}
+                    </Typography>
+                    <Typography>{t('profile:roles.creator_description')}</Typography>
+                  </>
+                }
+              />
+              <FormControlLabel
+                control={<Checkbox checked={roles.includes(RoleName.Editor)} value={RoleName.Editor} />}
+                label={
+                  <>
+                    <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
+                      {t('profile:roles.editor')}
+                    </Typography>
+                    <Typography>{t('profile:roles.editor_description')}</Typography>
+                  </>
+                }
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={roles.includes(RoleName.InstitutionAdmin)} value={RoleName.InstitutionAdmin} />
+                }
+                label={
+                  <>
+                    <Typography variant="overline" sx={{ fontSize: '0.9rem' }}>
+                      {t('profile:roles.institution_admin')}
+                    </Typography>
+                    <Typography>{t('profile:roles.institution_admin_description')}</Typography>
+                  </>
+                }
+              />
+            </FormGroup>
+          </FormControl>
+        </>
+      )}
+    </FieldArray>
   );
 };
