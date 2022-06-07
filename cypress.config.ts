c18a4bfb3acdd1a0783d2ec160cb8d387d4b3b55
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-06-07 12:43:53 +0200

    Minor improvements

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
+import cypressPlugin from './cypress/plugins/index.js';
 
 export default defineConfig({
   projectId: '1rrqbw',
   video: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return cypressPlugin(on, config);
     },
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
-})
+});
