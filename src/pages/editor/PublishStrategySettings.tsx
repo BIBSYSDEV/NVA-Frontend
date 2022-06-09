ea79142d81468c6e67ecda35aec1933bf326a42b
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-06-09 15:54:28 +0200

    NP-9155 Update publish strategies

--- src/pages/editor/PublishStrategySettings.tsx
+++ src/pages/editor/PublishStrategySettings.tsx
@@ -1,12 +1,14 @@
 import { Box, ButtonBase, styled, Typography } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { useState } from 'react';
-
-enum WorkflowValue {
-  RegistratorPublishesMetadataOnly,
-  RegistratorPublishesMetadataAndFiles,
-  RegistratorCannotPublish,
-}
+import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { RootState } from '../../redux/store';
+import { CustomerInstitution, PublicationWorkflowType } from '../../types/customerInstitution.types';
+import { useFetch } from '../../utils/hooks/useFetch';
+import { updateCustomerInstitution } from '../../api/customerInstitutionsApi';
+import { isErrorStatus, isSuccessStatus } from '../../utils/constants';
+import { setNotification } from '../../redux/notificationSlice';
 
 const StyledAccessRight = styled('div')({
   display: 'flex',
@@ -26,20 +28,43 @@ interface PublishStrategySettingsProps {
 const WorkflowButton = styled(ButtonBase, { shouldForwardProp: (prop) => prop !== 'isSelected' })(
   ({ isSelected }: PublishStrategySettingsProps) => ({
     padding: '0.5rem',
-    border: '1px solid',
+    border: isSelected ? '2px solid' : '1px solid',
     borderRadius: '0.5rem',
     opacity: isSelected ? 1 : 0.5,
   })
 );
 
 export const PublishStrategySettings = () => {
-  const [value, setValue] = useState(WorkflowValue.RegistratorPublishesMetadataOnly);
+  const dispatch = useDispatch();
+  const { t } = useTranslation('editor');
+  const user = useSelector((store: RootState) => store.user);
+  const [customer, isLoadingCustomer] = useFetch<CustomerInstitution>({ url: user?.customerId ?? '' });
+  const [isUpdating, setIsUpdating] = useState(false);
+
+  const setPublicationWorkflow = async (workflow: PublicationWorkflowType) => {
+    if (customer) {
+      setIsUpdating(true);
+      const updateCustomerResponse = await updateCustomerInstitution({
+        ...customer,
+        publicationWorkflow: workflow,
+      });
+      if (isErrorStatus(updateCustomerResponse.status)) {
+        dispatch(setNotification({ message: t('feedback:error.update_publish_strategy'), variant: 'error' }));
+      } else if (isSuccessStatus(updateCustomerResponse.status)) {
+        dispatch(setNotification({ message: t('feedback:success.update_publish_strategy'), variant: 'success' }));
+      }
+      setIsUpdating(false);
+    }
+  };
+
+  const isLoading = isLoadingCustomer || isUpdating;
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', maxWidth: '35rem' }}>
       <WorkflowButton
-        isSelected={value === WorkflowValue.RegistratorPublishesMetadataAndFiles}
-        onClick={() => setValue(WorkflowValue.RegistratorPublishesMetadataAndFiles)}>
+        disabled={isLoading}
+        isSelected={!isLoading && customer?.publicationWorkflow === 'RegistratorPublishesMetadataAndFiles'}
+        onClick={() => setPublicationWorkflow('RegistratorPublishesMetadataAndFiles')}>
         <Box>
           <Typography sx={{ fontWeight: 700, mb: '0.5rem', textAlign: 'center' }}>
             Registrator publiserer uten godkjenning fra Kurator
@@ -59,8 +84,9 @@ export const PublishStrategySettings = () => {
       </WorkflowButton>
 
       <WorkflowButton
-        isSelected={value === WorkflowValue.RegistratorPublishesMetadataOnly}
-        onClick={() => setValue(WorkflowValue.RegistratorPublishesMetadataOnly)}>
+        disabled={isLoading}
+        isSelected={!isLoading && customer?.publicationWorkflow === 'RegistratorPublishesMetadataOnly'}
+        onClick={() => setPublicationWorkflow('RegistratorPublishesMetadataOnly')}>
         <Box>
           <Typography sx={{ fontWeight: 700, mb: '0.5rem', textAlign: 'center' }}>
             Registrator publiserer uten godkjenning fra Kurator
@@ -76,8 +102,9 @@ export const PublishStrategySettings = () => {
       </WorkflowButton>
 
       <WorkflowButton
-        isSelected={value === WorkflowValue.RegistratorCannotPublish}
-        onClick={() => setValue(WorkflowValue.RegistratorCannotPublish)}>
+        disabled={isLoading}
+        isSelected={!isLoading && customer?.publicationWorkflow === 'RegistratorCannotPublish'}
+        onClick={() => setPublicationWorkflow('RegistratorCannotPublish')}>
         <Box>
           <Typography sx={{ fontWeight: 700, mb: '0.5rem', textAlign: 'center' }}>
             Registrator kan kun registrere
