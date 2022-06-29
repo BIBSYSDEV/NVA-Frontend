fe8703b43fb77e2927e7f2e036ba9a04af21fe96
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-06-29 15:42:59 +0200

    NP-9253 Minor changes for styling

--- src/components/PageWithSideMenu.tsx
+++ src/components/PageWithSideMenu.tsx
@@ -1,41 +1,33 @@
 import { Box, Button, ButtonProps, styled } from '@mui/material';
 import { PropsWithChildren } from 'react';
 import { Link, LinkProps } from 'react-router-dom';
-import { BackgroundDiv } from './styled/Wrappers';
 
 export const StyledPageWithSideMenu = styled(Box)(({ theme }) => ({
   width: '100%',
   minHeight: '40vh',
   display: 'grid',
   gap: '1rem',
-
   padding: '1rem',
-  [theme.breakpoints.down('sm')]: {
-    padding: 0,
-  },
 
   gridTemplateColumns: '1fr 4fr',
   [theme.breakpoints.down('md')]: {
+    padding: 0,
     gridTemplateColumns: '1fr',
   },
 }));
 
-export const StyledSideMenuHeader = styled(Box)(({ theme }) => ({
+export const StyledSideMenuHeader = styled(Box)({
   display: 'flex',
   alignItems: 'center',
   gap: '0.5rem',
   background: '#e3e0dd',
   padding: '0.5rem 1rem 0.5rem 1rem',
-
-  [theme.breakpoints.up('sm')]: {
-    margin: '-0.75rem -0.75rem 1rem -0.75rem',
-  },
-}));
+});
 
 export const SideMenu = ({ children }: PropsWithChildren<Record<never, never>>) => (
-  <BackgroundDiv component="nav" sx={{ p: '1rem' }}>
+  <Box component="nav" sx={{ bgcolor: 'background.paper' }}>
     {children}
-  </BackgroundDiv>
+  </Box>
 );
 
 export const NavigationList = ({ children }: PropsWithChildren<Record<never, never>>) => (
@@ -43,7 +35,7 @@ export const NavigationList = ({ children }: PropsWithChildren<Record<never, nev
     component="ul"
     sx={{
       listStyle: 'none',
-      p: 0,
+      px: '1rem',
       display: 'flex',
       flexDirection: 'column',
       gap: '1rem',
