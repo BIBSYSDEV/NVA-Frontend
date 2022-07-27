55804103535aad40060205878d588376d96cc7f5
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-07-27 11:19:43 +0200

    NP-9282 Update import

--- src/translations/i18n.ts
+++ src/translations/i18n.ts
@@ -1,15 +1,15 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import engTranslations from './engTranslations.json';
-import nobTranslations from './nobTranslations.json';
+import enTranslations from './enTranslations.json';
+import nbTranslations from './nbTranslations.json';
 
 i18n.use(LanguageDetector).init({
   resources: {
     eng: {
-      translation: engTranslations,
+      translation: enTranslations,
     },
     nob: {
-      translation: nobTranslations,
+      translation: nbTranslations,
     },
   },
   fallbackLng: 'nob',
