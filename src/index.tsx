db80e2aaa10bd1869bf03fc2d0200e4cb5226f8d
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-08-04 15:18:07 +0200

    a11y warnings in console

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { StrictMode } from 'react';
+import axe from '@axe-core/react';
+// import { StrictMode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { Provider as ReduxProvider } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
@@ -20,6 +22,13 @@ import '@fontsource/barlow/600.css';
 import '@fontsource/barlow/700.css';
 import '@fontsource/crimson-text/400.css';
 
+if (process.env.NODE_ENV !== 'production') {
+  // import('@axe-core/react').then((axe: any) => {
+  //   axe(React, ReactDOM, 1000); // WHY U NO WORK???111!!+?
+  // });
+  axe(React, ReactDOM, 1000);
+}
+
 if (USE_MOCK_DATA) {
   interceptRequestsOnMock();
 }
@@ -30,19 +39,19 @@ if ((window as any).Cypress) {
 }
 
 ReactDOM.render(
-  <StrictMode>
-    <BasicErrorBoundary>
-      <I18nextProvider i18n={i18n}>
-        <ReduxProvider store={store}>
-          <ThemeProvider theme={mainTheme}>
-            <CssBaseline />
-            <HelmetProvider>
-              <App />
-            </HelmetProvider>
-          </ThemeProvider>
-        </ReduxProvider>
-      </I18nextProvider>
-    </BasicErrorBoundary>
-  </StrictMode>,
+  // <StrictMode>
+  <BasicErrorBoundary>
+    <I18nextProvider i18n={i18n}>
+      <ReduxProvider store={store}>
+        <ThemeProvider theme={mainTheme}>
+          <CssBaseline />
+          <HelmetProvider>
+            <App />
+          </HelmetProvider>
+        </ThemeProvider>
+      </ReduxProvider>
+    </I18nextProvider>
+  </BasicErrorBoundary>,
+  //  </StrictMode>
   document.getElementById('root')
 );
