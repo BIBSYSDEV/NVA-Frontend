0891d11d2f5f641ee39cacf6a4e45e175b3e0e7c
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-08-11 13:29:59 +0200

    NP-9853 Show warning on contributors panel if user has not selected resource type yet

--- src/pages/registration/ContributorsPanel.tsx
+++ src/pages/registration/ContributorsPanel.tsx
@@ -1,48 +1,51 @@
-import { FormHelperText } from '@mui/material';
+import { Box, FormHelperText, Typography } from '@mui/material';
 import { ErrorMessage, FieldArray, FieldArrayRenderProps, FormikErrors, FormikTouched, useFormikContext } from 'formik';
-import { useEffect, useRef } from 'react';
-import { ContributorRole } from '../../types/contributor.types';
+import { useTranslation } from 'react-i18next';
+import WarningIcon from '@mui/icons-material/Warning';
 import { ContributorFieldNames } from '../../types/publicationFieldNames';
-import { EntityDescription, PublicationInstanceType, Registration } from '../../types/registration.types';
+import { EntityDescription, Registration } from '../../types/registration.types';
 import { contributorConfig, isDegree } from '../../utils/registration-helpers';
 import { Contributors } from './contributors_tab/Contributors';
 
 export const ContributorsPanel = () => {
+  const { t } = useTranslation();
   const {
     values: { entityDescription },
     errors,
     touched,
-    setFieldValue,
   } = useFormikContext<Registration>();
   const contributorsError = (errors.entityDescription as FormikErrors<EntityDescription>)?.contributors;
   const contributorsTouched = (touched.entityDescription as FormikTouched<EntityDescription>)?.contributors;
 
   const publicationInstanceType = entityDescription?.reference?.publicationInstance.type;
-  const contributors = entityDescription?.contributors ?? [];
-  const contributorsRef = useRef(contributors);
-
-  useEffect(() => {
-    // Ensure all contributors has a role by setting Creator role as default
-    const contributorsWithRole = contributorsRef.current.map((contributor) => ({
-      ...contributor,
-      role: contributor.role ?? ContributorRole.Creator,
-    }));
-    setFieldValue(ContributorFieldNames.Contributors, contributorsWithRole);
-  }, [setFieldValue]);
+  const contributorConfigResult = publicationInstanceType ? contributorConfig[publicationInstanceType] : null;
+  const primaryRoles = contributorConfigResult?.primaryRoles ?? [];
+  const secondaryRoles = contributorConfigResult?.secondaryRoles ?? [];
+  const shouldSeparateAddPrimaryRolesButtons = isDegree(publicationInstanceType as string);
 
   return (
     <>
       <FieldArray name={ContributorFieldNames.Contributors}>
-        {
-          ({ push, replace }: FieldArrayRenderProps) =>
-            publicationInstanceType ? (
-              <ContributorsContent
-                push={push}
-                replace={replace}
-                instanceType={publicationInstanceType}
-                separatePrimaryRoles={isDegree(publicationInstanceType)}
-              />
-            ) : null // TODO: must select type first
+        {({ push, replace }: FieldArrayRenderProps) =>
+          publicationInstanceType ? (
+            <>
+              {shouldSeparateAddPrimaryRolesButtons && primaryRoles.length > 1 ? (
+                primaryRoles.map((primaryRole) => (
+                  <Contributors push={push} replace={replace} primaryColorAddButton contributorRoles={[primaryRole]} />
+                ))
+              ) : (
+                <Contributors push={push} replace={replace} primaryColorAddButton contributorRoles={primaryRoles} />
+              )}
+              <Contributors push={push} replace={replace} contributorRoles={secondaryRoles} />
+
+              {/* TODO: Show contributors with roles that are not valid for this instanceType? */}
+            </>
+          ) : (
+            <Box sx={{ display: 'flex', gap: '0.5rem', mt: '1rem' }}>
+              <WarningIcon color="warning" />
+              <Typography fontWeight={500}>{t('registration.contributors.must_select_type_first')}</Typography>
+            </Box>
+          )
         }
       </FieldArray>
       {!!contributorsTouched && typeof contributorsError === 'string' && (
@@ -50,30 +53,6 @@ export const ContributorsPanel = () => {
           <ErrorMessage name={ContributorFieldNames.Contributors} />
         </FormHelperText>
       )}
-    </>
-  );
-};
-
-interface ContributorsContentProps extends Pick<FieldArrayRenderProps, 'push' | 'replace'> {
-  instanceType: PublicationInstanceType;
-  separatePrimaryRoles: boolean;
-}
-
-const ContributorsContent = ({ instanceType, separatePrimaryRoles, ...fieldArrayProps }: ContributorsContentProps) => {
-  const { primaryRoles, secondaryRoles } = contributorConfig[instanceType];
-
-  return (
-    <>
-      {separatePrimaryRoles && primaryRoles.length > 1 ? (
-        primaryRoles.map((primaryRole) => (
-          <Contributors {...fieldArrayProps} primaryColorAddButton contributorRoles={[primaryRole]} />
-        ))
-      ) : (
-        <Contributors {...fieldArrayProps} primaryColorAddButton contributorRoles={primaryRoles} />
-      )}
-      <Contributors {...fieldArrayProps} contributorRoles={secondaryRoles} />
-
-      {/* TODO: Show contributors with roles that are not valid for this instanceType? */}
     </>
   );
 };
