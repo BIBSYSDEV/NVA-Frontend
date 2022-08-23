e8904e98b7ac09e5d25d90838a7263cc17008de3
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-08-23 13:51:31 +0200

    NP-3056 Move fetching of institution user

--- src/pages/messages/WorklistPage.tsx
+++ src/pages/messages/WorklistPage.tsx
@@ -1,9 +1,10 @@
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { CircularProgress, Typography } from '@mui/material';
 import { PageHeader } from '../../components/PageHeader';
 import { SyledPageContent } from '../../components/styled/Wrappers';
-import { SearchApiPath } from '../../api/apiPaths';
+import { RoleApiPath, SearchApiPath } from '../../api/apiPaths';
 import { useFetch } from '../../utils/hooks/useFetch';
 import { DoiRequestConversation, PublicationConversation } from '../../types/publication_types/messages.types';
 import { ListSkeleton } from '../../components/ListSkeleton';
@@ -13,10 +14,27 @@ import { useFetchResource } from '../../utils/hooks/useFetchResource';
 import { Organization } from '../../types/organization.types';
 import { getLanguageString } from '../../utils/translation-helpers';
 import { WorklistItems } from './WorklistItems';
+import { InstitutionUser } from '../../types/user.types';
+import { setPartialUser } from '../../redux/userSlice';
 
 const WorklistPage = () => {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
   const user = useSelector((store: RootState) => store.user);
+
+  const [institutionUser, isLoadingInstitutionUser] = useFetch<InstitutionUser>({
+    url: user?.username ? `${RoleApiPath.Users}/${user.username}` : '',
+    errorMessage: t('feedback.error.get_roles'),
+    withAuthentication: true,
+  });
+
+  useEffect(() => {
+    if (institutionUser) {
+      const viewingScope = institutionUser.viewingScope?.includedUnits ?? [];
+      dispatch(setPartialUser({ viewingScope }));
+    }
+  }, [dispatch, institutionUser]);
+
   const viewingScopeId = user && user.viewingScope.length > 0 ? user.viewingScope[0] : '';
   const [viewingScopeOrganization, isLoadingViewingScopeOrganization] = useFetchResource<Organization>(viewingScopeId);
 
@@ -38,7 +56,7 @@ const WorklistPage = () => {
       ) : (
         <>
           {viewingScopeId ? (
-            isLoadingViewingScopeOrganization ? (
+            isLoadingInstitutionUser || isLoadingViewingScopeOrganization ? (
               <CircularProgress />
             ) : (
               viewingScopeOrganization && (
