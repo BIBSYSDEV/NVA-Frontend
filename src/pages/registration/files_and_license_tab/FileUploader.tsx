e644204effd2550f8fad2afc6e188e37961f22fa
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-08-25 11:28:40 +0200

    NP-3399 Bump uppy to v3

--- src/pages/registration/files_and_license_tab/FileUploader.tsx
+++ src/pages/registration/files_and_license_tab/FileUploader.tsx
@@ -10,16 +10,17 @@ interface FileUploaderProps {
 export const FileUploader = ({ addFile, uppy }: FileUploaderProps) => {
   useEffect(() => {
     if (uppy && !uppy.hasUploadSuccessEventListener) {
-      uppy.on('upload-success', (file, response: any) => {
-        // TODO: see if this removes need of any https://github.com/transloadit/uppy/pull/3141
-        const newFile: File = {
-          ...emptyFile,
-          identifier: response.uploadURL, // In reality an ID from completeMultipartUpload endpoint
-          name: file.name,
-          mimeType: file.type ?? '',
-          size: file.size,
-        };
-        addFile(newFile);
+      uppy.on('upload-success', (file, response) => {
+        if (file && response.uploadURL) {
+          const newFile: File = {
+            ...emptyFile,
+            identifier: response.uploadURL, // An ID from completeMultipartUpload endpoint
+            name: file.name,
+            mimeType: file.type ?? '',
+            size: file.size,
+          };
+          addFile(newFile);
+        }
       });
       // Avoid duplicating event listener
       uppy.hasUploadSuccessEventListener = true;
