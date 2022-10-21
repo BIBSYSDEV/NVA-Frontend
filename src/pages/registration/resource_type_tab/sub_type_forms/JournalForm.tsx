52ecac904fb7e0d98bd576073359a207a7be09f3
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-10-21 09:05:36 +0200

    NP-13634 New component for journal fields

--- src/pages/registration/resource_type_tab/sub_type_forms/JournalForm.tsx
+++ src/pages/registration/resource_type_tab/sub_type_forms/JournalForm.tsx
@@ -1,6 +1,5 @@
-import { ErrorMessage, Field, FieldProps, useFormikContext } from 'formik';
+import { useFormikContext } from 'formik';
 import { useTranslation } from 'react-i18next';
-import { Box, TextField, Typography } from '@mui/material';
 import { JournalType, ResourceFieldNames } from '../../../../types/publicationFieldNames';
 import { DoiField } from '../components/DoiField';
 import { NviValidation } from '../components/NviValidation';
@@ -12,6 +11,7 @@ import { JournalField } from '../components/JournalField';
 import { dataTestId } from '../../../../utils/dataTestIds';
 import { InputContainerBox } from '../../../../components/styled/Wrappers';
 import { PublicationChannelType } from '../../../../types/registration.types';
+import { JournalDetailsFields } from '../components/JournalDetailsFields';
 
 export const JournalForm = () => {
   const { t } = useTranslation();
@@ -39,90 +39,7 @@ export const JournalForm = () => {
           />
         )}
 
-        <Box
-          sx={{
-            display: 'grid',
-            gridTemplateColumns: { xs: '1fr', sm: 'repeat(4,1fr) auto 1fr' },
-            gap: '1rem',
-            alignItems: 'center',
-          }}>
-          <Field name={ResourceFieldNames.Volume}>
-            {({ field, meta: { error, touched } }: FieldProps<string>) => (
-              <TextField
-                {...field}
-                id={field.name}
-                value={field.value ?? ''}
-                data-testid={dataTestId.registrationWizard.resourceType.volumeField}
-                variant="filled"
-                label={t('registration.resource_type.volume')}
-                error={touched && !!error}
-                helperText={<ErrorMessage name={field.name} />}
-              />
-            )}
-          </Field>
-
-          <Field name={ResourceFieldNames.Issue}>
-            {({ field, meta: { error, touched } }: FieldProps<string>) => (
-              <TextField
-                {...field}
-                id={field.name}
-                value={field.value ?? ''}
-                data-testid={dataTestId.registrationWizard.resourceType.issueField}
-                variant="filled"
-                label={t('registration.resource_type.issue')}
-                error={touched && !!error}
-                helperText={<ErrorMessage name={field.name} />}
-              />
-            )}
-          </Field>
-
-          <Field name={ResourceFieldNames.PagesFrom}>
-            {({ field, meta: { error, touched } }: FieldProps<string>) => (
-              <TextField
-                {...field}
-                id={field.name}
-                data-testid={dataTestId.registrationWizard.resourceType.pagesFromField}
-                value={field.value ?? ''}
-                variant="filled"
-                label={t('registration.resource_type.pages_from')}
-                error={touched && !!error}
-                helperText={<ErrorMessage name={field.name} />}
-              />
-            )}
-          </Field>
-
-          <Field name={ResourceFieldNames.PagesTo}>
-            {({ field, meta: { error, touched } }: FieldProps<string>) => (
-              <TextField
-                {...field}
-                id={field.name}
-                data-testid={dataTestId.registrationWizard.resourceType.pagesToField}
-                variant="filled"
-                label={t('registration.resource_type.pages_to')}
-                value={field.value ?? ''}
-                error={touched && !!error}
-                helperText={<ErrorMessage name={field.name} />}
-              />
-            )}
-          </Field>
-
-          <Typography>{t('registration.resource_type.or')}</Typography>
-
-          <Field name={ResourceFieldNames.ArticleNumber}>
-            {({ field, meta: { error, touched } }: FieldProps<string>) => (
-              <TextField
-                {...field}
-                id={field.name}
-                data-testid={dataTestId.registrationWizard.resourceType.articleNumberField}
-                value={field.value ?? ''}
-                variant="filled"
-                label={t('registration.resource_type.article_number')}
-                error={touched && !!error}
-                helperText={<ErrorMessage name={field.name} />}
-              />
-            )}
-          </Field>
-        </Box>
+        <JournalDetailsFields />
       </InputContainerBox>
 
       {instanceType === JournalType.Article && (
--- src/pages/registration/resource_type_tab/sub_type_forms/media_types/MediaContributionPeriodicalForm.tsx
+++ src/pages/registration/resource_type_tab/sub_type_forms/media_types/MediaContributionPeriodicalForm.tsx
@@ -1,15 +1,9 @@
-import { ErrorMessage, Field, FieldProps } from 'formik';
-import { useTranslation } from 'react-i18next';
-import { Box, TextField, Typography } from '@mui/material';
-import { ResourceFieldNames } from '../../../../../types/publicationFieldNames';
-import { dataTestId } from '../../../../../utils/dataTestIds';
 import { DoiField } from '../../components/DoiField';
 import { JournalField } from '../../components/JournalField';
 import { PublicationChannelType } from '../../../../../types/registration.types';
+import { JournalDetailsFields } from '../../components/JournalDetailsFields';
 
 export const MediaContributionPeriodicalForm = () => {
-  const { t } = useTranslation();
-
   return (
     <>
       <DoiField />
@@ -19,90 +13,7 @@ export const MediaContributionPeriodicalForm = () => {
         unconfirmedContextType={PublicationChannelType.UnconfirmedMediaContributionPeriodical}
       />
 
-      <Box
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: { xs: '1fr', sm: 'repeat(4,1fr) auto 1fr' },
-          gap: '1rem',
-          alignItems: 'center',
-        }}>
-        <Field name={ResourceFieldNames.Volume}>
-          {({ field, meta: { error, touched } }: FieldProps<string>) => (
-            <TextField
-              {...field}
-              id={field.name}
-              value={field.value ?? ''}
-              data-testid={dataTestId.registrationWizard.resourceType.volumeField}
-              variant="filled"
-              label={t('registration.resource_type.volume')}
-              error={touched && !!error}
-              helperText={<ErrorMessage name={field.name} />}
-            />
-          )}
-        </Field>
-
-        <Field name={ResourceFieldNames.Issue}>
-          {({ field, meta: { error, touched } }: FieldProps<string>) => (
-            <TextField
-              {...field}
-              id={field.name}
-              value={field.value ?? ''}
-              data-testid={dataTestId.registrationWizard.resourceType.issueField}
-              variant="filled"
-              label={t('registration.resource_type.issue')}
-              error={touched && !!error}
-              helperText={<ErrorMessage name={field.name} />}
-            />
-          )}
-        </Field>
-
-        <Field name={ResourceFieldNames.PagesFrom}>
-          {({ field, meta: { error, touched } }: FieldProps<string>) => (
-            <TextField
-              {...field}
-              id={field.name}
-              data-testid={dataTestId.registrationWizard.resourceType.pagesFromField}
-              value={field.value ?? ''}
-              variant="filled"
-              label={t('registration.resource_type.pages_from')}
-              error={touched && !!error}
-              helperText={<ErrorMessage name={field.name} />}
-            />
-          )}
-        </Field>
-
-        <Field name={ResourceFieldNames.PagesTo}>
-          {({ field, meta: { error, touched } }: FieldProps<string>) => (
-            <TextField
-              {...field}
-              id={field.name}
-              data-testid={dataTestId.registrationWizard.resourceType.pagesToField}
-              variant="filled"
-              label={t('registration.resource_type.pages_to')}
-              value={field.value ?? ''}
-              error={touched && !!error}
-              helperText={<ErrorMessage name={field.name} />}
-            />
-          )}
-        </Field>
-
-        <Typography>{t('registration.resource_type.or')}</Typography>
-
-        <Field name={ResourceFieldNames.ArticleNumber}>
-          {({ field, meta: { error, touched } }: FieldProps<string>) => (
-            <TextField
-              {...field}
-              id={field.name}
-              data-testid={dataTestId.registrationWizard.resourceType.articleNumberField}
-              value={field.value ?? ''}
-              variant="filled"
-              label={t('registration.resource_type.article_number')}
-              error={touched && !!error}
-              helperText={<ErrorMessage name={field.name} />}
-            />
-          )}
-        </Field>
-      </Box>
+      <JournalDetailsFields />
     </>
   );
 };
