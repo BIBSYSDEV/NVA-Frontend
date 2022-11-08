cf2917e563da9fbb1f430da903fc6443ca19a471
Isar Buzza <isar@buzza.no>
2022-11-08 12:56:37 +0100

    NP-17311 'Previous Employment'-title is now hidden if there are none

--- src/pages/basic_data/SearchForCristinPerson.tsx
+++ src/pages/basic_data/SearchForCristinPerson.tsx
@@ -67,6 +67,12 @@ export const SearchForCristinPerson = ({
     }
   }, [setSelectedPerson, searchQueryIsNumber, searchQuery]);
 
+  const selectedPersonInactiveAffiliations =
+    selectedPerson?.affiliations.filter((affiliation) => !affiliation.active) ?? [];
+
+  const selectedPersonActiveAffiliations =
+    selectedPerson?.affiliations.filter((affiliation) => affiliation.active) ?? [];
+
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
       {!selectedPerson?.id ? (
@@ -134,36 +140,36 @@ export const SearchForCristinPerson = ({
           <div>
             <Typography variant="overline">{t('basic_data.person_register.current_employments')}</Typography>
             <Box component="ul" sx={{ my: 0, pl: '1rem' }}>
-              {selectedPerson.affiliations
-                .filter((affiliation) => affiliation.active)
-                .map((affiliation) => {
-                  const roleString = getLanguageString(affiliation.role.labels);
-                  return (
-                    <li key={affiliation.organization}>
-                      <Box sx={{ display: 'flex', gap: '0.5rem' }}>
-                        {roleString && <Typography>{roleString}:</Typography>}
-                        <AffiliationHierarchy unitUri={affiliation.organization} commaSeparated />
-                      </Box>
-                    </li>
-                  );
-                })}
+              {selectedPersonActiveAffiliations.map((affiliation) => {
+                const roleString = getLanguageString(affiliation.role.labels);
+                return (
+                  <li key={affiliation.organization}>
+                    <Box sx={{ display: 'flex', gap: '0.5rem' }}>
+                      {roleString && <Typography>{roleString}:</Typography>}
+                      <AffiliationHierarchy unitUri={affiliation.organization} commaSeparated />
+                    </Box>
+                  </li>
+                );
+              })}
             </Box>
-            <Typography variant="overline">{t('basic_data.person_register.previous_employments')}</Typography>
-            <Box component="ul" sx={{ my: 0, pl: '1rem' }}>
-              {selectedPerson.affiliations
-                .filter((affiliation) => !affiliation.active)
-                .map((affiliation) => {
-                  const roleString = getLanguageString(affiliation.role.labels);
-                  return (
-                    <li key={affiliation.organization}>
-                      <Box sx={{ display: 'flex', gap: '0.5rem' }}>
-                        {roleString && <Typography>{roleString}:</Typography>}
-                        <AffiliationHierarchy unitUri={affiliation.organization} commaSeparated />
-                      </Box>
-                    </li>
-                  );
-                })}
-            </Box>
+            {selectedPersonInactiveAffiliations.length > 0 && (
+              <>
+                <Typography variant="overline">{t('basic_data.person_register.previous_employments')}</Typography>
+                <Box component="ul" sx={{ my: 0, pl: '1rem' }}>
+                  {selectedPersonInactiveAffiliations.map((affiliation) => {
+                    const roleString = getLanguageString(affiliation.role.labels);
+                    return (
+                      <li key={affiliation.organization}>
+                        <Box sx={{ display: 'flex', gap: '0.5rem' }}>
+                          {roleString && <Typography>{roleString}:</Typography>}
+                          <AffiliationHierarchy unitUri={affiliation.organization} commaSeparated />
+                        </Box>
+                      </li>
+                    );
+                  })}
+                </Box>
+              </>
+            )}
           </div>
           <Button
             variant="outlined"
