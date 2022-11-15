2870dfd740e8bbdb9077adb05edb8f8c7ac97bb3
Isar Buzza <isar@buzza.no>
2022-11-15 16:14:25 +0100

    NP-17639 filtered curator list on editor page

--- src/pages/editor/EditorCurators.tsx
+++ src/pages/editor/EditorCurators.tsx
@@ -21,28 +21,32 @@ import { Organization } from '../../types/organization.types';
 import { getSortedSubUnits } from '../../utils/institutions-helpers';
 import { RoleApiPath } from '../../api/apiPaths';
 import { useFetch } from '../../utils/hooks/useFetch';
-import { UserList } from '../../types/user.types';
+import { RoleName, UserList } from '../../types/user.types';
+import { filterUsersByRole } from '../../utils/role-helpers';
 
 export const EditorCurators = () => {
   const { t } = useTranslation();
   const [rowsPerPage, setRowsPerPage] = useState(ROWS_PER_PAGE_OPTIONS[0]);
   const [page, setPage] = useState(0);
 
-  const user = useSelector((store: RootState) => store.user);
+  const curator = useSelector((store: RootState) => store.user);
   const [currentOrganization, isLoadingCurrentOrganization] = useFetchResource<Organization>(
-    user?.topOrgCristinId ?? ''
+    curator?.topOrgCristinId ?? ''
   );
   const [institutionUsers, isLoading] = useFetch<UserList>({
-    url: user?.customerId ? `${RoleApiPath.InstitutionUsers}?institution=${encodeURIComponent(user.customerId)}` : '',
+    url: curator?.customerId
+      ? `${RoleApiPath.InstitutionUsers}?institution=${encodeURIComponent(curator.customerId)}`
+      : '',
     errorMessage: t('feedback.error.get_users_for_institution'),
     withAuthentication: true,
   });
-  const users = institutionUsers?.users ?? [];
+
+  const curators = filterUsersByRole(institutionUsers?.users ?? [], RoleName.Curator);
 
   // Ensure selected page is not out of bounds due to manipulated userList
-  const validPage = users.length <= page * rowsPerPage ? 0 : page;
+  const validPage = curators.length <= page * rowsPerPage ? 0 : page;
 
-  const sortedList = users.sort((a, b) =>
+  const sortedList = curators.sort((a, b) =>
     `${a.givenName} ${a.familyName}`.toLocaleLowerCase() < `${b.givenName} ${b.familyName}`.toLocaleLowerCase() ? -1 : 1
   );
 
@@ -73,18 +77,18 @@ export const EditorCurators = () => {
                 </TableHead>
 
                 <TableBody>
-                  {sortedList.slice(validPage * rowsPerPage, validPage * rowsPerPage + rowsPerPage).map((user) => {
+                  {sortedList.slice(validPage * rowsPerPage, validPage * rowsPerPage + rowsPerPage).map((curator) => {
                     return (
-                      <TableRow key={user.username}>
+                      <TableRow key={curator.username}>
                         <TableCell>
-                          {user.givenName} {user.familyName}
+                          {curator.givenName} {curator.familyName}
                         </TableCell>
                         <TableCell>
                           {isLoadingCurrentOrganization ? (
                             <CircularProgress />
                           ) : (
                             <ViewingScopeCell
-                              user={user}
+                              user={curator}
                               options={currentOrganization ? getSortedSubUnits([currentOrganization]) : []}
                             />
                           )}
