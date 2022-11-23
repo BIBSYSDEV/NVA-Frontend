c8a0050a5024d9c5b5a31f2b76f24ea52be1a6e7
Isar Buzza <isar@buzza.no>
2022-11-23 13:06:25 +0100

    NP-17980 added delete button

--- src/pages/registration/files_and_license_tab/FilesTable.tsx
+++ src/pages/registration/files_and_license_tab/FilesTable.tsx
@@ -1,16 +1,38 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
-import React from 'react';
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import prettyBytes from 'pretty-bytes';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { AssociatedFile } from '../../../types/associatedArtifact.types';
+import { ConfirmDialog } from '../../../components/ConfirmDialog';
 
-export const FilesTable = () => {
+interface FilesTableProps {
+  files: AssociatedFile[];
+  removeFile: () => void;
+}
+
+export const FilesTable = ({ files, removeFile }: FilesTableProps) => {
   const { t } = useTranslation();
+  const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
+  const toggleOpenConfirmDialog = () => setOpenConfirmDialog(!openConfirmDialog);
+
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead sx={{ fontWeight: 'bold' }}>
           <TableRow>
             <TableCell>
-              <Typography>{t('registration.files_and_license.files')}</Typography>
+              <Typography>{t('common.name')}</Typography>
             </TableCell>
             <TableCell>
               <Typography>{t('registration.files_and_license.size')}</Typography>
@@ -29,7 +51,32 @@ export const FilesTable = () => {
             </TableCell>
           </TableRow>
         </TableHead>
-        <TableBody></TableBody>
+        <TableBody>
+          {files.map((file) => (
+            <TableRow key={file.identifier}>
+              <TableCell>{file.name}</TableCell>
+              <TableCell>{prettyBytes(file.size)}</TableCell>
+              <TableCell>Version</TableCell>
+              <TableCell>Embargo</TableCell>
+              <TableCell>License</TableCell>
+              <TableCell>
+                <Button color="error" startIcon={<DeleteIcon />} onClick={toggleOpenConfirmDialog} />
+                <ConfirmDialog
+                  open={openConfirmDialog}
+                  title={t('registration.files_and_license.remove_file')}
+                  onAccept={() => {
+                    removeFile();
+                    toggleOpenConfirmDialog();
+                  }}
+                  onCancel={toggleOpenConfirmDialog}>
+                  <Typography>
+                    {t('registration.files_and_license.remove_file_description', { fileName: file.name })}
+                  </Typography>
+                </ConfirmDialog>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
       </Table>
     </TableContainer>
   );
