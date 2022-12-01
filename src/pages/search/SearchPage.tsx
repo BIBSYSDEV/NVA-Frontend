25c78e2e018ff9c7c8ecbaa4851e8f83098481c5
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-12-01 14:00:47 +0100

    NP-21426 Use same outline for search page as other pages

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -2,9 +2,10 @@ import { Box, Divider, List } from '@mui/material';
 import { Formik, Form } from 'formik';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
 import { PageHeader } from '../../components/PageHeader';
 import { SearchBar } from './SearchBar';
-import { SyledPageContent } from '../../components/styled/Wrappers';
+import { BackgroundDiv, SyledPageContent } from '../../components/styled/Wrappers';
 import { createSearchConfigFromSearchParams, createSearchQuery, SearchParam } from '../../utils/searchHelpers';
 import { RegistrationFacetsFilter } from './filters/RegistrationFacetsFilter';
 import { RegistrationSearch } from './RegistrationSearch';
@@ -13,6 +14,7 @@ import { SearchResponse } from '../../types/common.types';
 import { Registration } from '../../types/registration.types';
 import { useFetch } from '../../utils/hooks/useFetch';
 import { SearchApiPath } from '../../api/apiPaths';
+import { SideNav, SideNavHeader, StyledPageWithSideMenu } from '../../components/PageWithSideMenu';
 
 const SearchPage = () => {
   const { t } = useTranslation();
@@ -27,49 +29,57 @@ const SearchPage = () => {
   const initialSearchParams = createSearchConfigFromSearchParams(params);
 
   return (
-    <SyledPageContent>
-      <PageHeader>{t('common.registrations')}</PageHeader>
-      <Formik
-        initialValues={initialSearchParams}
-        onSubmit={(values) => {
-          const queryString = createSearchQuery(values);
-          params.set(SearchParam.From, '0');
-          if (queryString) {
-            params.set(SearchParam.Query, queryString);
-          } else {
-            params.delete(SearchParam.Query);
-          }
-          history.push({ search: params.toString() });
-        }}>
-        <Form>
-          <Box
-            sx={{
-              display: 'grid',
-              gridTemplateRows: 'auto auto 1fr',
-              gridTemplateColumns: { xs: '1fr', md: '2fr auto 5fr 2fr' },
-              gridTemplateAreas: {
-                xs: "'searchbar' 'sorting' 'filters' 'advanced' 'results'",
-                md: "'filters divider searchbar sorting' 'filters divider advanced advanced' 'filters divider results results'",
-              },
-              columnGap: '2rem',
-              rowGap: '1rem',
-            }}>
-            <>
-              <List sx={{ gridArea: 'filters' }} disablePadding>
-                <RegistrationFacetsFilter aggregations={searchResults?.aggregations ?? {}} />
-              </List>
-              <Divider
-                orientation="vertical"
-                sx={{ gridArea: 'divider', display: { xs: 'none', md: 'inline-flex' } }}
-              />
-            </>
+    // <SyledPageContent>
+    //   <PageHeader>{t('common.registrations')}</PageHeader>
+    <Formik
+      initialValues={initialSearchParams}
+      onSubmit={(values) => {
+        const queryString = createSearchQuery(values);
+        params.set(SearchParam.From, '0');
+        if (queryString) {
+          params.set(SearchParam.Query, queryString);
+        } else {
+          params.delete(SearchParam.Query);
+        }
+        history.push({ search: params.toString() });
+      }}>
+      <Form>
+        <StyledPageWithSideMenu>
+          <SideNav>
+            <SideNavHeader icon={SearchIcon} text={t('common.search')} />
+            <List disablePadding>
+              <RegistrationFacetsFilter aggregations={searchResults?.aggregations ?? {}} />
+            </List>
+          </SideNav>
+          <BackgroundDiv>
             <SearchBar />
             <SortSelector />
             <RegistrationSearch searchResults={searchResults} isLoadingSearch={isLoadingSearch} />
-          </Box>
-        </Form>
-      </Formik>
-    </SyledPageContent>
+          </BackgroundDiv>
+        </StyledPageWithSideMenu>
+
+        {/* <Box
+          sx={{
+            display: 'grid',
+            gridTemplateRows: 'auto auto 1fr',
+            gridTemplateColumns: { xs: '1fr', md: '2fr auto 5fr 2fr' },
+            gridTemplateAreas: {
+              xs: "'searchbar' 'sorting' 'filters' 'advanced' 'results'",
+              md: "'filters divider searchbar sorting' 'filters divider advanced advanced' 'filters divider results results'",
+            },
+            columnGap: '2rem',
+            rowGap: '1rem',
+          }}>
+          <>
+            <Divider orientation="vertical" sx={{ gridArea: 'divider', display: { xs: 'none', md: 'inline-flex' } }} />
+          </>
+          <SearchBar />
+          <SortSelector />
+          <RegistrationSearch searchResults={searchResults} isLoadingSearch={isLoadingSearch} />
+        </Box> */}
+      </Form>
+    </Formik>
+    // </SyledPageContent>
   );
 };
 
