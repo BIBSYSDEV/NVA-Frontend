2b8a2897b9fb92f1a72c554fad206ab2fdc105bb
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2022-12-05 14:35:42 +0100

    NP-21426 Update design of search results

--- src/components/RegistrationList.tsx
+++ src/components/RegistrationList.tsx
@@ -1,11 +1,21 @@
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import { Box, Link as MuiLink, List, ListItem, ListItemText, Typography } from '@mui/material';
-import { displayDate } from '../utils/date-helpers';
+import {
+  Box,
+  Link as MuiLink,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import { visuallyHidden } from '@mui/utils';
 import { getRegistrationLandingPagePath, getResearchProfilePath } from '../utils/urlPaths';
 import { Registration } from '../types/registration.types';
 import { ErrorBoundary } from './ErrorBoundary';
-import { TruncatableTypography } from './TruncatableTypography';
 import { dataTestId } from '../utils/dataTestIds';
 import { getTitleString } from '../utils/registration-helpers';
 
@@ -13,65 +23,71 @@ interface RegistrationListProps {
   registrations: Registration[];
 }
 
-export const RegistrationList = ({ registrations }: RegistrationListProps) => (
-  <List>
-    {registrations.map((registration) => (
-      <ErrorBoundary key={registration.id}>
-        <RegistrationListItem registration={registration} />
-      </ErrorBoundary>
-    ))}
-  </List>
-);
-
-interface RegistrationListItemProps {
-  registration: Registration;
-}
-
-const RegistrationListItem = ({ registration }: RegistrationListItemProps) => {
+export const RegistrationList = ({ registrations }: RegistrationListProps) => {
   const { t } = useTranslation();
-  const { identifier, entityDescription } = registration;
-
-  const contributors = entityDescription?.contributors ?? [];
-  const focusedContributors = contributors.slice(0, 5);
-  const countRestContributors = contributors.length - focusedContributors.length;
 
   return (
-    <ListItem divider disableGutters>
-      <ListItemText disableTypography data-testid={dataTestId.startPage.searchResultItem}>
-        <Typography variant="overline" sx={{ color: 'primary.main' }}>
-          {t(`registration.publication_types.${entityDescription?.reference?.publicationInstance.type ?? ''}` as any)} -{' '}
-          {displayDate(entityDescription?.date)}
-        </Typography>
-        <Typography gutterBottom sx={{ fontSize: '1rem', fontWeight: '600', fontStyle: 'italic' }}>
-          <MuiLink component={Link} to={getRegistrationLandingPagePath(identifier)}>
-            {getTitleString(entityDescription?.mainTitle)}
-          </MuiLink>
-        </Typography>
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            columnGap: '1rem',
-            whiteSpace: 'nowrap',
-          }}>
-          {focusedContributors.map((contributor, index) => (
-            <Typography key={index} variant="body2">
-              {contributor.identity.id ? (
-                <MuiLink component={Link} to={getResearchProfilePath(contributor.identity.id)}>
-                  {contributor.identity.name}
-                </MuiLink>
-              ) : (
-                contributor.identity.name
-              )}
-            </Typography>
-          ))}
-          {countRestContributors > 0 && (
-            <Typography variant="body2">({t('common.x_others', { count: countRestContributors })})</Typography>
-          )}
-        </Box>
-
-        <TruncatableTypography>{entityDescription?.abstract}</TruncatableTypography>
-      </ListItemText>
-    </ListItem>
+    <TableContainer component={Paper}>
+      <Table>
+        <caption style={visuallyHidden}>{t('common.registrations')}</caption>
+        <TableHead sx={{ fontSize: 400 }}>
+          <TableRow>
+            <TableCell width={1}>{t('registration.resource_type.resource_type')}</TableCell>
+            <TableCell width={1}>{t('common.year')}</TableCell>
+            <TableCell>{t('search.title_and_contributors')}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {registrations.map((registration) => {
+            const { id, identifier, entityDescription } = registration;
+            const contributors = entityDescription?.contributors ?? [];
+            const focusedContributors = contributors.slice(0, 5);
+            const countRestContributors = contributors.length - focusedContributors.length;
+            return (
+              <TableRow key={id} data-testid={dataTestId.startPage.searchResultItem}>
+                <ErrorBoundary>
+                  <TableCell sx={{ fontWeight: 600 }}>
+                    {entityDescription?.reference?.publicationInstance.type &&
+                      t(`registration.publication_types.${entityDescription.reference.publicationInstance.type}`)}
+                  </TableCell>
+                  <TableCell>{entityDescription?.date?.year}</TableCell>
+                  <TableCell>
+                    <Typography gutterBottom sx={{ fontSize: '1rem', fontWeight: '600' }}>
+                      <MuiLink component={Link} to={getRegistrationLandingPagePath(identifier)}>
+                        {getTitleString(entityDescription?.mainTitle)}
+                      </MuiLink>
+                    </Typography>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        columnGap: '1rem',
+                        whiteSpace: 'nowrap',
+                      }}>
+                      {focusedContributors.map((contributor, index) => (
+                        <Typography key={index} variant="body2">
+                          {contributor.identity.id ? (
+                            <MuiLink component={Link} to={getResearchProfilePath(contributor.identity.id)}>
+                              {contributor.identity.name}
+                            </MuiLink>
+                          ) : (
+                            contributor.identity.name
+                          )}
+                        </Typography>
+                      ))}
+                      {countRestContributors > 0 && (
+                        <Typography variant="body2">
+                          ({t('common.x_others', { count: countRestContributors })})
+                        </Typography>
+                      )}
+                    </Box>
+                  </TableCell>
+                </ErrorBoundary>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
