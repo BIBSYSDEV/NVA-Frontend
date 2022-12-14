47bc71507c3b6bf6e92f771bf700f5bb65ae19fb
Isar Buzza <isar.kristoffer.buzza@no.experis.com>
2022-12-14 09:10:16 +0100

    NP-18020 added wrapper for menu buttons

--- src/layout/header/MenuButton.tsx
+++ src/layout/header/MenuButton.tsx
@@ -1,19 +1,26 @@
-import { Button, ButtonProps } from '@mui/material';
+import { Box, Button, ButtonProps } from '@mui/material';
 import { Link, LinkProps } from 'react-router-dom';
 
 interface MenuButtonProps extends ButtonProps, Pick<LinkProps, 'to'> {
   isSelected: boolean;
 }
 
-export const MenuButton = ({ isSelected, ...rest }: MenuButtonProps) => (
-  <Button
+export const MenuButton = ({ isSelected, sx, ...rest }: MenuButtonProps) => (
+  <Box
     sx={{
-      whiteSpace: 'nowrap',
+      height: '100%',
+      display: 'flex',
       borderBottom: isSelected ? '0.375rem solid white' : 'none',
-      borderRadius: '0px',
-      mb: isSelected ? '-0.375rem' : '0rem', // Negates element re-size when border appears
-    }}
-    LinkComponent={Link}
-    {...rest}
-  />
+      boxShadow: '-1px 7px 4px -3px rgba(0,0,0,0.3)',
+    }}>
+    <Button
+      sx={{
+        mb: isSelected ? '-0.375rem' : 'none',
+        whiteSpace: 'nowrap',
+        ...sx,
+      }}
+      LinkComponent={Link}
+      {...rest}
+    />
+  </Box>
 );
