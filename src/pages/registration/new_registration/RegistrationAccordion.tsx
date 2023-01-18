eb8c9e7104e30f3a6d7fe86bb68b5b1a4cefa6df
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-01-18 16:36:32 +0100

    NP-29038 Update wizard colors

--- src/pages/registration/new_registration/RegistrationAccordion.tsx
+++ src/pages/registration/new_registration/RegistrationAccordion.tsx
@@ -2,7 +2,7 @@ import { Accordion } from '@mui/material';
 import { styled } from '@mui/system';
 
 export const RegistrationAccordion = styled(Accordion)(({ theme }) => ({
-  bgcolor: 'secondary.dark !important',
+  background: theme.palette.secondary.dark,
   '.MuiAccordionSummary-content': {
     alignItems: 'center',
     padding: '1rem 0',
