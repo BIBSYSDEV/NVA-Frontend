e3b8be70f67ddc5aba015b488eee100973acaebb
Isar Buzza <isar@buzza.no>
2023-02-07 11:20:12 +0100

    NP-33499 added dataTestIds

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,6 +1,8 @@
 import { useTranslation } from 'react-i18next';
-import { Box, Link, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Box, Link as MuiLink, Typography } from '@mui/material';
 import { UrlPathTemplate } from '../utils/urlPaths';
+import { dataTestId } from '../utils/dataTestIds';
 
 export const Footer = () => {
   const { t } = useTranslation();
@@ -12,27 +14,29 @@ export const Footer = () => {
         padding: '1rem',
         bgcolor: 'info.light',
         display: 'grid',
-        gridTemplateColumns: { xs: '1fr', md: 'repeat(3, 1fr)' },
+        gridTemplateColumns: { xs: '1fr', lg: '1fr 2fr 1fr' },
         gap: '1rem',
       }}>
-      <Typography sx={{ color: 'primary.main', gridColumn: { xs: 1, md: 2 }, justifySelf: 'center' }}>
+      <Typography sx={{ color: 'primary.main', gridColumn: { xs: 1, lg: 2 }, justifySelf: 'center' }}>
         {t('about.footer_text')}
       </Typography>
       <Box
         sx={{
           display: 'flex',
-          justifySelf: { xs: 'center', md: 'end' },
+          justifySelf: { xs: 'center', lg: 'end' },
           gap: '0.5rem',
-          gridColumn: { xs: 1, md: 3 },
+          gridColumn: { xs: 1, lg: 3 },
         }}>
-        <Link
-          component="a"
+        <MuiLink
+          data-testid={dataTestId.footer.availabilityStatement}
           target="_blank"
           rel="noopener noreferrer"
           href={'https://uustatus.no/nb/erklaringer/publisert/bffb4b1d-25eb-4fe0-bac7-2f0b4a8e0fd9'}>
           {t('about.availability_statement')}
-        </Link>
-        <Link href={UrlPathTemplate.PrivacyPolicy}>{t('privacy.privacy_statement')}</Link>
+        </MuiLink>
+        <MuiLink data-testid={dataTestId.footer.privacyLink} component={Link} to={UrlPathTemplate.PrivacyPolicy}>
+          {t('privacy.privacy_statement')}
+        </MuiLink>
       </Box>
     </Box>
   );
