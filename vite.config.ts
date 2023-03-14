bf87ef59bcfcc5fc3dbf0254fd8f5731a9e358d8
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-03-14 13:19:02 +0100

    NP-39705 Remove redundant packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), splitVendorChunkPlugin()],
+  plugins: [react(), splitVendorChunkPlugin()],
   server: {
     open: true,
     port: 3000,
