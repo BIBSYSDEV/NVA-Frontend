903ddf48acb07d16daac013670d1b5c9d1e5deeb
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-04-02 18:48:40 +0200

    reties

--- cypress/e2e/about.spec.ts
+++ cypress/e2e/about.spec.ts
@@ -37,6 +37,6 @@ describe.only('About and Privacy policy', () => {
     cy.get(`[data-testid=${dataTestId.footer.privacyLink}]`).click();
     cy.url().should('include', UrlPathTemplate.PrivacyPolicy);
     cy.injectAxe();
-    cy.checkA11y(null, { interval: 100 }, terminalLog);
+    cy.checkA11y(null, { retries: 3, interval: 100 }, terminalLog);
   });
 });
