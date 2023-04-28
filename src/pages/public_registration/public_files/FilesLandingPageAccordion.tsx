c7a55b77adb99975c69740cf2ac9b2f74cedeefe
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-04-28 10:51:47 +0200

    NP-44634 Show banner for files awaiting approval

--- src/pages/public_registration/public_files/FilesLandingPageAccordion.tsx
+++ src/pages/public_registration/public_files/FilesLandingPageAccordion.tsx
@@ -10,6 +10,7 @@ import {
 } from '../../../utils/registration-helpers';
 import { PublicRegistrationContentProps } from '../PublicRegistrationContent';
 import { FileRow } from './FileRow';
+import { Box } from '@mui/material';
 
 const maxFileSizeForPreview = 10_000_000; //10 MB
 
@@ -22,7 +23,6 @@ export const FilesLandingPageAccordion = ({ registration }: PublicRegistrationCo
   const associatedFiles = getAssociatedFiles(registration.associatedArtifacts);
   const publishedFiles = associatedFiles.filter((file) => file.type === 'PublishedFile');
   const unpublishedFiles = associatedFiles.filter((file) => file.type === 'UnpublishedFile');
-
   const publishableFilesLength = publishedFiles.length + unpublishedFiles.length;
 
   const filesToShow = userIsRegistrationAdmin ? associatedFiles : publishedFiles;
@@ -36,6 +36,11 @@ export const FilesLandingPageAccordion = ({ registration }: PublicRegistrationCo
       dataTestId={dataTestId.registrationLandingPage.filesAccordion}
       defaultExpanded
       heading={t('registration.files_and_license.files_count', { count: publishableFilesLength })}>
+      {unpublishedFiles.length > 0 && (
+        <Box sx={{ bgcolor: 'secondary.dark', p: '0.5rem 2.5rem', width: 'fit-content' }}>
+          {t('registration.files_and_license.files_awaits_approval', { count: unpublishedFiles.length })}
+        </Box>
+      )}
       {filesToShow.map((file, index) => (
         <FileRow
           key={file.identifier}
