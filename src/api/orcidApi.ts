dda512c9e93bc269ecf56002a1e936065e226aab
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2023-05-03 16:19:58 +0200

    Update src/api/orcidApi.ts
    Co-authored-by: Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>

--- src/api/orcidApi.ts
+++ src/api/orcidApi.ts
@@ -2,10 +2,10 @@ import { authenticatedApiRequest2 } from './apiRequest';
 import { OrcidApiPath } from './apiPaths';
 import { OrcidCredentials } from '../types/orcid.types';
 
-export const postOrcidCredentials = async (orcidCredentials: OrcidCredentials) => {
-  return await authenticatedApiRequest2({
-    url: `${OrcidApiPath.Orcid}`,
+export const postOrcidCredentials = async (orcidCredentials: OrcidCredentials) => 
+  await authenticatedApiRequest2({
+    url: OrcidApiPath.Orcid,
     method: 'POST',
     data: orcidCredentials,
-  });
+)
 };
