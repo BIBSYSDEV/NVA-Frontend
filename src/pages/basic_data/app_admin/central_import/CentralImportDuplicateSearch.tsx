a6d952aaa6df85d89ee171bee93d6b161fd6ece5
Kir Truhacev <kir.truhacev@sikt.no>
2023-05-25 14:35:11 +0200

    code review

--- src/pages/basic_data/app_admin/central_import/CentralImportDuplicateSearch.tsx
+++ src/pages/basic_data/app_admin/central_import/CentralImportDuplicateSearch.tsx
@@ -1,14 +1,14 @@
 import { Divider, List, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { useFetch } from '../../../../utils/hooks/useFetch';
-import { SearchApiPath } from '../../../../api/apiPaths';
-import { SearchResponse } from '../../../../types/common.types';
 import { ListSkeleton } from '../../../../components/ListSkeleton';
 import { CentralImportResultItem } from './CentralImportResultItem';
 import { DescriptionFieldNames, ResourceFieldNames } from '../../../../types/publicationFieldNames';
 import { ROWS_PER_PAGE_OPTIONS } from '../../../../utils/constants';
 import { DuplicateSearchFilters } from '../../../../types/duplicateSearchTypes';
-import { ImportCandidate } from '../../../../types/importCandidate';
+import { useQuery } from '@tanstack/react-query';
+import { fetchImportCandidates } from '../../../../api/searchApi';
+import { setNotification } from '../../../../redux/notificationSlice';
+import { useDispatch } from 'react-redux';
 
 interface CentralImportDuplicateSearchProps {
   duplicateSearchFilters: DuplicateSearchFilters;
@@ -16,7 +16,7 @@ interface CentralImportDuplicateSearchProps {
 
 export const CentralImportDuplicateSearch = ({ duplicateSearchFilters }: CentralImportDuplicateSearchProps) => {
   const { t } = useTranslation();
-
+  const dispatch = useDispatch();
   const maxHits = ROWS_PER_PAGE_OPTIONS[0];
   const queryArray = [];
   duplicateSearchFilters.doi.length > 0 && queryArray.push(`${ResourceFieldNames.Doi}:"${duplicateSearchFilters.doi}"`);
@@ -29,30 +29,39 @@ export const CentralImportDuplicateSearch = ({ duplicateSearchFilters }: Central
   duplicateSearchFilters.yearPublished.length > 0 &&
     queryArray.push(`${DescriptionFieldNames.PublicationYear}:"${duplicateSearchFilters.yearPublished}"`);
 
-  const searchQuery = queryArray.length > 0 ? `query=(${queryArray.join(' AND ')})` : '';
+  const searchQuery = queryArray.length > 0 ? `query=(${queryArray.join(' AND ')}&results=${maxHits})` : '';
 
-  const [searchResults, isLoadingSearchResults] = useFetch<SearchResponse<ImportCandidate>>({
-    url: `${SearchApiPath.ImportCandidates}?${searchQuery}&results=${maxHits}`,
-    errorMessage: t('feedback.error.search'),
+  const importCandidateQuery = useQuery({
+    queryKey: ['importCandidates', searchQuery],
+    queryFn: () => fetchImportCandidates(),
+    onError: () =>
+      dispatch(
+        setNotification({
+          message: t('feedback.error.get_messages'),
+          variant: 'error',
+        })
+      ),
   });
+
+  const searchResults = importCandidateQuery.data?.hits ?? [];
 
   return (
     <>
-      {isLoadingSearchResults ? (
+      {importCandidateQuery.isLoading ? (
         <ListSkeleton minWidth={100} maxWidth={100} height={100} />
       ) : (
         searchResults && (
           <>
             <Typography variant="subtitle1">
               {t('basic_data.central_import.duplicate_search_hits_shown', {
-                ShownResultsCount: searchResults.hits.length,
-                TotalResultsCount: searchResults.size,
+                ShownResultsCount: searchResults.length,
+                TotalResultsCount: searchResults,
               })}
               :
             </Typography>
             <Divider />
             <List>
-              {searchResults.hits.map((importCandidate) => (
+              {searchResults.map((importCandidate) => (
                 <CentralImportResultItem importCandidate={importCandidate} key={importCandidate.id} />
               ))}
             </List>
--- src/pages/basic_data/app_admin/central_import/CentralImportPage.tsx
+++ src/pages/basic_data/app_admin/central_import/CentralImportPage.tsx
@@ -1,17 +1,15 @@
 import { useHistory } from 'react-router-dom';
-import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Divider, List, TablePagination, Typography } from '@mui/material';
-import { useFetch } from '../../../../utils/hooks/useFetch';
-import { SearchApiPath } from '../../../../api/apiPaths';
-import { SearchResponse } from '../../../../types/common.types';
 import { ListSkeleton } from '../../../../components/ListSkeleton';
 import { CentralImportResultItem } from './CentralImportResultItem';
 import { dataTestId } from '../../../../utils/dataTestIds';
 import { ROWS_PER_PAGE_OPTIONS } from '../../../../utils/constants';
 import { SearchParam } from '../../../../utils/searchHelpers';
-import { stringIncludesMathJax, typesetMathJax } from '../../../../utils/mathJaxHelpers';
-import { ImportCandidate } from '../../../../types/importCandidate';
+import { useQuery } from '@tanstack/react-query';
+import { fetchImportCandidates } from '../../../../api/searchApi';
+import { setNotification } from '../../../../redux/notificationSlice';
+import { useDispatch } from 'react-redux';
 
 export const CentralImportPage = () => {
   const { t } = useTranslation();
@@ -22,9 +20,17 @@ export const CentralImportPage = () => {
   const rowsPerPage = (resultsParam && +resultsParam) || ROWS_PER_PAGE_OPTIONS[1];
   const page = (fromParam && resultsParam && Math.floor(+fromParam / rowsPerPage)) || 0;
 
-  const [searchResults, isLoadingSearchResults] = useFetch<SearchResponse<ImportCandidate>>({
-    url: `${SearchApiPath.ImportCandidates}?${params.toString()}`,
-    errorMessage: t('feedback.error.search'),
+  const dispatch = useDispatch();
+  const importCandidateQuery = useQuery({
+    queryKey: ['importCandidates', []],
+    queryFn: () => fetchImportCandidates(),
+    onError: () =>
+      dispatch(
+        setNotification({
+          message: t('feedback.error.get_messages'),
+          variant: 'error',
+        })
+      ),
   });
 
   const updatePath = (from: string, results: string) => {
@@ -33,35 +39,29 @@ export const CentralImportPage = () => {
     history.push({ search: params.toString() });
   };
 
-  const importCandidates = searchResults?.hits ?? [];
-
-  useEffect(() => {
-    if (searchResults?.hits.some(({ mainTitle }) => stringIncludesMathJax(mainTitle))) {
-      typesetMathJax();
-    }
-  }, [searchResults]);
+  const searchResults = importCandidateQuery.data?.hits ?? [];
 
   return (
     <>
       <Typography variant="h3">{t('basic_data.central_import.publications')}</Typography>
-      {isLoadingSearchResults ? (
+      {importCandidateQuery.isLoading ? (
         <ListSkeleton minWidth={100} maxWidth={100} height={100} />
       ) : (
         searchResults && (
           <>
-            <Typography variant="subtitle1">{t('search.hits', { count: searchResults.size })}:</Typography>
+            <Typography variant="subtitle1">{t('search.hits', { count: searchResults.length })}:</Typography>
             <Divider />
             <List>
-              {importCandidates.map((publication) => (
-                <CentralImportResultItem importCandidate={publication} key={publication.id} />
+              {searchResults.map((importCandidate) => (
+                <CentralImportResultItem importCandidate={importCandidate} key={importCandidate.id} />
               ))}
             </List>
-            {importCandidates.length > 0 && (
+            {searchResults.length > 0 && (
               <TablePagination
                 data-testid={dataTestId.basicData.centralImport.searchPagination}
                 rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
                 component="div"
-                count={searchResults.size}
+                count={searchResults.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 onPageChange={(_, newPage) => updatePath((newPage * rowsPerPage).toString(), rowsPerPage.toString())}
