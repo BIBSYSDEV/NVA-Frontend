b3ec3ec83af9559b4402d0438fae1e154645ae2e
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-06-08 10:32:20 +0200

    NP-44832 Refetch logic to fetch curators

--- src/pages/editor/EditorCurators.tsx
+++ src/pages/editor/EditorCurators.tsx
@@ -14,6 +14,7 @@ import {
   TableRow,
   Typography,
 } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
 import { ROWS_PER_PAGE_OPTIONS } from '../../utils/constants';
 import { alternatingTableRowColor } from '../../themes/mainTheme';
 import { ViewingScopeCell } from '../basic_data/institution_admin/ViewingScopeCell';
@@ -21,98 +22,89 @@ import { RootState } from '../../redux/store';
 import { useFetchResource } from '../../utils/hooks/useFetchResource';
 import { Organization } from '../../types/organization.types';
 import { getSortedSubUnits } from '../../utils/institutions-helpers';
-import { RoleApiPath } from '../../api/apiPaths';
-import { useFetch } from '../../utils/hooks/useFetch';
-import { RoleName, UserList } from '../../types/user.types';
-import { filterUsersByRole } from '../../utils/role-helpers';
+import { RoleName } from '../../types/user.types';
+import { fetchUsers } from '../../api/roleApi';
 
 export const EditorCurators = () => {
   const { t } = useTranslation();
-  const [rowsPerPage, setRowsPerPage] = useState(ROWS_PER_PAGE_OPTIONS[0]);
+  const [rowsPerPage, setRowsPerPage] = useState(ROWS_PER_PAGE_OPTIONS[1]);
   const [page, setPage] = useState(0);
 
-  const user = useSelector((store: RootState) => store.user);
+  const { user } = useSelector((store: RootState) => store);
+
   const [currentOrganization, isLoadingCurrentOrganization] = useFetchResource<Organization>(
     user?.topOrgCristinId ?? ''
   );
-  const [institutionUsers, isLoading] = useFetch<UserList>({
-    url: user?.customerId ? `${RoleApiPath.InstitutionUsers}?institution=${encodeURIComponent(user.customerId)}` : '',
-    errorMessage: t('feedback.error.get_users_for_institution'),
-    withAuthentication: true,
-  });
 
-  const curators = filterUsersByRole(institutionUsers?.users ?? [], RoleName.Curator);
+  const customerId = user?.customerId ?? '';
+  const curatorsQuery = useQuery({
+    queryKey: ['curators', customerId],
+    enabled: !!customerId,
+    queryFn: () => (customerId ? fetchUsers(customerId, RoleName.Curator) : undefined),
+    meta: { errorMessage: t('feedback.error.get_users_for_institution') },
+  });
+  const curators = curatorsQuery.data ?? [];
 
   // Ensure selected page is not out of bounds due to manipulated userList
   const validPage = curators.length <= page * rowsPerPage ? 0 : page;
 
-  const sortedList = curators.sort((a, b) =>
-    `${a.givenName} ${a.familyName}`.toLocaleLowerCase() < `${b.givenName} ${b.familyName}`.toLocaleLowerCase() ? -1 : 1
-  );
-
-  return (
+  return curatorsQuery.isLoading ? (
+    <Box sx={{ display: 'flex', justifyContent: 'space-around', mt: '2rem' }}>
+      <CircularProgress aria-label={t('editor.curators.areas_of_responsibility')} />
+    </Box>
+  ) : (
     <>
-      {isLoading ? (
-        <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
-          <CircularProgress aria-label={t('editor.curators.areas_of_responsibility')} />
-        </Box>
+      {curators.length === 0 ? (
+        <Typography>
+          <i>{t('editor.curators.no_users_found')}</i>
+        </Typography>
       ) : (
         <>
-          {sortedList.length === 0 ? (
-            <Typography>
-              <i>{t('editor.curators.no_users_found')}</i>
-            </Typography>
-          ) : (
-            <>
-              <TableContainer component={Paper}>
-                <Table size="small" sx={alternatingTableRowColor}>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>{t('common.name')}</TableCell>
-                      <TableCell sx={{ minWidth: { xs: '15rem', md: '40%' } }}>
-                        {t('editor.curators.area_of_responsibility')}
-                      </TableCell>
-                    </TableRow>
-                  </TableHead>
+          <TableContainer component={Paper}>
+            <Table size="small" sx={alternatingTableRowColor}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>{t('common.name')}</TableCell>
+                  <TableCell sx={{ minWidth: { xs: '15rem', md: '40%' } }}>
+                    {t('editor.curators.area_of_responsibility')}
+                  </TableCell>
+                </TableRow>
+              </TableHead>
 
-                  <TableBody>
-                    {sortedList.slice(validPage * rowsPerPage, validPage * rowsPerPage + rowsPerPage).map((curator) => {
-                      return (
-                        <TableRow key={curator.username}>
-                          <TableCell>
-                            {curator.givenName} {curator.familyName}
-                          </TableCell>
-                          <TableCell>
-                            {isLoadingCurrentOrganization ? (
-                              <CircularProgress />
-                            ) : (
-                              <ViewingScopeCell
-                                user={curator}
-                                options={currentOrganization ? getSortedSubUnits([currentOrganization]) : []}
-                              />
-                            )}
-                          </TableCell>
-                        </TableRow>
-                      );
-                    })}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-              {sortedList.length > ROWS_PER_PAGE_OPTIONS[0] && (
-                <TablePagination
-                  rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
-                  component="div"
-                  count={sortedList.length}
-                  rowsPerPage={rowsPerPage}
-                  page={validPage}
-                  onPageChange={(_, newPage) => setPage(newPage)}
-                  onRowsPerPageChange={(event) => {
-                    setRowsPerPage(parseInt(event.target.value));
-                    setPage(0);
-                  }}
-                />
-              )}
-            </>
+              <TableBody>
+                {curators.slice(validPage * rowsPerPage, validPage * rowsPerPage + rowsPerPage).map((curator) => (
+                  <TableRow key={curator.username}>
+                    <TableCell>
+                      {curator.givenName} {curator.familyName}
+                    </TableCell>
+                    <TableCell>
+                      {isLoadingCurrentOrganization ? (
+                        <CircularProgress />
+                      ) : (
+                        <ViewingScopeCell
+                          user={curator}
+                          options={currentOrganization ? getSortedSubUnits([currentOrganization]) : []}
+                        />
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          {curators.length > ROWS_PER_PAGE_OPTIONS[0] && (
+            <TablePagination
+              rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
+              component="div"
+              count={curators.length}
+              rowsPerPage={rowsPerPage}
+              page={validPage}
+              onPageChange={(_, newPage) => setPage(newPage)}
+              onRowsPerPageChange={(event) => {
+                setRowsPerPage(parseInt(event.target.value));
+                setPage(0);
+              }}
+            />
           )}
         </>
       )}
