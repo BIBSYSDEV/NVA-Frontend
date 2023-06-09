3855cb820fb76b658b1dfb16f6f74bfba2015de2
Isar Buzza <isar.buzza@sikt.no>
2023-06-09 13:55:14 +0200

    NP-44570 use reactQuery for project search

--- src/pages/search/project_search/ProjectSearch.tsx
+++ src/pages/search/project_search/ProjectSearch.tsx
@@ -2,18 +2,19 @@ import { Box, List, Typography } from '@mui/material';
 import { Field, FieldProps } from 'formik';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
-import { CristinApiPath } from '../../../api/apiPaths';
 import { ListSkeleton } from '../../../components/ListSkeleton';
-import { SearchResponse } from '../../../types/common.types';
-import { CristinProject } from '../../../types/project.types';
-import { useFetch } from '../../../utils/hooks/useFetch';
 import { SearchParam } from '../../../utils/searchHelpers';
 import { CristinSearchPagination } from '../CristinSearchPagination';
 import { SearchTextField } from '../SearchTextField';
 import { ProjectListItem } from './ProjectListItem';
+import { useQuery } from '@tanstack/react-query';
+import { searchForProjects } from '../../../api/cristinApi';
+import { useDispatch } from 'react-redux';
+import { setNotification } from '../../../redux/notificationSlice';
 
 export const ProjectSearch = () => {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
   const location = useLocation();
   const projectSearchQueryParams = new URLSearchParams(location.search);
   projectSearchQueryParams.delete(SearchParam.Type);
@@ -24,13 +25,21 @@ export const ProjectSearch = () => {
   if (!projectSearchQueryParams.get(SearchParam.Results)) {
     projectSearchQueryParams.set(SearchParam.Results, '10');
   }
+  if (!projectSearchQueryParams.get(SearchParam.Page)) {
+    projectSearchQueryParams.set(SearchParam.Page, '1');
+  }
 
-  const queryParams = projectSearchQueryParams.toString();
+  const rowsPerPage = Number(projectSearchQueryParams.get(SearchParam.Results));
+  const page = Number(projectSearchQueryParams.get(SearchParam.Page));
+  const titleQuery = projectSearchQueryParams.get(SearchParam.Query);
 
-  const [projectsSearch, isLoadingProjectsSearch] = useFetch<SearchResponse<CristinProject>>({
-    url: queryParams ? `${CristinApiPath.Project}?${queryParams}` : '',
-    errorMessage: t('feedback.error.project_search'),
+  const projectsQuery = useQuery({
+    queryKey: ['projects', rowsPerPage, page, titleQuery],
+    queryFn: () => searchForProjects(rowsPerPage, page, { query: titleQuery ?? undefined }),
+    onError: () => dispatch(setNotification({ message: t('feedback.error.search'), variant: 'error' })),
   });
+
+  const projectsSearchResults = projectsQuery.data?.hits ?? [];
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
@@ -47,16 +56,16 @@ export const ProjectSearch = () => {
         )}
       </Field>
 
-      {isLoadingProjectsSearch ? (
+      {projectsQuery.isLoading ? (
         <ListSkeleton arrayLength={3} minWidth={40} height={100} />
-      ) : projectsSearch && projectsSearch.hits.length > 0 ? (
+      ) : projectsSearchResults && projectsSearchResults.length > 0 ? (
         <>
           <List>
-            {projectsSearch.hits.map((project) => (
+            {projectsSearchResults.map((project) => (
               <ProjectListItem key={project.id} project={project} />
             ))}
           </List>
-          <CristinSearchPagination totalCount={projectsSearch.size} />
+          <CristinSearchPagination totalCount={projectsQuery.data?.size ?? 0} />
         </>
       ) : (
         <Typography>{t('common.no_hits')}</Typography>
