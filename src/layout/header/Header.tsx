d21f55933cd0354246d955e778b0edaec8f9756b
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-06-27 15:37:31 +0200

    NP-44951 Move about NVA link to footer

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,10 +1,9 @@
-import { useState, MouseEvent, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
-import { AppBar, Box, Button, Divider, IconButton, Theme, Typography, useMediaQuery } from '@mui/material';
+import { AppBar, Box, Button, Divider, Theme, Typography, useMediaQuery } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import AssignmentIcon from '@mui/icons-material/AssignmentOutlined';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenterOutlined';
@@ -13,7 +12,6 @@ import { RootState } from '../../redux/store';
 import { UrlPathTemplate } from '../../utils/urlPaths';
 import { LoginButton } from './LoginButton';
 import { Logo } from './Logo';
-import { GeneralMenu } from './GeneralMenu';
 import { LanguageSelector } from './LanguageSelector';
 import { dataTestId } from '../../utils/dataTestIds';
 import { useFetch } from '../../utils/hooks/useFetch';
@@ -39,13 +37,8 @@ export const Header = () => {
     }
   }, [dispatch, customer]);
 
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
   const isLargeScreen = useMediaQuery((theme: Theme) => theme.breakpoints.up('lg'));
-
-  const handleClick = (event: MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
 
   return (
     <AppBar position="static" elevation={0} sx={{ color: 'white' }}>
@@ -55,24 +48,13 @@ export const Header = () => {
           display: 'grid',
           justifyItems: 'center',
           gridTemplateAreas: {
-            xs: '"other-menu logo user-menu"',
-            lg: '"other-menu logo search new-result user-menu"',
+            xs: '"language logo user-menu"',
+            lg: '"language logo search new-result user-menu"',
           },
           gridTemplateColumns: { xs: 'auto auto auto', lg: '1fr auto 1fr 10fr 5fr' },
           gap: '1rem',
           px: '1rem',
         }}>
-        <IconButton
-          data-testid={dataTestId.header.generalMenuButton}
-          onClick={handleClick}
-          title={t('common.menu')}
-          size="large"
-          color="inherit"
-          sx={{ gridArea: 'other-menu' }}>
-          <MenuIcon fontSize="large" />
-        </IconButton>
-        <GeneralMenu anchorEl={anchorEl} onClose={() => setAnchorEl(null)} />
-
         <Logo />
 
         {isLargeScreen && (
