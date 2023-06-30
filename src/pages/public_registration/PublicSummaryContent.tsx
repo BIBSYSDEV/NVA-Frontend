39ec0ea015be4485df4b1c746bc5e135d4bf0899
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-06-30 11:25:47 +0200

    NP-44954 Remove redundant accordion

--- src/pages/public_registration/PublicSummaryContent.tsx
+++ src/pages/public_registration/PublicSummaryContent.tsx
@@ -1,6 +1,5 @@
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { PublicRegistrationContentProps } from './PublicRegistrationContent';
 
 export const PublicSummaryContent = ({ registration }: PublicRegistrationContentProps) => {
@@ -16,18 +15,14 @@ export const PublicSummaryContent = ({ registration }: PublicRegistrationContent
         </Typography>
       )}
       {entityDescription.alternativeAbstracts.und && (
-        <Accordion elevation={0}>
-          <AccordionSummary expandIcon={<ExpandMoreIcon color="primary" />} sx={{ padding: '0' }}>
-            <Typography variant="h3" color="primary">
-              {t('registration.description.alternative_abstract')}
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails sx={{ padding: 0 }}>
-            <Typography style={{ whiteSpace: 'pre-line' }} paragraph>
-              {entityDescription.alternativeAbstracts.und}
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
+        <>
+          <Typography variant="h3" color="primary" gutterBottom>
+            {t('registration.description.alternative_abstract')}
+          </Typography>
+          <Typography style={{ whiteSpace: 'pre-line' }} paragraph>
+            {entityDescription.alternativeAbstracts.und}
+          </Typography>
+        </>
       )}
       {entityDescription.description && (
         <>
