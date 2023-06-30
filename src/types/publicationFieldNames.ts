d5654021cf478f82a0c4acbe43f61c97d7391893
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-06-30 13:36:08 +0200

    NP-44916 Update key for funding source search

--- src/types/publicationFieldNames.ts
+++ src/types/publicationFieldNames.ts
@@ -228,5 +228,5 @@ export enum SearchFieldName {
   TopLevelOrganizationId = 'topLevelOrganization.id',
   TopLevelOrganization = 'topLevelOrganization',
   ContributorId = 'entityDescription.contributors.identity.id',
-  FundingSource = 'fundings.identifier',
+  FundingSource = 'fundings.source.identifier',
 }
