67a168c86e1c5d82a9ade5a76d530cede396189b
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-07-04 08:42:12 +0100

    NP-44905 Update facets filter with FieldArray method

--- src/pages/search/registration_search/RegistrationSearchBar.tsx
+++ src/pages/search/registration_search/RegistrationSearchBar.tsx
@@ -19,17 +19,13 @@ interface RegistrationSearchBarProps {
 
 export const RegistrationSearchBar = ({ aggregations }: RegistrationSearchBarProps) => {
   const { t } = useTranslation();
-  const { values, submitForm, setFieldValue } = useFormikContext<SearchConfig>();
+  const { values, submitForm } = useFormikContext<SearchConfig>();
   const properties = values.properties ?? [];
 
   const showAdvancedSearch = properties.some(
     (property) =>
       !property.fieldName ||
       registrationFilters.some((filter) => filter.field === property.fieldName && filter.manuallyAddable)
-  );
-
-  const facetProperties = properties.filter((property) =>
-    registrationFilters.some((filter) => filter.field === property.fieldName && !filter.manuallyAddable)
   );
 
   return (
@@ -64,11 +60,14 @@ export const RegistrationSearchBar = ({ aggregations }: RegistrationSearchBarPro
 
       <FieldArray name="properties">
         {({ push, remove }: FieldArrayRenderProps) => (
-          <Box gridArea="advanced" sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-            {properties.map(
-              (property, index) =>
-                !property.fieldName ||
-                registrationFilters.find((filter) => filter.field === property.fieldName)?.manuallyAddable ? (
+          <>
+            <Box gridArea="advanced" sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+              {properties.map((property, index) => {
+                const thisFilter = registrationFilters.find((filter) => filter.field === property.fieldName);
+                if (property.fieldName && !thisFilter?.manuallyAddable) {
+                  return null;
+                }
+                return (
                   <AdvancedSearchRow
                     key={index}
                     propertySearchItem={property}
@@ -78,102 +77,99 @@ export const RegistrationSearchBar = ({ aggregations }: RegistrationSearchBarPro
                     }}
                     baseFieldName={`properties[${index}]`}
                   />
-                ) : null // Hide fields where user cannot set values themself. Typically terms based on aggregations (facets)
-            )}
-            <Box sx={{ display: 'flex', gap: '1rem' }}>
-              <Button
-                data-testid={dataTestId.startPage.advancedSearch.addFilterButton}
-                variant="outlined"
-                onClick={() => {
-                  const newPropertyFilter: PropertySearch = {
-                    fieldName: '',
-                    value: '',
-                    operator: ExpressionStatement.Contains,
-                  };
-                  push(newPropertyFilter);
-                }}
-                startIcon={<FilterAltIcon />}>
-                {t('search.add_filter')}
-              </Button>
-              {showAdvancedSearch && (
+                );
+              })}
+              <Box sx={{ display: 'flex', gap: '1rem' }}>
                 <Button
-                  variant="contained"
-                  type="submit"
-                  startIcon={<SearchIcon />}
-                  data-testid={dataTestId.startPage.advancedSearch.searchButton}>
-                  {t('common.search')}
+                  data-testid={dataTestId.startPage.advancedSearch.addFilterButton}
+                  variant="outlined"
+                  onClick={() => {
+                    const newPropertyFilter: PropertySearch = {
+                      fieldName: '',
+                      value: '',
+                      operator: ExpressionStatement.Contains,
+                    };
+                    push(newPropertyFilter);
+                  }}
+                  startIcon={<FilterAltIcon />}>
+                  {t('search.add_filter')}
                 </Button>
-              )}
+                {showAdvancedSearch && (
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    startIcon={<SearchIcon />}
+                    data-testid={dataTestId.startPage.advancedSearch.searchButton}>
+                    {t('common.search')}
+                  </Button>
+                )}
+              </Box>
             </Box>
-          </Box>
+
+            {aggregations && (
+              <Box sx={{ gridArea: 'facets', display: 'flex', gap: '0.25rem 0.5rem', flexWrap: 'wrap' }}>
+                {properties.map((property, index) => {
+                  const thisFilter = registrationFilters.find((filter) => filter.field === property.fieldName);
+
+                  if (!thisFilter || thisFilter.manuallyAddable) {
+                    return null;
+                  }
+
+                  const fieldName = t(thisFilter.i18nKey) as string;
+                  let fieldValueText = '';
+                  switch (thisFilter.field) {
+                    case ResourceFieldNames.RegistrationType:
+                      fieldValueText = t(`registration.publication_types.${property.value as PublicationInstanceType}`);
+                      break;
+                    case SearchFieldName.ContributorId:
+                      fieldValueText =
+                        aggregations.entityDescription.contributors.identity.id.buckets.find(
+                          (bucket) => bucket.key === property.value
+                        )?.name.buckets[0].key ?? t('common.unknown');
+                      break;
+                    case SearchFieldName.TopLevelOrganizationId: {
+                      const institutionLabels = aggregations.topLevelOrganization.id.buckets.find(
+                        (bucket) => bucket.key === property.value
+                      );
+                      fieldValueText = institutionLabels
+                        ? getLabelFromBucket(institutionLabels) ?? t('common.unknown')
+                        : t('common.unknown');
+                      break;
+                    }
+                    case SearchFieldName.FundingSource: {
+                      const fundingLabels = aggregations.fundings.identifier.buckets.find(
+                        (bucket) => bucket.key === property.value
+                      );
+                      fieldValueText = fundingLabels
+                        ? getLabelFromBucket(fundingLabels) ?? t('common.unknown')
+                        : t('common.unknown');
+                      break;
+                    }
+                    default:
+                      fieldValueText = typeof property.value === 'string' ? property.value : t('common.unknown');
+                  }
+
+                  return (
+                    <Button
+                      key={property.fieldName + index}
+                      variant="outlined"
+                      size="small"
+                      title={t('search.remove_filter')}
+                      sx={{ textTransform: 'none' }}
+                      endIcon={<ClearIcon />}
+                      onClick={() => {
+                        remove(index);
+                        submitForm();
+                      }}>
+                      {fieldName}: {fieldValueText}
+                    </Button>
+                  );
+                })}
+              </Box>
+            )}
+          </>
         )}
       </FieldArray>
-
-      {aggregations && facetProperties.length > 0 && (
-        <Box sx={{ gridArea: 'facets', display: 'flex', gap: '0.25rem 0.5rem', flexWrap: 'wrap' }}>
-          {facetProperties.map((property, index) => {
-            const thisFilter = registrationFilters.find((filter) => filter.field === property.fieldName);
-
-            if (!thisFilter) return null;
-
-            const fieldName = t(thisFilter.i18nKey) as string;
-            let fieldValueText = '';
-            switch (thisFilter.field) {
-              case ResourceFieldNames.RegistrationType:
-                fieldValueText = t(`registration.publication_types.${property.value as PublicationInstanceType}`);
-                break;
-              case SearchFieldName.ContributorId:
-                fieldValueText =
-                  aggregations.entityDescription.contributors.identity.id.buckets.find(
-                    (bucket) => bucket.key === property.value
-                  )?.name.buckets[0].key ?? t('common.unknown');
-                break;
-              case SearchFieldName.TopLevelOrganizationId: {
-                const institutionLabels = aggregations.topLevelOrganization.id.buckets.find(
-                  (bucket) => bucket.key === property.value
-                );
-                fieldValueText = institutionLabels
-                  ? getLabelFromBucket(institutionLabels) ?? t('common.unknown')
-                  : t('common.unknown');
-                break;
-              }
-              case SearchFieldName.FundingSource: {
-                const fundingLabels = aggregations.fundings.identifier.buckets.find(
-                  (bucket) => bucket.key === property.value
-                );
-                fieldValueText = fundingLabels
-                  ? getLabelFromBucket(fundingLabels) ?? t('common.unknown')
-                  : t('common.unknown');
-                break;
-              }
-            }
-
-            return (
-              <Button
-                key={property.fieldName + index}
-                variant="outlined"
-                size="small"
-                title={t('search.remove_filter')}
-                sx={{ textTransform: 'none' }}
-                endIcon={<ClearIcon />}
-                onClick={() => {
-                  const indexToRemove = properties.findIndex(
-                    (prop) =>
-                      prop.fieldName === thisFilter.field &&
-                      prop.value === property.value &&
-                      prop.operator === property.operator
-                  );
-
-                  const newProperties = properties.filter((_, i) => i !== indexToRemove);
-                  setFieldValue('properties', newProperties);
-                  submitForm();
-                }}>
-                {fieldName}: {fieldValueText}
-              </Button>
-            );
-          })}
-        </Box>
-      )}
     </Box>
   );
 };
