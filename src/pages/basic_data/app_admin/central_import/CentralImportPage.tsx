835f28c75a8a588264fc9ba6d6b2b844d765fa06
Kir Truhacev <kir.truhacev@sikt.no>
2023-08-01 11:26:03 +0200

    code review

--- src/pages/basic_data/app_admin/central_import/CentralImportPage.tsx
+++ src/pages/basic_data/app_admin/central_import/CentralImportPage.tsx
@@ -19,9 +19,8 @@ export const CentralImportPage = () => {
   const fromParam = params.get(SearchParam.From);
   const rowsPerPage = (resultsParam && +resultsParam) || ROWS_PER_PAGE_OPTIONS[1];
   const page = (fromParam && resultsParam && Math.floor(+fromParam / rowsPerPage)) || 0;
-  const query = '';
   const importCandidateQuery = useQuery({
-    queryKey: ['importCandidates', rowsPerPage, page, query],
+    queryKey: ['importCandidates', rowsPerPage, page, ''],
     queryFn: () => fetchImportCandidates(rowsPerPage, page * rowsPerPage, ''),
     meta: { errorMessage: t('feedback.error.get_registrations') },
   });
