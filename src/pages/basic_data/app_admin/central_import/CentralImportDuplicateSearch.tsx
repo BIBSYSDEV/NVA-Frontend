567b503c2b32d44eed31e82e1d13f8b236319a5e
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-08-14 15:55:57 +0200

    NP-45023 Search for duplicated registrations

--- src/pages/basic_data/app_admin/central_import/CentralImportDuplicateSearch.tsx
+++ src/pages/basic_data/app_admin/central_import/CentralImportDuplicateSearch.tsx
@@ -1,13 +1,14 @@
-import { Divider, List, Typography } from '@mui/material';
+import { Divider, Typography } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { fetchImportCandidates } from '../../../../api/searchApi';
+import { fetchResults } from '../../../../api/searchApi';
 import { ListPagination } from '../../../../components/ListPagination';
 import { ListSkeleton } from '../../../../components/ListSkeleton';
+import { RegistrationList } from '../../../../components/RegistrationList';
 import { DuplicateSearchFilters } from '../../../../types/duplicateSearchTypes';
+import { DescriptionFieldNames, ResourceFieldNames } from '../../../../types/publicationFieldNames';
 import { ROWS_PER_PAGE_OPTIONS } from '../../../../utils/constants';
-import { CentralImportResultItem } from './CentralImportResultItem';
 
 interface CentralImportDuplicateSearchProps {
   duplicateSearchFilters: DuplicateSearchFilters;
@@ -19,29 +20,30 @@ export const CentralImportDuplicateSearch = ({ duplicateSearchFilters }: Central
   const [rowsPerPage, setRowsPerPage] = useState(ROWS_PER_PAGE_OPTIONS[0]);
 
   const queryArray = [];
-  duplicateSearchFilters.doi.length > 0 && queryArray.push(`doi:"${duplicateSearchFilters.doi}"`);
-  duplicateSearchFilters.title.length > 0 && queryArray.push(`mainTitle:"${duplicateSearchFilters.title}"`);
+  duplicateSearchFilters.doi.length > 0 && queryArray.push(`${ResourceFieldNames.Doi}:"${duplicateSearchFilters.doi}"`);
+  duplicateSearchFilters.title.length > 0 &&
+    queryArray.push(`${DescriptionFieldNames.Title}:"${duplicateSearchFilters.title}"`);
   duplicateSearchFilters.author.length > 0 &&
     queryArray.push(`entityDescription.contributors.identity.name:"${duplicateSearchFilters.author}"`);
   duplicateSearchFilters.issn.length > 0 &&
     queryArray.push(`entityDescription.reference.publicationContext.printIssn:"${duplicateSearchFilters.issn}"`);
   duplicateSearchFilters.yearPublished.length > 0 &&
-    queryArray.push(`publicationYear:"${duplicateSearchFilters.yearPublished}"`);
+    queryArray.push(`${DescriptionFieldNames.PublicationYear}:"${duplicateSearchFilters.yearPublished}"`);
 
   const searchQuery = queryArray.length > 0 ? queryArray.join(' AND ') : '';
 
-  const importCandidateQuery = useQuery({
-    queryKey: ['importCandidates', rowsPerPage, page, searchQuery],
-    queryFn: () => fetchImportCandidates(rowsPerPage, (page - 1) * rowsPerPage, searchQuery),
+  const duplicatesQuery = useQuery({
+    queryKey: ['registrationsSearch', rowsPerPage, page, searchQuery],
+    queryFn: () => fetchResults(rowsPerPage, (page - 1) * rowsPerPage, searchQuery),
     meta: { errorMessage: t('feedback.error.get_registrations') },
   });
 
-  const searchResults = importCandidateQuery.data?.hits ?? [];
-  const totalCount = importCandidateQuery.data?.size ?? 0;
+  const searchResults = duplicatesQuery.data?.hits ?? [];
+  const totalCount = duplicatesQuery.data?.size ?? 0;
 
   return (
     <>
-      {importCandidateQuery.isLoading ? (
+      {duplicatesQuery.isLoading ? (
         <ListSkeleton minWidth={100} maxWidth={100} height={100} />
       ) : (
         searchResults && (
@@ -49,16 +51,12 @@ export const CentralImportDuplicateSearch = ({ duplicateSearchFilters }: Central
             <Typography variant="subtitle1">
               {t('basic_data.central_import.duplicate_search_hits_shown', {
                 ShownResultsCount: searchResults.length,
-                TotalResultsCount: importCandidateQuery.data?.size,
+                TotalResultsCount: duplicatesQuery.data?.size,
               })}
               :
             </Typography>
             <Divider />
-            <List>
-              {searchResults.map((importCandidate) => (
-                <CentralImportResultItem importCandidate={importCandidate} key={importCandidate.id} />
-              ))}
-            </List>
+            <RegistrationList registrations={duplicatesQuery.data?.hits ?? []} />
             <ListPagination
               count={totalCount}
               rowsPerPage={rowsPerPage}
