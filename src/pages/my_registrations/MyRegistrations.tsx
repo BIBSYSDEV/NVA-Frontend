c794bfd7cd0304f4da9c1ca01403ae80c7f88a16
Isar Buzza <isar.buzza@sikt.no>
2023-08-15 11:46:18 +0200

    NP-38859 added button and confirm dialog

--- src/pages/my_registrations/MyRegistrations.tsx
+++ src/pages/my_registrations/MyRegistrations.tsx
@@ -1,11 +1,17 @@
-import { Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
-import { fetchRegistrationsByOwner } from '../../api/registrationApi';
+import { deleteRegistration, fetchRegistrationsByOwner } from '../../api/registrationApi';
 import { ListSkeleton } from '../../components/ListSkeleton';
-import { RegistrationStatus } from '../../types/registration.types';
+import { Registration, RegistrationStatus } from '../../types/registration.types';
 import { MyRegistrationsList } from './MyRegistrationsList';
+import { ConfirmDialog } from '../../components/ConfirmDialog';
+import { useState } from 'react';
+import { setNotification } from '../../redux/notificationSlice';
+import { isErrorStatus, isSuccessStatus } from '../../utils/constants';
+import { getIdentifierFromId } from '../../utils/general-helpers';
+import { useDispatch } from 'react-redux';
 
 interface MyRegistrationsProps {
   selectedPublished: boolean;
@@ -14,6 +20,11 @@ interface MyRegistrationsProps {
 
 export const MyRegistrations = ({ selectedUnpublished, selectedPublished }: MyRegistrationsProps) => {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [registrationToDelete, setRegistrationToDelete] = useState<Registration>();
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const registrationsQuery = useQuery({
     queryKey: ['by-owner'],
@@ -44,6 +55,28 @@ export const MyRegistrations = ({ selectedUnpublished, selectedPublished }: MyRe
       return 0;
     });
 
+  const deleteDraftRegistration = async () => {
+    if (!registrationToDelete) {
+      return;
+    }
+    const identifierToDelete = getIdentifierFromId(registrationToDelete.id);
+    setIsDeleting(true);
+    const deleteRegistrationResponse = await deleteRegistration(identifierToDelete);
+    if (isErrorStatus(deleteRegistrationResponse.status)) {
+      dispatch(setNotification({ message: t('feedback.error.delete_registration'), variant: 'error' }));
+      setIsDeleting(false);
+    } else if (isSuccessStatus(deleteRegistrationResponse.status)) {
+      dispatch(setNotification({ message: t('feedback.success.delete_registration'), variant: 'success' }));
+      setIsDeleting(false);
+      registrationsQuery.refetch();
+      setShowDeleteModal(false);
+    }
+  };
+
+  const deleteAllOnClick = () => {
+    setShowDeleteModal(true);
+  };
+
   return (
     <>
       <Helmet>
@@ -54,9 +87,14 @@ export const MyRegistrations = ({ selectedUnpublished, selectedPublished }: MyRe
           <ListSkeleton minWidth={100} maxWidth={100} height={100} />
         ) : (
           <>
-            <Typography gutterBottom variant="h2">
-              {t('common.result_registrations')}
-            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Typography gutterBottom variant="h2">
+                {t('common.result_registrations')}
+              </Typography>
+              <Button variant="outlined" color="error" onClick={deleteAllOnClick}>
+                <Typography>{t('common.delete_all')}</Typography>
+              </Button>
+            </Box>
             <MyRegistrationsList
               registrations={filteredRegistrations.length > 0 ? filteredRegistrations : registrations}
               refetchRegistrations={registrationsQuery.refetch}
@@ -64,6 +102,16 @@ export const MyRegistrations = ({ selectedUnpublished, selectedPublished }: MyRe
           </>
         )}
       </div>
+
+      <ConfirmDialog
+        open={!!showDeleteModal}
+        title={t('my_page.registrations.delete_registration')}
+        onAccept={deleteDraftRegistration}
+        onCancel={() => setShowDeleteModal(false)}
+        isLoading={isDeleting}
+        dialogDataTestId="confirm-delete-dialog">
+        <Typography>Slett alle upubliserte registreringer?</Typography>
+      </ConfirmDialog>
     </>
   );
 };
