d26a45ff0f3493b8330613a3a8ac717b99e10d5e
Isar Buzza <isar.buzza@sikt.no>
2023-08-21 09:08:18 +0200

    added promies.all to delete function

--- src/pages/my_registrations/MyRegistrations.tsx
+++ src/pages/my_registrations/MyRegistrations.tsx
@@ -1,18 +1,17 @@
-import { useState } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
-import { LoadingButton } from '@mui/lab';
-import { ListSkeleton } from '../../components/ListSkeleton';
-import { MyRegistrationsList } from './MyRegistrationsList';
-import { RegistrationPreview, RegistrationStatus } from '../../types/registration.types';
 import { deleteRegistration, fetchRegistrationsByOwner } from '../../api/registrationApi';
 import { ConfirmDialog } from '../../components/ConfirmDialog';
+import { ListSkeleton } from '../../components/ListSkeleton';
 import { setNotification } from '../../redux/notificationSlice';
+import { RegistrationStatus } from '../../types/registration.types';
 import { isErrorStatus, isSuccessStatus } from '../../utils/constants';
 import { getIdentifierFromId } from '../../utils/general-helpers';
+import { MyRegistrationsList } from './MyRegistrationsList';
 
 interface MyRegistrationsProps {
   selectedPublished: boolean;
@@ -55,30 +54,31 @@ export const MyRegistrations = ({ selectedUnpublished, selectedPublished }: MyRe
       return 0;
     });
 
-  const deleteAllDraftRegistrations = () => {
+  const deleteAllDraftRegistrations = async () => {
     const draftRegistrations = registrations.filter(({ status }) => status === RegistrationStatus.Draft);
 
     setIsDeleting(true);
 
-    draftRegistrations.forEach((registration) => {
-      const deleteDraftRegistration = async (registration: RegistrationPreview) => {
-        const identifierToDelete = getIdentifierFromId(registration.id);
-        const deleteRegistrationResponse = await deleteRegistration(identifierToDelete);
+    const deletePromises = draftRegistrations.map(async (registration) => {
+      const identifierToDelete = getIdentifierFromId(registration.id);
+      const deleteRegistrationResponse = await deleteRegistration(identifierToDelete);
 
-        if (isErrorStatus(deleteRegistrationResponse.status)) {
-          dispatch(setNotification({ message: t('feedback.error.delete_registration'), variant: 'error' }));
-          setIsDeleting(false);
-        } else if (isSuccessStatus(deleteRegistrationResponse.status)) {
-          dispatch(setNotification({ message: t('feedback.success.delete_registration'), variant: 'success' }));
-          registrationsQuery.refetch();
-        }
-      };
-
-      deleteDraftRegistration(registration);
+      if (isErrorStatus(deleteRegistrationResponse.status)) {
+        dispatch(setNotification({ message: t('feedback.error.delete_registration'), variant: 'error' }));
+      } else if (isSuccessStatus(deleteRegistrationResponse.status)) {
+        dispatch(setNotification({ message: t('feedback.success.delete_registration'), variant: 'success' }));
+        registrationsQuery.refetch();
+      }
     });
 
-    setIsDeleting(false);
-    setShowDeleteModal(false);
+    try {
+      await Promise.all(deletePromises);
+    } catch (error) {
+      dispatch(setNotification({ message: t('feedback.error.delete_registration'), variant: 'error' }));
+    } finally {
+      setIsDeleting(false);
+      setShowDeleteModal(false);
+    }
   };
 
   const deleteAllOnClick = () => {
@@ -99,11 +99,9 @@ export const MyRegistrations = ({ selectedUnpublished, selectedPublished }: MyRe
               <Typography gutterBottom variant="h2">
                 {t('common.result_registrations')}
               </Typography>
-              <LoadingButton variant="outlined" color="error" onClick={deleteAllOnClick}>
-                <Typography color={'error.main'}>
-                  {t('my_page.registrations.delete_all_draft_registrations')}
-                </Typography>
-              </LoadingButton>
+              <Button variant="contained" onClick={deleteAllOnClick}>
+                {t('my_page.registrations.delete_all_draft_registrations')}
+              </Button>
             </Box>
             <MyRegistrationsList
               registrations={filteredRegistrations.length > 0 ? filteredRegistrations : []}
