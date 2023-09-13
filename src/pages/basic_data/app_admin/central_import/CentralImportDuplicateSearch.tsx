edb9aa2e493d2539a3a30ec25baf5e974982f158
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-09-13 17:15:16 +0200

    NP-45247 Add radio button before matching search hits

--- src/pages/basic_data/app_admin/central_import/CentralImportDuplicateSearch.tsx
+++ src/pages/basic_data/app_admin/central_import/CentralImportDuplicateSearch.tsx
@@ -1,11 +1,12 @@
-import { Box, Typography } from '@mui/material';
+import { Box, FormControl, FormControlLabel, Radio, RadioGroup, Typography } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { fetchResults } from '../../../../api/searchApi';
 import { ListPagination } from '../../../../components/ListPagination';
 import { ListSkeleton } from '../../../../components/ListSkeleton';
-import { RegistrationList } from '../../../../components/RegistrationList';
+import { RegistrationListItemContent } from '../../../../components/RegistrationList';
+import { SearchListItem } from '../../../../components/styled/Wrappers';
 import { DuplicateSearchFilters } from '../../../../types/duplicateSearchTypes';
 import { DescriptionFieldNames, ResourceFieldNames } from '../../../../types/publicationFieldNames';
 import { ROWS_PER_PAGE_OPTIONS } from '../../../../utils/constants';
@@ -18,6 +19,8 @@ export const CentralImportDuplicateSearch = ({ duplicateSearchFilters }: Central
   const { t } = useTranslation();
   const [page, setPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(ROWS_PER_PAGE_OPTIONS[0]);
+
+  const [selectedRegistrationIdentifier, setSelectedRegistrationIdentifier] = useState('');
 
   const queryArray = [];
   duplicateSearchFilters.doi.length > 0 && queryArray.push(`${ResourceFieldNames.Doi}:"${duplicateSearchFilters.doi}"`);
@@ -52,7 +55,24 @@ export const CentralImportDuplicateSearch = ({ duplicateSearchFilters }: Central
           </Typography>
           {duplicateCandidatesSize > 0 && (
             <>
-              <RegistrationList registrations={duplicateCandidatesQuery.data?.hits ?? []} />
+              <FormControl>
+                <RadioGroup
+                  value={selectedRegistrationIdentifier}
+                  onChange={(event) => setSelectedRegistrationIdentifier(event.target.value)}>
+                  {duplicateCandidatesQuery.data?.hits.map((registration) => (
+                    <FormControlLabel
+                      key={registration.identifier}
+                      value={registration.identifier}
+                      control={<Radio />}
+                      label={
+                        <SearchListItem sx={{ borderLeftColor: 'registration.main' }}>
+                          <RegistrationListItemContent registration={registration} />
+                        </SearchListItem>
+                      }
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
               <ListPagination
                 count={duplicateCandidatesSize}
                 rowsPerPage={rowsPerPage}
