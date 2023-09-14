23eda369bccef97b4a48ae6c1e7b74738d5526c3
Isar Buzza <isar.buzza@sikt.no>
2023-09-14 14:28:47 +0200

    NP-45211 added layout for keywords

--- src/pages/my_page/user_profile/MyFieldAndBackground.tsx
+++ src/pages/my_page/user_profile/MyFieldAndBackground.tsx
@@ -1,5 +1,5 @@
 import { LoadingButton } from '@mui/lab';
-import { Box, TextField, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { Field, FieldProps, Form, Formik, FormikProps } from 'formik';
 import { useTranslation } from 'react-i18next';
@@ -9,6 +9,7 @@ import { BackgroundDiv } from '../../../components/styled/Wrappers';
 import { setNotification } from '../../../redux/notificationSlice';
 import { RootState } from '../../../redux/store';
 import { FlatCristinPerson } from '../../../types/user.types';
+import { SearchTextField } from '../../search/SearchTextField';
 import { ResearchProfilePanel } from './ResearchProfilePanel';
 
 type PersonBackgroundFormData = Pick<FlatCristinPerson, 'background'>;
@@ -76,54 +77,76 @@ export const MyFieldAndBackground = () => {
         sx={{
           bgcolor: 'info.light',
         }}>
-        <Typography variant="h2">{t('my_page.my_profile.background')}</Typography>
         <Formik initialValues={initialValues} onSubmit={(values) => updatePerson.mutate(values)} enableReinitialize>
           {({ isSubmitting, dirty }: FormikProps<PersonBackgroundFormData>) => (
-            <Form>
-              <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', mt: '1rem' }}>
-                <Typography variant="h3">{t('my_page.my_profile.field_and_background.norwegian')}</Typography>
-                <Field name={'background.no'}>
-                  {({ field }: FieldProps<string>) => (
-                    <>
-                      <TextField
-                        {...field}
-                        inputProps={{ maxLength: 200 }}
-                        label={t('my_page.my_profile.background')}
-                        variant="filled"
-                        multiline
-                        rows="3"
-                        placeholder={t('my_page.my_profile.field_and_background.background_placeholder')}
-                      />
-                      <Typography sx={{ alignSelf: 'end' }}>{field.value.length}/200</Typography>
-                    </>
-                  )}
-                </Field>
+            <>
+              <Box>
+                <Typography variant="h2">{t('my_page.my_profile.field_and_background.field')}</Typography>
+                <Typography variant="h3" sx={{ mb: '1rem', mt: '1.5rem' }}>
+                  {t('my_page.my_profile.field_and_background.field_text')}
+                </Typography>
+                <SearchTextField sx={{ bgcolor: 'white' }} />
+                <Typography variant="body1" fontStyle={'italic'} sx={{ mb: '2rem' }}>
+                  {t('my_page.my_profile.field_and_background.keywords_search_text')}
+                </Typography>
+                <Typography variant="h3" sx={{ mb: '1rem', mt: '1.5rem' }}>
+                  {t('my_page.my_profile.field_and_background.geographical_area')}
+                </Typography>
+                <SearchTextField sx={{ bgcolor: 'white' }} />
+                <Typography variant="body1" fontStyle={'italic'}>
+                  {t('my_page.my_profile.field_and_background.keywords_search_text')}
+                </Typography>
               </Box>
-              <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', mt: '1rem' }}>
-                <Typography variant="h3">{t('my_page.my_profile.field_and_background.english')}</Typography>
-                <Field name={'background.en'}>
-                  {({ field }: FieldProps<string>) => (
-                    <>
-                      <TextField
-                        {...field}
-                        inputProps={{ maxLength: 200 }}
-                        label={t('my_page.my_profile.background')}
-                        variant="filled"
-                        multiline
-                        rows="3"
-                        placeholder={t('my_page.my_profile.field_and_background.background_placeholder')}
-                      />
-                      <Typography sx={{ alignSelf: 'end' }}>{field.value.length}/200</Typography>
-                    </>
-                  )}
-                </Field>
-              </Box>
-              <Box sx={{ display: 'flex', justifyContent: 'end', mt: '1rem' }}>
-                <LoadingButton loading={isSubmitting} disabled={!dirty} variant="contained" type="submit">
-                  {t('common.save')}
-                </LoadingButton>
-              </Box>
-            </Form>
+              <Form>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', mt: '4rem' }}>
+                  <Typography variant="h2" sx={{ mb: '1rem' }}>
+                    {t('my_page.my_profile.background')}
+                  </Typography>
+                  <Typography variant="h3">{t('my_page.my_profile.field_and_background.norwegian')}</Typography>
+                  <Field name={'background.no'}>
+                    {({ field }: FieldProps<string>) => (
+                      <>
+                        <TextField
+                          {...field}
+                          inputProps={{ maxLength: 200 }}
+                          label={t('my_page.my_profile.background')}
+                          variant="filled"
+                          multiline
+                          rows="3"
+                          placeholder={t('my_page.my_profile.field_and_background.background_placeholder')}
+                        />
+                        <Typography sx={{ alignSelf: 'end' }}>{field.value.length}/200</Typography>
+                      </>
+                    )}
+                  </Field>
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', mt: '1rem' }}>
+                  <Typography variant="h3">{t('my_page.my_profile.field_and_background.english')}</Typography>
+                  <Field name={'background.en'}>
+                    {({ field }: FieldProps<string>) => (
+                      <>
+                        <TextField
+                          {...field}
+                          inputProps={{ maxLength: 200 }}
+                          label={t('my_page.my_profile.background')}
+                          variant="filled"
+                          multiline
+                          rows="3"
+                          placeholder={t('my_page.my_profile.field_and_background.background_placeholder')}
+                        />
+                        <Typography sx={{ alignSelf: 'end' }}>{field.value.length}/200</Typography>
+                      </>
+                    )}
+                  </Field>
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center', mt: '1rem' }}>
+                  <Button>{t('common.cancel')}</Button>
+                  <LoadingButton loading={isSubmitting} disabled={!dirty} variant="contained" type="submit">
+                    {t('common.save')}
+                  </LoadingButton>
+                </Box>
+              </Form>
+            </>
           )}
         </Formik>
       </BackgroundDiv>
