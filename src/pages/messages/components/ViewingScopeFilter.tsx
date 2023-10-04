25f0191d2c66b7f3435a00dea7b6f681b0fcd669
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-10-04 15:34:53 +0200

    Allow removing organization

--- src/pages/messages/components/ViewingScopeFilter.tsx
+++ src/pages/messages/components/ViewingScopeFilter.tsx
@@ -1,3 +1,4 @@
+import CancelIcon from '@mui/icons-material/Cancel';
 import {
   Autocomplete,
   Box,
@@ -6,13 +7,14 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  IconButton,
   Paper,
   Skeleton,
   TextField,
   Typography,
 } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { fetchOrganization } from '../../../api/cristinApi';
@@ -23,7 +25,7 @@ import { getLanguageString } from '../../../utils/translation-helpers';
 
 interface ViewingScopeFilterProps {
   viewingScopeIds: string[];
-  setOrganizationFilter: (ids: string[]) => void;
+  setOrganizationFilter: Dispatch<SetStateAction<string[]>>;
 }
 
 export const ViewingScopeFilter = ({ viewingScopeIds, setOrganizationFilter }: ViewingScopeFilterProps) => {
@@ -48,9 +50,13 @@ export const ViewingScopeFilter = ({ viewingScopeIds, setOrganizationFilter }: V
     : [];
 
   return (
-    <Box component="article" sx={{ m: '1rem', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-      {viewingScopeIds.map((viwewingScopeId) => (
-        <ViewingScopeItem key={viwewingScopeId} viwewingScopeId={viwewingScopeId} />
+    <Box component="article" sx={{ m: '1rem', display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+      {viewingScopeIds.map((viewingScopeId) => (
+        <ViewingScopeItem
+          key={viewingScopeId}
+          viewingScopeId={viewingScopeId}
+          setOrganizationFilter={setOrganizationFilter}
+        />
       ))}
       <BetaFunctionality>
         <Button sx={{ width: 'fit-content', alignSelf: 'center' }} onClick={toggleDialog} size="small">
@@ -89,25 +95,44 @@ export const ViewingScopeFilter = ({ viewingScopeIds, setOrganizationFilter }: V
   );
 };
 
-const ViewingScopeItem = ({ viwewingScopeId }: { viwewingScopeId: string }) => {
+interface ViewingScopeItemProps extends Pick<ViewingScopeFilterProps, 'setOrganizationFilter'> {
+  viewingScopeId: string;
+}
+
+const ViewingScopeItem = ({ viewingScopeId, setOrganizationFilter }: ViewingScopeItemProps) => {
   const { t } = useTranslation();
 
   const organizationQuery = useQuery({
-    enabled: !!viwewingScopeId,
-    queryKey: [viwewingScopeId],
-    queryFn: () => fetchOrganization(viwewingScopeId),
+    enabled: !!viewingScopeId,
+    queryKey: [viewingScopeId],
+    queryFn: () => fetchOrganization(viewingScopeId),
     meta: { errorMessage: t('feedback.error.get_institution') },
     staleTime: Infinity,
     cacheTime: 1_800_000,
   });
 
   return (
-    <Paper sx={{ p: '0.75rem', bgcolor: 'white' }} elevation={0}>
+    <Paper
+      sx={{
+        p: '0.75rem',
+        bgcolor: 'white',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        gap: '0.25rem',
+      }}
+      elevation={0}>
       {organizationQuery.isLoading ? (
-        <Skeleton />
+        <Skeleton sx={{ width: '100%' }} />
       ) : (
         <Typography sx={{ fontWeight: 700 }}>{getLanguageString(organizationQuery.data?.labels)}</Typography>
       )}
+      <IconButton
+        size="small"
+        color="primary"
+        onClick={() => setOrganizationFilter((state) => state.filter((id) => id !== viewingScopeId))}>
+        <CancelIcon />
+      </IconButton>
     </Paper>
   );
 };
