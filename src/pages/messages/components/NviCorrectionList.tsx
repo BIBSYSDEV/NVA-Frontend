2342897abc65c6fa1489b2b179b21324381b71f2
Isar Buzza <isar.buzza@sikt.no>
2023-10-17 12:24:36 +0200

    added translation to iframe title

--- src/pages/messages/components/NviCorrectionList.tsx
+++ src/pages/messages/components/NviCorrectionList.tsx
@@ -1,9 +1,13 @@
+import { useTranslation } from 'react-i18next';
+
 export const NviCorrectionList = () => {
+  const { t } = useTranslation();
+
   return (
     <iframe
       style={{ border: 'none', height: '80vh' }}
       id="inlineFrameExample"
-      title="NVI rapport"
+      title={t('tasks.correction_list')}
       width="100%"
       src="https://rapport-dv.uhad.no/t/DUCT/views/Ryddelister_2023/Story1?%3Aembed=y&%3AisGuestRedirectFromVizportal=y"></iframe>
   );
