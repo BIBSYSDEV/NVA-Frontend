5fa19578aee2dd3350986516da4561cda17aa7eb
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-11-14 16:46:21 +0100

    Add clinical treatment studies button

--- src/pages/reports/ReportsPage.tsx
+++ src/pages/reports/ReportsPage.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import clinicalTreatmentStudiesThumbnail from '../../resources/images/clinical-treatment-studies-thumbnail.png';
 import internationalCooperationThumbnail from '../../resources/images/international-cooperation-report-thumbnail.png';
 import nviReportThumbnail from '../../resources/images/nvi-report-thumbnail.png';
 import { UrlPathTemplate } from '../../utils/urlPaths';
@@ -28,6 +29,12 @@ const ReportsPage = () => {
         description={t('search.reports.external_reports')}
         imageSrc={internationalCooperationThumbnail}
         path={UrlPathTemplate.ReportsInternationalCooperation}
+      />
+      <ReportButton
+        title={t('search.reports.clinical_treatment_studies')}
+        description={t('search.reports.external_reports')}
+        imageSrc={clinicalTreatmentStudiesThumbnail}
+        path={UrlPathTemplate.ReportsClinicalTreatmentStudies}
       />
     </Box>
   );
