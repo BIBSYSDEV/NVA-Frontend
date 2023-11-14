b50e4bea27ca57cb6f2224a5fd325cdff5abf2ee
Isar Buzza <isar.buzza@sikt.no>
2023-11-14 12:12:11 +0100

    created check for updating employment when creating new confirmed person

--- src/pages/basic_data/institution_admin/AddEmployeePage.tsx
+++ src/pages/basic_data/institution_admin/AddEmployeePage.tsx
@@ -77,7 +77,13 @@ export const AddEmployeePage = () => {
         dispatch(setNotification({ message: t('feedback.error.create_user'), variant: 'error' }));
       } else if (isSuccessStatus(createPersonResponse.status)) {
         personId = createPersonResponse.data.id;
-        await new Promise((resolve) => setTimeout(resolve, 10_000)); // Wait 10sec before creating NVA User. TODO: NP-9121
+
+        if (!nationalId) {
+          dispatch(setNotification({ message: t('feedback.success.create_person'), variant: 'success' }));
+          resetForm();
+        } else {
+          await new Promise((resolve) => setTimeout(resolve, 10_000)); // Wait 10sec before creating NVA User. TODO: NP-9121
+        }
       }
     } else {
       // Add employment to existing Person
@@ -115,7 +121,7 @@ export const AddEmployeePage = () => {
         validationSchema={addEmployeeValidationSchema}
         onSubmit={onSubmit}
         validateOnMount>
-        {({ isValid, isSubmitting, values, setFieldValue, errors }: FormikProps<AddEmployeeData>) => (
+        {({ isSubmitting, values, setFieldValue, errors }: FormikProps<AddEmployeeData>) => (
           <Form noValidate>
             <Box
               sx={{
