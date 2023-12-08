3583e97f9c60408c4ff8ca89321b7736c5bb512e
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2023-12-08 11:54:57 +0100

    Keep more query params after submiting form

--- src/pages/dashboard/HomePage.tsx
+++ src/pages/dashboard/HomePage.tsx
@@ -39,8 +39,6 @@ enum SearchTypeValue {
   Project = 'project',
 }
 
-const defaultResultSize = ROWS_PER_PAGE_OPTIONS[0].toString();
-
 const HomePage = () => {
   const { t } = useTranslation();
   const history = useHistory();
@@ -55,26 +53,23 @@ const HomePage = () => {
   const personIsSeleced = paramsSearchType === SearchTypeValue.Person;
   const projectIsSelected = paramsSearchType === SearchTypeValue.Project;
 
-  const requestParams = new URLSearchParams(history.location.search);
-  requestParams.delete(SearchParam.Type);
+  const rowsPerPage = Number(params.get(SearchParam.Results) ?? ROWS_PER_PAGE_OPTIONS[0]);
+  const page = Number(params.get(SearchParam.Page) ?? 1);
 
-  const rowsPerPage = Number(requestParams.get(SearchParam.Results) ?? 10);
-  const page = Number(requestParams.get(SearchParam.Page) ?? 1);
-
+  const registrationSearchTerm = params.get(ResultParam.Query);
   const registrationsQueryConfig: FetchResultsParams = {
-    query: requestParams.get(ResultParam.Query),
-    category: requestParams.get(ResultParam.Category) as PublicationInstanceType | null,
-    topLevelOrganization: requestParams.get(ResultParam.TopLevelOrganization),
-    fundingSource: requestParams.get(ResultParam.FundingSource),
-    contributor: requestParams.get(ResultParam.Contributor),
-    contributorShould: requestParams.get(ResultParam.ContributorShould),
-    title: requestParams.get(ResultParam.Title),
-    sort: requestParams.get(ResultParam.Sort) as SortOrder | null,
-    order: requestParams.get(ResultParam.Order),
-    from: Number(requestParams.get(SearchParam.From) ?? 0),
+    query: registrationSearchTerm,
+    category: params.get(ResultParam.Category) as PublicationInstanceType | null,
+    topLevelOrganization: params.get(ResultParam.TopLevelOrganization),
+    fundingSource: params.get(ResultParam.FundingSource),
+    contributor: params.get(ResultParam.Contributor),
+    contributorShould: params.get(ResultParam.ContributorShould),
+    title: params.get(ResultParam.Title),
+    sort: params.get(ResultParam.Sort) as SortOrder | null,
+    order: params.get(ResultParam.Order),
+    from: Number(params.get(SearchParam.From) ?? 0),
     results: rowsPerPage,
   };
-
   const registrationQuery = useQuery({
     queryKey: ['registrations', registrationsQueryConfig],
     queryFn: () => fetchResults(registrationsQueryConfig),
@@ -82,10 +77,11 @@ const HomePage = () => {
     keepPreviousData: true,
   });
 
+  const personSearchTerm = params.get(PersonSearchParameter.Name) ?? '.';
   const personQueryParams: PersonSearchParams = {
-    name: requestParams.get(PersonSearchParameter.Name) ?? '.',
-    organization: requestParams.get(PersonSearchParameter.Organization),
-    sector: requestParams.get(PersonSearchParameter.Sector),
+    name: personSearchTerm,
+    organization: params.get(PersonSearchParameter.Organization),
+    sector: params.get(PersonSearchParameter.Sector),
   };
   const personQuery = useQuery({
     enabled: personIsSeleced,
@@ -95,18 +91,18 @@ const HomePage = () => {
     keepPreviousData: true,
   });
 
+  const projectSearchTerm = params.get(SearchParam.Query);
   const projectQueryParams: ProjectsSearchParams = {
-    coordinatingFacet: requestParams.get(ProjectSearchParameter.CoordinatingFacet),
-    categoryFacet: requestParams.get(ProjectSearchParameter.CategoryFacet),
-    fundingSourceFacet: requestParams.get(ProjectSearchParameter.FundingSourceFacet),
-    healthProjectFacet: requestParams.get(ProjectSearchParameter.HealthProjectFacet),
-    participantFacet: requestParams.get(ProjectSearchParameter.ParticipantFacet),
-    participantOrgFacet: requestParams.get(ProjectSearchParameter.ParticipantOrgFacet),
-    responsibleFacet: requestParams.get(ProjectSearchParameter.ResponsibleFacet),
-    sectorFacet: requestParams.get(ProjectSearchParameter.SectorFacet),
-    query: requestParams.get(SearchParam.Query),
+    coordinatingFacet: params.get(ProjectSearchParameter.CoordinatingFacet),
+    categoryFacet: params.get(ProjectSearchParameter.CategoryFacet),
+    fundingSourceFacet: params.get(ProjectSearchParameter.FundingSourceFacet),
+    healthProjectFacet: params.get(ProjectSearchParameter.HealthProjectFacet),
+    participantFacet: params.get(ProjectSearchParameter.ParticipantFacet),
+    participantOrgFacet: params.get(ProjectSearchParameter.ParticipantOrgFacet),
+    responsibleFacet: params.get(ProjectSearchParameter.ResponsibleFacet),
+    sectorFacet: params.get(ProjectSearchParameter.SectorFacet),
+    query: projectSearchTerm,
   };
-
   const projectQuery = useQuery({
     enabled: projectIsSelected,
     queryKey: ['projects', rowsPerPage, page, projectQueryParams],
@@ -127,12 +123,15 @@ const HomePage = () => {
       validateOnChange={false}
       validateOnBlur={false}
       onSubmit={(values) => {
-        const previousParamsResults = params.get(SearchParam.Results);
-        const newSearchParams = new URLSearchParams();
         if (resultIsSelected) {
-          const newRegistrationParams = requestParams;
-          if (values.searchTerm) {
-            newRegistrationParams.set(SearchParam.Query, values.searchTerm);
+          if (values.searchTerm !== registrationSearchTerm) {
+            if (values.searchTerm) {
+              params.set(ResultParam.Query, values.searchTerm);
+            } else {
+              params.delete(ResultParam.Query);
+            }
+            params.set(ResultParam.Results, rowsPerPage.toString());
+            params.set(ResultParam.From, '0');
           }
 
           const contributorNames =
@@ -140,7 +139,7 @@ const HomePage = () => {
               ?.filter((property) => property.fieldName === ResultParam.ContributorShould && property.value)
               .map((property) => property.value) ?? [];
           if (contributorNames.length > 0) {
-            newRegistrationParams.set(ResultParam.ContributorShould, contributorNames.join(','));
+            params.set(ResultParam.ContributorShould, contributorNames.join(','));
           }
 
           const title =
@@ -148,29 +147,26 @@ const HomePage = () => {
               ?.filter((property) => property.fieldName === ResultParam.Title && property.value)
               .map((property) => property.value) ?? [];
           if (title.length > 0) {
-            newRegistrationParams.set(ResultParam.Title, title.join(','));
+            params.set(ResultParam.Title, title.join(','));
           }
-
-          newRegistrationParams.set(SearchParam.Results, previousParamsResults ?? defaultResultSize);
-          newRegistrationParams.set(SearchParam.From, '0');
-          history.push({ search: newRegistrationParams.toString() });
         } else if (personIsSeleced) {
-          newSearchParams.set(SearchParam.Type, SearchTypeValue.Person);
-          if (values.searchTerm) {
-            newSearchParams.set(SearchParam.Name, values.searchTerm);
-            newSearchParams.set(SearchParam.Results, previousParamsResults ?? defaultResultSize);
-            newSearchParams.set(SearchParam.Page, '1');
+          if (values.searchTerm !== personSearchTerm) {
+            params.set(SearchParam.Name, values.searchTerm ?? '.');
+            params.set(SearchParam.Results, rowsPerPage.toString());
+            params.set(SearchParam.Page, '1');
           }
-          history.push({ search: newSearchParams.toString() });
         } else if (projectIsSelected) {
-          newSearchParams.set(SearchParam.Type, SearchTypeValue.Project);
-          if (values.searchTerm) {
-            newSearchParams.set(SearchParam.Query, values.searchTerm);
-            newSearchParams.set(SearchParam.Results, previousParamsResults ?? defaultResultSize);
-            newSearchParams.set(SearchParam.Page, '1');
+          if (values.searchTerm !== projectSearchTerm) {
+            if (values.searchTerm) {
+              params.set(SearchParam.Query, values.searchTerm);
+            } else {
+              params.delete(SearchParam.Query);
+            }
+            params.set(SearchParam.Results, rowsPerPage.toString());
+            params.set(SearchParam.Page, '1');
           }
-          history.push({ search: newSearchParams.toString() });
         }
+        history.push({ search: params.toString() });
       }}>
       {({ setValues }: FormikHelpers<SearchConfig>) => (
         <Form style={{ width: '100%' }}>
