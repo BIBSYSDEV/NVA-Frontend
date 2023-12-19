0956c1578d04e38c7421feabb0d9e7987f7fa986
siricv <siri.vikan@sikt.no>
2023-12-19 13:09:57 +0100

    logo width 6rem, removed store icon

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,7 +3,6 @@ import AssignmentIcon from '@mui/icons-material/AssignmentOutlined';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenterOutlined';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import SearchIcon from '@mui/icons-material/Search';
-import StoreIcon from '@mui/icons-material/Store';
 import { AppBar, Box, Button, Theme, Typography, useMediaQuery } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
@@ -121,7 +120,6 @@ export const Header = () => {
               {organization?.acronym &&
                 (user?.isEditor ? (
                   <>
-                    <StoreIcon fontSize="small" sx={{ alignSelf: 'center' }} />
                     <MenuButton
                       sx={{
                         fontSize: '1.25rem',
@@ -138,7 +136,6 @@ export const Header = () => {
                   </>
                 ) : (
                   <>
-                    <StoreIcon fontSize="small" />
                     <Typography
                       variant="h1"
                       component="span"
