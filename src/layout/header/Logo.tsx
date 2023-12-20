d461c0073a97c5672c084caaebf1daaa668f1abf
siricv <50354322+siricv@users.noreply.github.com>
2023-12-20 15:08:01 +0100

    Np 45701 update header (#4925)
    * new elements ok, needs adjustment
    
    * removed about-page test
    
    * logo width 14rem
    
    * logo width 6rem
    
    * logo width 6rem, removed store icon
    
    * New registration underline
    
    * New registration underline, without storeIcon
    
    * underline new registration removed for registration wizard
    
    * underline new registration removed, deleted comments
    
    * removed unused data-testid
    
    * removed a box in Logo.tsx
    
    * removed text Nasjonalt vitenarkiv earlier when resizing the screen
    
    * removed line component={RouterLink}

--- src/layout/header/Logo.tsx
+++ src/layout/header/Logo.tsx
@@ -1,13 +1,32 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Button, Theme, Typography, useMediaQuery } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import { Link as RouterLink } from 'react-router-dom';
 import { UrlPathTemplate } from '../../utils/urlPaths';
 
-export const Logo = () => (
-  <Box sx={{ gridArea: 'logo' }}>
-    <Button data-testid="logo" component={RouterLink} to={UrlPathTemplate.Home}>
+export const Logo = () => {
+  const { t } = useTranslation();
+  const showFullAppName = useMediaQuery((theme: Theme) => theme.breakpoints.down('lg'));
+
+  return (
+    <Button sx={{ gridArea: 'logo' }} data-testid="logo" component={RouterLink} to={UrlPathTemplate.Home}>
       <Typography variant="h5" component="span" sx={{ color: 'white', fontWeight: 900, fontSize: '3rem' }}>
         NVA
       </Typography>
+
+      {!showFullAppName && (
+        <Typography
+          variant="h1"
+          component="span"
+          sx={{
+            ml: '1.2rem',
+            color: 'white',
+            fontWeight: 20,
+            fontSize: '1rem',
+            maxWidth: '6rem',
+          }}>
+          {t('common.page_title')}
+        </Typography>
+      )}
     </Button>
-  </Box>
-);
+  );
+};
