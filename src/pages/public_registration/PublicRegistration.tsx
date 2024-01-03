92294db1c402507ac3d783ab47b0ff8f2f0b1826
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-01-03 14:27:11 +0100

    NP-45830 Add back button for search hits

--- src/pages/public_registration/PublicRegistration.tsx
+++ src/pages/public_registration/PublicRegistration.tsx
@@ -1,18 +1,39 @@
+import SearchIcon from '@mui/icons-material/Search';
 import { Box } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { Link, useLocation } from 'react-router-dom';
+import { StyledMinimizedMenuButton } from '../../components/SideMenu';
 import { RegistrationLandingPage } from './RegistrationLandingPage';
 
-const PublicRegistration = () => (
-  <Box
-    sx={{
-      p: { xs: '0.5rem', md: '1rem' },
-      width: '100%',
-      display: 'grid',
-      gridTemplateColumns: { xs: '1fr', md: '1fr 5fr' },
-    }}>
-    <Box sx={{ gridColumn: '2' }}>
-      <RegistrationLandingPage />
+export interface PreviousPathLocationState {
+  previousPath?: string;
+}
+
+const PublicRegistration = () => {
+  const { t } = useTranslation();
+  const location = useLocation<PreviousPathLocationState>();
+  const previousPath = location.state?.previousPath;
+
+  return (
+    <Box
+      sx={{
+        p: { xs: '0.5rem', md: '1rem' },
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: { xs: '1fr', md: '1fr 5fr' },
+      }}>
+      {previousPath && (
+        <Box sx={{ alignSelf: 'start', justifySelf: 'start' }} component={Link} to={previousPath}>
+          <StyledMinimizedMenuButton title={t('common.search')}>
+            <SearchIcon />
+          </StyledMinimizedMenuButton>
+        </Box>
+      )}
+      <Box sx={{ gridColumn: '2' }}>
+        <RegistrationLandingPage />
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default PublicRegistration;
