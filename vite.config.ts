94fd3841e29edb1190cbcd0f72888ce499dc1de9
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-01-25 14:31:44 +0100

    Handle file uploads

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
