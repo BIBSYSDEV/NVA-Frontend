cd6c211c20e6ea3ae62285c7c888c53d6715fe67
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2024-02-02 12:28:40 +0100

    Update src/pages/public_registration/DeletedPublicationInformation.tsx
    Co-authored-by: Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>

--- src/pages/public_registration/DeletedPublicationInformation.tsx
+++ src/pages/public_registration/DeletedPublicationInformation.tsx
@@ -36,7 +36,6 @@ export const DeletedPublicationInformation = ({ registration }: DeletePublicatio
               justifyContent: 'center',
               backgroundColor: 'registration.main',
               padding: '1rem',
-              gap: '1.5rem',
               my: '1rem',
             }}>
             <Typography variant="h2" component="h1">
