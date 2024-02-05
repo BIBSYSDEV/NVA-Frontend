c37c77f8116f0989b51e3e6fd25a7342cec58923
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-02-05 09:50:08 +0100

    Add name search for curators

--- src/pages/editor/EditorCurators.tsx
+++ src/pages/editor/EditorCurators.tsx
@@ -8,6 +8,7 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
   Typography,
 } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
@@ -24,6 +25,7 @@ import { CuratorRow } from './CuratorRow';
 
 export const EditorCurators = () => {
   const { t } = useTranslation();
+  const [searchQuery, setSearchQuery] = useState('');
   const [rowsPerPage, setRowsPerPage] = useState(ROWS_PER_PAGE_OPTIONS[0]);
   const [page, setPage] = useState(1);
 
@@ -37,47 +39,63 @@ export const EditorCurators = () => {
     meta: { errorMessage: t('feedback.error.get_users_for_institution') },
   });
   const curators = curatorsQuery.data ?? [];
+  const filteredCurators = curators.filter((curator) => {
+    const name = `${curator.givenName} ${curator.familyName}`.toLowerCase();
+    return name.includes(searchQuery.toLowerCase());
+  });
 
   // Ensure selected page is not out of bounds due to manipulated userList
-  const validPage = curators.length <= (page - 1) * rowsPerPage ? 1 : page;
-  const curatorsOnPage = curators.slice((validPage - 1) * rowsPerPage, validPage * rowsPerPage);
+  const validPage = filteredCurators.length <= (page - 1) * rowsPerPage ? 1 : page;
+  const curatorsOnPage = filteredCurators.slice((validPage - 1) * rowsPerPage, validPage * rowsPerPage);
 
-  return curatorsQuery.isLoading ? (
-    <Box sx={{ display: 'flex', justifyContent: 'space-around', mt: '2rem' }}>
-      <CircularProgress aria-label={t('editor.curators.areas_of_responsibility')} />
-    </Box>
-  ) : curators.length === 0 ? (
-    <Typography>
-      <i>{t('editor.curators.no_users_found')}</i>
-    </Typography>
-  ) : (
+  return (
     <>
-      <TableContainer component={Paper} sx={{ mb: '0.5rem' }}>
-        <Table size="small" sx={alternatingTableRowColor}>
-          <TableHead>
-            <TableRow>
-              <TableCell>{t('common.name')}</TableCell>
-              <TableCell>{t('editor.curators.area_of_responsibility')}</TableCell>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {curatorsOnPage.map((curator) => (
-              <CuratorRow key={curator.username} curator={curator} />
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <ListPagination
-        count={curators.length}
-        rowsPerPage={rowsPerPage}
-        page={validPage}
-        onPageChange={(newPage) => setPage(newPage)}
-        onRowsPerPageChange={(newRowsPerPage) => {
-          setRowsPerPage(newRowsPerPage);
-          setPage(1);
-        }}
+      <TextField
+        type="search"
+        variant="filled"
+        label={t('common.search_by_name')}
+        onChange={(event) => setSearchQuery(event.currentTarget.value)}
+        fullWidth
+        sx={{ maxWidth: '25rem', mb: '1rem' }}
       />
+      {curatorsQuery.isLoading ? (
+        <Box sx={{ display: 'flex', justifyContent: 'space-around', mt: '2rem' }}>
+          <CircularProgress aria-label={t('editor.curators.areas_of_responsibility')} />
+        </Box>
+      ) : filteredCurators.length === 0 ? (
+        <Typography>
+          <i>{t('editor.curators.no_users_found')}</i>
+        </Typography>
+      ) : (
+        <>
+          <TableContainer component={Paper} sx={{ mb: '0.5rem' }}>
+            <Table size="small" sx={alternatingTableRowColor}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>{t('common.name')}</TableCell>
+                  <TableCell>{t('editor.curators.area_of_responsibility')}</TableCell>
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {curatorsOnPage.map((curator) => (
+                  <CuratorRow key={curator.username} curator={curator} />
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <ListPagination
+            count={filteredCurators.length}
+            rowsPerPage={rowsPerPage}
+            page={validPage}
+            onPageChange={(newPage) => setPage(newPage)}
+            onRowsPerPageChange={(newRowsPerPage) => {
+              setRowsPerPage(newRowsPerPage);
+              setPage(1);
+            }}
+          />
+        </>
+      )}
     </>
   );
 };
