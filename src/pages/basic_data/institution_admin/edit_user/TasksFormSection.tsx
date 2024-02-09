dfca2d4e8d5b38f505672c30793b1e0eb2cc1536
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-02-09 12:29:00 +0100

    Np 45912 add employee with area of responsibility (#5117)
    * Use consistent route props
    
    * Allow setting area of responsibility for new employee
    
    * Remove unused files
    
    * Reuse functionality for editing user when adding employment
    
    * Update layout
    
    * Rename variable
    
    * Generalize selection of area of responsibility
    
    * Set area of responsibility implicitly
    
    * Remove old role selector component
    
    * Renaming variables and removing redundant styling
    
    * Disable roles when submitting
    
    * Hide added organizations from options
    
    * Include viewing scope when creating user

--- src/pages/basic_data/institution_admin/edit_user/TasksFormSection.tsx
+++ src/pages/basic_data/institution_admin/edit_user/TasksFormSection.tsx
@@ -1,9 +1,7 @@
 import { Typography } from '@mui/material';
-import { useFormikContext } from 'formik';
 import { useTranslation } from 'react-i18next';
 import { RoleName } from '../../../../types/user.types';
-import { AreaOfResponsibility } from './AreaOfResponsibility';
-import { UserFormData } from './UserFormDialog';
+import { AreaOfResponsibility, AreaOfResponsibilityProps } from './AreaOfResponsibility';
 
 export const rolesWithAreaOfResponsibility = [
   RoleName.DoiCurator,
@@ -12,20 +10,23 @@ export const rolesWithAreaOfResponsibility = [
   RoleName.NviCurator,
 ];
 
-export const TasksFormSection = () => {
+interface TasksFormSectionProps extends AreaOfResponsibilityProps {
+  roles?: RoleName[];
+}
+
+export const TasksFormSection = ({ roles = [], viewingScopes, updateViewingScopes }: TasksFormSectionProps) => {
   const { t } = useTranslation();
-  const { values } = useFormikContext<UserFormData>();
-  const userIsCurator = values.user?.roles.some((role) => rolesWithAreaOfResponsibility.includes(role.rolename));
+  const curatorRoleIsAdded = roles.some((rolename) => rolesWithAreaOfResponsibility.includes(rolename));
 
   return (
     <section>
       <Typography variant="h3" gutterBottom>
         {t('editor.curators.area_of_responsibility')}
       </Typography>
-      {!userIsCurator ? (
+      {!curatorRoleIsAdded ? (
         <Typography>{t('basic_data.person_register.must_be_curator_to_have_area_of_responsibility')}</Typography>
       ) : (
-        <AreaOfResponsibility />
+        <AreaOfResponsibility viewingScopes={viewingScopes} updateViewingScopes={updateViewingScopes} />
       )}
     </section>
   );
