d343b2101e24e637316ed8445e5d4c800134e76a
Isar Buzza <isar.buzza@sikt.no>
2024-02-22 14:01:09 +0100

    changed display method of selected languages

--- src/pages/search/advanced_search/LanguageFilter.tsx
+++ src/pages/search/advanced_search/LanguageFilter.tsx
@@ -1,5 +1,4 @@
-import ClearIcon from '@mui/icons-material/Clear';
-import { FormControl, IconButton, InputLabel, MenuItem, Select } from '@mui/material';
+import { Box, Chip, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router';
 import { ResultParam } from '../../../api/searchApi';
@@ -14,16 +13,18 @@ export const LanguageFilter = () => {
   const languageParam = searchParams.get(ResultParam.PublicationLanguageShould)?.split(',') || [];
 
   const handleChange = (selectedUris: string[] | null) => {
-    if (selectedUris) {
-      const selectedLanguages = selectedUris
+    if (selectedUris && selectedUris.length > 0) {
+      const languages = selectedUris
         .map((iso6393Code) => {
           const selectedLanguage = languageOptions.find((language) => language.iso6393Code === iso6393Code);
           return selectedLanguage ? selectedLanguage.iso6393Code : null;
         })
         .filter(Boolean);
 
-      if (selectedLanguages.length > 0) {
-        searchParams.set(ResultParam.PublicationLanguageShould, selectedLanguages.join(','));
+      if (languages.length > 0) {
+        searchParams.set(ResultParam.PublicationLanguageShould, languages.join(','));
+      } else {
+        searchParams.delete(ResultParam.PublicationLanguageShould);
       }
     } else {
       searchParams.delete(ResultParam.PublicationLanguageShould);
@@ -32,14 +33,19 @@ export const LanguageFilter = () => {
     history.push({ search: searchParams.toString() });
   };
 
-  const clearValues = () => {
-    searchParams.delete(ResultParam.PublicationLanguageShould);
-    history.push({ search: searchParams.toString() });
+  const handleDelete = (iso6393CodeToDelete?: string) => () => {
+    selectedLanguages.length > 1
+      ? handleChange(languageParam.filter((iso6393Code) => iso6393Code !== iso6393CodeToDelete))
+      : handleChange([]);
   };
 
+  const selectedLanguages = languageParam
+    .map((iso6393Code) => languageOptions.find((language) => language.iso6393Code === iso6393Code))
+    .filter(Boolean);
+
   return (
-    <>
-      <FormControl fullWidth sx={{ maxWidth: '15rem' }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', maxWidth: '15rem' }}>
+      <FormControl fullWidth sx={{ width: '15rem' }}>
         <InputLabel>{t('registration.description.primary_language')}</InputLabel>
         <Select
           multiple
@@ -49,17 +55,25 @@ export const LanguageFilter = () => {
           onChange={(event) => {
             handleChange(event.target.value as string[]);
           }}
+          renderValue={() => t('search.choose_one_or_more')}
           variant="outlined">
           {languageOptions.map(({ uri, iso6393Code, nob, eng }) => (
             <MenuItem value={iso6393Code} key={uri} data-testid={`publication-language-${uri}`}>
-              {i18n.language === 'nob' || i18n.language === 'nno' ? nob : eng}
+              {i18n.language === 'nob' ? nob : eng}
             </MenuItem>
           ))}
         </Select>
       </FormControl>
-      <IconButton onClick={clearValues}>
-        <ClearIcon fontSize="small" />
-      </IconButton>
-    </>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+        {selectedLanguages.map((language, index) => (
+          <Chip
+            sx={{ mb: '0.25rem' }}
+            key={index}
+            label={i18n.language === 'nob' ? language?.nob : language?.eng}
+            onDelete={handleDelete(language?.iso6393Code)}
+          />
+        ))}
+      </Box>
+    </Box>
   );
 };
