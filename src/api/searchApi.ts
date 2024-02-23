f4a8520d238a177ec1f85e7b4e2ddd8bb4896810
iBuzza <53341294+iBuzza@users.noreply.github.com>
2024-02-23 12:13:28 +0100

    NP-46111 Add language filter to advanced search (#5175)
    * NP-46111 Add language filter to advanced search WIP
    
    * using query param to test wip
    
    * updated search param to publicationLanguage
    
    * made select multiple wip
    
    * show correct selection in select value field
    
    * added input label and form control
    
    * moved some props from select to form control
    
    * sort params
    
    * use PublicationLanguageShould + trim search param from uri
    
    * removed unused search param
    
    * use isoCode6393 instead of uri as value
    
    * cleanup
    
    * changed display method of selected languages
    
    * code review
    
    * code review

--- src/api/searchApi.ts
+++ src/api/searchApi.ts
@@ -157,6 +157,7 @@ export enum ResultParam {
   Journal = 'journal',
   Order = 'order',
   Project = 'project',
+  PublicationLanguageShould = 'publicationLanguageShould',
   PublicationYearBefore = 'publicationYearBefore',
   PublicationYearSince = 'publicationYearSince',
   PublicationYearShould = 'publicationYearShould',
@@ -193,6 +194,7 @@ export interface FetchResultsParams {
   [ResultParam.Journal]?: string | null;
   [ResultParam.Order]?: string | null;
   [ResultParam.Project]?: string | null;
+  [ResultParam.PublicationLanguageShould]?: string | null;
   [ResultParam.PublicationYearBefore]?: string | null;
   [ResultParam.PublicationYearSince]?: string | null;
   [ResultParam.PublicationYearShould]?: string | null;
@@ -268,6 +270,9 @@ export const fetchResults = async (params: FetchResultsParams, signal?: AbortSig
   }
   if (params.project) {
     searchParams.set(ResultParam.Project, params.project);
+  }
+  if (params.publicationLanguageShould) {
+    searchParams.set(ResultParam.PublicationLanguageShould, params.publicationLanguageShould);
   }
   if (params.publicationYearBefore) {
     const beforeYearNumber = +params.publicationYearBefore;
