a17f6e6e6da1207f7ed2018ace73f57196cdb098
iBuzza <53341294+iBuzza@users.noreply.github.com>
2024-02-23 13:51:31 +0100

    Use apiRequest2 for Publivation Channel search (#5238)

--- src/api/publicationChannelApi.ts
+++ src/api/publicationChannelApi.ts
@@ -2,7 +2,7 @@ import { SearchResponse } from '../types/common.types';
 import { Journal, Publisher, Series } from '../types/registration.types';
 import { getYearQuery } from '../utils/registration-helpers';
 import { PublicationChannelApiPath } from './apiPaths';
-import { authenticatedApiRequest2 } from './apiRequest';
+import { apiRequest2, authenticatedApiRequest2 } from './apiRequest';
 
 export interface CreateJournalPayload {
   name: string;
@@ -48,7 +48,7 @@ export const createPublisher = async (newPublisher: CreatePublisherPayload) => {
 };
 
 export const searchForSeries = async (query: string, year: string) => {
-  const searchForSeriesResponse = await authenticatedApiRequest2<SearchResponse<Series>>({
+  const searchForSeriesResponse = await apiRequest2<SearchResponse<Series>>({
     url: PublicationChannelApiPath.Series,
     method: 'GET',
     params: {
@@ -61,7 +61,7 @@ export const searchForSeries = async (query: string, year: string) => {
 };
 
 export const searchForPublishers = async (query: string, year: string) => {
-  const searchForPublishersResponse = await authenticatedApiRequest2<SearchResponse<Publisher>>({
+  const searchForPublishersResponse = await apiRequest2<SearchResponse<Publisher>>({
     url: PublicationChannelApiPath.Publisher,
     method: 'GET',
     params: {
@@ -74,7 +74,7 @@ export const searchForPublishers = async (query: string, year: string) => {
 };
 
 export const searchForJournals = async (query: string, year: string) => {
-  const searchForJournalsResponse = await authenticatedApiRequest2<SearchResponse<Journal>>({
+  const searchForJournalsResponse = await apiRequest2<SearchResponse<Journal>>({
     url: PublicationChannelApiPath.Journal,
     method: 'GET',
     params: {
