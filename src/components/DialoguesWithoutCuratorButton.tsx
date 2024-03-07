d4ff62a908058b8f496097b3d2bc8c22c216485f
joachimjorgensen <joachim.jorgensen@sikt.no>
2024-03-07 11:05:04 +0100

    Alter the button to a toggle-button for better visibility

--- src/components/DialoguesWithoutCuratorButton.tsx
+++ src/components/DialoguesWithoutCuratorButton.tsx
@@ -1,5 +1,5 @@
 import { ChatBubble } from '@mui/icons-material';
-import { Button, Typography } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import { TicketSearchParam } from '../api/searchApi';
@@ -10,26 +10,36 @@ export const DialoguesWithoutCuratorButton = () => {
   const { t } = useTranslation();
   const history = useHistory();
   const searchParams = new URLSearchParams(history.location.search);
+  const statusNew: TicketStatus = 'New';
 
-  const onClick = () => {
-    const statusNew: TicketStatus = 'New';
-    searchParams.set(TicketSearchParam.Status, statusNew);
-    searchParams.delete(TicketSearchParam.Assignee);
+  const toggleDialoguesWithoutCurators = () => {
+    if (dialoguesWithoutCuratorSelected()) {
+      searchParams.delete(TicketSearchParam.Status);
+    } else {
+      searchParams.set(TicketSearchParam.Status, statusNew);
+      searchParams.delete(TicketSearchParam.Assignee);
+    }
     history.push({ search: searchParams.toString() });
+  };
+
+  const dialoguesWithoutCuratorSelected = () => {
+    return searchParams.get(TicketSearchParam.Status) === statusNew && !searchParams.get(TicketSearchParam.Assignee);
   };
 
   return (
     <Button
       fullWidth
-      size="large"
-      variant="outlined"
+      size="medium"
+      variant={dialoguesWithoutCuratorSelected() ? 'contained' : 'outlined'}
       color="primary"
       sx={{ textTransform: 'none' }}
       startIcon={<ChatBubble />}
-      onClick={() => onClick()}
+      onClick={toggleDialoguesWithoutCurators}
       title={t('tasks.dialogues_without_curator')}
       data-testid={dataTestId.tasksPage.dialoguesWithoutCuratorButton}>
-      <Typography noWrap>{t('tasks.dialogues_without_curator')}</Typography>
+      <Box component="span" sx={{ whiteSpace: 'nowrap' }}>
+        {t('tasks.dialogues_without_curator')}
+      </Box>
     </Button>
   );
 };
