68a5788d354d6a5a8947c348ce1ad887a78a14db
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-03-25 11:49:34 +0100

    Revert "Search for distinct parameters when using query (#5336)"
    This reverts commit 28098ab6ac8541af6105fff8af41253714e8516e.

--- src/api/searchApi.ts
+++ src/api/searchApi.ts
@@ -184,7 +184,6 @@ export enum ResultParam {
   CristinIdentifier = 'cristinIdentifier',
   Doi = 'doi',
   ExcludeSubunits = 'excludeSubunits',
-  Fields = 'fields',
   Files = 'files',
   From = 'from',
   FundingIdentifier = 'fundingIdentifier',
@@ -357,7 +356,6 @@ export const fetchResults = async (params: FetchResultsParams, signal?: AbortSig
   }
   if (params.query) {
     searchParams.set(ResultParam.Query, params.query);
-    searchParams.set(ResultParam.Fields, `${ResultParam.Title},${ResultParam.Abstract},${ResultParam.ContributorName}`);
   }
   if (params.scientificIndex) {
     searchParams.set(ResultParam.ScientificReportPeriodBeforeParam, (+params.scientificIndex + 1).toString());
