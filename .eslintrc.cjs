f82ea42035384b331c4cb3718debed1937f57642
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-03-25 16:17:15 +0100

    Add react-query eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,26 +6,26 @@ module.exports = {
     ecmaVersion: 2022,
   },
   settings: { react: { version: 'detect' } },
-  plugins: ['react', 'react-hooks', 'jsx-a11y', '@typescript-eslint' /*, 'prettier', '@tanstack/query'*/],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', '@typescript-eslint', '@tanstack/query' /*, 'prettier'*/],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
 
     // 'plugin:cypress/recommended', // avoid showing cypress variables as error
     // 'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display Prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    // 'plugin:@tanstack/eslint-plugin-query/recommended',
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'react/jsx-key': 'off', // TODO: Violations of this rule should be resolved, so we can implement this rule
+    'react/no-children-prop': 'off', // TODO: Violations of this rule should be resolved, so we can implement this rule
     'no-console': 'warn',
     'no-debugger': 'warn',
     '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
-    'react/jsx-key': 'off', // TODO: Violations of this rule should be resolved, so we can implement this rule
-    'react/no-children-prop': 'off', // TODO: Violations of this rule should be resolved, so we can implement this rule
   },
   // rules: {
   //   'prettier/prettier': 2,
