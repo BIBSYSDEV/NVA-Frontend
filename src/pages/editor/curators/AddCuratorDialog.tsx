f2d471ceb70e70ea7f4ae0fa554926bec2c45054
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-04-11 15:50:28 +0200

    Ensure correct types for user form

--- src/pages/editor/curators/AddCuratorDialog.tsx
+++ src/pages/editor/curators/AddCuratorDialog.tsx
@@ -1,3 +1,4 @@
+import { LoadingButton } from '@mui/lab';
 import {
   Autocomplete,
   Button,
@@ -14,7 +15,7 @@ import {
   Typography,
 } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
-import { Form, Formik, FormikProps, useFormikContext } from 'formik';
+import { Form, Formik, FormikProps } from 'formik';
 import { t } from 'i18next';
 import { ChangeEvent, useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -23,7 +24,7 @@ import { fetchUser } from '../../../api/roleApi';
 import { fetchEmployees } from '../../../api/searchApi';
 import { AutocompleteTextField } from '../../../components/AutocompleteTextField';
 import { RootState } from '../../../redux/store';
-import { CristinPerson, RoleName } from '../../../types/user.types';
+import { CristinPerson, InstitutionUser, RoleName } from '../../../types/user.types';
 import { getIdentifierFromId } from '../../../utils/general-helpers';
 import { useDebounce } from '../../../utils/hooks/useDebounce';
 import { getFullCristinName, getValueByKey } from '../../../utils/user-helpers';
@@ -69,173 +70,176 @@ export const AddCuratorDialog = ({ onClose, open }: AddCuratorDialogProps) => {
     retry: false,
   });
 
-  const isLoading = employeeSearchQuery.isFetching;
-
   return (
     <Dialog open={open} onClose={closeDialog} maxWidth="md" fullWidth>
       <DialogTitle>{t('editor.curators.add_curator')}</DialogTitle>
-      <Formik
-        initialValues={userQuery.data ?? {}}
-        enableReinitialize
-        onSubmit={(values) => console.log('submit', values)}>
-        {({ values, setFieldValue, dirty }: FormikProps<any>) => (
-          <Form noValidate>
-            <DialogContent>
-              <Autocomplete
-                options={employeeSearchQuery.data?.hits ?? []}
-                renderOption={(props, option) => (
-                  <li {...props} key={option.id}>
-                    {getFullCristinName(option.names)}
-                  </li>
-                )}
-                onInputChange={(_, value, reason) => {
-                  if (reason === 'clear' || reason === 'reset') {
-                    setSearchQuery('');
-                    setSelectedPerson(null);
-                  } else {
-                    setSearchQuery(value);
-                  }
-                }}
-                onChange={async (_, value) => {
-                  setSearchQuery('');
-                  setSelectedPerson(value);
-                }}
-                getOptionLabel={(option) => getFullCristinName(option.names)}
-                isOptionEqualToValue={(option, value) => option.id === value.id}
-                loading={isLoading}
-                sx={{ mb: '1rem' }}
-                renderInput={(params) => (
-                  <AutocompleteTextField
-                    // data-testid={dataTestId.registrationLandingPage.tasksPanel.assigneeSearchField}
-                    {...params}
-                    label={t('common.person')}
-                    isLoading={isLoading}
-                    placeholder={t('common.search')}
-                    showSearchIcon
-                  />
-                )}
-              />
 
-              {selectedPerson && (
-                <>
-                  {userQuery.isLoading ? (
-                    <CircularProgress />
-                  ) : userQuery.data && values ? (
-                    <UserForm />
-                  ) : (
-                    <>
-                      <Typography>
-                        Brukeren du ønsker å legge til som kurator må logge inn i løsningnen for å få tildelt en bruker
-                        før du kan gi dem kuratortilgang.
-                      </Typography>
-                      <Typography>
-                        Be {getFullCristinName(selectedPerson.names)} om å logge inn i NVA før du prøver igjen.
-                      </Typography>
-                    </>
-                  )}
-                </>
-              )}
-            </DialogContent>
-            <DialogActions sx={{ justifyContent: 'center' }}>
-              <Button onClick={closeDialog}>{t('common.cancel')}</Button>
-              <Button type="submit" variant="contained" disabled={Object.keys(values).length === 0 || !dirty}>
-                {t('common.add')}
-              </Button>
-            </DialogActions>
-          </Form>
+      <DialogContent>
+        <Autocomplete
+          options={employeeSearchQuery.data?.hits ?? []}
+          renderOption={(props, option) => (
+            <li {...props} key={option.id}>
+              {getFullCristinName(option.names)}
+            </li>
+          )}
+          onInputChange={(_, value, reason) => {
+            if (reason === 'clear' || reason === 'reset') {
+              setSearchQuery('');
+              setSelectedPerson(null);
+            } else {
+              setSearchQuery(value);
+            }
+          }}
+          onChange={async (_, value) => {
+            setSearchQuery('');
+            setSelectedPerson(value);
+          }}
+          getOptionLabel={(option) => getFullCristinName(option.names)}
+          isOptionEqualToValue={(option, value) => option.id === value.id}
+          loading={employeeSearchQuery.isFetching}
+          sx={{ mb: '1rem' }}
+          renderInput={(params) => (
+            <AutocompleteTextField
+              // data-testid={dataTestId.registrationLandingPage.tasksPanel.assigneeSearchField}
+              {...params}
+              label={t('common.person')}
+              isLoading={employeeSearchQuery.isFetching}
+              placeholder={t('common.search')}
+              showSearchIcon
+            />
+          )}
+        />
+
+        {selectedPerson && (
+          <>
+            {userQuery.isLoading ? (
+              <CircularProgress />
+            ) : userQuery.data ? (
+              <UserForm closeDialog={closeDialog} initialValues={userQuery.data} />
+            ) : (
+              <>
+                <Typography>
+                  Brukeren du ønsker å legge til som kurator må logge inn i løsningnen for å få tildelt en bruker før du
+                  kan gi dem kuratortilgang.
+                </Typography>
+                <Typography>
+                  Be {getFullCristinName(selectedPerson.names)} om å logge inn i NVA før du prøver igjen.
+                </Typography>
+              </>
+            )}
+          </>
         )}
-      </Formik>
+      </DialogContent>
+
+      {(!selectedPerson || !userQuery.data) && (
+        <DialogActions sx={{ justifyContent: 'center' }}>
+          <Button onClick={closeDialog}>{t('common.cancel')}</Button>
+        </DialogActions>
+      )}
     </Dialog>
   );
 };
 
-const UserForm = () => {
-  const { setFieldValue, values } = useFormikContext<any>();
+interface UserFormProps {
+  closeDialog: () => void;
+  initialValues: InstitutionUser;
+}
 
+const UserForm = ({ closeDialog, initialValues }: UserFormProps) => {
   return (
-    <>
-      <Typography variant="h3" gutterBottom>
-        {t('editor.curators.area_of_responsibility')}
-      </Typography>
-      <AreaOfResponsibility
-        viewingScopes={values.viewingScope?.includedUnits ?? []}
-        updateViewingScopes={(newViewingScopes) => setFieldValue('viewingScope.includedUnits', newViewingScopes)}
-      />
-      <Typography variant="h3" sx={{ mt: '1rem' }}>
-        {t('my_page.my_profile.heading.roles')}
-      </Typography>
-      <FormControl
-        component="fieldset"
-        onChange={(event: ChangeEvent<any>) => {
-          const role = event.target.value as RoleName;
-          const roleExists = values.roles?.some((r: any) => r.rolename === role);
+    <Formik initialValues={initialValues} enableReinitialize onSubmit={(values) => console.log('submit', values)}>
+      {({ values, setFieldValue, dirty, isSubmitting }: FormikProps<InstitutionUser>) => (
+        <Form noValidate>
+          <Typography variant="h3" gutterBottom>
+            {t('editor.curators.area_of_responsibility')}
+          </Typography>
+          <AreaOfResponsibility
+            viewingScopes={values.viewingScope?.includedUnits ?? []}
+            updateViewingScopes={(newViewingScopes) => setFieldValue('viewingScope.includedUnits', newViewingScopes)}
+          />
+          <Typography variant="h3" sx={{ mt: '1rem' }}>
+            {t('my_page.my_profile.heading.roles')}
+          </Typography>
+          <FormControl
+            component="fieldset"
+            onChange={(event: ChangeEvent<any>) => {
+              const role = event.target.value as RoleName;
+              const roleExists = values.roles?.some((r: any) => r.rolename === role);
 
-          const newRoles = roleExists
-            ? values.roles.filter((selectedRole: any) => selectedRole.rolename !== role)
-            : [...values.roles, { type: 'Role', rolename: role }];
-          console.log(newRoles);
+              const newRoles = roleExists
+                ? values.roles.filter((selectedRole: any) => selectedRole.rolename !== role)
+                : [...values.roles, { type: 'Role', rolename: role }];
 
-          setFieldValue('roles', newRoles);
-        }}
-        // data-testid={dataTestId.basicData.personAdmin.roleSelector}
-      >
-        <FormGroup sx={{ gap: '0.5rem' }}>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={values.roles?.some((r: any) => r.rolename === RoleName.PublishingCurator)}
-                value={RoleName.PublishingCurator}
+              setFieldValue('roles', newRoles);
+            }}
+            // data-testid={dataTestId.basicData.personAdmin.roleSelector}
+          >
+            <FormGroup sx={{ gap: '0.5rem' }}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={values.roles.some((role) => role.rolename === RoleName.PublishingCurator)}
+                    value={RoleName.PublishingCurator}
+                  />
+                }
+                label={t('my_page.roles.publishing_curator')}
               />
-            }
-            label={t('my_page.roles.publishing_curator')}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={values.roles?.some((r: any) => r.rolename === RoleName.CuratorThesis)}
-                value={RoleName.CuratorThesis}
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={values.roles.some((role) => role.rolename === RoleName.CuratorThesis)}
+                    value={RoleName.CuratorThesis}
+                  />
+                }
+                label={'Studentoppgave'}
               />
-            }
-            label={'Studentoppgave'}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={values.roles?.some((r: any) => r.rolename === RoleName.CuratorThesisEmbargo)}
-                value={RoleName.CuratorThesisEmbargo}
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={values.roles.some((role) => role.rolename === RoleName.CuratorThesisEmbargo)}
+                    value={RoleName.CuratorThesisEmbargo}
+                  />
+                }
+                label={'Embargo'}
               />
-            }
-            label={'Embargo'}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={values.roles?.some((r: any) => r.rolename === RoleName.DoiCurator)}
-                value={RoleName.DoiCurator}
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={values.roles.some((role) => role.rolename === RoleName.DoiCurator)}
+                    value={RoleName.DoiCurator}
+                  />
+                }
+                label={t('my_page.roles.doi_curator')}
               />
-            }
-            label={t('my_page.roles.doi_curator')}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={values.roles?.some((r: any) => r.rolename === RoleName.SupportCurator)}
-                value={RoleName.SupportCurator}
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={values.roles.some((role) => role.rolename === RoleName.SupportCurator)}
+                    value={RoleName.SupportCurator}
+                  />
+                }
+                label={t('my_page.roles.support_curator')}
               />
-            }
-            label={t('my_page.roles.support_curator')}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={values.roles?.some((r: any) => r.rolename === RoleName.NviCurator)}
-                value={RoleName.NviCurator}
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={values.roles.some((role) => role.rolename === RoleName.NviCurator)}
+                    value={RoleName.NviCurator}
+                  />
+                }
+                label={t('my_page.roles.nvi_curator')}
               />
-            }
-            label={t('my_page.roles.nvi_curator')}
-          />
-        </FormGroup>
-      </FormControl>
-    </>
+            </FormGroup>
+          </FormControl>
+
+          <DialogActions sx={{ justifyContent: 'center', p: 0 }}>
+            <Button onClick={closeDialog}>{t('common.cancel')}</Button>
+            <LoadingButton loading={isSubmitting} type="submit" variant="contained" disabled={!dirty}>
+              {t('common.add')}
+            </LoadingButton>
+          </DialogActions>
+        </Form>
+      )}
+    </Formik>
   );
 };
