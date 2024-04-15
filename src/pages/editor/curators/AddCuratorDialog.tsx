73ef1c2b390ce86baba73acbe11cdf176d43b3b2
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-04-15 14:38:31 +0200

    Show updated values for user after PUT

--- src/pages/editor/curators/AddCuratorDialog.tsx
+++ src/pages/editor/curators/AddCuratorDialog.tsx
@@ -212,7 +212,10 @@ const UserForm = ({ closeDialog, currentUser, initialValues, refetchCurators }: 
   });
 
   return (
-    <Formik initialValues={initialValues} onSubmit={async (values) => await userMutation.mutateAsync(values)}>
+    <Formik
+      initialValues={initialValues}
+      enableReinitialize
+      onSubmit={async (values) => await userMutation.mutateAsync(values)}>
       {({ values, setFieldValue, dirty, isSubmitting }: FormikProps<InstitutionUser>) => (
         <Form noValidate>
           <Typography variant="h3" gutterBottom>
