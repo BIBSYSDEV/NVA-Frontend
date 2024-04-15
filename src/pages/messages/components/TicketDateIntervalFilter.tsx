ef74c073e94bdf0e8c4021e15bac0dfac6d5bba0
Isar Buzza <isar.buzza@sikt.no>
2024-04-15 15:37:22 +0200

    Updates date handling in regards to new endpoint

--- src/pages/messages/components/TicketDateIntervalFilter.tsx
+++ src/pages/messages/components/TicketDateIntervalFilter.tsx
@@ -1,14 +1,18 @@
 import { Box, BoxProps } from '@mui/material';
 import { DatePicker, DatePickerProps } from '@mui/x-date-pickers';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import { TicketSearchParam } from '../../../api/searchApi';
 
 const commonDatepickerProps: Partial<DatePickerProps<Date>> = {
-  views: ['day'],
+  format: 'dd.MM.yyyy',
+  views: ['year', 'month', 'day'],
   disableHighlightToday: true,
 
-  slotProps: { textField: { sx: { maxWidth: '10rem' }, size: 'small' } },
+  slotProps: {
+    textField: { sx: { maxWidth: '10rem' }, size: 'small' },
+  },
 };
 
 interface TicketDateIntervalFilterProps {
@@ -21,47 +25,48 @@ export const TicketDateIntervalFilter = ({ datePickerProps, boxProps }: TicketDa
   const history = useHistory();
   const searchParams = new URLSearchParams(history.location.search);
 
-  const selectedDateSinceParam = searchParams.get(TicketSearchParam.FromDate);
-  const selectedDateBeforeParam = searchParams.get(TicketSearchParam.ToDate);
+  const selectedDatesParam = searchParams.get(TicketSearchParam.CreatedDate);
+  const dateArray = selectedDatesParam ? selectedDatesParam.split(',') : [];
 
-  const selectedDateSinceDate = selectedDateSinceParam ? new Date(selectedDateSinceParam) : undefined;
-  const selectedDateBeforeDate = selectedDateBeforeParam ? new Date(selectedDateBeforeParam) : undefined;
+  const [firstDate, setFirstDate] = useState<Date | null>(dateArray.length ? new Date(dateArray.shift() || '') : null);
+  const [lastDate, setLastDate] = useState<Date | null>(dateArray.length ? new Date(dateArray.pop() || '') : null);
 
-  const onChangeDate = (newDate: Date | null, param: TicketSearchParam.FromDate | TicketSearchParam.ToDate) => {
-    if (newDate) {
-      const date = newDate.toISOString().slice(0, 10); // date formatted as 'yyyy-mm-dd'
-      searchParams.set(param, date);
-      history.push({ search: searchParams.toString() });
-    } else {
-      searchParams.delete(param);
-      history.push({ search: searchParams.toString() });
+  const onChangeDate = (newDate: Date | null, type: 'from' | 'to') => {
+    let firstDateString = firstDate ? firstDate.toISOString().slice(0, 10) : '';
+    let lastDateString = lastDate ? lastDate.toISOString().slice(0, 10) : '';
+
+    if (type === 'from') {
+      firstDateString = newDate ? newDate.toISOString().slice(0, 10) : '';
+      setFirstDate(newDate);
+    } else if (type === 'to') {
+      lastDateString = newDate ? newDate.toISOString().slice(0, 10) : '';
+      setLastDate(newDate);
     }
-  };
 
-  const defaultMaxDate = new Date();
+    const newDateParam = `${firstDateString},${lastDateString}`;
+    if (newDateParam !== ',') {
+      searchParams.set(TicketSearchParam.CreatedDate, newDateParam);
+    } else {
+      searchParams.delete(TicketSearchParam.CreatedDate);
+    }
+    history.push({ search: searchParams.toString() });
+  };
 
   return (
     <Box sx={{ display: 'flex', justifyContent: 'space-evenly', gap: '1rem', ...boxProps?.sx }}>
       <DatePicker
         {...commonDatepickerProps}
         {...datePickerProps}
-        label={t('search.year_from')}
-        value={selectedDateSinceDate}
-        maxDate={
-          selectedDateBeforeDate
-            ? new Date(selectedDateBeforeDate.getFullYear(), 11, 31, 23, 59, 59, 999)
-            : defaultMaxDate
-        }
-        onChange={(date) => onChangeDate(date, TicketSearchParam.FromDate)}
+        label={t('registration.resource_type.date_from')}
+        value={firstDate}
+        onChange={(date) => onChangeDate(date, 'from')}
       />
       <DatePicker
         {...commonDatepickerProps}
         {...datePickerProps}
-        label={t('search.year_to')}
-        value={selectedDateBeforeDate}
-        minDate={selectedDateSinceDate}
-        maxDate={defaultMaxDate}
-        onChange={(date) => onChangeDate(date, TicketSearchParam.ToDate)}
+        label={t('registration.resource_type.date_to')}
+        value={lastDate}
+        onChange={(date) => onChangeDate(date, 'to')}
       />
     </Box>
   );
