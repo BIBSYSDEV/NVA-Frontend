09f7f61072deb8824514b98dbc32de018a8ec5fc
joachimjorgensen <joachim.jorgensen@sikt.no>
2024-04-15 14:42:22 +0200

    Extract log-item logic out of return function. Use common interface for all log entries

--- src/pages/public_registration/LogPanel.tsx
+++ src/pages/public_registration/LogPanel.tsx
@@ -1,4 +1,4 @@
-import { PublishingTicket, Ticket, TicketStatus, TicketType } from '../../types/publication_types/ticket.types';
+import { PublishingTicket, Ticket, TicketType } from '../../types/publication_types/ticket.types';
 import { Box, Skeleton, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from '@tanstack/react-query';
@@ -6,26 +6,13 @@ import { fetchOrganization } from '../../api/cristinApi';
 import { fetchUser } from '../../api/roleApi';
 import { Registration } from '../../types/registration.types';
 import { getFullName } from '../../utils/user-helpers';
-import { DoiRequestMessagesColumn } from '../messages/components/DoiRequestMessagesColumn';
 import { StyledStatusMessageBox } from '../messages/components/PublishingRequestMessagesColumn';
-import { CompletedPublishingRequestStatusBox } from './action_accordions/CompletedPublishingRequestStatusBox';
 
-const ticketStatusesToShow: TicketStatus[] = ['Completed', 'Closed'];
-const ticketTypesToShow: TicketType[] = ['PublishingRequest', 'DoiRequest'];
-
-type LogList = (Ticket | NonTicketLogEntry)[];
-
-interface NonTicketLogEntry {
+type LogList = LogItem[];
+interface LogItem {
   modifiedDate: string;
   description: string;
-}
-
-function isTicket(object: any): object is Ticket {
-  return ('type' as TicketType) in object;
-}
-
-function isNonTicketLogEntry(object: any): object is NonTicketLogEntry {
-  return 'description' in object;
+  type: TicketType;
 }
 
 interface LogPanelProps {
@@ -56,39 +43,72 @@ export const LogPanel = ({ tickets, registration }: LogPanelProps) => {
     cacheTime: 1_800_000, // 30 minutes
   });
 
-  const logs: LogList = [...tickets];
+  const logs: LogList = [];
 
   if (registration.publishedDate) {
-    const registrationPublished: NonTicketLogEntry = {
+    const registrationPublished: LogItem = {
       modifiedDate: registration.publishedDate,
       description: t('registration.status.PUBLISHED_METADATA'),
+      type: 'PublishingRequest',
     };
     logs.push(registrationPublished);
   }
 
   if (registration.publishedDate && registration.publishedDate < registration.modifiedDate) {
-    const registrationLastModified: NonTicketLogEntry = {
+    const registrationLastModified: LogItem = {
       modifiedDate: registration.modifiedDate,
       description: t('common.last_modified'),
+      type: 'PublishingRequest',
     };
     logs.push(registrationLastModified);
   }
 
-  const closedPublishingRequests = tickets.filter(
-    (ticket) => ticket.type === 'PublishingRequest' && ticket.status === 'Closed'
-  );
-  closedPublishingRequests.forEach((ticket) => {
-    const fileRejected: NonTicketLogEntry = {
-      modifiedDate: ticket.modifiedDate,
-      description: t('my_page.messages.files_rejected'),
-    };
-    logs.push(fileRejected);
+  tickets.forEach((ticket) => {
+    switch (ticket.type) {
+      case 'PublishingRequest': {
+        if (ticket.status === 'Completed' && (ticket as PublishingTicket).approvedFiles.length > 0) {
+          logs.push({
+            modifiedDate: ticket.modifiedDate,
+            description: t('my_page.messages.files_published', {
+              count: (ticket as PublishingTicket).approvedFiles.length,
+            }),
+            type: 'PublishingRequest',
+          });
+        } else if (ticket.status === 'Closed') {
+          logs.push({
+            modifiedDate: ticket.modifiedDate,
+            description: t('my_page.messages.files_rejected'),
+            type: 'PublishingRequest',
+          });
+        }
+        break;
+      }
+      case 'DoiRequest': {
+        if (ticket.status === 'Completed') {
+          logs.push({
+            modifiedDate: ticket.modifiedDate,
+            description: t('my_page.messages.doi_completed'),
+            type: 'DoiRequest',
+          });
+        } else if (ticket.status === 'Closed') {
+          logs.push({
+            modifiedDate: ticket.modifiedDate,
+            description: t('my_page.messages.doi_closed'),
+            type: 'DoiRequest',
+          });
+        }
+        break;
+      }
+      default: {
+        break;
+      }
+    }
   });
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', mt: '0.5rem' }}>
       {registration && (
-        <StyledStatusMessageBox key="registration-created-log-entry" sx={{ bgcolor: 'publishingRequest.main' }}>
+        <StyledStatusMessageBox sx={{ bgcolor: 'publishingRequest.main' }}>
           <Box sx={{ display: 'flex', flexDirection: 'column' }}>
             <Typography>{t('common.created')}:</Typography>
             {organizationQuery.isLoading || userQuery.isLoading ? (
@@ -104,30 +124,15 @@ export const LogPanel = ({ tickets, registration }: LogPanelProps) => {
         </StyledStatusMessageBox>
       )}
       {logs
-        .filter(
-          (logEntry) =>
-            isNonTicketLogEntry(logEntry) ||
-            (isTicket(logEntry) &&
-              ticketStatusesToShow.includes(logEntry.status) &&
-              ticketTypesToShow.includes(logEntry.type))
-        )
         .sort((a, b) => new Date(a.modifiedDate).getTime() - new Date(b.modifiedDate).getTime())
-        .map((logEntry, index) => {
-          if (isTicket(logEntry) && logEntry.type === 'PublishingRequest') {
-            return <CompletedPublishingRequestStatusBox key={logEntry.id} ticket={logEntry as PublishingTicket} />;
-          }
-          if (isTicket(logEntry) && logEntry.type === 'DoiRequest') {
-            return <DoiRequestMessagesColumn key={logEntry.id} ticket={logEntry} />;
-          }
-          if (isNonTicketLogEntry(logEntry)) {
-            return (
-              <StyledStatusMessageBox key={index} sx={{ bgcolor: 'publishingRequest.main' }}>
-                <Typography>{logEntry.description}</Typography>
-                <Typography>{new Date(logEntry.modifiedDate).toLocaleDateString()}</Typography>
-              </StyledStatusMessageBox>
-            );
-          }
-          return null;
+        .map((logItem, index) => {
+          const bgColor = logItem.type === 'PublishingRequest' ? 'publishingRequest.main' : 'doiRequest.main';
+          return (
+            <StyledStatusMessageBox key={index} sx={{ bgcolor: `${bgColor}` }}>
+              <Typography>{logItem.description}</Typography>
+              <Typography>{new Date(logItem.modifiedDate).toLocaleDateString()}</Typography>
+            </StyledStatusMessageBox>
+          );
         })}
     </Box>
   );
