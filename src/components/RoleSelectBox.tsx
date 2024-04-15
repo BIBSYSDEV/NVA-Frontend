d5b1d7ae0206d732186d248f3483adf6bfea0336
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-04-15 15:54:48 +0200

    Update RoleSelectBoxProps

--- src/components/RoleSelectBox.tsx
+++ src/components/RoleSelectBox.tsx
@@ -1,24 +1,21 @@
 import { Checkbox, CheckboxProps, FormControlLabel, FormControlLabelProps, Typography } from '@mui/material';
 
 interface RoleSelectBoxProps
-  extends Pick<FormControlLabelProps, 'sx' | 'disabled'>,
+  extends Pick<FormControlLabelProps, 'sx' | 'disabled' | 'label'>,
     Pick<CheckboxProps, 'checked' | 'value'> {
-  label: string;
   description?: string;
 }
 
-export const RoleSelectBox = ({ label, description, disabled, checked, value, sx }: RoleSelectBoxProps) => {
-  return (
-    <FormControlLabel
-      sx={{ p: '0.25rem', pr: '1rem', borderRadius: '0.5rem', ...sx }}
-      disabled={disabled}
-      control={<Checkbox checked={checked} value={value} />}
-      label={
-        <>
-          <Typography sx={{ fontWeight: 600, color: disabled ? undefined : 'textPrimary' }}>{label}</Typography>
-          <Typography>{description}</Typography>
-        </>
-      }
-    />
-  );
-};
+export const RoleSelectBox = ({ label, description, disabled, checked, value, sx }: RoleSelectBoxProps) => (
+  <FormControlLabel
+    sx={{ p: '0.25rem', pr: '1rem', borderRadius: '0.5rem', ...sx }}
+    disabled={disabled}
+    control={<Checkbox checked={checked} value={value} />}
+    label={
+      <>
+        <Typography sx={{ fontWeight: 600, color: disabled ? undefined : 'textPrimary' }}>{label}</Typography>
+        <Typography>{description}</Typography>
+      </>
+    }
+  />
+);
