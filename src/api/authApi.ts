b92b8abc61fc64b93044bf0cfbbad5d9716000f7
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-04-25 15:33:08 +0200

    Remove retries for fetching user attributes

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -3,16 +3,11 @@ import { FeideUser } from '../types/user.types';
 import { USE_MOCK_DATA } from '../utils/constants';
 import { UrlPathTemplate } from '../utils/urlPaths';
 
-export const getUserAttributes = async (retryNumber = 0): Promise<FeideUser | null> => {
+export const getUserAttributes = async (): Promise<FeideUser | null> => {
   try {
     const userAttributes = (await fetchUserAttributes()) as FeideUser;
     return userAttributes;
-  } catch (error) {
-    if (retryNumber < 3) {
-      // Retry when user has signed in, as the user attributes are not always available immediately for some reason
-      await new Promise((resolve) => setTimeout(resolve, 1000 * (retryNumber + 1)));
-      return await getUserAttributes(retryNumber + 1);
-    }
+  } catch {
     return null;
   }
 };
