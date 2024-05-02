8946e891572c08ea4154f807df6be5c9cb283560
nina.andersen <denektenina@gmail.com>
2024-05-02 10:01:16 +0200

    NP-46630: Download-button is now disabled after click to prevent user from clicking several times, until search is changed.

--- src/pages/search/ExportResultsButton.tsx
+++ src/pages/search/ExportResultsButton.tsx
@@ -4,6 +4,7 @@ import { API_URL } from '../../utils/constants';
 import { SearchApiPath } from '../../api/apiPaths';
 import { Button } from '@mui/material';
 import { dataTestId } from '../../utils/dataTestIds';
+import { useEffect, useState } from 'react';
 
 interface ExportResultsButtonProps {
   searchParams: URLSearchParams;
@@ -11,13 +12,18 @@ interface ExportResultsButtonProps {
 
 export const ExportResultsButton = ({ searchParams }: ExportResultsButtonProps) => {
   const { t } = useTranslation();
+  const [isClicked, setIsClicked] = useState(false);
+
+  useEffect(() => setIsClicked(false), [searchParams]);
 
   return (
     <Button
       href={`${API_URL.slice(0, -1)}${SearchApiPath.RegistrationsExport}?${searchParams.toString()}`}
+      onClick={() => setIsClicked(true)}
       title={t('search.export')}
       data-testid={dataTestId.startPage.advancedSearch.downloadResultsButton}
       variant="outlined"
+      disabled={isClicked}
       download>
       <FileDownloadOutlinedIcon />
     </Button>
