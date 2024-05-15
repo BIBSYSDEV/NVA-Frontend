d7e915b62257aeed5dc18f3e2148ba768b0ec270
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-05-15 14:16:48 +0200

    Add forgotten DOI component

---
+++ src/pages/basic_data/app_admin/central_import/CompareDoiField.tsx
@@ -0,0 +1,24 @@
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import { Box, Button, TextField } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
+export const CompareDoiField = ({ doi }: { doi: string }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Box sx={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
+      <TextField variant="standard" size="small" sx={{ flex: '1 10rem' }} disabled multiline value={doi} />
+      {doi && (
+        <Button
+          href={doi}
+          endIcon={<OpenInNewIcon />}
+          size="small"
+          target="_blank"
+          rel="noopener noreferrer"
+          variant="outlined">
+          {t('common.open')}
+        </Button>
+      )}
+    </Box>
+  );
+};
