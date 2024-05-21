93cb57e74e2078dff8505e3bcc819751c3098431
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-05-21 14:19:19 +0200

    Add query key for organization fetching

--- src/components/institution/OrganizationTree.tsx
+++ src/components/institution/OrganizationTree.tsx
@@ -14,7 +14,8 @@ export const OrganizationTree = ({ unitUri }: AffiliationHierarchyProps) => {
   const { t } = useTranslation();
 
   const organizationQuery = useQuery({
-    queryKey: [unitUri],
+    enabled: !!unitUri,
+    queryKey: ['organization', unitUri],
     queryFn: () => fetchOrganization(unitUri),
     meta: { errorMessage: t('feedback.error.get_institution') },
     staleTime: Infinity,
