9558f328ab6e5114e969d58d4b8ec66c804c36af
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-05-27 16:12:53 +0200

    Np 46739 use norwegian date formats (#5684)
    * Ensure all date strings are formatted according to norwegian rules
    
    * Ensure DatePickers show date in Norwegian format, and supports i18n

--- src/pages/public_registration/LogPanel.tsx
+++ src/pages/public_registration/LogPanel.tsx
@@ -6,6 +6,7 @@ import { fetchUser } from '../../api/roleApi';
 import { AssociatedFile } from '../../types/associatedArtifact.types';
 import { PublishingTicket, Ticket, TicketType } from '../../types/publication_types/ticket.types';
 import { Registration } from '../../types/registration.types';
+import { toDateString } from '../../utils/date-helpers';
 import { getAssociatedFiles } from '../../utils/registration-helpers';
 import { getFullName } from '../../utils/user-helpers';
 import { StyledStatusMessageBox } from '../messages/components/PublishingRequestMessagesColumn';
@@ -130,7 +131,7 @@ export const LogPanel = ({ tickets, registration }: LogPanelProps) => {
         <StyledStatusMessageBox sx={{ bgcolor: 'publishingRequest.main' }}>
           <Typography>{t('common.created')}</Typography>
           <Tooltip title={new Date(registration.createdDate).toLocaleTimeString()} enterDelay={tooltipDelay}>
-            <Typography>{new Date(registration.createdDate).toLocaleDateString()}</Typography>
+            <Typography>{toDateString(registration.createdDate)}</Typography>
           </Tooltip>
           {organizationQuery.isPending || userQuery.isPending ? (
             <Skeleton sx={{ width: '4rem' }} />
@@ -150,7 +151,7 @@ export const LogPanel = ({ tickets, registration }: LogPanelProps) => {
             <StyledStatusMessageBox key={index} sx={{ bgcolor: ticketColor[logItem.type] }}>
               <Typography>{logItem.description}</Typography>
               <Tooltip title={modifiedDate.toLocaleTimeString()} enterDelay={tooltipDelay}>
-                <Typography>{modifiedDate.toLocaleDateString()}</Typography>
+                <Typography>{toDateString(modifiedDate)}</Typography>
               </Tooltip>
               {logItem.filesInfo?.approvedFileNames && logItem.filesInfo.approvedFileNames.length > 0 && (
                 <Box
--- src/utils/date-helpers.ts
+++ src/utils/date-helpers.ts
@@ -1,9 +1,10 @@
-import { enUS as englishLocale, nb as norwegianLocale } from 'date-fns/locale';
+import { enUS as englishPickerLocale, nbNO as norwegianPickerLocale } from '@mui/x-date-pickers/locales';
+import { enGB as englishDateLocale, nb as norwegianDateLocale } from 'date-fns/locale';
 import { RegistrationDate } from '../types/registration.types';
 
 export const displayDate = (date: Omit<RegistrationDate, 'type'> | undefined) => {
   if (date?.month && date?.day) {
-    return new Date(+date.year, +date.month - 1, +date.day).toLocaleDateString();
+    return toDateString(new Date(+date.year, +date.month - 1, +date.day));
   } else if (date?.year) {
     return date.year;
   } else {
@@ -13,11 +14,28 @@ export const displayDate = (date: Omit<RegistrationDate, 'type'> | undefined) =>
 
 export const getDateFnsLocale = (language: string) => {
   if (language === 'nob' || language === 'nno') {
-    return norwegianLocale;
+    return norwegianDateLocale;
   }
-  return englishLocale;
+  return englishDateLocale;
+};
+
+export const getDatePickerLocaleText = (language: string) => {
+  if (language === 'nob' || language === 'nno') {
+    return norwegianPickerLocale.components.MuiLocalizationProvider.defaultProps.localeText;
+  }
+  return englishPickerLocale.components.MuiLocalizationProvider.defaultProps.localeText;
 };
 
 export const formatDateStringToISO = (date: Date) => {
   return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+};
+
+export const toDateString = (date: Date | string | number) => {
+  if (!date) {
+    return '';
+  }
+
+  const dateObject = date instanceof Date ? date : new Date(date);
+
+  return dateObject.toLocaleDateString('nb-NO', { year: 'numeric', month: '2-digit', day: '2-digit' });
 };
