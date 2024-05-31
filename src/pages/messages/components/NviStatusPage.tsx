e671f31794596af9f6a6eacc850afb86cc0ceea0
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-05-31 10:37:22 +0200

    Split table components

--- src/pages/messages/components/NviStatusPage.tsx
+++ src/pages/messages/components/NviStatusPage.tsx
@@ -1,8 +1,5 @@
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import {
   Box,
-  IconButton,
   Paper,
   Table,
   TableBody,
@@ -13,7 +10,6 @@ import {
   Typography,
 } from '@mui/material';
 import { visuallyHidden } from '@mui/utils';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { useFetchNviCandidates } from '../../../api/hooks/useFetchNviCandidates';
@@ -21,9 +17,8 @@ import { useFetchOrganization } from '../../../api/hooks/useFetchOrganization';
 import { BackgroundDiv } from '../../../components/styled/Wrappers';
 import { RootState } from '../../../redux/store';
 import { OrganizationApprovalStatusDetail } from '../../../types/nvi.types';
-import { Organization } from '../../../types/organization.types';
 import { isValidUrl } from '../../../utils/general-helpers';
-import { getLanguageString } from '../../../utils/translation-helpers';
+import { NviStatusTableRow } from './NviStatusTableRow';
 
 interface NviStatusPageProps {
   activeYear: number | string;
@@ -69,52 +64,10 @@ export const NviStatusPage = ({ activeYear }: NviStatusPageProps) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {institution && <OrganizationTableRow organization={institution} aggregations={nviAggregations} />}
+            {institution && <NviStatusTableRow organization={institution} aggregations={nviAggregations} />}
           </TableBody>
         </Table>
       </TableContainer>
     </BackgroundDiv>
-  );
-};
-interface OrganizationTableRowProps {
-  organization: Organization;
-  aggregations?: OrganizationApprovalStatusDetail;
-  level?: number;
-}
-
-const OrganizationTableRow = ({ organization, aggregations, level = 0 }: OrganizationTableRowProps) => {
-  const { t } = useTranslation();
-  const [expanded, setExpanded] = useState(level === 0);
-
-  const hasSubUnits = organization.hasPart && organization.hasPart.length > 0;
-  const thisAggregations = aggregations?.organizations?.[organization.id];
-
-  return (
-    <>
-      <TableRow sx={{ bgcolor: level % 2 === 0 ? undefined : '#FEFBF3' }}>
-        <TableCell sx={{ pl: `${1 + level * 1.5}rem`, py: '1rem' }}>{getLanguageString(organization.labels)}</TableCell>
-        <TableCell align="center">{thisAggregations?.status.New?.docCount.toLocaleString() ?? 0}</TableCell>
-        <TableCell align="center">{thisAggregations?.status.Pending?.docCount.toLocaleString() ?? 0}</TableCell>
-        <TableCell align="center">{thisAggregations?.status.Approved?.docCount.toLocaleString() ?? 0}</TableCell>
-        <TableCell align="center">{thisAggregations?.status.Rejected?.docCount.toLocaleString() ?? 0}</TableCell>
-        <TableCell align="center">{thisAggregations?.docCount.toLocaleString() ?? 0}</TableCell>
-        <TableCell align="center">
-          {thisAggregations?.points.value.toLocaleString(undefined, { maximumFractionDigits: 2 }) ?? 0}
-        </TableCell>
-        <TableCell align="center">{thisAggregations?.status.Dispute?.docCount.toLocaleString() ?? 0}</TableCell>
-        <TableCell>
-          {hasSubUnits && level !== 0 && (
-            <IconButton onClick={() => setExpanded(!expanded)} title={t('tasks.nvi.show_subunits')}>
-              {expanded ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>
-          )}
-        </TableCell>
-      </TableRow>
-
-      {expanded &&
-        organization.hasPart?.map((subUnit) => (
-          <OrganizationTableRow key={subUnit.id} organization={subUnit} aggregations={aggregations} level={level + 1} />
-        ))}
-    </>
   );
 };
---
+++ src/pages/messages/components/NviStatusTableRow.tsx
@@ -0,0 +1,51 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { IconButton, TableCell, TableRow } from '@mui/material';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { OrganizationApprovalStatusDetail } from '../../../types/nvi.types';
+import { Organization } from '../../../types/organization.types';
+import { getLanguageString } from '../../../utils/translation-helpers';
+
+interface NviStatusTableRowProps {
+  organization: Organization;
+  aggregations?: OrganizationApprovalStatusDetail;
+  level?: number;
+}
+
+export const NviStatusTableRow = ({ organization, aggregations, level = 0 }: NviStatusTableRowProps) => {
+  const { t } = useTranslation();
+  const [expanded, setExpanded] = useState(level === 0);
+
+  const hasSubUnits = organization.hasPart && organization.hasPart.length > 0;
+  const thisAggregations = aggregations?.organizations?.[organization.id];
+
+  return (
+    <>
+      <TableRow sx={{ bgcolor: level % 2 === 0 ? undefined : '#FEFBF3' }}>
+        <TableCell sx={{ pl: `${1 + level * 1.5}rem`, py: '1rem' }}>{getLanguageString(organization.labels)}</TableCell>
+        <TableCell align="center">{thisAggregations?.status.New?.docCount.toLocaleString() ?? 0}</TableCell>
+        <TableCell align="center">{thisAggregations?.status.Pending?.docCount.toLocaleString() ?? 0}</TableCell>
+        <TableCell align="center">{thisAggregations?.status.Approved?.docCount.toLocaleString() ?? 0}</TableCell>
+        <TableCell align="center">{thisAggregations?.status.Rejected?.docCount.toLocaleString() ?? 0}</TableCell>
+        <TableCell align="center">{thisAggregations?.docCount.toLocaleString() ?? 0}</TableCell>
+        <TableCell align="center">
+          {thisAggregations?.points.value.toLocaleString(undefined, { maximumFractionDigits: 2 }) ?? 0}
+        </TableCell>
+        <TableCell align="center">{thisAggregations?.status.Dispute?.docCount.toLocaleString() ?? 0}</TableCell>
+        <TableCell>
+          {hasSubUnits && level !== 0 && (
+            <IconButton onClick={() => setExpanded(!expanded)} title={t('tasks.nvi.show_subunits')}>
+              {expanded ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          )}
+        </TableCell>
+      </TableRow>
+
+      {expanded &&
+        organization.hasPart?.map((subUnit) => (
+          <NviStatusTableRow key={subUnit.id} organization={subUnit} aggregations={aggregations} level={level + 1} />
+        ))}
+    </>
+  );
+};
