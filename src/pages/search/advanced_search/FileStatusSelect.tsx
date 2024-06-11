aafec0010ff0b91651371524f350008f9daa83ac
Isar Buzza <isar.buzza@sikt.no>
2024-06-11 17:37:02 +0200

    code review

--- src/pages/search/advanced_search/FileStatusSelect.tsx
+++ src/pages/search/advanced_search/FileStatusSelect.tsx
@@ -1,5 +1,4 @@
 import { MenuItem, Select, SelectChangeEvent } from '@mui/material';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import { ResultParam } from '../../../api/searchApi';
@@ -8,20 +7,18 @@ import { dataTestId } from '../../../utils/dataTestIds';
 enum FileStatus {
   hasPublicFiles = 'hasPublicFiles',
   noFiles = 'noFiles',
-  showAll = 'showAll',
 }
 
 export const FileStatusSelect = () => {
   const { t } = useTranslation();
   const history = useHistory();
   const searchParams = new URLSearchParams(history.location.search);
-  const selectedParam = searchParams.get(ResultParam.Files) ?? FileStatus.showAll;
-  const [isOpen, setIsOpen] = useState(false);
+  const selectedParam = searchParams.get(ResultParam.Files) ?? '';
 
   const handleChange = (event: SelectChangeEvent) => {
     const newValue = event.target.value;
 
-    if (newValue !== FileStatus.showAll) {
+    if (newValue) {
       searchParams.set(ResultParam.Files, newValue);
     } else {
       searchParams.delete(ResultParam.Files);
@@ -34,16 +31,12 @@ export const FileStatusSelect = () => {
     <Select
       sx={{ minWidth: '10rem' }}
       size="small"
-      role="combobox"
       data-testid={dataTestId.startPage.advancedSearch.fileStatusSelect}
-      SelectDisplayProps={{ 'aria-labelledby': 'file-status-select-label' }}
-      aria-expanded={isOpen}
-      onOpen={() => setIsOpen(true)}
-      onClose={() => setIsOpen(false)}
-      defaultValue={FileStatus.showAll}
+      labelId="file-status-select-label"
+      displayEmpty
       value={selectedParam}
       onChange={handleChange}>
-      <MenuItem value={FileStatus.showAll}>{t('common.show_all')}</MenuItem>
+      <MenuItem value={''}>{t('common.show_all')}</MenuItem>
       <MenuItem value={FileStatus.noFiles}>{t('registration.files_and_license.registration_without_file')}</MenuItem>
       <MenuItem value={FileStatus.hasPublicFiles}>
         {t('registration.files_and_license.registration_with_file')}
