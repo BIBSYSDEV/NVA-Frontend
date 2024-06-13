5d0c6deaa5937946b5858e1e95dd959b985642a5
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-06-13 11:17:46 +0200

    Np 46642 nvi menu component (#5751)
    * Reuse NVI candidates query hook
    
    * Sopprt link back to correct nvi search
    
    * Reorder hook params
    
    * Update hook usage
    
    * Remove wrongly committed functionality
    
    * Update param name
    
    * Move assignee filter to main content
    
    * Move status filter to url param
    
    * Fix styling of assignee selector
    
    * Move all filter state to url params
    
    * Move year filter
    
    * FIx layout
    
    * Simplify styling
    
    * Ensure nvi filter always is valid
    
    * Update type usages
    
    * Minor cleanup
    
    * Add data-testid
    
    * Improve type
    
    * Remove redundant export
    
    * Add affiliation selector for area of responsibility
    
    * Remove old area of responsibility selector
    
    * Minor simplifications
    
    * Optional sx prop
    
    * Start splitting NVI menu into its own component
    
    * Use nvi params hook
    
    * Handle pagination reset (for most cases)
    
    * Reset pagination for all filters
    
    * Show correction list for NVI curator
    
    * Ensure NVI status is showing for the correct year
    
    * Set default value for status filter
    
    * Add loading indicator for NVI status panel
    
    * Use hook instead of prop
    
    * Include forgotten file

---
+++ src/utils/hooks/useNviCandidatesParams.ts
@@ -0,0 +1,31 @@
+import { useLocation } from 'react-router-dom';
+import { FetchNviCandidatesParams, NviCandidatesSearchParam } from '../../api/searchApi';
+import { NviCandidateSearchStatus } from '../../types/nvi.types';
+import { ROWS_PER_PAGE_OPTIONS } from '../constants';
+
+const defaultNviYear = new Date().getFullYear();
+
+export const useNviCandidatesParams = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  const affiliations = searchParams.get(NviCandidatesSearchParam.Affiliations)?.split(',') ?? null;
+  const aggregation = searchParams.get(NviCandidatesSearchParam.Aggregation) as 'all' | NviCandidateSearchStatus | null;
+  const assignee = searchParams.get(NviCandidatesSearchParam.Assignee);
+  const filter = (searchParams.get(NviCandidatesSearchParam.Filter) as NviCandidateSearchStatus | null) ?? 'pending';
+  const offset = (searchParams.get(NviCandidatesSearchParam.Offset) as number | null) ?? 0;
+  const query = searchParams.get(NviCandidatesSearchParam.Query);
+  const size = (searchParams.get(NviCandidatesSearchParam.Size) as number | null) ?? ROWS_PER_PAGE_OPTIONS[0];
+  const year = (searchParams.get(NviCandidatesSearchParam.Year) as number | null) ?? defaultNviYear;
+
+  return {
+    affiliations,
+    aggregation,
+    assignee,
+    filter,
+    offset,
+    query,
+    size,
+    year,
+  } satisfies FetchNviCandidatesParams;
+};
