d6e40a450016f89fa7fba302fca004406cb1f715
iBuzza <53341294+iBuzza@users.noreply.github.com>
2024-06-18 15:58:38 +0200

    NP-45906 add delete on message (#5677)
    * Added new function for deleting ticket messages
    
    * added return to delete function
    
    * Added new menu to messages for deleting
    
    * added check for delete priviliges
    
    * changed onDelete to return promise for closing ConfirmDialog
    
    * Added props for dialog title and content
    
    * removed expand to delete functionality on MessageItem
    
    * added prop to check for deletion priviliges
    
    * Refactored delete function
    
    * Translation changes
    
    * added function to truncate sender name
    
    * minor changes
    
    * Moved truncateName function to user-helpers
    
    * Added canDelete prop to MessageList
    
    * updated success and error message
    
    * Text and translation updates
    
    * added checks for deleting doi messages
    
    * Created own hook for deleteMessageMutation
    
    * Improved error handling
    
    * Moved useDispatch into hook
    
    * changed name of hook
    
    * cleanup
    
    * Refactored API function, onAccept and added i18n to aria-label
    
    * Created seperate menu components for LP and NVI (wip)
    
    * minor changes
    
    * minor changes and cleanup
    
    * added data-testids to new menu component
    
    * Changed conditions for deleting note and displaying menu
    
    * minor changes
    
    * Changed and moved delete function
    
    * Handle name truncation with css
    
    * minor changes

---
+++ src/pages/messages/components/MessageMenu.tsx
@@ -0,0 +1,77 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Typography } from '@mui/material';
+import { MouseEvent, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ConfirmDialog } from '../../../components/ConfirmDialog';
+import { dataTestId } from '../../../utils/dataTestIds';
+import { useDeleteTicketMessage } from '../../../utils/hooks/useDeleteTicketMessage';
+
+interface MessageMenuProps {
+  canDeleteMessage: boolean;
+  ticketId: string;
+  messageIdentifier: string;
+  refetchData?: () => void;
+}
+
+export const MessageMenu = ({ ticketId, messageIdentifier, refetchData, canDeleteMessage }: MessageMenuProps) => {
+  const { t } = useTranslation();
+  const [showConfirmDialog, setShowConfirmDialog] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const deleteTicketMessageMutation = useDeleteTicketMessage(ticketId, refetchData);
+
+  const handleClickMenuAnchor = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  return (
+    <section>
+      <IconButton
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        data-testid={dataTestId.registrationLandingPage.tasksPanel.messageOptionsButton}
+        aria-label={t('common.delete')}
+        size="small"
+        sx={{ alignSelf: 'end' }}
+        onClick={handleClickMenuAnchor}>
+        <MoreVertIcon fontSize="inherit" />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={() => setAnchorEl(null)}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}>
+        <MenuItem
+          data-testid={dataTestId.registrationLandingPage.tasksPanel.deleteMessageButton}
+          disabled={!canDeleteMessage}
+          onClick={() => {
+            setShowConfirmDialog(true);
+            setAnchorEl(null);
+          }}>
+          <ListItemIcon>
+            <DeleteIcon />
+          </ListItemIcon>
+          <ListItemText>{t('common.delete')}</ListItemText>
+        </MenuItem>
+      </Menu>
+
+      <ConfirmDialog
+        open={showConfirmDialog}
+        title={t('my_page.messages.delete_message')}
+        onAccept={async () => {
+          await deleteTicketMessageMutation.mutateAsync(messageIdentifier);
+          setShowConfirmDialog(false);
+        }}
+        isLoading={deleteTicketMessageMutation.isPending}
+        onCancel={() => setShowConfirmDialog(false)}>
+        <Typography>{t('my_page.messages.delete_message_description')}</Typography>
+      </ConfirmDialog>
+    </section>
+  );
+};
