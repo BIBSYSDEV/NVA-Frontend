aa92a70fc8e77c2069b5f6467749cc4f11e73a67
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-07-11 14:37:40 +0200

    Extend official eslint plugins instead of overwriting custom stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
@@ -15,7 +16,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
-    'react/react-in-jsx-scope': 'off',
     'react/no-children-prop': 'off', // TODO: Violations of this rule should be resolved, so we can implement this rule
     'no-console': 'warn',
     'no-debugger': 'warn',
