77b3efefb62fd8c9227b89709e70dc4c04aff7cf
Joachim Jørgensen <joachim.jorgensen@Joachims-MacBook-Pro.local>
2024-07-29 17:14:16 +0200

    Do not display publishing tickets without files

--- src/utils/log/ticketEntryGenerator.ts
+++ src/utils/log/ticketEntryGenerator.ts
@@ -3,12 +3,13 @@ import { generateActionsFromTicket } from './actionGenerator';
 import { generateTitleFromTicket } from './titleGenerator';
 import { getAssociatedFiles } from '../registration-helpers';
 import { LogEntry } from '../../types/log.types';
-import { Ticket } from '../../types/publication_types/ticket.types';
+import { PublishingTicket, Ticket } from '../../types/publication_types/ticket.types';
 import { Registration } from '../../types/registration.types';
 
 export function generateTicketLogEntries(tickets: Ticket[], registration: Registration, t: TFunction): LogEntry[] {
   return tickets
     .filter((ticket) => ticket.type === 'PublishingRequest' || ticket.type === 'DoiRequest')
+    .filter(onlyPublishingRequestsWithFiles)
     .map((ticket) => generateTicketLogEntry(ticket, registration, t));
 }
 
@@ -21,4 +22,15 @@ function generateTicketLogEntry(ticket: Ticket, registration: Registration, t: T
     modifiedDate: ticket.modifiedDate,
     actions: generateActionsFromTicket(ticket, associatedFiles, t),
   };
+}
+
+// NB: Will interfere with tickets with only metadata updates if they are added in the future.
+// For now this serves to avoid a corner case where publishing metadata without files creates
+// a "metadata updated" entry when it's actually only a "metadata published" entry.
+function onlyPublishingRequestsWithFiles(ticket: Ticket) {
+  return (
+    ticket.type !== 'PublishingRequest' ||
+    (ticket as PublishingTicket).filesForApproval.length > 0 ||
+    (ticket as PublishingTicket).approvedFiles.length > 0
+  );
 }
