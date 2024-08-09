72a9588f3a5b6227d47de7b9640f940858399826
nina.andersen <denektenina@gmail.com>
2024-08-09 11:22:07 +0200

    Initial setup of stepper and description view

--- src/pages/project/new_project/EditProject.tsx
+++ src/pages/project/new_project/EditProject.tsx
@@ -1,16 +1,32 @@
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useFetchProject } from '../../../api/hooks/useFetchProject';
+import { PageSpinner } from '../../../components/PageSpinner';
 import { StyledPageContent } from '../../../components/styled/Wrappers';
+import { RootState } from '../../../redux/store';
 import { IdentifierParams } from '../../../utils/urlPaths';
+import { Forbidden } from '../../errorpages/Forbidden';
+import { canEditProject } from '../../registration/description_tab/projects_field/projectHelpers';
 import { ProjectForm } from './ProjectForm';
 
 const EditProject = () => {
+  const { t } = useTranslation();
   const { identifier } = useParams<IdentifierParams>();
+  const projectQuery = useFetchProject(decodeURIComponent(identifier));
+  const project = projectQuery.data;
+  const user = useSelector((store: RootState) => store.user);
+  const userCanEditProject = canEditProject(user, project);
 
-  return (
+  return projectQuery.isPending ? (
+    <PageSpinner aria-label={t('common.result')} />
+  ) : !userCanEditProject ? (
+    <Forbidden />
+  ) : project ? (
     <StyledPageContent>
-      <ProjectForm projectId={decodeURIComponent(identifier)} />
+      <ProjectForm project={project} />
     </StyledPageContent>
-  );
+  ) : null;
 };
 
 export default EditProject;
---
+++ src/pages/project/new_project/ProjectDescriptionForm.tsx
@@ -0,0 +1,184 @@
+import { Autocomplete, Box, styled, TextField, Typography } from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers';
+import { ErrorMessage, Field, FieldProps, useFormikContext } from 'formik';
+import { useTranslation } from 'react-i18next';
+import { ErrorBoundary } from '../../../components/ErrorBoundary';
+import { cristinKeywords } from '../../../resources/cristinKeywords';
+import { CristinProject, ProjectFieldName, SaveCristinProject, TypedLabel } from '../../../types/project.types';
+import { dataTestId } from '../../../utils/dataTestIds';
+import { getLanguageString } from '../../../utils/translation-helpers';
+import { isRekProject } from '../../registration/description_tab/projects_field/projectHelpers';
+
+const FormBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  backgroundColor: theme.palette.secondary.light,
+  padding: '1.75rem 1.25rem',
+  gap: '1.5rem',
+  borderRadius: '0.25rem',
+}));
+
+interface ProjectDescriptionFormProps {
+  project: CristinProject;
+}
+
+export const ProjectDescriptionForm = ({ project }: ProjectDescriptionFormProps) => {
+  const { t } = useTranslation();
+  const { values, setFieldValue, setFieldTouched, touched, errors } = useFormikContext<SaveCristinProject>();
+  const thisIsRekProject = isRekProject(project);
+
+  return (
+    <ErrorBoundary>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+        <FormBox>
+          <Typography variant="h2">{t('project.form.title_and_description')}</Typography>
+          <Field name={ProjectFieldName.Title}>
+            {({ field, meta: { touched, error } }: FieldProps<string>) => (
+              <TextField
+                {...field}
+                data-testid={dataTestId.registrationWizard.description.projectForm.titleField}
+                label={t('common.title')}
+                disabled={thisIsRekProject}
+                required
+                variant="filled"
+                fullWidth
+                error={touched && !!error}
+                helperText={<ErrorMessage name={field.name} />}
+              />
+            )}
+          </Field>
+          <Field name={ProjectFieldName.AcademicSummaryNo}>
+            {({ field }: FieldProps<string>) => (
+              <TextField
+                {...field}
+                value={field.value ?? ''}
+                variant="filled"
+                fullWidth
+                multiline
+                rows="4"
+                data-testid={dataTestId.registrationWizard.description.projectForm.scentificSummaryNorwegianField}
+                label={t('project.scientific_summary_norwegian')}
+              />
+            )}
+          </Field>
+          <Field name={ProjectFieldName.AcademicSummaryEn}>
+            {({ field }: FieldProps<string>) => (
+              <TextField
+                {...field}
+                value={field.value ?? ''}
+                variant="filled"
+                fullWidth
+                multiline
+                rows="4"
+                data-testid={dataTestId.registrationWizard.description.projectForm.scentificSummaryEnglishField}
+                label={t('project.scientific_summary_english')}
+              />
+            )}
+          </Field>
+          <Field name={ProjectFieldName.PopularScientificSummaryNo}>
+            {({ field }: FieldProps<string>) => (
+              <TextField
+                {...field}
+                value={field.value ?? ''}
+                variant="filled"
+                fullWidth
+                multiline
+                rows="3"
+                data-testid={dataTestId.registrationWizard.description.projectForm.popularScienceSummaryNorwegianField}
+                label={t('project.popular_science_summary_norwegian')}
+              />
+            )}
+          </Field>
+          <Field name={ProjectFieldName.PopularScientificSummaryEn}>
+            {({ field }: FieldProps<string>) => (
+              <TextField
+                {...field}
+                value={field.value ?? ''}
+                variant="filled"
+                fullWidth
+                multiline
+                rows="3"
+                data-testid={dataTestId.registrationWizard.description.projectForm.popularScienceSummaryEnglishField}
+                label={t('project.popular_science_summary_english')}
+              />
+            )}
+          </Field>
+        </FormBox>
+        <FormBox>
+          <Typography variant="h2">{t('project.form.keywords')}</Typography>
+          <Field name={ProjectFieldName.Keywords}>
+            {({ field, form: { setFieldValue } }: FieldProps<TypedLabel[]>) => (
+              <Autocomplete
+                options={cristinKeywords}
+                multiple
+                data-testid={dataTestId.registrationWizard.description.projectForm.keywordsField}
+                getOptionLabel={(option) => getLanguageString(option.label)}
+                isOptionEqualToValue={(option, value) => option.type === value.type}
+                value={field.value}
+                onChange={(_, value) => setFieldValue(field.name, value)}
+                renderInput={(params) => <TextField {...params} variant="filled" label={t('project.keywords')} />}
+              />
+            )}
+          </Field>
+        </FormBox>
+        <FormBox>
+          <Typography variant="h2" sx={{ pb: '0.25rem' }}>
+            {t('project.form.date')}
+          </Typography>
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Field name={ProjectFieldName.StartDate}>
+              {({ field, meta: { touched, error } }: FieldProps<string>) => (
+                <DatePicker
+                  label={t('common.start_date')}
+                  disabled={thisIsRekProject}
+                  onChange={(date) => {
+                    !touched && setFieldTouched(field.name, true, false);
+                    setFieldValue(field.name, date ?? '');
+                  }}
+                  value={field.value ? new Date(field.value) : null}
+                  maxDate={values.endDate ? new Date(values.endDate) : undefined}
+                  slotProps={{
+                    textField: {
+                      inputProps: {
+                        'data-testid': dataTestId.registrationWizard.description.projectForm.startDateField,
+                      },
+                      variant: 'filled',
+                      onBlur: () => !touched && setFieldTouched(field.name),
+                      required: true,
+                      error: touched && !!error,
+                      helperText: <ErrorMessage name={field.name} />,
+                    },
+                  }}
+                />
+              )}
+            </Field>
+
+            <Field name={ProjectFieldName.EndDate}>
+              {({ field, meta: { touched, error } }: FieldProps<string>) => (
+                <DatePicker
+                  label={t('common.end_date')}
+                  disabled={thisIsRekProject}
+                  onChange={(date) => {
+                    !touched && setFieldTouched(field.name, true, false);
+                    setFieldValue(field.name, date);
+                  }}
+                  value={field.value ? new Date(field.value) : null}
+                  minDate={values.startDate ? new Date(values.startDate) : undefined}
+                  slotProps={{
+                    textField: {
+                      inputProps: { 'data-testid': dataTestId.registrationWizard.description.projectForm.endDateField },
+                      variant: 'filled',
+                      required: true,
+                      error: touched && !!error,
+                      helperText: <ErrorMessage name={field.name} />,
+                    },
+                  }}
+                />
+              )}
+            </Field>
+          </Box>
+        </FormBox>
+      </Box>
+    </ErrorBoundary>
+  );
+};
