e9c6bd20c9f5071af33e63174ddb0659e6e04068
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-08-09 10:18:43 +0200

    Add forgotten file

---
+++ src/components/SelectableButton.tsx
@@ -0,0 +1,21 @@
+import { Button, ButtonProps } from '@mui/material';
+import { Link, LinkProps } from 'react-router-dom';
+
+export interface SelectableButtonProps extends ButtonProps, Partial<Pick<LinkProps, 'to'>> {
+  isSelected?: boolean;
+}
+
+export const SelectableButton = ({ isSelected, sx, ...rest }: SelectableButtonProps) => (
+  <Button
+    sx={{
+      textTransform: 'none',
+      bgcolor: isSelected ? 'primary.main' : 'background.default',
+      justifyContent: 'start',
+      boxShadow: '0px 3px 3px 0px rgba(0, 0, 0, 0.20)',
+      ...sx,
+    }}
+    variant={isSelected ? 'contained' : 'outlined'}
+    LinkComponent={rest.to ? Link : undefined}
+    {...rest}
+  />
+);
