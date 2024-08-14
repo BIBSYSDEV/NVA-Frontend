6a6d0f99fc35dcb6253517ee1a1e1c73f39f14d0
Isar Buzza <isar.buzza@sikt.no>
2024-08-14 15:06:28 +0200

    simplified layout

--- src/pages/registration/description_tab/projects_field/ProjectItem.tsx
+++ src/pages/registration/description_tab/projects_field/ProjectItem.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { useFetchProjectQuery } from '../../../../utils/hooks/useFetchProjectQuery';
-import { Box, IconButton, Link as MuiLink, Typography } from '@mui/material';
+import { Box, Divider, IconButton, Link as MuiLink, Typography } from '@mui/material';
 import { dataTestId } from '../../../../utils/dataTestIds';
 import { getProjectPath } from '../../../../utils/urlPaths';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
@@ -24,39 +24,37 @@ export const ProjectItem = ({ projectId, removeProject }: ProjectItemProps) => {
   const isFetching = projectQuery.isFetching;
 
   return (
-    <Box
-      sx={{
-        display: 'grid',
-        gridTemplateColumns: { sm: '1fr', md: '1fr 1fr auto' },
-        gap: '1rem',
-        bgcolor: 'secondary.light',
-        p: '1rem',
-        border: '1px solid lightgray',
-        borderRadius: '8px',
-      }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <Typography fontWeight="bold">{t('project.project').toUpperCase()}</Typography>
-        <Box sx={{ display: 'grid', gridTemplateRows: '1fr auto', gap: '1rem', height: '100%' }}>
+    <>
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: { sm: '1fr', md: '1fr 1fr auto' },
+          gap: '1rem',
+          bgcolor: 'secondary.light',
+          p: '1rem',
+          border: '1px solid lightgray',
+          borderRadius: '8px',
+        }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+          <Typography fontWeight="bold">{t('project.project').toUpperCase()}</Typography>
           <div>
             <Typography fontWeight="bold">{t('common.title')}:</Typography>
-            <Box sx={{ display: 'flex', gap: '2rem' }}>
-              {isFetching ? (
-                <StyledListSkeleton />
-              ) : project ? (
-                <MuiLink
-                  sx={{ display: 'flex', gap: '1rem', alignItems: 'center' }}
-                  component={Link}
-                  data-testid={dataTestId.registrationWizard.description.projectLink(project?.id)}
-                  to={getProjectPath(project.id)}
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  {project.title}
-                  <OpenInNewIcon fontSize="small" />
-                </MuiLink>
-              ) : (
-                <Typography>-</Typography>
-              )}
-            </Box>
+            {isFetching ? (
+              <StyledListSkeleton />
+            ) : project ? (
+              <MuiLink
+                sx={{ display: 'flex', gap: '1rem', alignItems: 'center' }}
+                component={Link}
+                data-testid={dataTestId.registrationWizard.description.projectLink(project.id)}
+                to={getProjectPath(project.id)}
+                target="_blank"
+                rel="noopener noreferrer">
+                {project.title}
+                <OpenInNewIcon fontSize="small" />
+              </MuiLink>
+            ) : (
+              <Typography>-</Typography>
+            )}
           </div>
           <div>
             <Typography fontWeight="bold">{t('project.coordinating_institution')}:</Typography>
@@ -69,59 +67,56 @@ export const ProjectItem = ({ projectId, removeProject }: ProjectItemProps) => {
             )}
           </div>
         </Box>
-      </Box>
 
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <Typography fontWeight="bold">{t('common.funding').toUpperCase()}</Typography>
-        {project?.funding?.length ? (
-          project.funding.map((funding) => (
-            <Box
-              key={funding.identifier}
-              sx={{ display: 'grid', gridTemplateRows: '1fr auto', gap: '1rem', height: '100%' }}>
-              <div>
-                <Typography fontWeight="bold">{t('registration.description.funding.funder')}:</Typography>
-                {isFetching ? <StyledListSkeleton /> : <Typography>{getLanguageString(funding.labels)}</Typography>}
-              </div>
-              <div>
-                <Typography fontWeight="bold">{t('project.grant_id')}:</Typography>
-                {isFetching ? (
-                  <StyledListSkeleton />
-                ) : funding.identifier ? (
-                  fundingSourceIsNfr(funding.source) ? (
-                    <Box sx={{ display: 'flex', gap: '2rem', height: 'fit-content' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+          <Typography fontWeight="bold">{t('common.funding').toUpperCase()}</Typography>
+          {project?.funding?.length ? (
+            project.funding.map((funding, index) => (
+              <Box key={funding.identifier} sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+                {index > 0 && <Divider />}
+                <div>
+                  <Typography fontWeight="bold">{t('registration.description.funding.funder')}:</Typography>
+                  {isFetching ? <StyledListSkeleton /> : <Typography>{getLanguageString(funding.labels)}</Typography>}
+                </div>
+                <div>
+                  <Typography fontWeight="bold">{t('project.grant_id')}:</Typography>
+                  {isFetching ? (
+                    <StyledListSkeleton />
+                  ) : funding.identifier ? (
+                    fundingSourceIsNfr(funding.source) ? (
                       <MuiLink
                         sx={{ display: 'flex', gap: '1rem' }}
-                        component={Link}
                         data-testid={dataTestId.registrationWizard.description.nfrProjectLink(funding.identifier)}
-                        to={getNfrProjectUrl(funding.identifier)}
+                        href={getNfrProjectUrl(funding.identifier)}
                         target="_blank"
                         rel="noopener noreferrer">
                         {funding.identifier}
                         <OpenInNewIcon fontSize="small" />
                       </MuiLink>
-                    </Box>
+                    ) : (
+                      <Typography>{funding.identifier}</Typography>
+                    )
                   ) : (
-                    <Typography>{funding.identifier}</Typography>
-                  )
-                ) : (
-                  <Typography>-</Typography>
-                )}
-              </div>
-            </Box>
-          ))
-        ) : (
-          <Typography fontStyle="italic">{t('project.project_has_no_funding')}</Typography>
-        )}
+                    <Typography>-</Typography>
+                  )}
+                </div>
+              </Box>
+            ))
+          ) : (
+            <Typography fontStyle="italic">{t('project.project_has_no_funding')}</Typography>
+          )}
+        </Box>
+
+        <IconButton
+          size="small"
+          sx={{ alignSelf: 'center' }}
+          color="primary"
+          onClick={() => removeProject(projectId)}
+          title={t('common.remove')}
+          data-testid={dataTestId.registrationWizard.description.removeProjectButton}>
+          <CancelIcon />
+        </IconButton>
       </Box>
-      <IconButton
-        size="small"
-        sx={{ alignSelf: 'center' }}
-        color="primary"
-        onClick={() => removeProject(projectId)}
-        title={t('common.remove')}
-        data-testid={dataTestId.registrationWizard.description.removeProjectButton}>
-        <CancelIcon />
-      </IconButton>
-    </Box>
+    </>
   );
 };
