8ad69059117ab2fcbd0f45afc520b207268ea240
Nina Andersen <denektenina@gmail.com>
2024-08-15 13:09:55 +0200

    Show error message on category when there is a duplicate (#6058)

--- src/api/hooks/useDuplicateRegistrationSearch.ts
+++ src/api/hooks/useDuplicateRegistrationSearch.ts
@@ -2,7 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 import { fetchResults, FetchResultsParams } from '../searchApi';
 
-export const useDuplicateRegistrationSearch = (title: string, publishedYear?: string) => {
+export const useDuplicateRegistrationSearch = (
+  title: string | undefined,
+  identifier?: string,
+  publishedYear?: string
+) => {
   const { t } = useTranslation();
 
   const searchConfig: FetchResultsParams = {
@@ -10,6 +14,7 @@ export const useDuplicateRegistrationSearch = (title: string, publishedYear?: st
   };
 
   const titleSearch = useQuery({
+    enabled: !!title,
     queryKey: ['registrations', searchConfig],
     queryFn: () => fetchResults(searchConfig),
     meta: { errorMessage: t('feedback.error.get_registrations') },
@@ -17,9 +22,14 @@ export const useDuplicateRegistrationSearch = (title: string, publishedYear?: st
 
   const registrationsWithSimilarName = titleSearch.data?.hits ?? [];
   const duplicateRegistration = registrationsWithSimilarName.find((reg) => {
+    if (!title) {
+      return false;
+    }
+
+    const isSameRegistration = reg.identifier === identifier;
     const hasSameName = reg.entityDescription?.mainTitle.toLowerCase() === title.toLowerCase();
 
-    if (!hasSameName) {
+    if (!hasSameName || isSameRegistration) {
       return false;
     }
 
