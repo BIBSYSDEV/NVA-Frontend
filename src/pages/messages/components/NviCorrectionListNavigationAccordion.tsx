848dc314119eb68af9958d7c667960ddd578cb9e
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-08-15 09:56:37 +0200

    Remove beta tag for correction lists

--- src/pages/messages/components/NviCorrectionListNavigationAccordion.tsx
+++ src/pages/messages/components/NviCorrectionListNavigationAccordion.tsx
@@ -3,7 +3,6 @@ import { Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import { ResultParam } from '../../../api/searchApi';
-import { BetaFunctionality } from '../../../components/BetaFunctionality';
 import { NavigationListAccordion } from '../../../components/NavigationListAccordion';
 import { NavigationList } from '../../../components/PageWithSideMenu';
 import { SelectableButton } from '../../../components/SelectableButton';
@@ -32,40 +31,38 @@ export const NviCorrectionListNavigationAccordion = () => {
           {t('tasks.nvi.correction_list_type.correction_list_duct')}
         </SelectableButton>
 
-        <BetaFunctionality>
-          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
-            <SelectableButton
-              data-testid={dataTestId.tasksPage.correctionList.applicableCategoriesWithNonApplicableChannelButton}
-              isSelected={selectedNviList === 'ApplicableCategoriesWithNonApplicableChannel'}
-              onClick={() => {
-                if (selectedNviList !== 'ApplicableCategoriesWithNonApplicableChannel') {
-                  searchParams.set(
-                    nviCorrectionListQueryKey,
-                    'ApplicableCategoriesWithNonApplicableChannel' satisfies CorrectionListId
-                  );
-                  searchParams.delete(ResultParam.From);
-                  history.push({ search: searchParams.toString() });
-                }
-              }}>
-              {t('tasks.nvi.correction_list_type.applicable_category_in_non_applicable_channel')}
-            </SelectableButton>
-            <SelectableButton
-              data-testid={dataTestId.tasksPage.correctionList.nonApplicableCategoriesWithApplicableChannelButton}
-              isSelected={selectedNviList === 'NonApplicableCategoriesWithApplicableChannel'}
-              onClick={() => {
-                if (selectedNviList !== 'NonApplicableCategoriesWithApplicableChannel') {
-                  searchParams.set(
-                    nviCorrectionListQueryKey,
-                    'NonApplicableCategoriesWithApplicableChannel' satisfies CorrectionListId
-                  );
-                  searchParams.delete(ResultParam.From);
-                  history.push({ search: searchParams.toString() });
-                }
-              }}>
-              {t('tasks.nvi.correction_list_type.non_applicable_category_in_applicable_channel')}
-            </SelectableButton>
-          </Box>
-        </BetaFunctionality>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+          <SelectableButton
+            data-testid={dataTestId.tasksPage.correctionList.applicableCategoriesWithNonApplicableChannelButton}
+            isSelected={selectedNviList === 'ApplicableCategoriesWithNonApplicableChannel'}
+            onClick={() => {
+              if (selectedNviList !== 'ApplicableCategoriesWithNonApplicableChannel') {
+                searchParams.set(
+                  nviCorrectionListQueryKey,
+                  'ApplicableCategoriesWithNonApplicableChannel' satisfies CorrectionListId
+                );
+                searchParams.delete(ResultParam.From);
+                history.push({ search: searchParams.toString() });
+              }
+            }}>
+            {t('tasks.nvi.correction_list_type.applicable_category_in_non_applicable_channel')}
+          </SelectableButton>
+          <SelectableButton
+            data-testid={dataTestId.tasksPage.correctionList.nonApplicableCategoriesWithApplicableChannelButton}
+            isSelected={selectedNviList === 'NonApplicableCategoriesWithApplicableChannel'}
+            onClick={() => {
+              if (selectedNviList !== 'NonApplicableCategoriesWithApplicableChannel') {
+                searchParams.set(
+                  nviCorrectionListQueryKey,
+                  'NonApplicableCategoriesWithApplicableChannel' satisfies CorrectionListId
+                );
+                searchParams.delete(ResultParam.From);
+                history.push({ search: searchParams.toString() });
+              }
+            }}>
+            {t('tasks.nvi.correction_list_type.non_applicable_category_in_applicable_channel')}
+          </SelectableButton>
+        </Box>
       </NavigationList>
     </NavigationListAccordion>
   );
