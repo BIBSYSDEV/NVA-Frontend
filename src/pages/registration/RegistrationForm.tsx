454eb1a2090e52c29d32a6df80dc80aedda429ca
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-08-27 10:36:23 +0200

    Skip NVI warning in wizard when no one has approved it yet

--- src/pages/registration/RegistrationForm.tsx
+++ src/pages/registration/RegistrationForm.tsx
@@ -68,7 +68,7 @@ export const RegistrationForm = ({ identifier }: RegistrationFormProps) => {
   });
   const isNviCandidate =
     nviCandidateQuery.data?.period.status === 'OpenPeriod' &&
-    nviCandidateQuery.data.approvals.some((status) => status.status !== 'Pending');
+    nviCandidateQuery.data.approvals.some((approval) => approval.status === 'Approved');
 
   const initialTabNumber = new URLSearchParams(history.location.search).get('tab');
   const [tabNumber, setTabNumber] = useState(initialTabNumber ? +initialTabNumber : RegistrationTab.Description);
