fb79bd79b25449c83f2856a2d8199d69b309670e
nina.andersen <denektenina@gmail.com>
2024-08-28 14:22:40 +0200

    Removed stepper on mobile

--- src/pages/project/project_wizard/ProjectFormStepper.tsx
+++ src/pages/project/project_wizard/ProjectFormStepper.tsx
@@ -1,4 +1,4 @@
-import { Step, StepButton, StepLabel, Stepper } from '@mui/material';
+import { Step, StepButton, StepLabel, Stepper, Theme, useMediaQuery } from '@mui/material';
 import { useFormikContext } from 'formik';
 import { useTranslation } from 'react-i18next';
 import { CristinProject, ProjectTabs } from '../../../types/project.types';
@@ -21,6 +21,7 @@ export const ProjectFormStepper = ({
   const { t } = useTranslation();
   const { errors, touched } = useFormikContext<CristinProject>();
 
+  const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
   const tabErrors = getProjectTabErrors(errors, touched);
   const descriptionTabHasError = tabErrors[ProjectTabs.Description].length > 0;
   const detailsTabHasError = tabErrors[ProjectTabs.Details].length > 0;
@@ -34,7 +35,7 @@ export const ProjectFormStepper = ({
     }
   };
 
-  return (
+  return isMobile ? null : (
     <Stepper nonLinear activeStep={tabNumber}>
       <Step completed={maxVisitedTab >= ProjectTabs.Description}>
         <StepButton
