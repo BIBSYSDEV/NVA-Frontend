f4c024e3c23276e56d38c7f021f7d2c45737a0d5
Nina Andersen <denektenina@gmail.com>
2024-09-09 09:33:21 +0200

    Added cancel-button to the new project-page (#6203)

--- src/pages/project/project_wizard/CreateProject.tsx
+++ src/pages/project/project_wizard/CreateProject.tsx
@@ -1,17 +1,26 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { useFetchOrganization } from '../../../api/hooks/useFetchOrganization';
+import { CancelButton } from '../../../components/buttons/CancelButton';
 import { PageHeader } from '../../../components/PageHeader';
-import { StyledPageContent, WizardStartPageWrapper } from '../../../components/styled/Wrappers';
+import {
+  StyledPageContent,
+  StyledRightAlignedFooter,
+  WizardStartPageWrapper,
+} from '../../../components/styled/Wrappers';
 import { RootState } from '../../../redux/store';
 import { emptyProject } from '../../../types/project.types';
+import { dataTestId } from '../../../utils/dataTestIds';
+import { UrlPathTemplate } from '../../../utils/urlPaths';
 import { CreateNfrProject } from './CreateNfrProject';
 import { EmptyProjectForm } from './EmptyProjectForm';
 import { ProjectForm } from './ProjectForm';
 
 const CreateProject = () => {
   const { t } = useTranslation();
+  const history = useHistory();
   const user = useSelector((store: RootState) => store.user);
   const topOrgCristinId = user?.topOrgCristinId ?? '';
   const currentInstitutionQuery = useFetchOrganization(topOrgCristinId);
@@ -40,6 +49,12 @@ const CreateProject = () => {
               setShowProjectForm={setShowProjectForm}
             />
           </WizardStartPageWrapper>
+          <StyledRightAlignedFooter sx={{ mt: '2rem' }}>
+            <CancelButton
+              testId={dataTestId.projectForm.cancelNewProjectButton}
+              onClick={() => history.push(UrlPathTemplate.MyPageMyProjectRegistrations)}
+            />
+          </StyledRightAlignedFooter>
         </>
       )}
     </StyledPageContent>
