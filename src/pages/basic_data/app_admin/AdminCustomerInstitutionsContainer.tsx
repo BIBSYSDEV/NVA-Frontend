6ab5ad5dfea881f3dd70579ffd6addf6ba55dcf0
Isar Buzza <isar.buzza@sikt.no>
2024-09-09 14:35:34 +0200

    updated AdminCustomerInstitutionContainer

--- src/pages/basic_data/app_admin/AdminCustomerInstitutionsContainer.tsx
+++ src/pages/basic_data/app_admin/AdminCustomerInstitutionsContainer.tsx
@@ -1,11 +1,11 @@
-import { useHistory } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { BackgroundDiv } from '../../../components/styled/Wrappers';
 import { AdminCustomerInstitution } from './AdminCustomerInstitution';
 import { AdminCustomerInstitutions } from './AdminCustomerInstitutions';
 
 export const AdminCustomerInstitutionsContainer = () => {
-  const history = useHistory();
-  const customerId = new URLSearchParams(history.location.search).get('id'); // Will be "new" if creating new Customer
+  const location = useLocation();
+  const customerId = new URLSearchParams(location.search).get('id'); // Will be "new" if creating new Customer
 
   return (
     <BackgroundDiv>
