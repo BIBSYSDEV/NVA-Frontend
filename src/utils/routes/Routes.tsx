59c86be3d51eb29339e522bb4e87c9449f456570
Isar Buzza <isar.buzza@sikt.no>
2024-09-10 14:30:16 +0200

    updated PrivateRoute to return Forbidden if not authorized

--- src/utils/routes/Routes.tsx
+++ src/utils/routes/Routes.tsx
@@ -1,10 +1,9 @@
 import { ReactElement } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Forbidden } from '../../pages/errorpages/Forbidden';
 
 interface PrivateRouteProps {
   isAuthorized: boolean;
   element: ReactElement;
 }
 
-export const PrivateRoute = ({ isAuthorized, element }: PrivateRouteProps) =>
-  isAuthorized ? element : <Navigate to="/forbidden" replace />;
+export const PrivateRoute = ({ isAuthorized, element }: PrivateRouteProps) => (isAuthorized ? element : <Forbidden />);
