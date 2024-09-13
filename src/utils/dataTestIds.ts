a3a6e69034c3145a33e2595359d3a3f3921490a4
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-09-13 12:03:46 +0200

    Add data-testid

--- src/utils/dataTestIds.ts
+++ src/utils/dataTestIds.ts
@@ -661,6 +661,7 @@ export const dataTestId = {
     areaOfResponsibilitySelector: 'area-of-responsibility-selector',
     correctionList: {
       antologyWithoutChapterButton: 'antology-without-chapter-button',
+      booksWithLessThan50PagesButton: 'books-with-less-than-50-pages-button',
       applicableCategoriesWithNonApplicableChannelButton: 'applicable-categories-with-non-applicable-channel-button',
       correctionListAccordion: 'correction-list-accordion',
       correctionListRadioButton: 'correction-list-radio-button',
