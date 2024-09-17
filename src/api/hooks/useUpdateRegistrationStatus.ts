e856e6c23849ca4c782b2362696b9a829be290ce
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-09-17 13:38:34 +0200

    Show new result status without refetching

--- src/api/hooks/useUpdateRegistrationStatus.ts
+++ src/api/hooks/useUpdateRegistrationStatus.ts
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { setNotification } from '../../redux/notificationSlice';
@@ -14,12 +14,27 @@ interface UpdateRequest {
 export const useUpdateRegistrationStatus = () => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: ({ registrationIdentifier, data }: UpdateRequest) =>
       updateRegistrationStatus(registrationIdentifier, data),
-    onSuccess: (_, variables) => {
+    onSuccess: (response, variables) => {
       dispatch(setNotification({ message: t('feedback.success.update_registration'), variant: 'success' }));
+      if (response.data) {
+        const key1 = ['registration', response.data.identifier];
+        if (queryClient.getQueryData(key1)) {
+          queryClient.setQueryData(key1, response.data);
+        }
+        const key2 = ['registration', response.data.identifier, true];
+        if (queryClient.getQueryData(key2)) {
+          queryClient.setQueryData(key2, response.data);
+        }
+        const key3 = ['registration', response.data.identifier, false];
+        if (queryClient.getQueryData(key3)) {
+          queryClient.setQueryData(key3, response.data);
+        }
+      }
       variables.onSuccess?.();
     },
     onError: () => dispatch(setNotification({ message: t('feedback.error.update_registration'), variant: 'error' })),
