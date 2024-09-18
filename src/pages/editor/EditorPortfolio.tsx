179857a4c9343debb5398f95a79e31e7e392aebf
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-09-18 15:46:07 +0200

    Sow results from current org in results portfolio

--- src/pages/editor/EditorPortfolio.tsx
+++ src/pages/editor/EditorPortfolio.tsx
@@ -1,3 +1,24 @@
+import { Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { useRegistrationSearch } from '../../api/hooks/useRegistrationSearch';
+import { RootState } from '../../redux/store';
+import { RegistrationSearch } from '../search/registration_search/RegistrationSearch';
+
 export const EditorPortfolio = () => {
-  return <p>TODO</p>;
+  const { t } = useTranslation();
+  const user = useSelector((state: RootState) => state.user);
+  const organizationId = user?.topOrgCristinId;
+
+  const registrationQuery = useRegistrationSearch({
+    enabled: !!organizationId,
+    params: { unit: organizationId },
+  });
+
+  return (
+    <>
+      <Typography variant="h1">{'Resultatportefølje'}</Typography>
+      <RegistrationSearch registrationQuery={registrationQuery} />
+    </>
+  );
 };
