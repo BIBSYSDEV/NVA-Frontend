1b0b6cc9483d4493ea350a9574d584d0c8df12d4
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-09-19 14:12:45 +0200

    Set page title

--- src/pages/editor/EditorPortfolio.tsx
+++ src/pages/editor/EditorPortfolio.tsx
@@ -1,25 +1,21 @@
 import { Typography } from '@mui/material';
+import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
 import { useCustomerRegistrationSearch } from '../../api/hooks/useFetchCustomerRegistrationSearch';
-import { RootState } from '../../redux/store';
 import { useRegistrationsQueryParams } from '../../utils/hooks/useRegistrationSearchParams';
 import { RegistrationSearch } from '../search/registration_search/RegistrationSearch';
 
 export const EditorPortfolio = () => {
   const { t } = useTranslation();
-  const user = useSelector((state: RootState) => state.user);
-  const organizationId = user?.topOrgCristinId;
 
   const params = useRegistrationsQueryParams();
-
-  const registrationQuery = useCustomerRegistrationSearch({
-    enabled: !!organizationId,
-    params,
-  });
+  const registrationQuery = useCustomerRegistrationSearch({ params });
 
   return (
     <>
+      <Helmet>
+        <title>{t('common.result_portfolio')}</title>
+      </Helmet>
       <Typography variant="h1">{t('common.result_portfolio')}</Typography>
       <RegistrationSearch registrationQuery={registrationQuery} />
     </>
