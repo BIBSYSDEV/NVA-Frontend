9b560c2a130683e853e57ef4894d6ef86ca02026
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-09-19 14:06:52 +0200

    Use dedicated hook to handle query params

--- src/pages/editor/EditorPortfolio.tsx
+++ src/pages/editor/EditorPortfolio.tsx
@@ -1,25 +1,21 @@
 import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
 import { useCustomerRegistrationSearch } from '../../api/hooks/useFetchCustomerRegistrationSearch';
-import { CustomerResultParam } from '../../api/searchApi';
 import { RootState } from '../../redux/store';
-import { RegistrationStatus } from '../../types/registration.types';
+import { useRegistrationsQueryParams } from '../../utils/hooks/useRegistrationSearchParams';
 import { RegistrationSearch } from '../search/registration_search/RegistrationSearch';
 
 export const EditorPortfolio = () => {
   const { t } = useTranslation();
   const user = useSelector((state: RootState) => state.user);
   const organizationId = user?.topOrgCristinId;
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
 
-  const selectedStatuses = queryParams.get(CustomerResultParam.Status) as RegistrationStatus[] | null;
+  const params = useRegistrationsQueryParams();
 
   const registrationQuery = useCustomerRegistrationSearch({
     enabled: !!organizationId,
-    params: { query: '', status: selectedStatuses },
+    params,
   });
 
   return (
