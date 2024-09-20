4b91a3d99484892ef58687d6aef63a5e4f65a8ce
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-09-20 13:40:51 +0200

    Allow adding query to portfolio search

--- src/pages/editor/EditorPortfolio.tsx
+++ src/pages/editor/EditorPortfolio.tsx
@@ -2,6 +2,7 @@ import { Typography } from '@mui/material';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import { useCustomerRegistrationSearch } from '../../api/hooks/useFetchCustomerRegistrationSearch';
+import { SearchForm } from '../../components/SearchForm';
 import { useRegistrationsQueryParams } from '../../utils/hooks/useRegistrationSearchParams';
 import { RegistrationSearch } from '../search/registration_search/RegistrationSearch';
 
@@ -19,6 +20,7 @@ export const EditorPortfolio = () => {
       <Typography variant="h1" gutterBottom>
         {t('common.result_portfolio')}
       </Typography>
+      <SearchForm placeholder={t('search.search_placeholder')} sx={{ my: '1rem' }} />
       <RegistrationSearch registrationQuery={registrationQuery} />
     </>
   );
