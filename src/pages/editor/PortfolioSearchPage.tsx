a37e48f0b414bb5f8c08f4dc4451a70d9e8a41ca
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-09-23 15:05:44 +0200

    Add query field to portfolio page

--- src/pages/editor/PortfolioSearchPage.tsx
+++ src/pages/editor/PortfolioSearchPage.tsx
@@ -1,6 +1,8 @@
 import { Typography } from '@mui/material';
 import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
 import { useCustomerRegistrationSearch } from '../../api/hooks/useFetchCustomerRegistrationSearch';
+import { SearchForm } from '../../components/SearchForm';
 import { useRegistrationsQueryParams } from '../../utils/hooks/useRegistrationSearchParams';
 import { RegistrationSearch } from '../search/registration_search/RegistrationSearch';
 
@@ -9,6 +11,7 @@ interface PortfolioSearchPageProps {
 }
 
 export const PortfolioSearchPage = ({ title }: PortfolioSearchPageProps) => {
+  const { t } = useTranslation();
   const params = useRegistrationsQueryParams();
   const registrationQuery = useCustomerRegistrationSearch(params);
 
@@ -20,6 +23,7 @@ export const PortfolioSearchPage = ({ title }: PortfolioSearchPageProps) => {
       <Typography variant="h1" gutterBottom>
         {title}
       </Typography>
+      <SearchForm placeholder={t('search.search_placeholder')} sx={{ my: '1rem' }} />
       <RegistrationSearch registrationQuery={registrationQuery} />
     </section>
   );
