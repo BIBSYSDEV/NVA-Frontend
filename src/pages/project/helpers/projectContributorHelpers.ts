dd8e039fce6564fb539b8b4f36b81cfb20f21294
Nina Andersen <denektenina@gmail.com>
2024-09-26 12:34:38 +0200

    New buttons and logic for adding an undefined contributor (#6276)

--- src/pages/project/helpers/projectContributorHelpers.ts
+++ src/pages/project/helpers/projectContributorHelpers.ts
@@ -1,9 +1,11 @@
 import { ProjectContributor, ProjectContributorRole, ProjectContributorType } from '../../../types/project.types';
-import { CristinPerson } from '../../../types/user.types';
+import { CristinPerson, CristinPersonAffiliation } from '../../../types/user.types';
 import { getValueByKey } from '../../../utils/user-helpers';
 import {
+  addRoles,
   deleteProjectManagerRoleFromContributor,
   findProjectManagerRole,
+  hasEmptyAffiliation,
   isNonProjectManagerRole,
   isProjectManagerRole,
   replaceRolesOnContributor,
@@ -17,76 +19,147 @@ export const getNonProjectManagerContributors = (contributors: ProjectContributo
   return contributors.filter((contributor) => contributor.roles.some((role) => isNonProjectManagerRole(role)));
 };
 
-export const hasUnidentifiedContributor = (contributors: ProjectContributor[]) =>
-  contributors.some((contributor) => !contributor.identity || !contributor.identity.id);
-
 export enum AddContributorErrors {
   NO_PERSON_TO_ADD,
+  INDEX_OUT_OF_BOUNDS,
+  NO_SEARCH_TERM,
   SAME_ROLE_WITH_SAME_AFFILIATION,
   ALREADY_HAS_A_PROJECT_MANAGER,
+  CANNOT_ADD_ANOTHER_PROJECT_MANAGER_ROLE,
+  CAN_ONLY_REPLACE_UNIDENTIFIED_CONTRIBUTORS,
+  CAN_ONLY_ADD_ONE_PROJECT_MANAGER_ROLE,
 }
+
+const checkIfExistingProjectManager = (contributors: ProjectContributor[], indexToReplace: number) => {
+  const indexOfExistingProjectManager = contributors.findIndex((contributor) => findProjectManagerRole(contributor));
+
+  // It's ok to have an existing Project Manager if we are going to replace it
+  if (indexOfExistingProjectManager > -1 && indexToReplace !== indexOfExistingProjectManager) {
+    return AddContributorErrors.ALREADY_HAS_A_PROJECT_MANAGER;
+  }
+};
+
+const checkIfDefinedProjectManagerRole = (contributors: ProjectContributor[]) => {
+  const indexOfExistingProjectManager = contributors.findIndex((contributor) => findProjectManagerRole(contributor));
+
+  if (indexOfExistingProjectManager > -1) {
+    const existingProjectManager = contributors[indexOfExistingProjectManager];
+    const existingProjectManagerRole = findProjectManagerRole(existingProjectManager);
+
+    // It's ok to have an existing Project Manager if its affiliation is undefined
+    if (existingProjectManagerRole && !hasEmptyAffiliation(existingProjectManagerRole)) {
+      return AddContributorErrors.CANNOT_ADD_ANOTHER_PROJECT_MANAGER_ROLE;
+    }
+  }
+};
+
+const checkIfRoleAndTypeDuplicate = (
+  personToAdd: CristinPerson,
+  roles: ProjectContributorRole[],
+  roleToAddTo: ProjectContributorType
+) => {
+  const sameRoleAndSameType = roles.some(
+    (role) =>
+      role.type === roleToAddTo &&
+      personToAdd.affiliations.some((affiliation) => affiliation.organization === role.affiliation?.id)
+  );
+
+  if (sameRoleAndSameType) {
+    return AddContributorErrors.SAME_ROLE_WITH_SAME_AFFILIATION;
+  }
+};
+
+const addEmptyRoleIfNecessary = (existingRoles: ProjectContributorRole[], roleToAddTo: ProjectContributorType) => {
+  if (existingRoles.filter((role) => role.type === roleToAddTo).length === 0) {
+    return [
+      ...existingRoles,
+      {
+        type: roleToAddTo,
+        affiliation: undefined,
+      } as ProjectContributorRole,
+    ];
+  }
+  return existingRoles;
+};
+
+const isAlreadyAdded = (
+  existingRoles: ProjectContributorRole[],
+  cristinAffiliation: CristinPersonAffiliation,
+  roleToAddTo: ProjectContributorType
+) => {
+  return existingRoles.some(
+    (role) => role.type === roleToAddTo && role.affiliation?.id === cristinAffiliation.organization
+  );
+};
 
 export const addContributor = (
   personToAdd: CristinPerson | undefined,
   contributors: ProjectContributor[],
-  roleToAddTo: ProjectContributorType
+  roleToAddTo: ProjectContributorType,
+  indexToReplace = -1
 ): { newContributors?: ProjectContributor[]; error?: AddContributorErrors } => {
-  // Must have person to add
   if (!personToAdd) {
     return { error: AddContributorErrors.NO_PERSON_TO_ADD };
   }
 
-  // Cannot add project manager if we already have one
-  if (roleToAddTo === 'ProjectManager') {
-    const existingProjectManager = contributors.find((contributor) => findProjectManagerRole(contributor));
+  if (indexToReplace > contributors.length - 1 || indexToReplace < -1) {
+    return { error: AddContributorErrors.INDEX_OUT_OF_BOUNDS };
+  }
 
-    if (existingProjectManager) {
-      return { error: AddContributorErrors.ALREADY_HAS_A_PROJECT_MANAGER };
+  if (indexToReplace > -1 && contributors[indexToReplace].identity.id) {
+    return { error: AddContributorErrors.CAN_ONLY_REPLACE_UNIDENTIFIED_CONTRIBUTORS };
+  }
+
+  if (roleToAddTo === 'ProjectManager') {
+    if (personToAdd.affiliations.length > 1) {
+      return { error: AddContributorErrors.CAN_ONLY_ADD_ONE_PROJECT_MANAGER_ROLE };
+    }
+
+    const projectManagerError = checkIfExistingProjectManager(contributors, indexToReplace);
+    if (projectManagerError) return { error: projectManagerError }; // Can only replace, not add Project Manager if we already have one
+
+    if (personToAdd.affiliations.length > 0) {
+      const projectManagerErrorRoleError = checkIfDefinedProjectManagerRole(contributors);
+      if (projectManagerErrorRoleError) return { error: projectManagerErrorRoleError }; // Can not add affiliations if the project manager has one
     }
   }
 
-  let newContributor: ProjectContributor;
+  const newContributor: ProjectContributor = {
+    identity: {
+      type: 'Person',
+      id: personToAdd.id,
+      firstName: getValueByKey('FirstName', personToAdd.names),
+      lastName: getValueByKey('LastName', personToAdd.names),
+    },
+    roles: [],
+  };
 
-  // If the user to add already exists in the contributor list
+  // Check if the user to add already exists in the contributor list
   const existingContributorIndex = contributors.findIndex((contributor) => contributor.identity.id === personToAdd.id);
 
   if (existingContributorIndex > -1) {
-    // Cannot have same roletype and affiliation
-    const sameRoleAndSameType = contributors[existingContributorIndex].roles.some((role) => {
-      return (
-        role.type === roleToAddTo &&
-        personToAdd.affiliations.some((affiliation) => affiliation.organization === role.affiliation?.id)
-      );
-    });
+    // The existing contributor cannot have same affiliation on the same role type as the person to add
+    const roleAndTypeDuplicateError = checkIfRoleAndTypeDuplicate(
+      personToAdd,
+      contributors[existingContributorIndex].roles,
+      roleToAddTo
+    );
+    if (roleAndTypeDuplicateError) return { error: roleAndTypeDuplicateError };
 
-    if (sameRoleAndSameType) {
-      return { error: AddContributorErrors.SAME_ROLE_WITH_SAME_AFFILIATION };
-    }
-
-    // Replace the empty roles on the type
-    newContributor = {
-      ...contributors[existingContributorIndex],
-      roles: contributors[existingContributorIndex].roles.filter(
-        (role) => role.type !== roleToAddTo || (role.type === roleToAddTo && role.affiliation)
-      ),
-    };
-  } else {
-    newContributor = {
-      identity: {
-        type: 'Person',
-        id: personToAdd.id,
-        firstName: getValueByKey('FirstName', personToAdd.names),
-        lastName: getValueByKey('LastName', personToAdd.names),
-      },
-      roles: [],
-    };
+    // Add affiliations from the existing participant since we are merging them
+    newContributor.roles = addRoles(newContributor.roles, contributors[existingContributorIndex].roles, roleToAddTo);
   }
 
-  // Adding 1 or more affiliations
+  // If we are replacing an index we must keep its affiliations as well
+  if (indexToReplace > -1) {
+    newContributor.roles = addRoles(newContributor.roles, contributors[indexToReplace].roles, roleToAddTo);
+  }
+
+  // Adding 1 or more affiliations from the selected user
   if (personToAdd.affiliations.length > 0) {
-    newContributor.roles = [...newContributor.roles].concat(
+    newContributor.roles = newContributor.roles.concat(
       personToAdd.affiliations
-        .filter((_, index) => roleToAddTo !== 'ProjectManager' || index === 0) // For project managers, we only allow one affiliation
+        .filter((cristinAffiliation) => !isAlreadyAdded(newContributor.roles, cristinAffiliation, roleToAddTo))
         .map((affiliation) => {
           return {
             type: roleToAddTo,
@@ -95,24 +168,85 @@ export const addContributor = (
         })
     );
   } else {
-    // Only adding empty role if no other roles present on the role
-    if (newContributor.roles.filter((role) => role.type === roleToAddTo).length === 0) {
-      newContributor.roles.push({
-        type: roleToAddTo,
-        affiliation: undefined,
-      } as ProjectContributorRole);
-    }
+    newContributor.roles = addEmptyRoleIfNecessary(newContributor.roles, roleToAddTo);
   }
 
   const newContributors = [...contributors];
 
-  if (existingContributorIndex > -1) {
+  if (indexToReplace > -1) {
+    newContributors[indexToReplace] = newContributor;
+    if (existingContributorIndex > -1 && indexToReplace !== existingContributorIndex) {
+      newContributors.splice(existingContributorIndex, 1);
+    }
+  } else if (existingContributorIndex > -1) {
     newContributors[existingContributorIndex] = newContributor;
   } else {
     newContributors.push(newContributor);
   }
 
   return { newContributors };
+};
+
+const createNamesFromInput = (searchTerm: string) => {
+  const names = searchTerm.split(' ');
+  let firstName, lastName;
+
+  if (names.length > 1) {
+    const namesWithoutLastName = names.slice(0, -1);
+    firstName = namesWithoutLastName.join(' ');
+    lastName = names[names.length - 1];
+  } else {
+    firstName = names[0];
+    lastName = '';
+  }
+
+  return { firstName, lastName };
+};
+
+export const addUnidentifiedProjectContributor = (
+  searchTerm: string,
+  contributors: ProjectContributor[],
+  roleToAddTo: ProjectContributorType,
+  indexToReplace = -1
+): { newContributors?: ProjectContributor[]; error?: AddContributorErrors } => {
+  if (!searchTerm) {
+    return { error: AddContributorErrors.NO_SEARCH_TERM };
+  }
+
+  // Cannot add project manager if we already have one
+  if (roleToAddTo === 'ProjectManager') {
+    const projectManagerError = checkIfExistingProjectManager(contributors, indexToReplace);
+    if (projectManagerError) return { error: projectManagerError };
+  }
+
+  const { firstName, lastName } = createNamesFromInput(searchTerm);
+
+  const newContributor: ProjectContributor = {
+    identity: {
+      type: 'Person',
+      firstName: firstName,
+      lastName: lastName,
+    },
+    roles: [],
+  };
+
+  // If we are replacing an index we must keep its roles
+  if (indexToReplace > -1) {
+    newContributor.roles = addRoles(newContributor.roles, contributors[indexToReplace].roles, roleToAddTo);
+  }
+
+  // It needs at least one role to contain the role type
+  newContributor.roles = addEmptyRoleIfNecessary(newContributor.roles, roleToAddTo);
+
+  const newContributors = [...contributors];
+
+  if (indexToReplace > -1) {
+    newContributors[indexToReplace] = newContributor;
+  } else {
+    newContributors.push(newContributor);
+  }
+
+  return { newContributors: newContributors };
 };
 
 export const removeProjectParticipant = (contributors: ProjectContributor[], contributorIndex: number) => {
@@ -148,4 +282,27 @@ export const removeProjectManager = (contributors: ProjectContributor[]) => {
   const newContributors = [...contributors];
   newContributors.splice(projectManagerIndex, 1);
   return newContributors;
+};
+
+const rolesAreEqual = (r1: ProjectContributorRole[], r2: ProjectContributorRole[]) => {
+  if (r1.length !== r2.length) return false;
+
+  r1.forEach((role, index) => {
+    if (role.type !== r2[index].type) return false;
+    if (role.affiliation !== r2[index].affiliation) return false;
+  });
+
+  return true;
+};
+
+export const contributorsAreEqual = (c1: ProjectContributor, c2: ProjectContributor) => {
+  if (c1.identity.id && c2.identity.id) return c1.identity.id === c2.identity.id;
+  else if ((!c1.identity.id && c2.identity.id) || (c1.identity.id && !c2.identity.id)) return false;
+  else
+    return (
+      c1.identity.type === c2.identity.type &&
+      c1.identity.firstName === c2.identity.firstName &&
+      c1.identity.lastName === c2.identity.lastName &&
+      rolesAreEqual(c1.roles, c2.roles)
+    );
 };
