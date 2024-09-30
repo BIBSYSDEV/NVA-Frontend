414effc66cb2e7748ba24cd92cb6dbdf78b70f57
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-09-30 14:58:43 +0200

    Add missing page titles

--- src/pages/basic_data/app_admin/central_import/CentralImportCandidateMerge.tsx
+++ src/pages/basic_data/app_admin/central_import/CentralImportCandidateMerge.tsx
@@ -3,6 +3,7 @@ import { Box, Button, Typography } from '@mui/material';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { Form, Formik, FormikProps } from 'formik';
 import { getLanguageByUri } from 'nva-language';
+import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { Redirect, useHistory, useParams } from 'react-router-dom';
@@ -120,6 +121,9 @@ export const CentralImportCandidateMerge = () => {
             gridTemplateColumns: '1fr auto 1fr',
             alignItems: 'center',
           }}>
+          <Helmet>
+            <title>{t('basic_data.central_import.central_import')}</title>
+          </Helmet>
           <Typography sx={{ gridColumn: '1/-1' }}>
             {t('basic_data.central_import.merge_candidate.merge_details_1')}
           </Typography>
