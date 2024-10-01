fe234895b3be07e0332ad61252bf7cf719ed357b
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-10-01 13:20:54 +0200

    Add sorting to my projects page

--- src/pages/my_page/user_profile/MyProjects.tsx
+++ src/pages/my_page/user_profile/MyProjects.tsx
@@ -3,9 +3,11 @@ import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { searchForProjects } from '../../../api/cristinApi';
+import { ProjectsSearchParams, searchForProjects } from '../../../api/cristinApi';
 import { ListPagination } from '../../../components/ListPagination';
 import { ListSkeleton } from '../../../components/ListSkeleton';
+import { projectSortOptions } from '../../../components/ProjectSortSelector';
+import { SortSelectorWithoutParams } from '../../../components/SortSelectorWithoutParams';
 import { RootState } from '../../../redux/store';
 import { ROWS_PER_PAGE_OPTIONS } from '../../../utils/constants';
 import { getIdentifierFromId } from '../../../utils/general-helpers';
@@ -20,10 +22,17 @@ export const MyProjects = () => {
 
   const [page, setPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(ROWS_PER_PAGE_OPTIONS[0]);
+  const [projectSort, setProjectSort] = useState(projectSortOptions[0]);
+
+  const projectsQueryConfig: ProjectsSearchParams = {
+    participant: userCristinId,
+    orderBy: projectSort.orderBy,
+    sort: projectSort.sortOrder,
+  };
 
   const projectsQuery = useQuery({
-    queryKey: ['projects', rowsPerPage, page, userCristinId],
-    queryFn: () => searchForProjects(rowsPerPage, page, { participant: userCristinId }),
+    queryKey: ['projects', rowsPerPage, page, projectsQueryConfig],
+    queryFn: () => searchForProjects(rowsPerPage, page, projectsQueryConfig),
     meta: { errorMessage: t('feedback.error.project_search') },
   });
 
@@ -45,7 +54,14 @@ export const MyProjects = () => {
           onRowsPerPageChange={(newRowsPerPage) => {
             setRowsPerPage(newRowsPerPage);
             setPage(1);
-          }}>
+          }}
+          sortingComponent={
+            <SortSelectorWithoutParams
+              options={projectSortOptions}
+              value={projectSort}
+              setValue={(value) => setProjectSort(value)}
+            />
+          }>
           <List>
             {projects.map((project) => (
               <ProjectListItem
