4580af9ef35c9baad35a164b160ec11ffd1bb31f
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-10-03 12:52:40 +0200

    Np 47766 use allowed operations for ticket actions (#6352)
    * Use allowedOperations for DoiRequest
    
    * Allow completing support tickets with allowedOperations
    
    * Remove unused property
    
    * Use new access rights for file publishing

--- src/utils/registration-helpers.ts
+++ src/utils/registration-helpers.ts
@@ -55,6 +55,7 @@ import {
   PublicationInstanceType,
   Publisher,
   Registration,
+  RegistrationOperation,
   RelatedDocument,
   Series,
 } from '../types/registration.types';
@@ -116,9 +117,6 @@ export const nviApplicableTypes: PublicationInstanceType[] = [
   BookType.AcademicMonograph,
   ChapterType.AcademicChapter,
 ];
-
-export const userHasSameCustomerAsRegistration = (user: User | null, registration?: Registration) =>
-  !!user?.customerId && registration?.publisher.id === user.customerId;
 
 export const userIsValidImporter = (user: User | null, registration?: Registration) =>
   !!user && !!registration && user.isInternalImporter && registration.type === 'ImportCandidate';
@@ -661,26 +659,44 @@ export const getOutputName = (item: OutputItem): string => {
   }
 };
 
+/**
+ * @deprecated Use userHasAccessRight instead
+ */
 export const userCanEditRegistration = (registration: Registration) =>
   registration.allowedOperations?.includes('update') ?? false;
 
+/**
+ * @deprecated Use userHasAccessRight instead
+ */
 export const userCanUnpublishRegistration = (registration: Registration) =>
   registration.allowedOperations?.includes('unpublish') ?? false;
 
+/**
+ * @deprecated Use userHasAccessRight instead
+ */
 export const userCanTerminateRegistration = (registration: Registration) =>
   registration.allowedOperations?.includes('terminate') ?? false;
 
+/**
+ * @deprecated Use userHasAccessRight instead
+ */
 export const userCanRepublishRegistration = (registration: Registration) =>
   registration.allowedOperations?.includes('republish') ?? false;
 
-export const userCanPublishRegistration = (registration: Registration) =>
-  registration.allowedOperations?.includes('ticket/publish') ?? false;
-
+/**
+ * @deprecated Use userHasAccessRight instead
+ */
 export const userCanDeleteRegistration = (registration: Registration) =>
   registration.allowedOperations?.includes('delete') ?? false;
 
+/**
+ * @deprecated Use userHasAccessRight instead
+ */
 export const userCanEditPublishedFile = (registration: Registration) =>
   registration.allowedOperations?.includes('update-including-files') ?? false;
+
+export const userHasAccessRight = (registration: Registration, operation: RegistrationOperation) =>
+  registration.allowedOperations?.includes(operation) ?? false;
 
 export const hyphenateIsrc = (isrc: string) =>
   isrc ? `${isrc.substring(0, 2)}-${isrc.substring(2, 5)}-${isrc.substring(5, 7)}-${isrc.substring(7, 12)}` : '';
