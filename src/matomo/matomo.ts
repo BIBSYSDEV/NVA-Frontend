c0fd02eae47c884a11025baa1b2cbd18ac19e961
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-10-10 12:02:15 +0200

    Configure logging of console errors (#6380)

--- src/matomo/matomo.ts
+++ src/matomo/matomo.ts
@@ -18,4 +18,26 @@ export const initializeMatomo = (matomoContainerUrl: string) => {
   newScriptElement.async = true;
   newScriptElement.src = matomoContainerUrl;
   firstScriptElement.parentNode?.insertBefore(newScriptElement, firstScriptElement);
+
+  // Log global JavaScript errors
+  window.onerror = function (message, source, lineno, colno, error) {
+    _mtm.push({
+      event: 'JavaScript Error',
+      errorMessage: message,
+      errorSource: `${source}:${lineno}:${colno}`,
+      errorStack: error ? error.stack : '',
+    });
+  };
+
+  // Log console errors
+  /* eslint-disable no-console */
+  const originalConsoleError = console.error;
+  console.error = function (message, ...optionalParams) {
+    _mtm.push({
+      event: 'trackJsError',
+      error: message,
+      optionalParams: optionalParams,
+    });
+    originalConsoleError.apply(console, [message, ...optionalParams]); // Ensure error is still shown in console
+  };
 };
