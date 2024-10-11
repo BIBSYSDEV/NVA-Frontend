61a86d1e253d69363ae9bde0e3eaf20e24ac5ac2
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-10-11 12:13:59 +0200

    Revert "Trigger errors to test Crash Analytics (#6367)" (#6384)
    This reverts commit 0eef5015df66f91e203df10f281397c3c13ac2bc.

--- src/pages/editor/PortfolioSearchPage.tsx
+++ src/pages/editor/PortfolioSearchPage.tsx
@@ -1,8 +1,7 @@
-import { Button, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import { useCustomerRegistrationSearch } from '../../api/hooks/useFetchCustomerRegistrationSearch';
-import { BetaFunctionality } from '../../components/BetaFunctionality';
 import { SearchForm } from '../../components/SearchForm';
 import { useRegistrationsQueryParams } from '../../utils/hooks/useRegistrationSearchParams';
 import { RegistrationSearch } from '../search/registration_search/RegistrationSearch';
@@ -21,28 +20,6 @@ export const PortfolioSearchPage = ({ title }: PortfolioSearchPageProps) => {
       <Helmet>
         <title>{title}</title>
       </Helmet>
-      <BetaFunctionality>
-        <Button
-          onClick={() => {
-            throw new Error();
-          }}>
-          Default Error
-        </Button>
-        <Button
-          onClick={() => {
-            throw new Error('Custom error text');
-          }}>
-          Custom Error
-        </Button>
-        <Button
-          onClick={() => {
-            new Promise((resolve, reject) => {
-              reject('Reject promise!');
-            });
-          }}>
-          Reject promise
-        </Button>
-      </BetaFunctionality>
       <Typography variant="h1" gutterBottom>
         {title}
       </Typography>
