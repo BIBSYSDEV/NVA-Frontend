6b43dc90f0b54a920d3ebbe4d578152e1f215d99
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-10-18 11:48:40 +0200

    Fix sorting of comments

--- src/pages/messages/components/NviCandidateActions.tsx
+++ src/pages/messages/components/NviCandidateActions.tsx
@@ -140,7 +140,7 @@ export const NviCandidateActions = ({ nviCandidate, nviCandidateQueryKey }: NviC
   const sortedNotes = [...generalNotes, ...rejectionNotes, ...approvalNotes].sort((a, b) => {
     const dateA = new Date(a.date);
     const dateB = new Date(b.date);
-    return dateB.getTime() - dateA.getTime();
+    return dateA.getTime() - dateB.getTime();
   });
 
   return (
