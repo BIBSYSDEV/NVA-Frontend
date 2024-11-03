188ed61721bdc1985185fe631c923cafe8860b59
Isar Buzza <isar@buzza.no>
2024-11-03 19:21:47 +0100

    fixed list

--- src/pages/public_registration/public_links/ShowRelatedDocuments.tsx
+++ src/pages/public_registration/public_links/ShowRelatedDocuments.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem } from '@mui/material';
+import { List, ListItem, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { RelatedDocument } from '../../../types/registration.types';
 import { ShowRelatedRegistrationUris } from './ShowRelatedRegistrationUris';
@@ -10,20 +10,26 @@ interface ShowRelatedDocumentsProps {
 export const ShowRelatedDocuments = ({ related }: ShowRelatedDocumentsProps) => {
   const { t } = useTranslation();
 
-  return related
-    ?.sort((a, b) => (a.sequence && b.sequence ? a.sequence - b.sequence : 0))
-    .map((document, index) => {
-      return document.type === 'ConfirmedDocument' ? (
-        <ShowRelatedRegistrationUris
-          links={[document.identifier]}
-          loadingLabel={t('registration.resource_type.related_results')}
-        />
-      ) : (
-        <List disablePadding>
+  if (related.length === 0) {
+    return null;
+  }
+
+  return (
+    <List>
+      {related
+        .sort((a, b) => (a.sequence && b.sequence ? a.sequence - b.sequence : 0))
+        .map((document, index) => (
           <ListItem key={index} disableGutters>
-            {document.text}
+            {document.type === 'ConfirmedDocument' ? (
+              <ShowRelatedRegistrationUris
+                links={[document.identifier]}
+                loadingLabel={t('registration.resource_type.related_results')}
+              />
+            ) : (
+              <Typography>{document.text}</Typography>
+            )}
           </ListItem>
-        </List>
-      );
-    });
+        ))}
+    </List>
+  );
 };
