8eec62d6bfce671c1c82946bcd0ed00742f3420e
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-11-07 12:53:06 +0100

    NP-47872 Allow previewing and downloading registration files from new endpoint (#6539)
    * Allow previewing and downloading result files
    
    * Minor simplifications
    
    * Remove redundant mocking

--- src/utils/testfiles/mockFiles.ts
+++ src/utils/testfiles/mockFiles.ts
@@ -2,4 +2,3 @@ export const mockFileUploadUrl = 'http://localhost:3000/api/file-upload/';
 export const mockCreateUpload = { uploadId: 'upoadId', key: 'key' };
 export const mockPrepareUpload = { url: mockFileUploadUrl };
 export const mockCompleteUpload = { location: '123-123-123' };
-export const mockDownload = { presignedDownloadUrl: 'http://localhost:3000/api/file-download/' };
