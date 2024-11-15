d1bd4105eca2434558df5ade55d916717cf349fc
Isar Buzza <isar.buzza@sikt.no>
2024-11-15 11:00:43 +0100

    cleanup

--- src/pages/registration/resource_type_tab/sub_type_forms/research_data_types/RelatedResourceRow.tsx
+++ src/pages/registration/resource_type_tab/sub_type_forms/research_data_types/RelatedResourceRow.tsx
@@ -1,6 +1,5 @@
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import { Box, Button, Link, ListItem, Skeleton, Typography } from '@mui/material';
-
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ConfirmDialog } from '../../../../../components/ConfirmDialog';
