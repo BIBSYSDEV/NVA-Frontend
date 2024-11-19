e410ef61afbcddf9152e095d7c31582280b0ade2
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-11-19 15:43:54 +0100

    Add tests for thesis file checks

--- src/pages/registration/helpers/fileHelpers.test.ts
+++ src/pages/registration/helpers/fileHelpers.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test } from 'vitest';
 import { AssociatedFile, emptyFile } from '../../../types/associatedArtifact.types';
+import { DegreeType } from '../../../types/publicationFieldNames';
 import { emptyRegistration, Registration } from '../../../types/registration.types';
 import { User } from '../../../types/user.types';
 import { userCanEditFile } from './fileHelpers';
@@ -62,6 +63,54 @@ describe('userCanEditFile', () => {
     const registration: Registration = { ...emptyRegistration, type: 'ImportCandidate' };
     const user: User = { ...emptyUser, isInternalImporter: false };
     const result = userCanEditFile(emptyFile, user, registration);
+    expect(result).toBe(false);
+  });
+
+  test('returns true if the result is a degree and the user is a thesis curator that belongs to the same institution as the file uploader', () => {
+    const file: AssociatedFile = {
+      ...emptyFile,
+      uploadDetails: { type: 'UserUploadDetails', uploadedBy: '123@1.0.0.0', uploadedDate: '' },
+    };
+    const user: User = { ...emptyUser, isThesisCurator: true, nvaUsername: '123@1.0.0.0' };
+    const registration: Registration = {
+      entityDescription: {
+        reference: { publicationInstance: { type: DegreeType.Bachelor as any } as any } as any,
+      } as any,
+    } as Registration;
+
+    const result = userCanEditFile(file, user, registration);
+    expect(result).toBe(true);
+  });
+
+  test('returns false if the result is a degree and the user is a publishing curator that belongs to the same institution as the file uploader', () => {
+    const file: AssociatedFile = {
+      ...emptyFile,
+      uploadDetails: { type: 'UserUploadDetails', uploadedBy: '123@1.0.0.0', uploadedDate: '' },
+    };
+    const user: User = { ...emptyUser, isPublishingCurator: true, nvaUsername: '123@1.0.0.0' };
+    const registration: Registration = {
+      entityDescription: {
+        reference: { publicationInstance: { type: DegreeType.Bachelor as any } as any } as any,
+      } as any,
+    } as Registration;
+
+    const result = userCanEditFile(file, user, registration);
+    expect(result).toBe(false);
+  });
+
+  test('returns false if the result is a degree and the user is a thesis curator that belongs to another institution than the file uploader', () => {
+    const file: AssociatedFile = {
+      ...emptyFile,
+      uploadDetails: { type: 'UserUploadDetails', uploadedBy: '123@1.0.0.0', uploadedDate: '' },
+    };
+    const user: User = { ...emptyUser, isThesisCurator: true, nvaUsername: '123@2.0.0.0' };
+    const registration: Registration = {
+      entityDescription: {
+        reference: { publicationInstance: { type: DegreeType.Bachelor as any } as any } as any,
+      } as any,
+    } as Registration;
+
+    const result = userCanEditFile(file, user, registration);
     expect(result).toBe(false);
   });
 });
