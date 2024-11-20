6ed04e24f395a418970fc90442df9fcbaafa9c7a
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-11-20 08:07:32 +0100

    Add tests for thesis embargo

--- src/pages/registration/helpers/fileHelpers.test.ts
+++ src/pages/registration/helpers/fileHelpers.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { AssociatedFile, emptyFile } from '../../../types/associatedArtifact.types';
+import { AssociatedFile, emptyFile, FileType } from '../../../types/associatedArtifact.types';
 import { DegreeType } from '../../../types/publicationFieldNames';
 import { emptyRegistration, Registration } from '../../../types/registration.types';
 import { User } from '../../../types/user.types';
@@ -113,4 +113,75 @@ describe('userCanEditFile', () => {
     const result = userCanEditFile(file, user, registration);
     expect(result).toBe(false);
   });
+
+  test('returns false for an embargoed thesis file if the user does not have the required rights', () => {
+    const currentYear = new Date().getFullYear();
+    const file: AssociatedFile = {
+      ...emptyFile,
+      type: FileType.OpenFile,
+      embargoDate: new Date(currentYear + 1, 0, 1),
+      uploadDetails: { type: 'UserUploadDetails', uploadedBy: '123@1.0.0.0', uploadedDate: '' },
+    };
+    const user: User = {
+      ...emptyUser,
+      isPublishingCurator: true,
+      isThesisCurator: true,
+      isEmbargoThesisCurator: false,
+      nvaUsername: '123@1.0.0.0',
+    };
+    const registration: Registration = {
+      entityDescription: {
+        reference: { publicationInstance: { type: DegreeType.Bachelor as any } as any } as any,
+      } as any,
+    } as Registration;
+
+    const result = userCanEditFile(file, user, registration);
+    expect(result).toBe(false);
+  });
+
+  test('returns true for an embargoed thesis file if the user is EmbargoThesisCurator', () => {
+    const currentYear = new Date().getFullYear();
+    const file: AssociatedFile = {
+      ...emptyFile,
+      type: FileType.OpenFile,
+      embargoDate: new Date(currentYear + 1, 0, 1),
+      uploadDetails: { type: 'UserUploadDetails', uploadedBy: '123@1.0.0.0', uploadedDate: '' },
+    };
+    const user: User = { ...emptyUser, isEmbargoThesisCurator: true, nvaUsername: '123@1.0.0.0' };
+    const registration: Registration = {
+      entityDescription: {
+        reference: { publicationInstance: { type: DegreeType.Bachelor as any } as any } as any,
+      } as any,
+    } as Registration;
+
+    const result = userCanEditFile(file, user, registration);
+    expect(result).toBe(true);
+  });
+
+  // test('returns true for a pending file if the user is the uploader', () => {
+  //   vi.mocked(isPendingOpenFile).mockReturnValue(true);
+  //   const file: AssociatedFile = {
+  //     ...emptyFile,
+  //     uploadDetails: { type: 'UserUploadDetails', uploadedBy: 'user@institution.edu' },
+  //     type: FileType.PendingInternalFile,
+  //   };
+  //   const user: User = { ...emptyUser, nvaUsername: 'user@institution.edu' };
+
+  //   const result = userCanEditFile(file, user, emptyRegistration);
+  //   expect(result).toBe(true);
+  // });
+
+  // test('returns false for an approved file if the user is not a publishing curator for the uploader', () => {
+  //   vi.mocked(isOpenFile).mockReturnValue(true);
+  //   const file: AssociatedFile = {
+  //     ...emptyFile,
+  //     uploadDetails: { type: 'UserUploadDetails', uploadedBy: 'user@institution.edu' },
+  //   };
+  //   const user: User = { ...emptyUser, isPublishingCurator: false, nvaUsername: 'curator@other.edu' };
+
+  //   const result = userCanEditFile(file, user, emptyRegistration);
+  //   expect(result).toBe(false);
+  // });
+
+  // Test returns true if curator
 });
