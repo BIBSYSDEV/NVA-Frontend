35a34720bc439325a3113340a33d33395f33b052
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-11-21 10:03:44 +0100

    Show icon header on landing page

--- src/components/RegistrationIconHeader.tsx
+++ src/components/RegistrationIconHeader.tsx
@@ -1,5 +1,5 @@
 import NotesIcon from '@mui/icons-material/Notes';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, TypographyProps } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { PublicationInstanceType, RegistrationDate } from '../types/registration.types';
 import { displayDate } from '../utils/date-helpers';
@@ -8,12 +8,14 @@ interface RegistrationIconHeaderProps {
   publicationInstanceType?: PublicationInstanceType | '';
   publicationDate?: Omit<RegistrationDate, 'type'>;
   showYearOnly?: boolean;
+  textColor?: TypographyProps['color'];
 }
 
 export const RegistrationIconHeader = ({
   publicationInstanceType,
   publicationDate,
   showYearOnly = false,
+  textColor,
 }: RegistrationIconHeaderProps) => {
   const { t } = useTranslation();
 
@@ -21,12 +23,12 @@ export const RegistrationIconHeader = ({
     <Box sx={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
       <NotesIcon sx={{ bgcolor: 'registration.main', borderRadius: '0.4rem', color: 'black' }} />
 
-      <Typography sx={{ color: 'primary.main' }}>
+      <Typography sx={{ color: textColor }}>
         {publicationInstanceType ? t(`registration.publication_types.${publicationInstanceType}`) : t('common.result')}
       </Typography>
 
       {publicationDate?.year && (
-        <Typography sx={{ fontWeight: 'bold', color: 'primary.main' }}>
+        <Typography sx={{ fontWeight: 'bold', color: textColor }}>
           {showYearOnly ? publicationDate.year : displayDate(publicationDate)}
         </Typography>
       )}
