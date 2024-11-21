1e17ce497b010c06b18eec21d4200d554d1b1a09
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-11-21 12:31:36 +0100

    Show no files icon for editor

--- src/pages/editor/CategoriesWithFiles.tsx
+++ src/pages/editor/CategoriesWithFiles.tsx
@@ -62,7 +62,7 @@ const CategoriesWithFilesForCustomer = ({ customer }: CategoriesWithFilesForCust
 
   return (
     <>
-      <CategorySelector selectedCategories={selectedCategories} onCategoryClick={onSelectType} />
+      <CategorySelector selectedCategories={selectedCategories} onCategoryClick={onSelectType} showNoFilesIcon />
 
       <Box sx={{ display: 'flex', justifyContent: 'center', gap: '1rem', mt: '2rem' }}>
         <Button onClick={() => setSelectedCategories(customer.allowFileUploadForTypes)}>{t('common.cancel')}</Button>
