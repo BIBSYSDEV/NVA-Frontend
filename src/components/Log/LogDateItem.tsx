2471618ed6d9ec1ff1df6d8bd76262c1498787a3
Isar Buzza <isar.buzza@sikt.no>
2024-11-22 13:01:08 +0100

    minor changes from code review

--- src/components/Log/LogDateItem.tsx
+++ src/components/Log/LogDateItem.tsx
@@ -1,21 +1,20 @@
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import DateRangeIcon from '@mui/icons-material/DateRange';
 import { Box, Typography } from '@mui/material';
+import { toDateString } from '../../utils/date-helpers';
 
 interface LogDateItemProps {
-  date: Date | string | number;
+  date: Date;
 }
 
 export const LogDateItem = ({ date }: LogDateItemProps) => {
-  const dateObject = date instanceof Date ? date : new Date(date);
-
-  const dateString = dateObject.toLocaleDateString('nb-NO', { year: 'numeric', month: '2-digit', day: '2-digit' });
-  const timeString = dateObject.toLocaleTimeString('nb-NO', { hour: '2-digit', minute: '2-digit' });
+  const dateString = toDateString(date);
+  const timeString = date.toLocaleTimeString('nb-NO', { hour: '2-digit', minute: '2-digit' });
 
   return (
     <Box sx={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
       <DateRangeIcon color="primary" fontSize="small" />
-      <Typography>{dateString},</Typography>
+      <Typography>{dateString}</Typography>
       <AccessTimeIcon color="primary" fontSize="small" />
       <Typography>{timeString}</Typography>
     </Box>
