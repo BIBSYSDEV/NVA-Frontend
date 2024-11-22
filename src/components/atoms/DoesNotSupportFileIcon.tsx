bda135d27427414330d79e3b624630d7e166d536
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-11-22 09:21:00 +0100

    Update file icon

--- src/components/atoms/DoesNotSupportFileIcon.tsx
+++ src/components/atoms/DoesNotSupportFileIcon.tsx
@@ -1,7 +1,7 @@
-import FolderOffIcon from '@mui/icons-material/FolderOff';
+import InsertPageBreakIcon from '@mui/icons-material/InsertPageBreak';
 import { useTranslation } from 'react-i18next';
 
 export const DoesNotSupportFileIcon = () => {
   const { t } = useTranslation();
-  return <FolderOffIcon titleAccess={t('editor.does_not_support_file_upload')} fontSize="small" />;
+  return <InsertPageBreakIcon titleAccess={t('editor.does_not_support_file_upload')} fontSize="small" />;
 };
