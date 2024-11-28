2fa1dc5ed7dae38b30317d754b79851c6ce63f01
Isar Buzza <isar.buzza@sikt.no>
2024-11-28 14:21:31 +0100

    moved filters to InstitutionList

--- src/pages/basic_data/app_admin/AdminCustomerInstitutions.tsx
+++ src/pages/basic_data/app_admin/AdminCustomerInstitutions.tsx
@@ -1,15 +1,10 @@
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
-import { Box } from '@mui/system';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { CustomerInstitutionApiPath } from '../../../api/apiPaths';
 import { PageHeader } from '../../../components/PageHeader';
 import { PageSpinner } from '../../../components/PageSpinner';
 import { CustomerList } from '../../../types/customerInstitution.types';
-import { dataTestId } from '../../../utils/dataTestIds';
 import { useFetch } from '../../../utils/hooks/useFetch';
 import { sortCustomerInstitutions } from '../../../utils/institutions-helpers';
-import { SearchTextField } from '../../search/SearchTextField';
 import { InstitutionList } from './InstitutionList';
 
 export const AdminCustomerInstitutions = () => {
@@ -20,11 +15,6 @@ export const AdminCustomerInstitutions = () => {
     errorMessage: t('feedback.error.get_customers'),
   });
 
-  const [customerStatus, setCustomerStatus] = useState('');
-  const customers = customerInstitutions?.customers ?? [];
-
-  console.log(customers);
-
   return (
     <>
       <PageHeader id="admin-institutions-label">{t('basic_data.institutions.admin_institutions')}</PageHeader>
@@ -32,25 +22,7 @@ export const AdminCustomerInstitutions = () => {
         <PageSpinner aria-labelledby="admin-institutions-label" />
       ) : (
         customerInstitutions && (
-          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
-            <Box sx={{ display: 'grid', gridTemplateColumns: '4fr 1fr', columnGap: '0.5rem', width: '50%' }}>
-              <SearchTextField />
-              <FormControl fullWidth>
-                <InputLabel id={'customer-active-select'}>{t('tasks.display_options')}</InputLabel>
-                <Select
-                  data-testid={dataTestId.tasksPage.unreadSearchSelect}
-                  size="small"
-                  value={'show-all'}
-                  labelId={'customer-active-select'}
-                  label={t('tasks.display_options')}>
-                  <MenuItem value={'show-all'}>{t('common.show_all')}</MenuItem>
-                  <MenuItem value={'active'}>Kun aktive</MenuItem>
-                  <MenuItem value={'inactive'}>Kun inaktive</MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
-            <InstitutionList institutions={sortCustomerInstitutions(customers)} />
-          </Box>
+          <InstitutionList institutions={sortCustomerInstitutions(customerInstitutions.customers)} />
         )
       )}
     </>
--- src/pages/basic_data/app_admin/InstitutionList.tsx
+++ src/pages/basic_data/app_admin/InstitutionList.tsx
@@ -1,7 +1,11 @@
 import EditIcon from '@mui/icons-material/Edit';
 import {
   Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
   Paper,
+  Select,
   Table,
   TableBody,
   TableCell,
@@ -10,13 +14,16 @@ import {
   TableRow,
   Typography,
 } from '@mui/material';
+import { Box } from '@mui/system';
 import { visuallyHidden } from '@mui/utils';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link as RouterLink } from 'react-router-dom';
 import { alternatingTableRowColor } from '../../../themes/mainTheme';
 import { SimpleCustomerInstitution } from '../../../types/customerInstitution.types';
 import { dataTestId } from '../../../utils/dataTestIds';
 import { getAdminInstitutionPath } from '../../../utils/urlPaths';
+import { SearchTextField } from '../../search/SearchTextField';
 
 interface InstitutionListProps {
   institutions: SimpleCustomerInstitution[];
@@ -25,46 +32,69 @@ interface InstitutionListProps {
 export const InstitutionList = ({ institutions }: InstitutionListProps) => {
   const { t } = useTranslation();
 
+  const [customerStatus, setCustomerStatus] = useState('show-all');
+  const activeCustomers = institutions.filter((customer) => customer.active);
+  const inactiveCustomers = institutions.filter((customer) => !customer.active);
+
   return (
-    <TableContainer component={Paper}>
-      <Table data-testid={dataTestId.basicData.customers.customerList} sx={alternatingTableRowColor}>
-        <caption style={visuallyHidden}>{t('basic_data.institutions.admin_institutions')}</caption>
-        <TableHead>
-          <TableRow>
-            <TableCell>{t('common.name')}</TableCell>
-            <TableCell>{t('basic_data.institutions.doi_prefix')}</TableCell>
-            <TableCell>{t('common.status')}</TableCell>
-            <TableCell>{t('common.actions')}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {institutions.map((institution) => (
-            <TableRow key={institution.id}>
-              <TableCell>
-                <Typography>{institution.displayName ?? institution.id}</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>{institution.doiPrefix}</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>
-                  {institution.active ? t('basic_data.institutions.active') : t('basic_data.institutions.not_active')}
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Button
-                  variant="outlined"
-                  startIcon={<EditIcon />}
-                  component={RouterLink}
-                  data-testid={dataTestId.basicData.customers.editInstitutionButton(institution.id)}
-                  to={getAdminInstitutionPath(institution.id)}>
-                  <Typography>{t('common.edit')}</Typography>
-                </Button>
-              </TableCell>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+      <Box sx={{ display: 'grid', gridTemplateColumns: '4fr 1fr', columnGap: '0.5rem', width: '50%' }}>
+        <SearchTextField />
+        <FormControl fullWidth>
+          <InputLabel id={'customer-active-select'}>{t('tasks.display_options')}</InputLabel>
+          <Select
+            data-testid={dataTestId.tasksPage.unreadSearchSelect}
+            size="small"
+            value={customerStatus}
+            labelId={'customer-active-select'}
+            label={t('tasks.display_options')}>
+            <MenuItem value={'show-all'}>{t('common.show_all')}</MenuItem>
+            <MenuItem value={'active'}>Kun aktive</MenuItem>
+            <MenuItem value={'inactive'}>Kun inaktive</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+
+      <TableContainer component={Paper}>
+        <Table data-testid={dataTestId.basicData.customers.customerList} sx={alternatingTableRowColor}>
+          <caption style={visuallyHidden}>{t('basic_data.institutions.admin_institutions')}</caption>
+          <TableHead>
+            <TableRow>
+              <TableCell>{t('common.name')}</TableCell>
+              <TableCell>{t('basic_data.institutions.doi_prefix')}</TableCell>
+              <TableCell>{t('common.status')}</TableCell>
+              <TableCell>{t('common.actions')}</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {institutions.map((institution) => (
+              <TableRow key={institution.id}>
+                <TableCell>
+                  <Typography>{institution.displayName ?? institution.id}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{institution.doiPrefix}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>
+                    {institution.active ? t('basic_data.institutions.active') : t('basic_data.institutions.not_active')}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Button
+                    variant="outlined"
+                    startIcon={<EditIcon />}
+                    component={RouterLink}
+                    data-testid={dataTestId.basicData.customers.editInstitutionButton(institution.id)}
+                    to={getAdminInstitutionPath(institution.id)}>
+                    <Typography>{t('common.edit')}</Typography>
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 };
