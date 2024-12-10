7d598b7cf0a1a84fa4baf1d6c56c7fa3791b43ab
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2024-12-10 15:22:12 +0100

    Reset reported year filter when removing reported checkmark

--- src/pages/search/advanced_search/NviReportedYearFilter.tsx
+++ src/pages/search/advanced_search/NviReportedYearFilter.tsx
@@ -1,4 +1,5 @@
 import { MenuItem, TextField } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import { ResultParam } from '../../../api/searchApi';
 import { useRegistrationsQueryParams } from '../../../utils/hooks/useRegistrationSearchParams';
@@ -8,6 +9,7 @@ import { syncParamsWithSearchFields } from '../../../utils/searchHelpers';
 const relevantNviYears = getNviYearFilterValues(new Date().getFullYear() - 1);
 
 export const NviReportedYearFilter = () => {
+  const { t } = useTranslation();
   const history = useHistory();
   const searchParams = new URLSearchParams(history.location.search);
 
@@ -17,9 +19,9 @@ export const NviReportedYearFilter = () => {
     <TextField
       select
       disabled={!scientificIndexStatus}
-      value={scientificReportPeriodSince}
+      value={scientificReportPeriodSince ?? ''}
       size="small"
-      label="Rapporteringsår"
+      label={t('search.advanced_search.nvi_reported_year')}
       sx={{ width: '10rem' }}
       onChange={(event) => {
         const selectedYear = event.target.value;
