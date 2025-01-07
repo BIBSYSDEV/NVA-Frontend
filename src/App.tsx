68f728f8de1e4182eed195171118a0178eaa5474
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-01-07 10:13:31 +0100

    Move routes to AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,22 @@
 import { Amplify } from 'aws-amplify';
-import { lazy, Suspense, useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import { createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { getUserAttributes } from './api/authApi';
+import { AppRoutes } from './AppRoutes';
 import { AcceptTermsDialog } from './components/AcceptTermsDialog';
 import { CreateCristinPersonDialog } from './components/CreateCristinPersonDialog';
 import { PageSpinner } from './components/PageSpinner';
 import { SelectCustomerInstitutionDialog } from './components/SelectCustomerInstitutionDialog';
-import { Layout } from './Layout';
-import LoginPage from './layout/LoginPage';
-import Logout from './layout/Logout';
 import { useMatomoTracking } from './matomo/useMatomoTracking';
-import NotFound from './pages/errorpages/NotFound';
-import ProjectsPage from './pages/projects/ProjectsPage';
-import EditRegistration from './pages/registration/new_registration/EditRegistration';
 import { RootState } from './redux/store';
 import { setUser } from './redux/userSlice';
 import { authOptions } from './utils/aws-config';
 import { USE_MOCK_DATA } from './utils/constants';
-import { PrivateRoute } from './utils/routes/Routes';
 import { mockUser } from './utils/testfiles/mock_feide_user';
-import { SplashRoutes, UrlPathTemplate } from './utils/urlPaths';
-import { hasCuratorRole } from './utils/user-helpers';
+import { UrlPathTemplate } from './utils/urlPaths';
 
 const getLanguageTagValue = (language: string) => {
   if (language === 'eng') {
@@ -41,18 +34,6 @@ if (
   // Without this the user will be redirected to / for some reason
   window.location.href = window.location.href.replace('#', '?');
 }
-
-const Dashboard = lazy(() => import('./pages/dashboard/Dashboard'));
-const BasicDataPage = lazy(() => import('./pages/basic_data/BasicDataPage'));
-const EditorPage = lazy(() => import('./pages/editor/InstitutionPage'));
-const CreateProject = lazy(() => import('./pages/project/project_wizard/CreateProject'));
-const EditProject = lazy(() => import('./pages/project/project_wizard/EditProject'));
-const PublicRegistration = lazy(() => import('./pages/public_registration/PublicRegistration'));
-const MyPagePage = lazy(() => import('./pages/my_page/MyPagePage'));
-const PrivacyPolicy = lazy(() => import('./pages/infopages/PrivacyPolicy'));
-const PublicResearchProfile = lazy(() => import('./pages/research_profile/PublicResearchProfile'));
-const TasksPage = lazy(() => import('./pages/messages/TasksPage'));
-const SignedOutPage = lazy(() => import('./pages/infopages/SignedOutPage'));
 
 const Root = () => {
   useMatomoTracking();
@@ -90,14 +71,6 @@ const Root = () => {
   const mustCreatePerson = user && !user.cristinId;
   const mustSelectCustomer = user && user.cristinId && user.allowedCustomers.length > 1 && !user.customerId;
 
-  const isAuthenticated = !!user;
-  const hasCustomerId = isAuthenticated && !!user.customerId;
-  const isCreator = hasCustomerId && user.isCreator;
-  const isCurator = hasCuratorRole(user);
-  const isEditor = hasCustomerId && user.isEditor;
-  const canSeeBasicData = hasCustomerId && (user.isAppAdmin || user.isInstitutionAdmin || user.isInternalImporter);
-  const isNviCurator = !!user?.isNviCurator;
-
   return (
     <>
       <Helmet defaultTitle={t('common.page_title')} titleTemplate={`%s - ${t('common.page_title')}`}>
@@ -108,76 +81,7 @@ const Root = () => {
       {mustCreatePerson && <CreateCristinPersonDialog user={user} />}
       {mustSelectCustomer && <SelectCustomerInstitutionDialog allowedCustomerIds={user.allowedCustomers} />}
 
-      {isLoadingUserAttributes ? (
-        <PageSpinner aria-label={t('common.page_title')} />
-      ) : (
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path={UrlPathTemplate.Root} element={<Dashboard />}>
-              <Route path={UrlPathTemplate.Search} element={<Dashboard />} />
-              <Route path={UrlPathTemplate.Reports} element={<Dashboard />} />
-              <Route path={UrlPathTemplate.ReportsNvi} element={<Dashboard />} />
-              <Route path={UrlPathTemplate.ReportsInternationalCooperation} element={<Dashboard />} />
-              <Route path={UrlPathTemplate.ReportsClinicalTreatmentStudies} element={<Dashboard />} />
-            </Route>
-
-            <Route path={UrlPathTemplate.PrivacyPolicy} element={<PrivacyPolicy />} />
-            <Route path={UrlPathTemplate.ResearchProfile} element={<PublicResearchProfile />} />
-            <Route path={UrlPathTemplate.RegistrationLandingPage} element={<PublicRegistration />} />
-            <Route path={UrlPathTemplate.Projects} element={<ProjectsPage />} />
-            <Route path={UrlPathTemplate.Login} element={<LoginPage />} />
-            <Route path={UrlPathTemplate.Logout} element={<Logout />} />
-            <Route path={UrlPathTemplate.SignedOut} element={<SignedOutPage />} />
-
-            {/* Authenticated routes */}
-            <Route
-              path={SplashRoutes.MyPage}
-              element={<PrivateRoute element={<MyPagePage />} isAuthorized={isAuthenticated} />}
-            />
-
-            {/* Creator routes */}
-            <Route
-              path={UrlPathTemplate.RegistrationWizard}
-              element={
-                <PrivateRoute isAuthorized={isCreator || isCurator || isEditor} element={<EditRegistration />} />
-              }
-            />
-            <Route
-              path={UrlPathTemplate.RegistrationNew}
-              element={<PrivateRoute isAuthorized={isCreator} element={<EditRegistration />} />}
-            />
-            <Route
-              path={UrlPathTemplate.ProjectsEdit}
-              element={<PrivateRoute isAuthorized={isCreator} element={<EditProject />} />}
-            />
-            <Route
-              path={UrlPathTemplate.ProjectsNew}
-              element={<PrivateRoute isAuthorized={isCreator} element={<CreateProject />} />}
-            />
-
-            {/* Curator routes */}
-            <Route
-              path={SplashRoutes.Tasks}
-              element={<PrivateRoute isAuthorized={isCurator || isNviCurator} element={<TasksPage />} />}
-            />
-
-            {/* Basic Data routes */}
-            <Route
-              path={SplashRoutes.BasicData}
-              element={<PrivateRoute isAuthorized={canSeeBasicData} element={<BasicDataPage />} />}
-            />
-
-            {/* Institution routes */}
-            <Route
-              path={SplashRoutes.Institution}
-              element={<PrivateRoute isAuthorized={hasCustomerId} element={<EditorPage />} />}
-            />
-
-            {/* Wildcard path must be last, otherwise it will catch all routes */}
-            <Route path={UrlPathTemplate.Wildcard} element={<NotFound />} />
-          </Route>
-        </Routes>
-      )}
+      {isLoadingUserAttributes ? <PageSpinner aria-label={t('common.page_title')} /> : <AppRoutes />}
     </>
   );
 };
---
+++ src/AppRoutes.tsx
@@ -0,0 +1,104 @@
+import { Logout } from '@mui/icons-material';
+import { lazy } from 'react';
+import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+import LoginPage from './layout/LoginPage';
+import NotFound from './pages/errorpages/NotFound';
+import EditRegistration from './pages/registration/new_registration/EditRegistration';
+import { RootState } from './redux/store';
+import { PrivateRoute } from './utils/routes/Routes';
+import { SplashRoutes, UrlPathTemplate } from './utils/urlPaths';
+import { hasCuratorRole } from './utils/user-helpers';
+
+const Dashboard = lazy(() => import('./pages/dashboard/Dashboard'));
+const BasicDataPage = lazy(() => import('./pages/basic_data/BasicDataPage'));
+const EditorPage = lazy(() => import('./pages/editor/InstitutionPage'));
+const CreateProject = lazy(() => import('./pages/project/project_wizard/CreateProject'));
+const EditProject = lazy(() => import('./pages/project/project_wizard/EditProject'));
+const PublicRegistration = lazy(() => import('./pages/public_registration/PublicRegistration'));
+const MyPagePage = lazy(() => import('./pages/my_page/MyPagePage'));
+const PrivacyPolicy = lazy(() => import('./pages/infopages/PrivacyPolicy'));
+const PublicResearchProfile = lazy(() => import('./pages/research_profile/PublicResearchProfile'));
+const TasksPage = lazy(() => import('./pages/messages/TasksPage'));
+const SignedOutPage = lazy(() => import('./pages/infopages/SignedOutPage'));
+const ProjectsPage = lazy(() => import('./pages/projects/ProjectsPage'));
+
+export const AppRoutes = () => {
+  const user = useSelector((store: RootState) => store.user);
+
+  const isAuthenticated = !!user;
+  const hasCustomerId = isAuthenticated && !!user.customerId;
+  const isCreator = hasCustomerId && user.isCreator;
+  const isCurator = hasCuratorRole(user);
+  const isEditor = hasCustomerId && user.isEditor;
+  const canSeeBasicData = hasCustomerId && (user.isAppAdmin || user.isInstitutionAdmin || user.isInternalImporter);
+  const isNviCurator = !!user?.isNviCurator;
+
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path={UrlPathTemplate.Root} element={<Dashboard />}>
+          <Route path={UrlPathTemplate.Search} element={<Dashboard />} />
+          <Route path={UrlPathTemplate.Reports} element={<Dashboard />} />
+          <Route path={UrlPathTemplate.ReportsNvi} element={<Dashboard />} />
+          <Route path={UrlPathTemplate.ReportsInternationalCooperation} element={<Dashboard />} />
+          <Route path={UrlPathTemplate.ReportsClinicalTreatmentStudies} element={<Dashboard />} />
+        </Route>
+
+        <Route path={UrlPathTemplate.PrivacyPolicy} element={<PrivacyPolicy />} />
+        <Route path={UrlPathTemplate.ResearchProfile} element={<PublicResearchProfile />} />
+        <Route path={UrlPathTemplate.RegistrationLandingPage} element={<PublicRegistration />} />
+        <Route path={UrlPathTemplate.Projects} element={<ProjectsPage />} />
+        <Route path={UrlPathTemplate.Login} element={<LoginPage />} />
+        <Route path={UrlPathTemplate.Logout} element={<Logout />} />
+        <Route path={UrlPathTemplate.SignedOut} element={<SignedOutPage />} />
+
+        {/* Authenticated routes */}
+        <Route
+          path={SplashRoutes.MyPage}
+          element={<PrivateRoute element={<MyPagePage />} isAuthorized={isAuthenticated} />}
+        />
+
+        {/* Creator routes */}
+        <Route
+          path={UrlPathTemplate.RegistrationWizard}
+          element={<PrivateRoute isAuthorized={isCreator || isCurator || isEditor} element={<EditRegistration />} />}
+        />
+        <Route
+          path={UrlPathTemplate.RegistrationNew}
+          element={<PrivateRoute isAuthorized={isCreator} element={<EditRegistration />} />}
+        />
+        <Route
+          path={UrlPathTemplate.ProjectsEdit}
+          element={<PrivateRoute isAuthorized={isCreator} element={<EditProject />} />}
+        />
+        <Route
+          path={UrlPathTemplate.ProjectsNew}
+          element={<PrivateRoute isAuthorized={isCreator} element={<CreateProject />} />}
+        />
+
+        {/* Curator routes */}
+        <Route
+          path={SplashRoutes.Tasks}
+          element={<PrivateRoute isAuthorized={isCurator || isNviCurator} element={<TasksPage />} />}
+        />
+
+        {/* Basic Data routes */}
+        <Route
+          path={SplashRoutes.BasicData}
+          element={<PrivateRoute isAuthorized={canSeeBasicData} element={<BasicDataPage />} />}
+        />
+
+        {/* Institution routes */}
+        <Route
+          path={SplashRoutes.Institution}
+          element={<PrivateRoute isAuthorized={hasCustomerId} element={<EditorPage />} />}
+        />
+
+        {/* Wildcard path must be last, otherwise it will catch all routes */}
+        <Route path={UrlPathTemplate.Wildcard} element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
