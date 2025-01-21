b5e6a23a05e8db03202d9cea5785f08afbc32f13
Isar Buzza <isar.buzza@sikt.no>
2025-01-21 08:42:21 +0100

    moved button outside grid

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -26,189 +26,187 @@ export const Footer = () => {
   return (
     <Box
       component="footer"
-      sx={{
-        padding: '1rem',
-        bgcolor: 'info.light',
-        display: 'grid',
-        gridTemplateColumns: { xs: '1fr', md: '1fr 1fr 1fr' },
-        columnGap: '3rem',
-        color: 'primary.main',
-      }}>
+      sx={{ display: 'flex', flexDirection: 'column', padding: '1rem', bgcolor: 'info.light', color: 'primary.main' }}>
       <Button
-        sx={{ width: 'fit-content', justifySelf: 'center', gridColumn: { md: '1 / span 3' } }}
+        sx={{ width: 'fit-content', alignSelf: 'center' }}
         data-testid={dataTestId.footer.toTopButton}
         onClick={() => window.scroll({ top: 0, behavior: 'smooth' })}
         startIcon={<KeyboardArrowUpIcon />}>
         {t('footer.to_top')}
       </Button>
-      <div>
-        <Box sx={{ display: 'flex', gap: '0.5rem', justifyContent: 'space-between' }}>
-          <Typography color="primary" sx={{ fontWeight: 'bold' }}>
-            {t('footer.about_nva')}
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: { xs: '1fr', md: '1fr 1fr 1fr' },
+          gap: '3rem',
+        }}>
+        <div>
+          <Box sx={{ display: 'flex', gap: '0.5rem', justifyContent: 'space-between' }}>
+            <Typography color="primary" sx={{ fontWeight: 'bold' }}>
+              {t('footer.about_nva')}
+            </Typography>
+
+            <StyledExternalLink
+              data-testid={dataTestId.footer.aboutLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              href={'https://sikt.no/tjenester/nasjonalt-vitenarkiv-nva'}>
+              {t('common.read_more')}
+              <OpenInNewIcon fontSize="small" />
+            </StyledExternalLink>
+          </Box>
+
+          <Divider sx={{ bgcolor: 'primary.main', mb: '0.5rem' }} />
+
+          <Typography color="primary">{t('footer.about_nva_text')}</Typography>
+
+          <Typography
+            color="primary"
+            sx={{
+              fontWeight: 'bold',
+              gap: '0.5rem',
+              mt: '2rem',
+            }}>
+            {t('footer.language_selector')}
           </Typography>
 
-          <StyledExternalLink
-            data-testid={dataTestId.footer.aboutLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            href={'https://sikt.no/tjenester/nasjonalt-vitenarkiv-nva'}>
-            {t('common.read_more')}
-            <OpenInNewIcon fontSize="small" />
-          </StyledExternalLink>
-        </Box>
+          <Divider sx={{ bgcolor: 'primary.main', mb: '0.5rem' }} />
 
-        <Divider sx={{ bgcolor: 'primary.main', mb: '0.5rem' }} />
-
-        <Typography color="primary">{t('footer.about_nva_text')}</Typography>
-
-        <Typography
-          color="primary"
-          sx={{
-            fontWeight: 'bold',
-            gap: '0.5rem',
-            mt: '2rem',
-          }}>
-          {t('footer.language_selector')}
-        </Typography>
-
-        <Divider sx={{ bgcolor: 'primary.main', mb: '0.5rem' }} />
-
-        <Box sx={{ mb: '3rem' }}>
           <LanguageSelector />
-        </Box>
-      </div>
+        </div>
 
-      <div>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            gap: '0.5rem',
-          }}>
-          <Typography color="primary" sx={{ fontWeight: 'bold' }}>
-            {t('footer.become_customer')}
-          </Typography>
+        <div>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              gap: '0.5rem',
+            }}>
+            <Typography color="primary" sx={{ fontWeight: 'bold' }}>
+              {t('footer.become_customer')}
+            </Typography>
 
+            <StyledExternalLink
+              data-testid={dataTestId.footer.becomeCustomer}
+              target="_blank"
+              rel="noopener noreferrer"
+              href={'https://sikt.no/tjenester/nasjonalt-vitenarkiv-nva#Tjenestebetingelser'}>
+              {t('common.read_more')}
+              <OpenInNewIcon fontSize="small" />
+            </StyledExternalLink>
+          </Box>
+
+          <Divider sx={{ bgcolor: 'primary.main', mb: '0.5rem' }} />
+
+          <Typography color="primary">{t('footer.about_sikt_text')}</Typography>
+
+          <Box sx={{ display: 'flex', gap: '0.5rem', justifyContent: 'space-between', mt: '2rem' }}>
+            <Typography color="primary" sx={{ fontWeight: 'bold' }}>
+              {t('footer.about_sikt')}
+            </Typography>
+
+            <StyledExternalLink
+              data-testid={dataTestId.footer.aboutSikt}
+              target="_blank"
+              rel="noopener noreferrer"
+              href={'https://sikt.no/'}>
+              {t('common.read_more')}
+              <OpenInNewIcon fontSize="small" />
+            </StyledExternalLink>
+          </Box>
+
+          <Divider sx={{ bgcolor: 'primary.main', mb: '0.5rem' }} />
+
+          <Typography color="primary">{t('about.footer_text')}</Typography>
+        </div>
+
+        <div>
           <StyledExternalLink
-            data-testid={dataTestId.footer.becomeCustomer}
+            sx={{ fontWeight: 'bold', mb: '1rem' }}
+            data-testid={dataTestId.footer.helpPage}
             target="_blank"
             rel="noopener noreferrer"
-            href={'https://sikt.no/tjenester/nasjonalt-vitenarkiv-nva#Tjenestebetingelser'}>
-            {t('common.read_more')}
+            href={'https://sikt.no/tjenester/nasjonalt-vitenarkiv-nva/hjelpeside-nva'}>
+            {t('footer.help_page')}
             <OpenInNewIcon fontSize="small" />
           </StyledExternalLink>
-        </Box>
-
-        <Divider sx={{ bgcolor: 'primary.main', mb: '0.5rem' }} />
-
-        <Typography color="primary">{t('footer.about_sikt_text')}</Typography>
-
-        <Box sx={{ display: 'flex', gap: '0.5rem', justifyContent: 'space-between', mt: '2rem' }}>
-          <Typography color="primary" sx={{ fontWeight: 'bold' }}>
-            {t('footer.about_sikt')}
-          </Typography>
 
           <StyledExternalLink
-            data-testid={dataTestId.footer.aboutSikt}
+            sx={{ fontWeight: 'bold', mb: '1rem' }}
+            data-testid={dataTestId.footer.systemStatusLink}
             target="_blank"
             rel="noopener noreferrer"
-            href={'https://sikt.no/'}>
-            {t('common.read_more')}
+            href={'https://sikt.no/driftsmeldinger'}>
+            {t('footer.system_status_link')}
             <OpenInNewIcon fontSize="small" />
           </StyledExternalLink>
-        </Box>
 
-        <Divider sx={{ bgcolor: 'primary.main', mb: '0.5rem' }} />
+          <Divider sx={{ bgcolor: 'primary.main', mb: '0.8rem' }} />
 
-        <Typography color="primary">{t('about.footer_text')}</Typography>
-      </div>
+          <StyledArrowLinkContainer>
+            <ArrowRightAltIcon />
+            <StyledExternalLink
+              data-testid={dataTestId.footer.newsLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              href={'https://pub.dialogapi.no/s/MjI2NDQ6MWVmZmQxOTktMGVkZi00YjZhLWE2ZjQtNzNmZWU2NjI2ZmFi'}>
+              {t('footer.news_link')}
+              <OpenInNewIcon fontSize="small" />
+            </StyledExternalLink>
+          </StyledArrowLinkContainer>
 
-      <div>
-        <StyledExternalLink
-          sx={{ fontWeight: 'bold', mb: '1rem' }}
-          data-testid={dataTestId.footer.helpPage}
-          target="_blank"
-          rel="noopener noreferrer"
-          href={'https://sikt.no/tjenester/nasjonalt-vitenarkiv-nva/hjelpeside-nva'}>
-          {t('footer.help_page')}
-          <OpenInNewIcon fontSize="small" />
-        </StyledExternalLink>
+          <StyledArrowLinkContainer>
+            <ArrowRightAltIcon />
+            <MuiLink
+              data-testid={dataTestId.footer.privacyLink}
+              color={'primary.main'}
+              component={Link}
+              to={UrlPathTemplate.PrivacyPolicy}>
+              {t('privacy.privacy_statement')}
+            </MuiLink>
+          </StyledArrowLinkContainer>
 
-        <StyledExternalLink
-          sx={{ fontWeight: 'bold', mb: '1rem' }}
-          data-testid={dataTestId.footer.systemStatusLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          href={'https://sikt.no/driftsmeldinger'}>
-          {t('footer.system_status_link')}
-          <OpenInNewIcon fontSize="small" />
-        </StyledExternalLink>
+          <StyledArrowLinkContainer>
+            <ArrowRightAltIcon />
+            <StyledExternalLink
+              data-testid={dataTestId.footer.availabilityStatement}
+              target="_blank"
+              rel="noopener noreferrer"
+              href={'https://uustatus.no/nb/erklaringer/publisert/bffb4b1d-25eb-4fe0-bac7-2f0b4a8e0fd9'}>
+              {t('about.availability_statement')}
+              <OpenInNewIcon fontSize="small" />
+            </StyledExternalLink>
+          </StyledArrowLinkContainer>
 
-        <Divider sx={{ bgcolor: 'primary.main', mb: '0.8rem' }} />
+          <StyledArrowLinkContainer>
+            <ArrowRightAltIcon />
+            <StyledExternalLink
+              target="_blank"
+              rel="noopener noreferrer"
+              href={'https://sikt.no/om-sikt/kontakt-oss?service=e588691c-22eb-49c3-ac50-755f5513a3f5#skjema'}>
+              {t('about.copyright_inquiries')}
+              <OpenInNewIcon fontSize="small" />
+            </StyledExternalLink>
+          </StyledArrowLinkContainer>
 
-        <StyledArrowLinkContainer>
-          <ArrowRightAltIcon />
-          <StyledExternalLink
-            data-testid={dataTestId.footer.newsLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            href={'https://pub.dialogapi.no/s/MjI2NDQ6MWVmZmQxOTktMGVkZi00YjZhLWE2ZjQtNzNmZWU2NjI2ZmFi'}>
-            {t('footer.news_link')}
-            <OpenInNewIcon fontSize="small" />
-          </StyledExternalLink>
-        </StyledArrowLinkContainer>
+          <StyledArrowLinkContainer>
+            <ArrowRightAltIcon />
 
-        <StyledArrowLinkContainer>
-          <ArrowRightAltIcon />
-          <MuiLink
-            data-testid={dataTestId.footer.privacyLink}
-            color={'primary.main'}
-            component={Link}
-            to={UrlPathTemplate.PrivacyPolicy}>
-            {t('privacy.privacy_statement')}
-          </MuiLink>
-        </StyledArrowLinkContainer>
-
-        <StyledArrowLinkContainer>
-          <ArrowRightAltIcon />
-          <StyledExternalLink
-            data-testid={dataTestId.footer.availabilityStatement}
-            target="_blank"
-            rel="noopener noreferrer"
-            href={'https://uustatus.no/nb/erklaringer/publisert/bffb4b1d-25eb-4fe0-bac7-2f0b4a8e0fd9'}>
-            {t('about.availability_statement')}
-            <OpenInNewIcon fontSize="small" />
-          </StyledExternalLink>
-        </StyledArrowLinkContainer>
-
-        <StyledArrowLinkContainer>
-          <ArrowRightAltIcon />
-          <StyledExternalLink
-            target="_blank"
-            rel="noopener noreferrer"
-            href={'https://sikt.no/om-sikt/kontakt-oss?service=e588691c-22eb-49c3-ac50-755f5513a3f5#skjema'}>
-            {t('about.copyright_inquiries')}
-            <OpenInNewIcon fontSize="small" />
-          </StyledExternalLink>
-        </StyledArrowLinkContainer>
-
-        <StyledArrowLinkContainer>
-          <ArrowRightAltIcon />
-
-          <Typography color="primary">
-            <Trans i18nKey="footer.technical_support_link">
-              <MuiLink
-                sx={{ color: 'primary.main' }}
-                href="mailto:kontakt@sikt.no"
-                target="_blank"
-                rel="noopener noreferrer"
-                data-testid={dataTestId.footer.technicalSupportLink}>
-                (i18n content: support email)
-              </MuiLink>
-            </Trans>
-          </Typography>
-        </StyledArrowLinkContainer>
-      </div>
+            <Typography color="primary">
+              <Trans i18nKey="footer.technical_support_link">
+                <MuiLink
+                  sx={{ color: 'primary.main' }}
+                  href="mailto:kontakt@sikt.no"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  data-testid={dataTestId.footer.technicalSupportLink}>
+                  (i18n content: support email)
+                </MuiLink>
+              </Trans>
+            </Typography>
+          </StyledArrowLinkContainer>
+        </div>
+      </Box>
     </Box>
   );
 };
