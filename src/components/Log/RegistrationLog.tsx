8c5d69ab10138d22dd2316bc4922ef9f16a842e8
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-01-23 11:31:25 +0100

    Np 48469 show hidden files count in log (#6936)
    * Remove unuesd translations
    
    * Show count of hidden files in log
    
    * Show number of hidden files in log
    
    * Show description if approved file has baan changes to hidden
    
    * Sort i18n keys
    
    * Remove redundant tag

--- src/components/Log/RegistrationLog.tsx
+++ src/components/Log/RegistrationLog.tsx
@@ -12,7 +12,10 @@ export const RegistrationLog = ({ log }: LogProps) => {
   return (
     <>
       <MetaDataLastUpdatedEntry metadataUpdated={log.metadataUpdated} />
-      <ArchivedFilesEntry numberOfArchivedFiles={log.numberOfArchivedFiles} />
+      <ArchivedFilesEntry
+        numberOfArchivedFiles={log.numberOfArchivedFiles}
+        numberOfHiddenFiles={log.numberOfHiddenFiles}
+      />
       {log.entries.map((entry, index) => (
         <LogEntry {...entry} key={index} />
       ))}
@@ -34,20 +37,28 @@ const MetaDataLastUpdatedEntry = ({ metadataUpdated }: Pick<LogType, 'metadataUp
   );
 };
 
-const ArchivedFilesEntry = ({ numberOfArchivedFiles }: Pick<LogType, 'numberOfArchivedFiles'>) => {
+const ArchivedFilesEntry = ({
+  numberOfArchivedFiles,
+  numberOfHiddenFiles,
+}: Pick<LogType, 'numberOfArchivedFiles' | 'numberOfHiddenFiles'>) => {
   const { t } = useTranslation();
-  if (!numberOfArchivedFiles) {
-    return;
+  if (numberOfArchivedFiles === 0 && numberOfHiddenFiles === 0) {
+    return null;
   }
 
   return (
     <Box sx={{ px: '0.5rem' }}>
-      <Divider />
-      <Box sx={{ display: 'flex', justifyContent: 'center', mt: '0.5rem' }}>
-        <Typography color="grey.700">
-          {t('log.number_of_archivedFiles_on_registration', { count: numberOfArchivedFiles })}
+      <Divider sx={{ mb: '0.5rem' }} />
+      {numberOfArchivedFiles > 0 && (
+        <Typography color="grey.700" sx={{ textAlign: 'center' }}>
+          {t('log.archived_files_on_registration', { count: numberOfArchivedFiles })}
         </Typography>
-      </Box>
+      )}
+      {numberOfHiddenFiles > 0 && (
+        <Typography color="grey.700" sx={{ textAlign: 'center' }}>
+          {t('log.hidden_files_on_registration', { count: numberOfHiddenFiles })}
+        </Typography>
+      )}
     </Box>
   );
 };
