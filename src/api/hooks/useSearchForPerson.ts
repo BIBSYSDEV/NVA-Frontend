0af91785d2a4ec3784a622b7a37d4e38d1f7e717
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-02-12 10:08:23 +0100

    Keep previous data for contributor when paginating

--- src/api/hooks/useSearchForPerson.ts
+++ src/api/hooks/useSearchForPerson.ts
@@ -1,8 +1,12 @@
-import { useQuery } from '@tanstack/react-query';
+import { Query, useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 import { PersonSearchParams, searchForPerson } from '../cristinApi';
 
-export const useSearchForPerson = (searchParams: PersonSearchParams) => {
+interface PersonSearchOptions extends PersonSearchParams {
+  keepPreviousData?: boolean;
+}
+
+export const useSearchForPerson = ({ keepPreviousData, ...searchParams }: PersonSearchOptions) => {
   const { t } = useTranslation();
 
   return useQuery({
@@ -10,5 +14,18 @@ export const useSearchForPerson = (searchParams: PersonSearchParams) => {
     queryKey: ['person', searchParams],
     queryFn: () => searchForPerson(searchParams),
     meta: { errorMessage: t('feedback.error.person_search') },
+    placeholderData: (data, query) =>
+      keepPreviousData ? keepSimilarPreviousData(data, query, searchParams.name) : undefined,
   });
+};
+
+const keepSimilarPreviousData = <T>(
+  previousData: T | undefined,
+  query: Query<T, Error, T, (string | PersonSearchParams)[]> | undefined,
+  searchTerm?: string | null
+) => {
+  // Keep previous data if query has the same search term
+  if (searchTerm && query?.queryKey.some((key) => typeof key === 'object' && key.name === searchTerm)) {
+    return previousData;
+  }
 };
