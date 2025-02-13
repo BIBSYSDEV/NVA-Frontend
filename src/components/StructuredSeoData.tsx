d0abc905d3bb090f97fa3c99d2e8e2da7d66345f
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-02-13 09:57:21 +0100

    Add required Highwire Press tags

--- src/components/StructuredSeoData.tsx
+++ src/components/StructuredSeoData.tsx
@@ -1,17 +1,30 @@
 import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { Registration, RegistrationDate } from '../types/registration.types';
+
+const getPublicationDateSeoString = (publicationDate: RegistrationDate) => {
+  if (publicationDate.year && publicationDate.month && publicationDate.day) {
+    return `${publicationDate.year}/${publicationDate.month}/${publicationDate.day}`;
+  }
+  if (publicationDate.year) {
+    return publicationDate.year;
+  }
+  return null;
+};
 
 interface StructuredSeoDataProps {
-  uri: string;
+  registration: Registration;
 }
 
-export const StructuredSeoData = ({ uri }: StructuredSeoDataProps) => {
+export const StructuredSeoData = ({ registration }: StructuredSeoDataProps) => {
   const [seoData, setSeoData] = useState('');
+
+  const registrationIsPublished = registration && registration.status === 'PUBLISHED';
 
   useEffect(() => {
     const fetchSeoData = async () => {
       try {
-        const fetchedSeoData = await fetch(uri, {
+        const fetchedSeoData = await fetch(registration.id, {
           headers: { Accept: 'application/vnd.schemaorg.ld+json' },
         });
         const structuredDataText = await fetchedSeoData.text();
@@ -21,14 +34,30 @@ export const StructuredSeoData = ({ uri }: StructuredSeoDataProps) => {
       }
     };
 
-    if (uri) {
+    if (registrationIsPublished) {
       fetchSeoData();
     }
-  }, [uri]);
+  }, [registration.id, registrationIsPublished]);
 
-  return seoData ? (
+  if (!registrationIsPublished) {
+    return null;
+  }
+
+  const citationPublicationDate =
+    registration.entityDescription?.publicationDate &&
+    getPublicationDateSeoString(registration.entityDescription.publicationDate);
+
+  return (
     <Helmet>
-      <script type="application/ld+json">{seoData}</script>
+      {seoData && <script type="application/ld+json">{seoData}</script>}
+
+      {registration.entityDescription?.mainTitle && (
+        <meta name="citation_title" content={registration.entityDescription.mainTitle} />
+      )}
+      {registration.entityDescription?.contributors?.map((contributor, index) => (
+        <meta name="citation_author" content={contributor.identity.name} key={index} />
+      ))}
+      {citationPublicationDate && <meta name="citation_publication_date" content={citationPublicationDate} />}
     </Helmet>
-  ) : null;
+  );
 };
