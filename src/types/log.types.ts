4e8d0768f1502d7941ce122bfac8ba69bba351ac
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-02-13 15:56:05 +0100

    Add DOI log entries

--- src/types/log.types.ts
+++ src/types/log.types.ts
@@ -53,7 +53,11 @@ interface PublicationLogEntry extends BaseLogEntry {
     | 'PublicationPublished'
     | 'PublicationUnpublished'
     | 'PublicationDeleted'
-    | 'PublicationRepublished';
+    | 'PublicationRepublished'
+    | 'DoiReserved' // TODO: new type?
+    | 'DoiRequested'
+    | 'DoiRejected'
+    | 'DoiAssigned';
 }
 
 interface PublicationImportedLogEntry extends Omit<PublicationLogEntry, 'topic'> {
