484203961e6d6636b76730942cd78f8d229cfd2c
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-02-13 10:47:19 +0100

    Add forgotten file

---
+++ src/utils/hooks/useJournalSeoData.ts
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchResource } from '../../api/commonApi';
+import { JournalRegistration } from '../../types/publication_types/journalRegistration.types';
+import { Registration, SerialPublication } from '../../types/registration.types';
+import { isJournal } from '../registration-helpers';
+
+export const useJournalSeoData = (registration: Registration) => {
+  const isJournalRegistration = isJournal(registration.entityDescription?.reference?.publicationInstance.type);
+  const journalId = isJournalRegistration
+    ? ((registration as JournalRegistration).entityDescription?.reference?.publicationContext.id ?? '')
+    : '';
+
+  const journalQuery = useQuery({
+    queryKey: ['channel', journalId],
+    enabled: !!journalId,
+    queryFn: () => fetchResource<SerialPublication>(journalId),
+    staleTime: Infinity,
+  });
+
+  const journalName = journalQuery.data?.name ?? '';
+  const onlineIssn = journalQuery.data?.onlineIssn ?? '';
+  const printIssn = journalQuery.data?.printIssn ?? '';
+
+  return { journalName, onlineIssn, printIssn };
+};
