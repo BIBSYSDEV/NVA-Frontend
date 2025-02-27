e44681f22f8ef4d14e200086bf5a9d2a2a466e70
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-02-27 10:11:36 +0100

    Fetch funding sources on demand (#7099)
    * Fetch funding sources on demand
    
    * Allow aborting fetch of funding sources

--- src/api/hooks/useFetchFundingSources.ts
+++ src/api/hooks/useFetchFundingSources.ts
@@ -2,12 +2,17 @@ import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 import { fetchFundingSources } from '../cristinApi';
 
-export const useFetchFundingSources = () => {
+interface FetchFundingSourcesOptions {
+  enabled?: boolean;
+}
+
+export const useFetchFundingSources = ({ enabled }: FetchFundingSourcesOptions = {}) => {
   const { t } = useTranslation();
 
   return useQuery({
+    enabled,
     queryKey: ['fundingSources'],
-    queryFn: fetchFundingSources,
+    queryFn: ({ signal }) => fetchFundingSources(signal),
     meta: { errorMessage: t('feedback.error.get_funding_sources') },
     staleTime: Infinity,
     gcTime: 1_800_000, // 30 minutes
