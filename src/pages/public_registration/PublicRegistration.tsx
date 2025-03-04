aadbbbbb0eb0978f91ea0441ff93e12b4cdd20d9
Isar Buzza <isar@buzza.no>
2025-03-04 17:35:54 +0100

    conditional grid columns if user is logged in or not

--- src/pages/public_registration/PublicRegistration.tsx
+++ src/pages/public_registration/PublicRegistration.tsx
@@ -1,20 +1,29 @@
 import SearchIcon from '@mui/icons-material/Search';
 import { Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router';
 import { MinimizedMenuIconButton } from '../../components/SideMenu';
+import { RootState } from '../../redux/store';
 import { PreviousPathLocationState } from '../../types/locationState.types';
 import { RegistrationLandingPage } from './RegistrationLandingPage';
-import { StyledPageWithSideMenu } from '../../components/PageWithSideMenu';
 
 const PublicRegistration = () => {
   const { t } = useTranslation();
   const location = useLocation();
   const locationState = location.state as PreviousPathLocationState;
   const previousPath = locationState?.previousPath;
+  const user = useSelector((store: RootState) => store.user);
 
   return (
-    <StyledPageWithSideMenu>
+    <Box
+      sx={{
+        p: { xs: '0.5rem', md: '1rem' },
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: { xs: '1fr', md: user ? 'auto 4fr' : '1fr 5fr' },
+        gap: '0.5rem',
+      }}>
       {previousPath && (
         <Box sx={{ alignSelf: 'start', justifySelf: 'start' }}>
           <MinimizedMenuIconButton title={t('common.search')} to={previousPath}>
@@ -25,7 +34,7 @@ const PublicRegistration = () => {
       <Box sx={{ gridColumn: { md: '2' } }}>
         <RegistrationLandingPage />
       </Box>
-    </StyledPageWithSideMenu>
+    </Box>
   );
 };
 
