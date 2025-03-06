cbd3a2fb994b2a9983c481d469770dd3e2eedbf2
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-03-06 14:03:23 +0100

    Translate some of the selected facets

--- src/pages/search/SelectedFacetsList.tsx
+++ src/pages/search/SelectedFacetsList.tsx
@@ -2,6 +2,7 @@ import ClearIcon from '@mui/icons-material/Clear';
 import { Box, Button } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useSearchParams } from 'react-router';
+import { PersonSearchParameter } from '../../api/cristinApi';
 import { ResultParam } from '../../api/searchApi';
 import { dataTestId } from '../../utils/dataTestIds';
 import { removeSearchParamValue, syncParamsWithSearchFields } from '../../utils/searchHelpers';
@@ -41,6 +42,23 @@ const SelectedFacetButton = ({ param, value }: SelectedFacet) => {
   const { t } = useTranslation();
   const [searchParams, setSearchParams] = useSearchParams();
 
+  let paramString = '';
+  let valueString = '';
+
+  switch (param) {
+    case PersonSearchParameter.Organization:
+      paramString = t('common.institution');
+      valueString = value;
+      break;
+    case PersonSearchParameter.Sector:
+      paramString = t('search.sector');
+      valueString = t(`basic_data.institutions.sector_values.${value}` as any);
+      break;
+    default:
+      valueString = typeof value === 'string' ? value : t('common.unknown');
+      break;
+  }
+
   return (
     <li style={{ listStyleType: 'none' }}>
       <Button
@@ -56,7 +74,7 @@ const SelectedFacetButton = ({ param, value }: SelectedFacet) => {
           newParams.delete(ResultParam.From);
           setSearchParams(newParams);
         }}>
-        {param}: {value}
+        {paramString}: {valueString}
       </Button>
     </li>
   );
