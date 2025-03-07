e8af44abe4c5cdf422aa884b114248929e269c33
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-03-07 09:03:32 +0100

    Restructure logic

--- src/pages/search/SelectedFacetsList.tsx
+++ src/pages/search/SelectedFacetsList.tsx
@@ -1,15 +1,7 @@
-import ClearIcon from '@mui/icons-material/Clear';
-import { Box, Button } from '@mui/material';
-import { ReactNode } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Box } from '@mui/material';
 import { useSearchParams } from 'react-router';
-import { PersonSearchParameter } from '../../api/cristinApi';
-import { ResultParam } from '../../api/searchApi';
 import { PersonAggregations } from '../../types/user.types';
-import { dataTestId } from '../../utils/dataTestIds';
-import { removeSearchParamValue, syncParamsWithSearchFields } from '../../utils/searchHelpers';
-import { getLanguageString } from '../../utils/translation-helpers';
-import { SelectedInstitutionFacetButton } from './registration_search/RegistrationSearchBar';
+import { SelectedFacetButton } from './SelectedFacetButton';
 
 interface SelectedFacet {
   param: string;
@@ -38,74 +30,10 @@ export const SelectedFacetsList = ({ facetParams, aggregations }: SelectedFacets
   }
 
   return (
-    <Box component="ul" sx={{ m: 0, p: 0, display: 'flex', gap: '0.25rem 0.5rem', flexWrap: 'wrap' }}>
+    <Box component="ul" sx={{ m: '0 0 0.5rem 0', p: 0, display: 'flex', gap: '0.25rem 0.5rem', flexWrap: 'wrap' }}>
       {selectedFacets.map(({ param, value }) => (
         <SelectedFacetButton key={`${param}-${value}`} param={param} value={value} aggregations={aggregations} />
       ))}
     </Box>
-  );
-};
-
-type SelectedFacetButtonProps = SelectedFacet & Pick<SelectedFacetsListProps, 'aggregations'>;
-
-const SelectedFacetButton = ({ param, value, aggregations }: SelectedFacetButtonProps) => {
-  const { t } = useTranslation();
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  let paramString = '';
-  let valueString: ReactNode = '';
-
-  switch (param) {
-    case PersonSearchParameter.Organization: {
-      paramString = t('common.institution');
-
-      const institutionLabels = aggregations?.organizationFacet?.find((bucket) => bucket.key === value)?.labels;
-      const institutionName = getLanguageString(institutionLabels);
-      if (institutionName) {
-        valueString = institutionName;
-      } else {
-        const institutionIdentifier = Number.isInteger(+value) ? `${value}.0.0.0` : value;
-        valueString = <SelectedInstitutionFacetButton institutionIdentifier={institutionIdentifier} />;
-      }
-      break;
-    }
-    case PersonSearchParameter.Sector:
-      paramString = t('search.sector');
-
-      const sectorLabels = aggregations?.sectorFacet?.find((bucket) => bucket.key === value)?.labels;
-      const sectorName = getLanguageString(sectorLabels);
-      if (sectorName) {
-        valueString = sectorName;
-      } else {
-        if (value === 'UC') {
-          valueString = t('basic_data.institutions.sector_values.UHI');
-        } else {
-          valueString = t(`basic_data.institutions.sector_values.${value}`, { defaultValue: value });
-        }
-      }
-      break;
-    default:
-      valueString = typeof value === 'string' ? value : t('common.unknown');
-      break;
-  }
-
-  return (
-    <li style={{ listStyleType: 'none' }}>
-      <Button
-        data-testid={dataTestId.startPage.advancedSearch.removeFacetButton}
-        variant="outlined"
-        size="small"
-        title={t('search.remove_filter')}
-        sx={{ textTransform: 'none' }}
-        endIcon={<ClearIcon />}
-        onClick={() => {
-          const syncedParams = syncParamsWithSearchFields(searchParams);
-          const newParams = removeSearchParamValue(syncedParams, param, value);
-          newParams.delete(ResultParam.From);
-          setSearchParams(newParams);
-        }}>
-        {paramString}: {valueString}
-      </Button>
-    </li>
   );
 };
