53a484d770aff62a263c3c76bfdd3e9623263521
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-03-12 16:38:51 +0100

    Get user attributes from tokens instead of cognito request

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,25 +1,8 @@
-import { fetchAuthSession, fetchUserAttributes } from 'aws-amplify/auth';
+import { AuthSession, fetchAuthSession } from 'aws-amplify/auth';
 import { CustomUserAttributes } from '../types/user.types';
 import { LocalStorageKey, USE_MOCK_DATA } from '../utils/constants';
 import { getCurrentPath } from '../utils/general-helpers';
 import { UrlPathTemplate } from '../utils/urlPaths';
-
-export const refreshSession = async () => {
-  try {
-    return await fetchAuthSession({ forceRefresh: true });
-  } catch {
-    return null;
-  }
-};
-
-export const getUserAttributes = async (): Promise<CustomUserAttributes | null> => {
-  try {
-    const userAttributes = (await fetchUserAttributes()) as CustomUserAttributes;
-    return userAttributes;
-  } catch {
-    return null;
-  }
-};
 
 export const getAccessToken = async () => {
   if (USE_MOCK_DATA) {
@@ -50,23 +33,25 @@ export const userIsAuthenticated = async () => {
 };
 
 export const getCustomUserAttributes = async () => {
-  // if (USE_MOCK_DATA) {
-  //   return '';
-  // }
   try {
     const currentSession = await fetchAuthSession();
-    const idTokenPayload = currentSession.tokens?.idToken?.payload;
-    const accessTokenPayload = currentSession.tokens?.accessToken?.payload;
-
-    if (idTokenPayload && accessTokenPayload) {
-      const customUserAttributes = Object.entries({ ...idTokenPayload, ...accessTokenPayload })
-        .filter(([key]) => key.startsWith('custom:'))
-        .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
-      return customUserAttributes as CustomUserAttributes;
-    } else {
-      return null;
-    }
+    const customTokenAttributes = getCustomTokenAttributes(currentSession.tokens);
+    return customTokenAttributes;
   } catch {
     return null;
   }
+};
+
+export const getCustomTokenAttributes = (tokens?: AuthSession['tokens']) => {
+  if (!tokens?.idToken?.payload || !tokens?.accessToken?.payload) {
+    return null;
+  }
+  const idTokenPayload = tokens?.idToken?.payload ?? {};
+  const accessTokenPayload = tokens?.accessToken?.payload ?? {};
+
+  const customAttributesObject = Object.entries({ ...idTokenPayload, ...accessTokenPayload })
+    .filter(([key]) => key.startsWith('custom:'))
+    .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {}) as CustomUserAttributes;
+
+  return customAttributesObject;
 };
