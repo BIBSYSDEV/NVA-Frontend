ff4da79f630c48a2c2d104578eb2d12ca4a57aed
Isar Buzza <isar.buzza@sikt.no>
2025-03-14 16:21:11 +0100

    Added detail to Notifier

--- src/redux/notificationSlice.ts
+++ src/redux/notificationSlice.ts
@@ -9,11 +9,11 @@ const notificationSlice = createSlice({
   initialState,
   reducers: {
     setNotification: (state, action: PayloadAction<Notification>) => {
-      const { message, variant } = action.payload;
+      const { message, variant, detail } = action.payload;
       if (typeof message !== 'string') {
         state = { message: i18n.t('feedback.error.an_error_occurred'), variant: 'error' };
       } else {
-        state = { message, variant };
+        state = { message, variant, detail };
       }
       return state;
     },
