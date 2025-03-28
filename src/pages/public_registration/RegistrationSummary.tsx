1fb49ff1836c10cd48c2e8669f2d2930d4261fb5
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-03-28 08:37:43 +0100

    Allow going back after opening result for published in link

--- src/pages/public_registration/RegistrationSummary.tsx
+++ src/pages/public_registration/RegistrationSummary.tsx
@@ -1,6 +1,7 @@
 import { Link, Skeleton } from '@mui/material';
 import { Link as RouterLink } from 'react-router';
 import { useFetchRegistration } from '../../api/hooks/useFetchRegistration';
+import { PreviousPathLocationState } from '../../types/locationState.types';
 import { getIdentifierFromId } from '../../utils/general-helpers';
 import { getTitleString } from '../../utils/registration-helpers';
 import { getRegistrationLandingPagePath } from '../../utils/urlPaths';
@@ -18,7 +19,10 @@ export const RegistrationSummary = ({ id }: RegistrationSummaryProps) => {
   return containerQuery.isPending ? (
     <Skeleton width={400} />
   ) : container ? (
-    <Link component={RouterLink} to={getRegistrationLandingPagePath(identifier)}>
+    <Link
+      component={RouterLink}
+      state={{ previousPath: `${location.pathname}${location.search}` } satisfies PreviousPathLocationState}
+      to={getRegistrationLandingPagePath(identifier)}>
       {getTitleString(container.entityDescription?.mainTitle)}
     </Link>
   ) : null;
