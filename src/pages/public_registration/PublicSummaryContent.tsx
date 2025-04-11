64c3fb3ec34f01182cc6a5dab1902401ad32c154
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-04-11 13:06:15 +0200

    Show language for abstract

--- src/pages/public_registration/PublicSummaryContent.tsx
+++ src/pages/public_registration/PublicSummaryContent.tsx
@@ -1,9 +1,10 @@
 import { Typography } from '@mui/material';
+import { getLanguageByIso6391Code, getLanguageByIso6392Code, getLanguageByIso6393Code } from 'nva-language';
 import { useTranslation } from 'react-i18next';
 import { PublicRegistrationContentProps } from './PublicRegistrationContent';
 
 export const PublicSummaryContent = ({ registration }: PublicRegistrationContentProps) => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const { entityDescription } = registration;
 
@@ -15,16 +16,36 @@ export const PublicSummaryContent = ({ registration }: PublicRegistrationContent
         </Typography>
       )}
 
-      {Object.entries(entityDescription.alternativeAbstracts).map(([langKey, abstract]) => (
-        <>
-          <Typography variant="h3" color="primary" gutterBottom>
-            {t('registration.description.alternative_abstract')} ({langKey})
-          </Typography>
-          <Typography style={{ whiteSpace: 'pre-line', overflowWrap: 'anywhere' }} sx={{ mb: '1rem' }}>
-            {abstract}
-          </Typography>
-        </>
-      ))}
+      {Object.entries(entityDescription.alternativeAbstracts).map(([languageKey, abstract]) => {
+        const languageObject =
+          languageKey === 'und'
+            ? null
+            : getLanguageByIso6391Code(languageKey) ||
+              getLanguageByIso6392Code(languageKey) ||
+              getLanguageByIso6393Code(languageKey);
+
+        const translatedLanguage =
+          i18n.language === 'nob'
+            ? languageObject?.nob.toLowerCase()
+            : i18n.language === 'nno'
+              ? languageObject?.nno.toLowerCase()
+              : languageObject?.eng;
+
+        const heading = translatedLanguage
+          ? `${t('registration.description.alternative_abstract')} (${translatedLanguage})`
+          : t('registration.description.alternative_abstract');
+
+        return (
+          <>
+            <Typography variant="h3" color="primary" gutterBottom>
+              {heading}
+            </Typography>
+            <Typography style={{ whiteSpace: 'pre-line', overflowWrap: 'anywhere' }} sx={{ mb: '1rem' }}>
+              {abstract}
+            </Typography>
+          </>
+        );
+      })}
 
       {entityDescription.description && (
         <>
