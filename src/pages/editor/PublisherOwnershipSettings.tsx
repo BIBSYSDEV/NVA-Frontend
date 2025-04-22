8b3735668a5f055031dded0c9f3f75f56eda63cd
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-04-22 15:11:26 +0200

    Add helper text to publisher ownership

--- src/pages/editor/PublisherOwnershipSettings.tsx
+++ src/pages/editor/PublisherOwnershipSettings.tsx
@@ -1,5 +1,6 @@
-import { Typography } from '@mui/material';
-import { useTranslation } from 'react-i18next';
+import AddIcon from '@mui/icons-material/Add';
+import { Button, Typography } from '@mui/material';
+import { Trans, useTranslation } from 'react-i18next';
 
 export const PublisherOwnershipSettings = () => {
   const { t } = useTranslation();
@@ -9,6 +10,18 @@ export const PublisherOwnershipSettings = () => {
       <Typography variant="h1" gutterBottom>
         {t('editor.institution.administer_publisher_channel_ownership')}
       </Typography>
+      <Trans
+        i18nKey="editor.institution.administer_publisher_channel_ownership_description"
+        components={{
+          p: <Typography gutterBottom />,
+          addOwnershipButton: (
+            <Button variant="outlined" startIcon={<AddIcon />} sx={{ my: '0.5rem', textTransform: 'none' }}>
+              {t('editor.institution.add_publisher_channel_ownership')}
+            </Button>
+          ),
+        }}>
+        <Typography />
+      </Trans>
     </>
   );
 };
