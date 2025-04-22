ad134751652b86022815cc40cbad27ca364792d7
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-04-22 16:16:29 +0200

    Add dialog component

--- src/pages/editor/PublisherOwnershipSettings.tsx
+++ src/pages/editor/PublisherOwnershipSettings.tsx
@@ -1,10 +1,15 @@
 import AddIcon from '@mui/icons-material/Add';
 import { Button, Typography } from '@mui/material';
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Trans, useTranslation } from 'react-i18next';
+import { AddChannelOwnershipDialog } from './AddChannelOwnershipDialog';
 
 export const PublisherOwnershipSettings = () => {
   const { t } = useTranslation();
+
+  const [openAddOwnershipDialog, setOpenAddOwnershipDialog] = useState(false);
+  const toggleAddOwnershipDialog = () => setOpenAddOwnershipDialog(!openAddOwnershipDialog);
 
   return (
     <>
@@ -19,13 +24,22 @@ export const PublisherOwnershipSettings = () => {
         components={{
           p: <Typography gutterBottom />,
           addOwnershipButton: (
-            <Button variant="outlined" startIcon={<AddIcon />} sx={{ my: '0.5rem', textTransform: 'none' }}>
+            <Button
+              variant="outlined"
+              startIcon={<AddIcon />}
+              sx={{ my: '0.5rem', textTransform: 'none' }}
+              onClick={toggleAddOwnershipDialog}>
               {t('editor.institution.add_publisher_channel_ownership')}
             </Button>
           ),
         }}>
         <Typography />
       </Trans>
+      <AddChannelOwnershipDialog
+        channelType="Publisher"
+        open={openAddOwnershipDialog}
+        closeDialog={toggleAddOwnershipDialog}
+      />
     </>
   );
 };
