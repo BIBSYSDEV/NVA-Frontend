748eed9ea9dbf53586298d9271e5fcd00496d3b0
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-04-23 11:17:49 +0200

    Allow claiming publisher

--- src/pages/editor/AddChannelOwnershipDialog.tsx
+++ src/pages/editor/AddChannelOwnershipDialog.tsx
@@ -1,6 +1,12 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogProps, DialogTitle, Typography } from '@mui/material';
+import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { setChannelClaim } from '../../api/customerInstitutionsApi';
+import { setNotification } from '../../redux/notificationSlice';
+import { RootState } from '../../redux/store';
+import { DegreeType } from '../../types/publicationFieldNames';
 import { Publisher } from '../../types/registration.types';
 import { SearchForPublisherFacetItem } from '../search/facet_search_fields/SearchForPublisherFacetItem';
 
@@ -11,16 +17,51 @@ interface AddChannelOwnershipDialogProps extends Pick<DialogProps, 'open'> {
 
 export const AddChannelOwnershipDialog = ({ channelType, open, closeDialog }: AddChannelOwnershipDialogProps) => {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+
+  const customer = useSelector((state: RootState) => state.customer);
   const [selectedChannel, setSelectedChannel] = useState<Publisher>();
+
+  const addChannelClaim = useMutation({
+    mutationFn: (publisher: Publisher) => {
+      if (customer?.id && publisher.id) {
+        const channelIdWithoutYear = publisher.id.replace(/\/\d{4}$/, '');
+        return setChannelClaim(customer.id, {
+          channel: channelIdWithoutYear,
+          constraints: {
+            scope: Object.values(DegreeType),
+            publishingPolicy: 'OwnerOnly',
+            editingPolicy: 'OwnerOnly',
+          },
+        });
+      }
+      return Promise.reject(new Error('Customer ID or Publisher ID is missing'));
+    },
+    onSuccess: () => {
+      dispatch(setNotification({ message: 'Kanaleierskap ble oppdatert', variant: 'success' }));
+      closeDialog();
+    },
+    onError: (error) => {
+      dispatch(
+        setNotification({
+          message: 'Kunne ikke sette kanaleierskap',
+          variant: 'error',
+          detail: (error as any).response?.data?.detail,
+        })
+      );
+    },
+  });
 
   return (
     <Dialog open={open} onClose={closeDialog}>
       <DialogTitle>{t('editor.institution.add_publisher_channel_ownership')}</DialogTitle>
       <form
-        onSubmit={(event) => {
+        onSubmit={async (event) => {
           event.preventDefault();
-          console.log(event.currentTarget);
           console.log('submit', selectedChannel);
+          if (selectedChannel) {
+            await addChannelClaim.mutateAsync(selectedChannel);
+          }
           closeDialog();
         }}>
         <DialogContent>
@@ -30,7 +71,6 @@ export const AddChannelOwnershipDialog = ({ channelType, open, closeDialog }: Ad
           />
           <SearchForPublisherFacetItem
             onSelectPublisher={(publisher) => {
-              console.log('velger', publisher);
               setSelectedChannel(publisher);
             }}
             textFieldProps={{ variant: 'filled', label: t('common.publisher'), required: true }}
