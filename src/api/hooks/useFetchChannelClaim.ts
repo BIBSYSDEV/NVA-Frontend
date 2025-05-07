acd207ab52568cd99f09591b340a384c8700bc6e
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-05-07 11:13:48 +0200

    Minor cleanup

--- src/api/hooks/useFetchChannelClaim.ts
+++ src/api/hooks/useFetchChannelClaim.ts
@@ -1,8 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
+import { useTranslation } from 'react-i18next';
 import { getIdentifierFromId } from '../../utils/general-helpers';
 import { fetchChannelClaim } from '../customerInstitutionsApi';
 
 export const useFetchChannelClaim = (id = '') => {
+  const { t } = useTranslation();
   const channelIdWithoutYear = id.replace(/\/\d{4}$/, ''); // TODO: move this and similar at AddChannelClaimDialog to helper function
   const channelIdentifier = getIdentifierFromId(channelIdWithoutYear);
 
@@ -10,6 +12,6 @@ export const useFetchChannelClaim = (id = '') => {
     queryKey: ['channelClaim', channelIdentifier],
     enabled: !!channelIdentifier,
     queryFn: ({ signal }) => fetchChannelClaim(channelIdentifier, signal),
-    meta: { errorMessage: 'Failed to fetch channel claim' },
+    meta: { errorMessage: t('feedback.error.get_channel_claim') },
   });
 };
