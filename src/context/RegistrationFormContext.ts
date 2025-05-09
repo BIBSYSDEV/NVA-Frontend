ad4b48c3bc73b3a86d8343999aad63830a79491f
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-05-09 08:57:43 +0200

    Add properties to form context

--- src/context/RegistrationFormContext.ts
+++ src/context/RegistrationFormContext.ts
@@ -1,8 +1,13 @@
 import { createContext } from 'react';
+import { ClaimedChannel } from '../types/customerInstitution.types';
 
 interface RegistrationFormContextType {
   disableNviCriticalFields: boolean;
   disableChannelClaimsFields: boolean;
+  publisherClaim?: ClaimedChannel;
+  // setPublisherClaim?: (claim: ClaimedChannel) => void;
+  seriesClaim?: ClaimedChannel;
+  journalClaim?: ClaimedChannel;
 }
 
 export const RegistrationFormContext = createContext<RegistrationFormContextType>({
