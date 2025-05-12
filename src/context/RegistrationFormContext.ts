8c26e23627ddd5e1744737036f94b8da5f065b95
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-05-12 09:29:04 +0200

    Simplify solution

--- src/context/RegistrationFormContext.ts
+++ src/context/RegistrationFormContext.ts
@@ -1,13 +1,8 @@
 import { createContext } from 'react';
-import { ClaimedChannel } from '../types/customerInstitution.types';
 
 interface RegistrationFormContextType {
   disableNviCriticalFields: boolean;
   disableChannelClaimsFields: boolean;
-  publisherClaim?: ClaimedChannel;
-  // setPublisherClaim?: (claim: ClaimedChannel) => void;
-  seriesClaim?: ClaimedChannel;
-  journalClaim?: ClaimedChannel;
 }
 
 export const RegistrationFormContext = createContext<RegistrationFormContextType>({
