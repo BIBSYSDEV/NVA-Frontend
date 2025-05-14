d64136a09b96f955b7afb5544f7025b9d13880d6
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-05-14 17:08:18 +0200

    Use hook to handle search params

--- src/pages/messages/components/ExcludeSubunitsCheckbox.tsx
+++ src/pages/messages/components/ExcludeSubunitsCheckbox.tsx
@@ -1,7 +1,8 @@
 import { Checkbox, FormControlLabel } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { useLocation, useNavigate } from 'react-router';
+import { useSearchParams } from 'react-router';
 import { dataTestId } from '../../../utils/dataTestIds';
+import { syncParamsWithSearchFields } from '../../../utils/searchHelpers';
 
 interface ExcludeSubunitsCheckboxProps {
   paramName: string;
@@ -11,26 +12,26 @@ interface ExcludeSubunitsCheckboxProps {
 
 export const ExcludeSubunitsCheckbox = ({ paramName, paginationParamName, disabled }: ExcludeSubunitsCheckboxProps) => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const isSelected = searchParams.get(paramName) === 'true';
+  const [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <FormControlLabel
       data-testid={dataTestId.tasksPage.nvi.excludeSubunitsCheckbox}
       onChange={(_, checked) => {
-        if (checked) {
-          searchParams.set(paramName, checked.toString());
-        } else {
-          searchParams.delete(paramName);
-        }
-        searchParams.delete(paginationParamName);
-        navigate({ search: searchParams.toString() });
+        setSearchParams((prev) => {
+          const syncedParams = syncParamsWithSearchFields(prev);
+          if (checked) {
+            syncedParams.set(paramName, checked.toString());
+          } else {
+            syncedParams.delete(paramName);
+          }
+          searchParams.delete(paginationParamName);
+          return syncedParams;
+        });
       }}
       disabled={disabled}
       label={t('tasks.nvi.exclude_subunits')}
-      control={<Checkbox checked={isSelected} />}
+      control={<Checkbox checked={searchParams.get(paramName) === 'true'} />}
     />
   );
 };
