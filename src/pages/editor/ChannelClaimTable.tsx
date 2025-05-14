435fb591cc0033d91f2b1a1c602ac217465f5738
Isar Buzza <isar.buzza@sikt.no>
2025-05-14 17:33:53 +0200

    NP-49126 Add helper text to channel claim table

--- src/pages/editor/ChannelClaimTable.tsx
+++ src/pages/editor/ChannelClaimTable.tsx
@@ -1,15 +1,21 @@
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
 import { useContext } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router';
 import { ChannelClaimContext } from '../../context/ChannelClaimContext';
 import { ClaimedChannel } from '../../types/customerInstitution.types';
 import { UrlPathTemplate } from '../../utils/urlPaths';
+import { HelperTextModal } from '../registration/HelperTextModal';
 import { ChannelClaimTableRow } from './ChannelClaimTableRow';
 
 interface ChannelClaimTableProps {
   channelClaimList: ClaimedChannel[];
 }
+
+const helperTextModalComponents = {
+  p: <Typography />,
+  heading: <Typography variant="h2" sx={{ mt: '1rem' }} />,
+};
 
 export const ChannelClaimTable = ({ channelClaimList }: ChannelClaimTableProps) => {
   const { t } = useTranslation();
@@ -22,10 +28,42 @@ export const ChannelClaimTable = ({ channelClaimList }: ChannelClaimTableProps) 
       <TableHead>
         <TableRow sx={{ bgcolor: 'secondary.main' }}>
           <TableCell>{channelType === 'publisher' ? t('common.publisher') : t('common.serial_publication')}</TableCell>
-          <TableCell>{t('editor.institution.channel_claims.channel_owner')}</TableCell>
-          <TableCell>{t('editor.institution.channel_claims.publishing_access')}</TableCell>
-          <TableCell>{t('editor.institution.channel_claims.editing_access')}</TableCell>
-          <TableCell>{t('editor.institution.channel_claims.category_limitations')}</TableCell>
+          <TableCell>
+            {t('editor.institution.channel_claims.channel_owner')}
+            <HelperTextModal modalTitle={t('editor.institution.channel_claims.channel_owner')}>
+              <Trans
+                i18nKey="editor.institution.channel_claims.channel_owner_helper_text"
+                components={helperTextModalComponents}
+              />
+            </HelperTextModal>
+          </TableCell>
+          <TableCell sx={{ textWrap: 'nowrap' }}>
+            {t('editor.institution.channel_claims.register_access')}
+            <HelperTextModal modalTitle={t('editor.institution.channel_claims.register_access')}>
+              <Trans
+                i18nKey="editor.institution.channel_claims.register_access_helper_text"
+                components={helperTextModalComponents}
+              />
+            </HelperTextModal>
+          </TableCell>
+          <TableCell sx={{ textWrap: 'nowrap' }}>
+            {t('editor.institution.channel_claims.editing_access')}
+            <HelperTextModal modalTitle={t('editor.institution.channel_claims.editing_access')}>
+              <Trans
+                i18nKey="editor.institution.channel_claims.editing_access_helper_text"
+                components={helperTextModalComponents}
+              />
+            </HelperTextModal>
+          </TableCell>
+          <TableCell sx={{ textWrap: 'nowrap' }}>
+            {t('editor.institution.channel_claims.category_limitations')}
+            <HelperTextModal modalTitle={t('editor.institution.channel_claims.category_limitations')}>
+              <Trans
+                i18nKey="editor.institution.channel_claims.category_limitations_helper_text"
+                components={helperTextModalComponents}
+              />
+            </HelperTextModal>
+          </TableCell>
           {isOnSettingsPage && <TableCell>{t('common.actions')}</TableCell>}
         </TableRow>
       </TableHead>
