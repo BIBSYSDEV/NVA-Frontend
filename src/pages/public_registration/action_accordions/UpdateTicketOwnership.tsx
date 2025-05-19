e22464d6c75fc45da23ccb46b1017f5869f83f1a
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-05-19 08:21:25 +0200

    Render name of org option WIP

--- src/pages/public_registration/action_accordions/UpdateTicketOwnership.tsx
+++ src/pages/public_registration/action_accordions/UpdateTicketOwnership.tsx
@@ -1,8 +1,20 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material';
-import { useState } from 'react';
+import {
+  Autocomplete,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Skeleton,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { HTMLAttributes, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
+import { useFetchOrganization } from '../../../api/hooks/useFetchOrganization';
 import { Ticket } from '../../../types/publication_types/ticket.types';
 import { dataTestId } from '../../../utils/dataTestIds';
+import { getLanguageString } from '../../../utils/translation-helpers';
 
 interface UpdateTicketOwnershipProps {
   ticket: Ticket;
@@ -13,6 +25,8 @@ export const UpdateTicketOwnership = ({ ticket }: UpdateTicketOwnershipProps) =>
 
   const [showUpdateOwnershipDialog, setShowUpdateOwnershipDialog] = useState(false);
   const toggleUpdateOwnershipDialog = () => setShowUpdateOwnershipDialog(!showUpdateOwnershipDialog);
+
+  const allowedInstitutions = ticket.availableInstitutions ?? [];
 
   return (
     <section>
@@ -35,6 +49,31 @@ export const UpdateTicketOwnership = ({ ticket }: UpdateTicketOwnershipProps) =>
         <DialogTitle>{t('registration.public_page.tasks_panel.move_task')}</DialogTitle>
         <DialogContent>
           <Typography gutterBottom>{t('registration.public_page.tasks_panel.move_task_dialog_description')}</Typography>
+
+          <Autocomplete
+            options={allowedInstitutions}
+            getOptionLabel={(option) => option}
+            // onChange={(_, value) => setSelectedCustomer(value)}
+            // isOptionEqualToValue={(option, value) => option.id === value.id}
+            // renderOption={({ key, ...props }, option) => {
+            //   const organization = organizations && organizations.find((org) => org.id === option.cristinId);
+            //   return organization ? (
+            //     <OrganizationRenderOption key={organization.id} props={props} option={organization} />
+            //   ) : (
+            //     <li {...props} key={option.id}>
+            //       <Typography fontWeight="bold">{option.displayName}</Typography>
+            //     </li>
+            //   );
+            // }}
+            renderOption={(props, option) => <OrganizationRenderOption key={option} props={props} option={option} />}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                placeholder={t('project.search_for_institution')}
+                slotProps={{ inputLabel: { 'aria-label': t('common.select_institution') } }}
+              />
+            )}
+          />
         </DialogContent>
         <DialogActions>
           <Button data-testid={dataTestId.common.cancel} onClick={toggleUpdateOwnershipDialog}>
@@ -51,5 +90,24 @@ export const UpdateTicketOwnership = ({ ticket }: UpdateTicketOwnershipProps) =>
         </DialogActions>
       </Dialog>
     </section>
+  );
+};
+
+interface OrganizationRenderOptionProps {
+  props: HTMLAttributes<HTMLLIElement>;
+  option: string;
+}
+
+const OrganizationRenderOption = ({ props, option }: OrganizationRenderOptionProps) => {
+  const organizationQuery = useFetchOrganization(option);
+
+  return (
+    <li {...props}>
+      {organizationQuery.isPending ? (
+        <Skeleton sx={{ width: '15rem' }} />
+      ) : (
+        getLanguageString(organizationQuery.data?.labels)
+      )}
+    </li>
   );
 };
