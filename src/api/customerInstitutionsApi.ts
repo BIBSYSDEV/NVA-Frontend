7c593e7f70440c90894c12e812734024646cfcd3
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-05-23 09:47:09 +0200

    Fix loading state of channel claim

--- src/api/customerInstitutionsApi.ts
+++ src/api/customerInstitutionsApi.ts
@@ -87,6 +87,8 @@ export const fetchClaimedChannels = async (channelType: 'publisher' | 'serial-pu
 };
 
 export const fetchChannelClaim = async (channelIdentifier: string, signal: AbortSignal) => {
+  await new Promise((resolve) => setTimeout(resolve, 5_000)); // Simulate async operation
+
   const getChannelClaim = await authenticatedApiRequest2<ClaimedChannel>({
     url: `${CustomerInstitutionApiPath.ChannelClaim}/${channelIdentifier}`,
     signal,
