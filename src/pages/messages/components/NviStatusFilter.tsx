d2e4eef2c7c7788f71430b320c1da33fb69e8aa3
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-07-01 11:21:27 +0200

    Support all filters

--- src/pages/messages/components/NviStatusFilter.tsx
+++ src/pages/messages/components/NviStatusFilter.tsx
@@ -1,8 +1,12 @@
 import { MenuItem, TextField } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useSearchParams } from 'react-router';
-import { NviCandidateFilter, NviCandidateGlobalStatus, NviCandidatesSearchParam } from '../../../api/searchApi';
-import { NviCandidateSearchStatus } from '../../../types/nvi.types';
+import {
+  NviCandidateFilter,
+  NviCandidateGlobalStatus,
+  NviCandidatesSearchParam,
+  NviCandidateStatus,
+} from '../../../api/searchApi';
 import { dataTestId } from '../../../utils/dataTestIds';
 import { useNviCandidatesParams } from '../../../utils/hooks/useNviCandidatesParams';
 import { syncParamsWithSearchFields } from '../../../utils/searchHelpers';
@@ -11,18 +15,7 @@ export const NviStatusFilter = () => {
   const { t } = useTranslation();
 
   const [, setSearchParams] = useSearchParams();
-  const { status } = useNviCandidatesParams();
-
-  const toggleValueFromSearchParams = (key: NviCandidatesSearchParam, value: string) => {
-    setSearchParams((params) => {
-      const syncedParams = syncParamsWithSearchFields(params);
-      syncedParams.delete(NviCandidatesSearchParam.Filter);
-      syncedParams.delete(NviCandidatesSearchParam.GlobalStatus);
-      syncedParams.delete(NviCandidatesSearchParam.Offset);
-      syncedParams.set(NviCandidatesSearchParam.Status, value);
-      return syncedParams;
-    });
-  };
+  const { status, globalStatus } = useNviCandidatesParams();
 
   return (
     <TextField
@@ -31,14 +24,29 @@ export const NviStatusFilter = () => {
       select
       size="small"
       label={t('common.status')}
-      value={status ?? ''}
+      value={status || globalStatus || ''}
       onChange={(event) => {
-        const newStatus = event.target.value;
-        toggleValueFromSearchParams(NviCandidatesSearchParam.Status, newStatus);
+        const newStatus = event.target.value as NviCandidateStatus | NviCandidateGlobalStatus;
+
+        setSearchParams((params) => {
+          const syncedParams = syncParamsWithSearchFields(params);
+          syncedParams.delete(NviCandidatesSearchParam.Filter);
+          syncedParams.delete(NviCandidatesSearchParam.GlobalStatus);
+          syncedParams.delete(NviCandidatesSearchParam.Offset);
+
+          if (newStatus === ('disputed' satisfies NviCandidateGlobalStatus)) {
+            syncedParams.delete(NviCandidatesSearchParam.Status);
+            syncedParams.set(NviCandidatesSearchParam.GlobalStatus, newStatus);
+          } else {
+            syncedParams.set(NviCandidatesSearchParam.Status, newStatus);
+          }
+
+          return syncedParams;
+        });
       }}>
-      <MenuItem value={'pending' satisfies NviCandidateSearchStatus}>{t('tasks.nvi.status.New')}</MenuItem>
-      <MenuItem value={'approved' satisfies NviCandidateSearchStatus}>{t('tasks.nvi.status.Approved')}</MenuItem>
-      <MenuItem value={'rejected' satisfies NviCandidateSearchStatus}>{t('tasks.nvi.status.Rejected')}</MenuItem>
+      <MenuItem value={'pending' satisfies NviCandidateStatus}>{t('tasks.nvi.status.New')}</MenuItem>
+      <MenuItem value={'approved' satisfies NviCandidateStatus}>{t('tasks.nvi.status.Approved')}</MenuItem>
+      <MenuItem value={'rejected' satisfies NviCandidateStatus}>{t('tasks.nvi.status.Rejected')}</MenuItem>
       <MenuItem value={'disputed' satisfies NviCandidateGlobalStatus}>{t('tasks.nvi.status.Dispute')}</MenuItem>
     </TextField>
   );
@@ -50,6 +58,8 @@ export const NviAvailabilityFilter = () => {
   const [, setSearchParams] = useSearchParams();
   const { filter, globalStatus, status } = useNviCandidatesParams();
 
+  const value = status ? filter || globalStatus || '' : filter || '';
+
   return (
     <TextField
       fullWidth
@@ -58,13 +68,15 @@ export const NviAvailabilityFilter = () => {
       slotProps={{ select: { displayEmpty: true }, inputLabel: { shrink: true } }}
       size="small"
       label={t('tasks.display_options')}
-      value={filter || globalStatus || ''}
+      value={value}
       onChange={(event) => {
         const newFilter = event.target.value;
 
         setSearchParams((prevParams) => {
           const syncedParams = syncParamsWithSearchFields(prevParams);
-          syncedParams.delete(NviCandidatesSearchParam.GlobalStatus);
+          if (status) {
+            syncedParams.delete(NviCandidatesSearchParam.GlobalStatus);
+          }
           syncedParams.delete(NviCandidatesSearchParam.Filter);
           syncedParams.delete(NviCandidatesSearchParam.Offset);
           if (
@@ -81,19 +93,30 @@ export const NviAvailabilityFilter = () => {
         });
       }}>
       <MenuItem value="">{t('common.show_all')}</MenuItem>
+
       {status === 'pending' && (
         <MenuItem value={'collaboration' satisfies NviCandidateFilter}>Vis kun sampublikasjoner</MenuItem>
       )}
+
       {status === 'approved' && (
         <MenuItem value={'approvedByOthers' satisfies NviCandidateFilter}>Kandidater alle har godkjent</MenuItem>
       )}
       {status === 'approved' && (
         <MenuItem value={'pending' satisfies NviCandidateGlobalStatus}>Kandidater andre må godkjenne</MenuItem>
       )}
+
       {status === 'rejected' && (
-        <MenuItem value={'rejectedCollaboration' satisfies NviCandidateSearchStatus}>
-          {t('tasks.nvi.waiting_for_other_institutions')}
-        </MenuItem>
+        <MenuItem value={'rejectedByOthers' satisfies NviCandidateFilter}>Kandidater alle har avvist</MenuItem>
+      )}
+      {status === 'rejected' && (
+        <MenuItem value={'pending' satisfies NviCandidateGlobalStatus}>Kandidater andre må kontrollere</MenuItem>
+      )}
+
+      {globalStatus === 'disputed' && (
+        <MenuItem value={'approvedByOthers' satisfies NviCandidateFilter}>Kandidater andre har godkjent</MenuItem>
+      )}
+      {globalStatus === 'disputed' && (
+        <MenuItem value={'rejectedByOthers' satisfies NviCandidateFilter}>Kandidater andre har avvist</MenuItem>
       )}
     </TextField>
   );
