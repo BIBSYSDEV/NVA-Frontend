4ac71ba1aed9348f46a21fdaaa69bfefbcaf44af
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-07-08 14:12:09 +0200

    Update ts eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 import tanstackQuery from '@tanstack/eslint-plugin-query';
-import eslintPluginTypeScript from '@typescript-eslint/eslint-plugin';
 import eslintParserTypeScript from '@typescript-eslint/parser';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
 
 export default [
   {
@@ -14,25 +14,25 @@ export default [
         project: './tsconfig.json',
       },
     },
-    plugins: {
-      '@typescript-eslint': eslintPluginTypeScript,
-    },
     settings: {
       react: {
         version: 'detect',
       },
     },
+  },
+  ...tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  reactHooks.configs['recommended-latest'],
+  ...tanstackQuery.configs['flat/recommended'],
+  jsxA11y.flatConfigs.recommended,
+
+  {
     rules: {
-      ...eslintPluginTypeScript.configs.recommended.rules,
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
       'no-console': 'warn',
       'no-debugger': 'warn',
     },
   },
-  react.configs.flat.recommended,
-  react.configs.flat['jsx-runtime'],
-  reactHooks.configs['recommended-latest'],
-  ...tanstackQuery.configs['flat/recommended'],
-  jsxA11y.flatConfigs.recommended,
 ];
