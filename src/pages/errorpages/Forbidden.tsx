ba4b4b25031513d7af24b9708bc381acd86e4be6
Isar Buzza <isar@buzza.no>
2025-07-08 15:17:32 +0200

    Updated text and added buttons

--- src/pages/errorpages/Forbidden.tsx
+++ src/pages/errorpages/Forbidden.tsx
@@ -1,20 +1,58 @@
-import { Box, Link as MuiLink, Typography } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router';
-import { UrlPathTemplate } from '../../utils/urlPaths';
+import LockOutlineIcon from '@mui/icons-material/LockOutline';
+import { Box, Button, Typography } from '@mui/material';
+import { Trans, useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router';
+import { LocalStorageKey } from '../../utils/constants';
+import { dataTestId } from '../../utils/dataTestIds';
+import { useAuthentication } from '../../utils/hooks/useAuthentication';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import LoginIcon from '@mui/icons-material/Login';
 
 export const Forbidden = () => {
   const { t } = useTranslation();
+  const { handleLogin } = useAuthentication();
+  const navigate = useNavigate();
 
   return (
     <Box data-testid="forbidden" sx={{ mt: '4rem' }}>
-      <Typography variant="h2" component="h1" sx={{ mb: '1rem' }}>
-        {t('authorization.forbidden')}
-      </Typography>
-      <Typography sx={{ mb: '1rem' }}>{t('authorization.forbidden_description')}</Typography>
-      <MuiLink component={Link} to={UrlPathTemplate.Root}>
-        <Typography>{t('authorization.back_to_home')}</Typography>
-      </MuiLink>
+      <span style={{ display: 'flex', gap: '0.2rem' }}>
+        <LockOutlineIcon />
+        <Typography variant="h2" component="h1" sx={{ mb: '1rem' }}>
+          {t('authorization.forbidden')}
+        </Typography>
+      </span>
+      <Trans
+        i18nKey="authorization.forbidden_description"
+        components={{
+          p: <Typography />,
+        }}
+      />
+
+      <Box sx={{ display: 'flex', gap: '1rem', mt: '1rem' }}>
+        <Button
+          sx={{ textTransform: 'none' }}
+          data-testid={dataTestId.common.cancel}
+          startIcon={<ArrowBackIcon />}
+          variant="outlined"
+          onClick={() => navigate(-1)}>
+          {t('common.go_back')}
+        </Button>
+
+        <Button
+          sx={{ textTransform: 'none' }}
+          endIcon={<LoginIcon />}
+          variant="contained"
+          data-testid={dataTestId.header.logInButton}
+          onClick={() => {
+            const redirectPath = new URLSearchParams(location.search).get(LocalStorageKey.RedirectPath);
+            if (redirectPath) {
+              localStorage.setItem(LocalStorageKey.RedirectPath, redirectPath);
+            }
+            handleLogin();
+          }}>
+          {t('authorization.login')}
+        </Button>
+      </Box>
     </Box>
   );
 };
