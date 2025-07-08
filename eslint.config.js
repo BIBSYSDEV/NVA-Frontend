c6ace0b92899518c99ab6b3d5fb724b072bd948d
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-07-08 12:44:37 +0200

    Update a11y eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import tanstackQuery from '@tanstack/eslint-plugin-query';
 import eslintPluginTypeScript from '@typescript-eslint/eslint-plugin';
 import eslintParserTypeScript from '@typescript-eslint/parser';
-import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 
@@ -16,7 +16,6 @@ export default [
     },
     plugins: {
       '@typescript-eslint': eslintPluginTypeScript,
-      'jsx-a11y': eslintPluginJsxA11y,
     },
     settings: {
       react: {
@@ -25,7 +24,6 @@ export default [
     },
     rules: {
       ...eslintPluginTypeScript.configs.recommended.rules,
-      ...eslintPluginJsxA11y.configs.recommended.rules,
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
       'no-console': 'warn',
@@ -36,4 +34,5 @@ export default [
   react.configs.flat['jsx-runtime'],
   reactHooks.configs['recommended-latest'],
   ...tanstackQuery.configs['flat/recommended'],
+  jsxA11y.flatConfigs.recommended,
 ];
