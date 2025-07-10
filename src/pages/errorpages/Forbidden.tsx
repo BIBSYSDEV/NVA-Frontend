62df1d9e5581878d61aa360234ce6b75e0133e5c
Isar Buzza <isar.buzza@sikt.no>
2025-07-10 11:16:07 +0200

    removed go back button

--- src/pages/errorpages/Forbidden.tsx
+++ src/pages/errorpages/Forbidden.tsx
@@ -1,10 +1,10 @@
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import LockOutlineIcon from '@mui/icons-material/LockOutline';
 import LoginIcon from '@mui/icons-material/Login';
 import { Box, Button, Typography } from '@mui/material';
 import { Trans, useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router';
+import { Link } from 'react-router';
 import { RootState } from '../../redux/store';
 import { LocalStorageKey } from '../../utils/constants';
 import { dataTestId } from '../../utils/dataTestIds';
@@ -14,7 +14,6 @@ import { UrlPathTemplate } from '../../utils/urlPaths';
 export const Forbidden = () => {
   const { t } = useTranslation();
   const { handleLogin } = useAuthentication();
-  const navigate = useNavigate();
   const user = useSelector((store: RootState) => store.user);
 
   return (
@@ -36,42 +35,32 @@ export const Forbidden = () => {
         <Typography>{t('authorization.forbidden_description_logged_in')}</Typography>
       )}
 
-      <Box sx={{ display: 'flex', gap: '1rem', mt: '1rem' }}>
+      {user ? (
         <Button
-          sx={{ textTransform: 'none' }}
-          data-testid={dataTestId.common.cancel}
-          startIcon={<ArrowBackIcon />}
-          variant="outlined"
-          onClick={() => navigate(-1)}>
-          {t('common.go_back')}
+          sx={{ textTransform: 'none', mt: '0.5rem' }}
+          endIcon={<LoginIcon />}
+          variant="contained"
+          data-testid={`${dataTestId.header.logInButton}-forbidden`}
+          onClick={() => {
+            const redirectPath = new URLSearchParams(location.search).get(LocalStorageKey.RedirectPath);
+            if (redirectPath) {
+              localStorage.setItem(LocalStorageKey.RedirectPath, redirectPath);
+            }
+            handleLogin();
+          }}>
+          {t('authorization.login')}
         </Button>
-
-        {!user ? (
-          <Button
-            sx={{ textTransform: 'none' }}
-            endIcon={<LoginIcon />}
-            variant="contained"
-            data-testid={`${dataTestId.header.logInButton}-forbidden`}
-            onClick={() => {
-              const redirectPath = new URLSearchParams(location.search).get(LocalStorageKey.RedirectPath);
-              if (redirectPath) {
-                localStorage.setItem(LocalStorageKey.RedirectPath, redirectPath);
-              }
-              handleLogin();
-            }}>
-            {t('authorization.login')}
-          </Button>
-        ) : (
-          <Button
-            data-testid={dataTestId.authorization.institutionAdminsLink}
-            component={Link}
-            to={UrlPathTemplate.InstitutionOverviewPage}
-            sx={{ textTransform: 'none' }}
-            variant="contained">
-            {t('overview_over_administrators')}
-          </Button>
-        )}
-      </Box>
+      ) : (
+        <Button
+          data-testid={dataTestId.authorization.institutionAdminsLink}
+          component={Link}
+          to={UrlPathTemplate.InstitutionOverviewPage}
+          endIcon={<ArrowForwardIcon />}
+          sx={{ textTransform: 'none', mt: '0.5rem' }}
+          variant="contained">
+          {t('overview_over_administrators')}
+        </Button>
+      )}
     </Box>
   );
 };
