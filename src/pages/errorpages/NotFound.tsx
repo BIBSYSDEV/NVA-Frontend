6da9f96cd6b77e722630e4454c1fad0b3491a8da
Isar Buzza <isar.buzza@sikt.no>
2025-07-10 12:26:59 +0200

    updated margin

--- src/pages/errorpages/NotFound.tsx
+++ src/pages/errorpages/NotFound.tsx
@@ -4,7 +4,7 @@ import { Trans } from 'react-i18next';
 
 const NotFound = () => {
   return (
-    <Box data-testid="404" sx={{ my: '4rem' }}>
+    <Box data-testid="404" sx={{ m: '4rem 2rem' }}>
       <Trans
         i18nKey="feedback.error.404_page"
         components={{
