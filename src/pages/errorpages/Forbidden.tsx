a588df579c7e0ed6d412e6dd6e44b0b33ff6cf83
Isar Buzza <isar.buzza@sikt.no>
2025-08-04 14:52:25 +0200

    improved semantics

--- src/pages/errorpages/Forbidden.tsx
+++ src/pages/errorpages/Forbidden.tsx
@@ -17,50 +17,52 @@ export const Forbidden = () => {
   const user = useSelector((store: RootState) => store.user);
 
   return (
-    <Box data-testid="forbidden" sx={{ my: '4rem' }}>
-      <span style={{ display: 'flex', gap: '0.5rem' }}>
-        <LockOutlineIcon />
-        <Typography gutterBottom variant="h1">
-          {t('authorization.forbidden')}
-        </Typography>
-      </span>
-      {!user ? (
-        <Trans
-          i18nKey="authorization.forbidden_description"
-          components={{
-            p: <Typography />,
-          }}
-        />
-      ) : (
-        <Typography>{t('authorization.forbidden_description_logged_in')}</Typography>
-      )}
-
-      {!user ? (
-        <Button
-          sx={{ textTransform: 'none', mt: '1rem' }}
-          endIcon={<LoginIcon />}
-          variant="contained"
-          data-testid={`${dataTestId.header.logInButton}-forbidden`}
-          onClick={() => {
-            const redirectPath = new URLSearchParams(location.search).get(LocalStorageKey.RedirectPath);
-            if (redirectPath) {
-              localStorage.setItem(LocalStorageKey.RedirectPath, redirectPath);
-            }
-            handleLogin();
-          }}>
-          {t('authorization.login')}
-        </Button>
-      ) : (
-        <Button
-          data-testid={dataTestId.authorization.institutionAdminsLink}
-          component={Link}
-          to={UrlPathTemplate.InstitutionOverviewPage}
-          endIcon={<ArrowForwardIcon />}
-          sx={{ textTransform: 'none', mt: '0.5rem' }}
-          variant="contained">
-          {t('overview_over_administrators')}
-        </Button>
-      )}
+    <Box data-testid="forbidden" sx={{ my: '4rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div>
+        <span style={{ display: 'flex', gap: '0.5rem' }}>
+          <LockOutlineIcon />
+          <Typography gutterBottom variant="h1">
+            {t('authorization.forbidden')}
+          </Typography>
+        </span>
+        {!user ? (
+          <>
+            <Trans
+              i18nKey="authorization.forbidden_description"
+              components={{
+                p: <Typography />,
+              }}
+            />
+            <Button
+              sx={{ textTransform: 'none', mt: '1rem', width: 'fit-content' }}
+              endIcon={<LoginIcon />}
+              variant="contained"
+              data-testid={`${dataTestId.header.logInButton}-forbidden`}
+              onClick={() => {
+                const redirectPath = new URLSearchParams(location.search).get(LocalStorageKey.RedirectPath);
+                if (redirectPath) {
+                  localStorage.setItem(LocalStorageKey.RedirectPath, redirectPath);
+                }
+                handleLogin();
+              }}>
+              {t('authorization.login')}
+            </Button>
+          </>
+        ) : (
+          <>
+            <Typography>{t('authorization.forbidden_description_logged_in')}</Typography>
+            <Button
+              data-testid={dataTestId.authorization.institutionAdminsLink}
+              component={Link}
+              to={UrlPathTemplate.InstitutionOverviewPage}
+              endIcon={<ArrowForwardIcon />}
+              sx={{ textTransform: 'none', mt: '0.5rem', width: 'fit-content' }}
+              variant="contained">
+              {t('overview_over_administrators')}
+            </Button>
+          </>
+        )}
+      </div>
     </Box>
   );
 };
