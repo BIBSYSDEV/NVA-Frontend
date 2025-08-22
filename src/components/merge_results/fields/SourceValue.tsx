73a2b9b87aaff5a37aba81e7a5cc2d5c37b216cb
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-08-22 12:50:35 +0200

    Np 49450 add compare for category (#7638)
    * Add component for handling description fields
    
    * Fix equality check
    
    * Split into new file
    
    * Add react-hook-form
    
    * Move form to context
    
    * Move content to its own component
    
    * Listen for updated value
    
    * Allow resetting value
    
    * Avoid jumping content
    
    * Working resize
    
    * Simplify boilerplate for auto resize
    
    * Move resize hook to its own file
    
    * Allow resetting user input
    
    * Update headings
    
    * Show target value in header
    
    * Move heading and content setup one level up
    
    * Use native FormProvider instead of custom context
    
    * Add translations
    
    * Move CompareMainTitle to new file
    
    * Move source value rendering to its own component
    
    * Minor fixes
    
    * Set import status
    
    * Move onsubmit function to parent
    
    * Optimistic update of result update
    
    * Fix mobile layout
    
    * Simplify logic
    
    * Use prop instead of duplicated logic
    
    * Add CompareAbstract
    
    * Add description field and handle empty values
    
    * Add divider between fields
    
    * Add field for comparing language
    
    * Translate language value
    
    * Remove unused variable
    
    * Move actions inside same BackgroundDiv as content
    
    * Add compare fields for journal fields
    
    * Add new wrapper component for simple text fields
    
    * Simplify compare logic for text fields
    
    * Simplify logic
    
    * Simplify type
    
    * Allow overwriting category
    
    * Show warning for non-compatible categories
    
    * Reset only instance type
    
    * Show main type when relevant
    
    * Move compare category logic to its own component
    
    * Show context type with seperate component
    
    * Add data-testid

--- src/components/merge_results/fields/SourceValue.tsx
+++ src/components/merge_results/fields/SourceValue.tsx
@@ -1,17 +1,19 @@
+import { Box } from '@mui/material';
+import { ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 
 interface SourceValueProps {
   label: string;
-  value: string | null | undefined;
+  value: ReactNode;
 }
 
-export const SourceValue = ({ label, value = '' }: SourceValueProps) => {
+export const SourceValue = ({ label, value }: SourceValueProps) => {
   const { t } = useTranslation();
 
   return (
-    <dl style={{ margin: 0 }}>
+    <Box component="dl" sx={{ m: 0, p: '0.5rem', bgcolor: '#FEFBF3' }}>
       <dt style={{ fontWeight: 'bold' }}>{label}</dt>
       <dd style={{ margin: 0, fontStyle: value ? 'normal' : 'italic' }}>{value || t('missing_value')}</dd>
-    </dl>
+    </Box>
   );
 };
