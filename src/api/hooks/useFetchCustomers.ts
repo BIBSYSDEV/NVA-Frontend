c9141c049dc92d459027ed7cf8d7ace6f3c3baf1
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-08-26 16:12:51 +0200

    Np 49503 show customer contact point (#7665)
    * Show service center URIs
    
    * Show table for contact points on top
    
    * Fix gaps
    
    * Handle missing org
    
    * Cache customer lookup
    
    * Fix wrappoing of close button
    
    * Update styling of contact info
    
    * Remove unused translation
    
    * Inform about lacking support page for institution
    
    * Handle orgs outside the ones displayed by default
    
    * Cleanup logic
    
    * Move file
    
    * Renaming variables
    
    * Use styled list components
    
    * Resolve function length warning

--- src/api/hooks/useFetchCustomers.ts
+++ src/api/hooks/useFetchCustomers.ts
@@ -1,13 +1,15 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
+import { CustomerList } from '../../types/customerInstitution.types';
 import { fetchCustomers } from '../customerInstitutionsApi';
 
-export const useFetchCustomers = () => {
+export const useFetchCustomers = (config?: Partial<UseQueryOptions<CustomerList>>) => {
   const { t } = useTranslation();
 
   return useQuery({
     queryKey: ['customers'],
     queryFn: ({ signal }) => fetchCustomers(signal),
     meta: { errorMessage: t('feedback.error.get_customers') },
+    ...config,
   });
 };
--- src/pages/public_registration/DetailsPanel.tsx
+++ src/pages/public_registration/DetailsPanel.tsx
@@ -10,16 +10,33 @@ import {
   IconButton,
   Link,
   Skeleton,
+  styled,
   Typography,
 } from '@mui/material';
 import { visuallyHidden } from '@mui/utils';
+import { useQueries } from '@tanstack/react-query';
 import { useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { useFetchPerson } from '../../../api/hooks/useFetchPerson';
-import { ContributorName } from '../../../components/ContributorName';
-import { OpenInNewLink } from '../../../components/OpenInNewLink';
-import { Contributor, ContributorRole } from '../../../types/contributor.types';
-import { dataTestId } from '../../../utils/dataTestIds';
+import { fetchOrganization } from '../../api/cristinApi';
+import { useFetchCustomers } from '../../api/hooks/useFetchCustomers';
+import { useFetchPerson } from '../../api/hooks/useFetchPerson';
+import { ContributorName } from '../../components/ContributorName';
+import { OpenInNewLink } from '../../components/OpenInNewLink';
+import { ConfirmedAffiliation, Contributor, ContributorRole } from '../../types/contributor.types';
+import { Organization } from '../../types/organization.types';
+import { dataTestId } from '../../utils/dataTestIds';
+import { getTopLevelOrganization, getUniqueOrganizations } from '../../utils/institutions-helpers';
+import { getLanguageString } from '../../utils/translation-helpers';
+
+const StyledList = styled('ul')({
+  padding: 0,
+});
+
+const StyledListItem = styled('li')({
+  marginBottom: '1rem',
+  marginLeft: 0,
+  listStyleType: 'none',
+});
 
 interface DetailsPanelProps {
   contributors: Contributor[];
@@ -31,15 +48,31 @@ export const DetailsPanel = ({ contributors }: DetailsPanelProps) => {
   const correspondingContributors = contributors.filter((contributor) => contributor.correspondingAuthor);
   const contactPersons = contributors.filter((contributor) => contributor.role.type === ContributorRole.ContactPerson);
 
+  const confirmedAffiliations = contributors.flatMap((contributor) =>
+    contributor.affiliations?.filter((affiliation) => affiliation.type === 'Organization' && affiliation.id)
+  ) as ConfirmedAffiliation[];
+  const uniqueAffiliations = getUniqueOrganizations(confirmedAffiliations);
+
+  const affiliationQueries = useQueries({
+    queries: uniqueAffiliations.map((affiliation) => ({
+      queryKey: ['organization', affiliation.id],
+      queryFn: () => fetchOrganization(affiliation.id),
+      meta: { errorMessage: t('feedback.error.get_institution') },
+      staleTime: Infinity,
+      gcTime: 1_800_000,
+    })),
+  });
+
+  const topLevelOrgs = affiliationQueries
+    .map((affiliationQuery) => (affiliationQuery.data ? getTopLevelOrganization(affiliationQuery.data) : null))
+    .filter(Boolean) as Organization[];
+  const institutions = getUniqueOrganizations(topLevelOrgs);
+
+  const customersData = useFetchCustomers({ enabled: institutions.length > 0, staleTime: 1_800_000 }); // Cache for 30 minutes
+  const customers = customersData.data?.customers ?? [];
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        p: '1rem',
-        bgcolor: 'secondary.main',
-        gap: '0.5rem',
-      }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', p: '1rem', bgcolor: 'secondary.main', gap: '0.5rem' }}>
       <Typography variant="h2" sx={visuallyHidden}>
         {t('details')}
       </Typography>
@@ -57,28 +90,54 @@ export const DetailsPanel = ({ contributors }: DetailsPanelProps) => {
       <Dialog
         data-testid={dataTestId.registrationLandingPage.detailsTab.resultContactModal}
         open={openModal}
+        maxWidth="md"
         onClose={() => setOpenModal(false)}>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <DialogTitle>{t('points_of_contact_for_result')}</DialogTitle>
-          <IconButton
-            sx={{ mr: '1rem', height: 'fit-content' }}
-            title={t('common.close')}
-            onClick={() => setOpenModal(false)}
-            data-testid={dataTestId.confirmDialog.cancelButton}>
-            <CloseIcon />
-          </IconButton>
-        </Box>
+        <DialogTitle sx={{ mr: '1rem' }}>{t('points_of_contact_for_result')}</DialogTitle>
+        <IconButton
+          sx={{ position: 'absolute', right: '0.5rem', top: '0.75rem' }}
+          title={t('common.close')}
+          onClick={() => setOpenModal(false)}
+          data-testid={dataTestId.confirmDialog.cancelButton}>
+          <CloseIcon />
+        </IconButton>
+
         <DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+          {institutions.length > 0 && (
+            <div>
+              <Typography variant="h2" gutterBottom>
+                {t('institutions_service_support')}
+              </Typography>
+              <StyledList>
+                {institutions.map((institution) => {
+                  const serviceCenterUri = customers.find(
+                    (customer) => customer.cristinId === institution.id
+                  )?.serviceCenterUri;
+
+                  return (
+                    <StyledListItem key={institution.id}>
+                      <Typography>{getLanguageString(institution.labels)}</Typography>
+                      {serviceCenterUri ? (
+                        <OpenInNewLink href={serviceCenterUri}>{serviceCenterUri}</OpenInNewLink>
+                      ) : (
+                        <Typography fontStyle="italic">{t('no_service_center')} </Typography>
+                      )}
+                    </StyledListItem>
+                  );
+                })}
+              </StyledList>
+            </div>
+          )}
+
           {contactPersons.length > 0 && (
             <div>
               <Typography variant="h2" gutterBottom>
                 {t('registration.contributors.types.ContactPerson')}
               </Typography>
-              <ul style={{ padding: 0 }}>
+              <StyledList>
                 {contactPersons.map((contributor, index) => (
                   <ContactPersonRow key={index} contributor={contributor} />
                 ))}
-              </ul>
+              </StyledList>
             </div>
           )}
           {correspondingContributors.length > 0 && (
@@ -86,11 +145,11 @@ export const DetailsPanel = ({ contributors }: DetailsPanelProps) => {
               <Typography variant="h2" gutterBottom>
                 {t('corresponding_contributor')}
               </Typography>
-              <ul style={{ padding: 0 }}>
+              <StyledList>
                 {correspondingContributors.map((contributor, index) => (
                   <ContactPersonRow key={index} contributor={contributor} />
                 ))}
-              </ul>
+              </StyledList>
             </div>
           )}
 
@@ -124,14 +183,7 @@ const ContactPersonRow = ({ contributor }: ContactPersonRowProps) => {
   const person = personQuery.data;
 
   return (
-    <li
-      style={{
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr',
-        marginBottom: '0.5rem',
-        marginLeft: 0,
-        alignItems: 'center',
-      }}>
+    <StyledListItem>
       {personQuery.isFetching ? (
         <>
           <Skeleton width="10rem" />
@@ -158,6 +210,6 @@ const ContactPersonRow = ({ contributor }: ContactPersonRowProps) => {
           )}
         </>
       )}
-    </li>
+    </StyledListItem>
   );
 };
