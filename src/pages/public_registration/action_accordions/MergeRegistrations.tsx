80b70e6c1ec7aee710826f5e9173b957dfae890c
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-08-27 15:56:06 +0200

    Show results to merge

--- src/pages/public_registration/action_accordions/MergeRegistrations.tsx
+++ src/pages/public_registration/action_accordions/MergeRegistrations.tsx
@@ -1,16 +1,22 @@
 import { Button, Dialog, DialogContent, DialogTitle, Typography } from '@mui/material';
 import { useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
+import { useFetchRegistration } from '../../../api/hooks/useFetchRegistration';
+import { MergeResultsWizard } from '../../../components/merge_results/MergeResultsWizard';
 import { Registration } from '../../../types/registration.types';
 import { dataTestId } from '../../../utils/dataTestIds';
+import { getIdentifierFromId } from '../../../utils/general-helpers';
 
 interface MergeRegistrationsProps {
   sourceRegistration: Registration;
 }
 
-export const MergeRegistrations = ({}: MergeRegistrationsProps) => {
+export const MergeRegistrations = ({ sourceRegistration }: MergeRegistrationsProps) => {
   const { t } = useTranslation();
   const [openDialog, setOpenDialog] = useState(false);
+
+  const targetRegistrationId = sourceRegistration.duplicateOf ?? '';
+  const targetRegistrationQuery = useFetchRegistration(getIdentifierFromId(targetRegistrationId));
 
   return (
     <section>
@@ -23,13 +29,24 @@ export const MergeRegistrations = ({}: MergeRegistrationsProps) => {
         variant="outlined"
         fullWidth
         size="small"
-        sx={{ bgcolor: 'white' }}>
+        sx={{ bgcolor: 'white' }}
+        onClick={() => setOpenDialog(true)}>
         {t('merge_results')}
       </Button>
 
-      <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)} maxWidth="lg" fullWidth>
         <DialogTitle>{t('merge_results')}</DialogTitle>
-        <DialogContent></DialogContent>
+        <DialogContent>
+          {targetRegistrationQuery.data && (
+            <MergeResultsWizard
+              sourceResult={sourceRegistration}
+              targetResult={targetRegistrationQuery.data}
+              onSave={(data) => {
+                console.log('Save data:', data);
+              }}
+            />
+          )}
+        </DialogContent>
       </Dialog>
     </section>
   );
