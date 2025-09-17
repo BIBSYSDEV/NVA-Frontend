62e2039c20f5a8e03ccb874589cb0f4487f02df4
nina.andersen <denektenina@gmail.com>
2025-09-17 14:15:13 +0200

    Refactor FrontPage layout to use StyledPageContent and simplify FrontPageHeading structure; update Norwegian translation

--- src/pages/frontpage/FrontPage.tsx
+++ src/pages/frontpage/FrontPage.tsx
@@ -1,26 +1,10 @@
-import { Box, Container } from '@mui/material';
 import { FrontPageHeading } from './FrontPageHeading';
+import { StyledPageContent } from '../../components/styled/Wrappers';
 
 const FrontPage = () => (
-  <Box
-    sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      bgcolor: '#EFEFEF',
-      width: '100%',
-      height: '100vh',
-    }}>
-    <Container
-      maxWidth="lg"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}>
-      <FrontPageHeading />
-    </Container>
-  </Box>
+  <StyledPageContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <FrontPageHeading />
+  </StyledPageContent>
 );
 
 export default FrontPage;
--- src/pages/frontpage/FrontPageHeading.tsx
+++ src/pages/frontpage/FrontPageHeading.tsx
@@ -1,17 +1,15 @@
-import { Box, Typography, Container } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 export const FrontPageHeading = () => {
   const { t } = useTranslation();
 
   return (
-    <Container maxWidth="md" sx={{ display: 'flex', justifyContent: 'center', pt: '3rem' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1.5rem' }}>
-        <Typography variant="h1" sx={{ fontSize: '3rem', color: '#120732' }}>
-          {t('common.page_title')}
-        </Typography>
-        <Typography sx={{ fontSize: '1rem' }}>{t('search_in_national_research_publication')}</Typography>
-      </Box>
-    </Container>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1.5rem', mt: '3rem' }}>
+      <Typography variant="h1" sx={{ fontSize: '3rem', color: '#120732' }}>
+        {t('common.page_title')}
+      </Typography>
+      <Typography sx={{ fontSize: '1rem' }}>{t('search_in_national_research_publication')}</Typography>
+    </Box>
   );
 };
