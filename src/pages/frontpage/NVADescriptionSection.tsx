0e01c83665b21eb512e8fdf454dced8a7600a6f5
nina.andersen <denektenina@gmail.com>
2025-09-22 14:00:21 +0200

    Made TypeCard "dumb"

--- src/pages/frontpage/NVADescriptionSection.tsx
+++ src/pages/frontpage/NVADescriptionSection.tsx
@@ -3,9 +3,22 @@ import { useTranslation } from 'react-i18next';
 import { TypeCard } from './TypeCard';
 import { FrontPageBox } from './styles';
 import { SearchTypeValue } from '../search/SearchTypeDropdown';
+import { SearchParam } from '../../utils/searchHelpers';
+import { UrlPathTemplate } from '../../utils/urlPaths';
+import { PersonIcon } from '../../components/atoms/PersonIcon';
+import { dataTestId } from '../../utils/dataTestIds';
+import { ProjectIcon } from '../../components/atoms/ProjectIcon';
+import { RegistrationIcon } from '../../components/atoms/RegistrationIcon';
+
+const getUrlParams = (type: SearchTypeValue) => {
+  const searchParams = new URLSearchParams();
+  searchParams.set(SearchParam.Type, type);
+  return searchParams.toString();
+};
 
 export const NVADescriptionSection = () => {
   const { t } = useTranslation();
+
   return (
     <FrontPageBox sx={{ bgcolor: 'white', alignItems: 'center' }}>
       <Typography component="h2" variant="h2" sx={{ fontSize: '1.5rem', color: '#120732' }}>
@@ -21,9 +34,26 @@ export const NVADescriptionSection = () => {
           gap: { xs: '1rem', sm: '0.5rem', md: '2rem' },
           flexDirection: { xs: 'column', sm: 'row' },
         }}>
-        <TypeCard type={SearchTypeValue.Project} />
-        <TypeCard type={SearchTypeValue.Result} />
-        <TypeCard type={SearchTypeValue.Person} />
+        <TypeCard
+          text={t('projects')}
+          icon={<ProjectIcon />}
+          dataTestId={dataTestId.frontPage.projectsLink}
+          pathName={UrlPathTemplate.Root}
+          parameters={getUrlParams(SearchTypeValue.Project)}
+        />
+        <TypeCard
+          text={t('results')}
+          icon={<RegistrationIcon />}
+          dataTestId={dataTestId.frontPage.registrationsLink}
+          pathName={UrlPathTemplate.Root}
+        />
+        <TypeCard
+          text={t('person_profiles')}
+          icon={<PersonIcon />}
+          dataTestId={dataTestId.frontPage.personsLink}
+          pathName={UrlPathTemplate.Root}
+          parameters={getUrlParams(SearchTypeValue.Person)}
+        />
       </Box>
     </FrontPageBox>
   );
--- src/pages/frontpage/TypeCard.tsx
+++ src/pages/frontpage/TypeCard.tsx
@@ -1,65 +1,24 @@
-import { useTranslation } from 'react-i18next';
 import { Box, Link, Typography } from '@mui/material';
 import { FrontPageBox } from './styles';
-import { SearchTypeValue } from '../search/SearchTypeDropdown';
-import { ProjectIcon } from '../../components/atoms/ProjectIcon';
-import { RegistrationIcon } from '../../components/atoms/RegistrationIcon';
-import { PersonIcon } from '../../components/atoms/PersonIcon';
-import { TFunction } from 'i18next';
 import { Link as RouterLink } from 'react-router';
-import { dataTestId } from '../../utils/dataTestIds';
-import { SearchParam } from '../../utils/searchHelpers';
-import { UrlPathTemplate } from '../../utils/urlPaths';
-
-const getValuesFromType = (type: SearchTypeValue, t: TFunction<'translation'>) => {
-  const searchParams = new URLSearchParams();
-  searchParams.set(SearchParam.Type, type);
-  const navigationObject = {
-    pathname: UrlPathTemplate.Root,
-    search: searchParams.toString(),
-  };
-
-  switch (type) {
-    case SearchTypeValue.Person:
-      return {
-        text: t('person_profiles'),
-        icon: <PersonIcon />,
-        dataTestId: dataTestId.frontPage.personsLink,
-        navigationObject: navigationObject,
-      };
-    case SearchTypeValue.Project:
-      return {
-        text: t('projects'),
-        icon: <ProjectIcon />,
-        dataTestId: dataTestId.frontPage.projectsLink,
-        navigationObject: navigationObject,
-      };
-    case SearchTypeValue.Result:
-    default:
-      return {
-        text: t('results'),
-        icon: <RegistrationIcon />,
-        dataTestId: dataTestId.frontPage.registrationsLink,
-        navigationObject: {
-          pathname: UrlPathTemplate.Root,
-        },
-      };
-  }
-};
 
 interface TypeCardProps {
-  type: SearchTypeValue;
+  text: string;
+  icon: React.ReactNode;
+  dataTestId: string;
+  pathName: string;
+  parameters?: string;
 }
 
-export const TypeCard = ({ type }: TypeCardProps) => {
-  const { t } = useTranslation();
-  const { text, icon, dataTestId, navigationObject } = getValuesFromType(type, t);
-
+export const TypeCard = ({ text, icon, dataTestId, pathName, parameters = '' }: TypeCardProps) => {
   return (
     <Link
       component={RouterLink}
       data-testid={dataTestId}
-      to={navigationObject}
+      to={{
+        pathname: pathName,
+        search: parameters,
+      }}
       sx={{ width: '100%', textDecoration: 'none' }}>
       <FrontPageBox
         sx={{ flex: '1', bgcolor: '#EFEFEF', alignItems: 'center', p: '1.5rem', cursor: 'pointer', height: '8rem' }}>
