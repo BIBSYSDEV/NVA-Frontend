1441b3a04c1f13137ad52aa93ce6c2e5c49431e0
nina.andersen <denektenina@gmail.com>
2025-09-23 07:20:50 +0200

    PR comments

--- src/pages/frontpage/TypeCard.tsx
+++ src/pages/frontpage/TypeCard.tsx
@@ -16,13 +16,7 @@ export const TypeCard = ({ text, icon, dataTestId, to }: TypeCardProps) => {
   return (
     <Link component={RouterLink} data-testid={dataTestId} to={to} sx={{ width: '100%' }}>
       <FrontPageBox sx={{ flex: '1', bgcolor: '#EFEFEF', alignItems: 'center', p: '1.5rem', height: '8rem' }}>
-        <Box
-          sx={{
-            flexGrow: 1,
-            alignContent: 'center',
-          }}>
-          {icon}
-        </Box>
+        <Box sx={{ flexGrow: 1, alignContent: 'center' }}>{icon}</Box>
         <Typography sx={{ fontSize: '0.8rem' }}>{text}</Typography>
       </FrontPageBox>
     </Link>
