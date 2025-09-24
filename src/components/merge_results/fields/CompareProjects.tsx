70f2767ff937c9dc23ccb4065eeb6949913b2e81
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-09-24 13:15:27 +0200

    Show project boxes

--- src/components/merge_results/fields/CompareProjects.tsx
+++ src/components/merge_results/fields/CompareProjects.tsx
@@ -1,7 +1,11 @@
-import { useContext } from 'react';
+import { Box, SxProps, Typography } from '@mui/material';
+import { Fragment, useContext } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { useFetchProject } from '../../../api/hooks/useFetchProject';
+import { ProjectListItem } from '../../../pages/search/project_search/ProjectListItem';
 import { Registration } from '../../../types/registration.types';
+import { isOnImportPage } from '../../../utils/urlPaths';
 import { MergeResultsWizardContext } from '../MergeResultsWizardContext';
 
 export const CompareProjects = () => {
@@ -16,9 +20,39 @@ export const CompareProjects = () => {
 
   return (
     <>
+      <Typography variant="h3" sx={{ display: { xs: 'block', md: 'none' } }}>
+        {isOnImportPage() ? t('basic_data.central_import.import_candidate') : t('unpublished_result')}
+      </Typography>
+
       {targetProjects.map((project) => (
-        <p key={project.id}>{project.name}</p>
+        <Fragment key={project.id}>
+          <ProjectBox />
+          <ProjectBox projectId={project.id} sx={{ gridColumn: 3 }} />
+        </Fragment>
+      ))}
+
+      {sourceProjects.map((project) => (
+        <Fragment key={project.id}>
+          <ProjectBox projectId={project.id} />
+          <ProjectBox sx={{ gridColumn: 3 }} />
+        </Fragment>
       ))}
     </>
   );
+};
+
+interface ProjectBoxProps {
+  projectId?: string;
+  sx?: SxProps;
+}
+
+const ProjectBox = ({ projectId, sx }: ProjectBoxProps) => {
+  const projectQuery = useFetchProject(projectId);
+  const project = projectQuery.data;
+
+  if (!project) {
+    return <Box sx={{ m: 0, p: '0.5rem', bgcolor: '#FEFBF3', height: '100%', ...sx }} />;
+  } else {
+    return <ProjectListItem project={project} sx={sx} />;
+  }
 };
