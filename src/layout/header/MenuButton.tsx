c09a0329b77f0c20a0ee651f3c46c4eb8ce16300
Isar Buzza <isar.buzza@sikt.no>
2025-09-29 16:32:56 +0200

    remove boxShadow

--- src/layout/header/MenuButton.tsx
+++ src/layout/header/MenuButton.tsx
@@ -11,7 +11,6 @@ const StyledMenuButtonContainer = styled(Box, {
   display: 'flex',
   borderBottom: isSelected ? '0.375rem solid' : 'none',
   borderBottomColor: theme.palette.secondary.main,
-  // boxShadow: isSelected ? '-1px 7px 4px -3px rgba(0,0,0,0.3)' : 'none',
 
   a: {
     marginBottom: isSelected ? '-0.375rem' : 0,
