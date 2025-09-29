47a3901ef46ac22d54e16d3496323a8f14b2134c
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-09-29 10:09:00 +0200

    Ensure redirects after login works as expected

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,22 +1,10 @@
 import { Box } from '@mui/material';
-import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router';
 import { HeadTitle } from '../../components/HeadTitle';
-import { LocalStorageKey } from '../../utils/constants';
 import HomePage from './HomePage';
 
 const Dashboard = () => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const loginPath = localStorage.getItem(LocalStorageKey.RedirectPath);
-    if (loginPath) {
-      localStorage.removeItem(LocalStorageKey.RedirectPath);
-      navigate(loginPath, { replace: true });
-    }
-  }, [navigate]);
 
   return (
     <Box
--- src/pages/frontpage/FrontPage.tsx
+++ src/pages/frontpage/FrontPage.tsx
@@ -1,17 +1,32 @@
-import { FrontPageHeading } from './FrontPageHeading';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 import { StyledPageContent } from '../../components/styled/Wrappers';
-import { SearchSection } from './SearchSection';
-import { NvaDescriptionSection } from './NvaDescriptionSection';
+import { LocalStorageKey } from '../../utils/constants';
 import { CategorySection } from './CategorySection';
+import { FrontPageHeading } from './FrontPageHeading';
+import { NvaDescriptionSection } from './NvaDescriptionSection';
+import { SearchSection } from './SearchSection';
 
-const FrontPage = () => (
-  <StyledPageContent
-    sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: { xs: '0.75rem', sm: '2rem' } }}>
-    <FrontPageHeading />
-    <SearchSection />
-    <NvaDescriptionSection />
-    <CategorySection />
-  </StyledPageContent>
-);
+const FrontPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loginPath = localStorage.getItem(LocalStorageKey.RedirectPath);
+    if (loginPath) {
+      localStorage.removeItem(LocalStorageKey.RedirectPath);
+      navigate(loginPath, { replace: true });
+    }
+  }, [navigate]);
+
+  return (
+    <StyledPageContent
+      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: { xs: '0.75rem', sm: '2rem' } }}>
+      <FrontPageHeading />
+      <SearchSection />
+      <NvaDescriptionSection />
+      <CategorySection />
+    </StyledPageContent>
+  );
+};
 
 export default FrontPage;
