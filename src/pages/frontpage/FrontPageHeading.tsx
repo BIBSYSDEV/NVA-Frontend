80b083142506e09a05220b93e7c7affc8d2d663f
nina.andersen <denektenina@gmail.com>
2025-09-30 07:49:03 +0200

    Improve layout and styling in various sections

--- src/pages/frontpage/FrontPageHeading.tsx
+++ src/pages/frontpage/FrontPageHeading.tsx
@@ -7,7 +7,7 @@ export const FrontPageHeading = () => {
 
   return (
     <VerticalBox sx={{ gap: '1.5rem', mt: { xs: '1rem', sm: '3rem' }, mb: '1rem' }}>
-      <Typography component="h1" variant="h1" sx={{ fontSize: { xs: '2.5rem', sm: '3rem' }, color: '#120732' }}>
+      <Typography component="h1" variant="h1" sx={{ fontSize: { xs: '2.25rem', sm: '3rem' }, color: '#120732' }}>
         {t('common.page_title')}
       </Typography>
       <Typography sx={{ fontSize: '1rem', color: '#120732' }}>
