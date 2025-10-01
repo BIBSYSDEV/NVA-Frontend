8f202c81ec0a02f46175ebe50d047e502571f1a9
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-10-01 15:12:34 +0200

    Remove redundant margin

--- src/components/merge_results/fields/ProjectBox.tsx
+++ src/components/merge_results/fields/ProjectBox.tsx
@@ -2,25 +2,23 @@ import { Box, styled, SxProps } from '@mui/material';
 import { useFetchProject } from '../../../api/hooks/useFetchProject';
 import { ProjectListItem } from '../../../pages/search/project_search/ProjectListItem';
 
-interface ProjectBoxProps {
-  projectId?: string;
-  sx?: SxProps;
-}
-
 export const StyledEmptyProjectBox = styled(Box)({
-  margin: 0,
   padding: '0.5rem',
   background: 'white',
   height: '100%',
 });
 
+interface ProjectBoxProps {
+  projectId?: string;
+  sx?: SxProps;
+}
+
 export const ProjectBox = ({ projectId, sx }: ProjectBoxProps) => {
   const projectQuery = useFetchProject(projectId);
-  const project = projectQuery.data;
 
-  if (!project) {
+  if (!projectQuery.data) {
     return <StyledEmptyProjectBox sx={sx} />;
   }
 
-  return <ProjectListItem project={project} sx={sx} />;
+  return <ProjectListItem project={projectQuery.data} sx={sx} />;
 };
