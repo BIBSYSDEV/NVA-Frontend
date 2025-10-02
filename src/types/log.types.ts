f23c09e75ef2c6f40ee947b96003931ec2f32285
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-10-02 13:49:39 +0200

    Show importSource for all log entries

--- src/types/log.types.ts
+++ src/types/log.types.ts
@@ -16,6 +16,7 @@ export interface LogEntryPerson {
 
 interface BaseLogEntry {
   timestamp: string;
+  importSource?: ImportSourceLogData;
 }
 
 interface PublicationLogEntry extends BaseLogEntry {
@@ -42,7 +43,6 @@ export interface ImportSourceLogData {
 interface PublicationImportLogEntry extends Omit<PublicationLogEntry, 'topic' | 'performedBy'> {
   topic: 'PublicationImported' | 'PublicationMerged';
   performedBy: LogEntryOrganization;
-  importSource: ImportSourceLogData;
 }
 
 export interface FileLogEntry extends BaseLogEntry {
