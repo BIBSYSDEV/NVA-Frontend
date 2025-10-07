3794f9c0e2be028e0efb89d094b61e9b09424006
Isar Buzza <53341294+iBuzza@users.noreply.github.com>
2025-10-07 12:26:33 +0200

    NP-49899 Add illustration to front page (#7846)
    * NP-49899 Add illustration to front page
    
    * minor changes
    
    * fix mobile padding

--- src/pages/frontpage/FrontPageHeading.tsx
+++ src/pages/frontpage/FrontPageHeading.tsx
@@ -1,18 +1,29 @@
-import { Typography } from '@mui/material';
+import { Box, Theme, Typography, useMediaQuery } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { VerticalBox } from '../../components/styled/Wrappers';
+import illustration from '../../resources/images/front-page-illustration.svg';
 
 export const FrontPageHeading = () => {
   const { t } = useTranslation();
+  const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
 
   return (
-    <VerticalBox sx={{ gap: '1.5rem', mt: { xs: '1rem', sm: '3rem' }, mb: '1rem' }}>
-      <Typography component="h1" variant="h1" sx={{ fontSize: { xs: '2.25rem', sm: '3rem' }, color: '#120732' }}>
-        {t('common.page_title')}
-      </Typography>
-      <Typography sx={{ fontSize: '1rem', color: '#120732' }}>
-        {t('search_in_national_research_publication')}
-      </Typography>
-    </VerticalBox>
+    <Box
+      sx={{
+        display: 'flex',
+        gap: '12rem',
+        mt: { xs: '1rem', sm: '3rem' },
+        px: { sm: '1rem', md: '3rem' },
+      }}>
+      <VerticalBox sx={{ gap: '1.5rem', mb: '1rem' }}>
+        <Typography variant="h1" sx={{ fontSize: { xs: '2.25rem', sm: '3rem' }, color: 'primary.main' }}>
+          {t('common.page_title')}
+        </Typography>
+        <Typography sx={{ fontSize: '1rem', color: 'primary.main' }}>
+          {t('search_in_national_research_publication')}
+        </Typography>
+      </VerticalBox>
+      {!isMobile && <Box component="img" src={illustration} alt="" sx={{ height: '17rem' }} />}
+    </Box>
   );
 };
