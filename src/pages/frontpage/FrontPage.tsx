5710fef65097013ed98f4c29b444a7b80c1bd4e5
Isar Buzza <isar.buzza@sikt.no>
2025-10-08 16:09:46 +0200

    organize imports

--- src/pages/frontpage/FrontPage.tsx
+++ src/pages/frontpage/FrontPage.tsx
@@ -1,14 +1,14 @@
+import { Box } from '@mui/material';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router';
+import { HeadTitle } from '../../components/HeadTitle';
 import { StyledPageContent } from '../../components/styled/Wrappers';
 import { LocalStorageKey } from '../../utils/constants';
 import { CategorySection } from './CategorySection';
 import { FrontPageHeading } from './FrontPageHeading';
 import { NvaDescriptionSection } from './NvaDescriptionSection';
 import { SearchSection } from './SearchSection';
-import { HeadTitle } from '../../components/HeadTitle';
-import { Box } from '@mui/material';
 
 const FrontPage = () => {
   const navigate = useNavigate();
