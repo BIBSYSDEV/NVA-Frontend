95762c832a04996b70b2d5cbaf1529e421718738
Isar Buzza <isar.buzza@sikt.no>
2025-10-08 16:08:42 +0200

    adjust margins

--- src/pages/frontpage/FrontPageHeading.tsx
+++ src/pages/frontpage/FrontPageHeading.tsx
@@ -15,7 +15,7 @@ export const FrontPageHeading = () => {
         mt: { xs: '1rem', sm: '3rem' },
         justifyContent: 'space-between',
       }}>
-      <VerticalBox sx={{ gap: '1.5rem', mb: '1rem', alignSelf: 'center', pl: { sm: '1rem', md: '3rem' } }}>
+      <VerticalBox sx={{ gap: '1.5rem', mb: '1rem', alignSelf: 'center', ml: { xs: '1rem', md: '3rem' } }}>
         <Typography variant="h1" sx={{ fontSize: { xs: '2.25rem', sm: '3rem' }, color: 'primary.main' }}>
           {t('common.page_title')}
         </Typography>
