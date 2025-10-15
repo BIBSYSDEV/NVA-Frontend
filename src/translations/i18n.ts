23d7948d03673be02426ba30a9c51029862c788a
nina.andersen <denektenina@gmail.com>
2025-10-15 14:34:23 +0200

    Simplified

--- src/translations/i18n.ts
+++ src/translations/i18n.ts
@@ -4,30 +4,27 @@ import enTranslations from './enTranslations.json';
 import nbTranslations from './nbTranslations.json';
 import nnTranslations from './nnTranslations.json';
 
-const supportedLanguages = [
+const handledLanguages = [
   'nob', // Norwegian Bokmål,
-  'nb', // Norwegian Bokmål, other language code
-  'nn', // Norwegian Nynorsk
+  'nb', // Norwegian Bokmål
   'eng', // English
-  'en', // English, other language code
-  'nno', // Norwegian Nynorsk, other language code
-  'sme', // All below arw different sami languages
-  'sma',
-  'smj',
-  'smn',
-  'sms',
-  'sje',
-  'sju',
-  'sjd',
-  'sjt',
+  'en', // English
+  'nn', // Norwegian Nynorsk
+  'nno', // Norwegian Nynorsk
+  'sme', // Northern Sami
+  'sma', // Southern Sami
+  'smj', // Lule Sami
+  'smn', // Inari Sami
+  'sms', // Skolt Sami
+  'sje', // Pite Sami
+  'sju', // Ume Sami
+  'sjd', // Kildin Sami
+  'sjt', // Ter Sami
 ];
 
 i18n.use(LanguageDetector).init({
   resources: {
     eng: {
-      translation: enTranslations,
-    },
-    en: {
       translation: enTranslations,
     },
     nob: {
@@ -36,61 +33,16 @@ i18n.use(LanguageDetector).init({
     nno: {
       translation: nnTranslations,
     },
-    nb: {
-      translation: nbTranslations,
-    },
-    nn: {
-      translation: nnTranslations,
-    },
-    sme: {
-      // When the user's browser language is a Sami variant, then the service should display in Bokmål
-      translation: nbTranslations,
-    },
-    sma: {
-      // Southern Sami
-      translation: nbTranslations,
-    },
-    smj: {
-      // Lule Sami
-      translation: nbTranslations,
-    },
-    smn: {
-      // Inari Sami
-      translation: nbTranslations,
-    },
-    sms: {
-      // Skolt Sami
-      translation: nbTranslations,
-    },
-    sje: {
-      // Pite Sami
-      translation: nbTranslations,
-    },
-    sju: {
-      // Ume Sami
-      translation: nbTranslations,
-    },
-    sjd: {
-      // Kildin Sami
-      translation: nbTranslations,
-    },
-    sjt: {
-      // Ter Sami
-      translation: nbTranslations,
-    },
   },
   contextSeparator: '__',
   fallbackLng: (langCode) => {
-    if (!langCode) {
-      // When the user's browser language is not specified, then the service should display in Bokmål
-      return ['nob'];
-    }
-    if (!supportedLanguages.includes(langCode)) {
+    if (!handledLanguages.includes(langCode) || langCode === 'en' || langCode === 'eng') {
       // When the user's browser language is not Norwegian (Bokmål, Nynorsk, or any Sami variant), then the service should display in English by default
       return ['eng'];
+    } else if (langCode === 'nn' || langCode === 'nno') {
+      return ['nno'];
     }
-    // Supported language
-    return [langCode];
+    return ['nob']; // When the user's browser language is not specified, and when Bokmål is selected, then the service should display in Bokmål
   },
   returnEmptyString: false,
   debug: false,
