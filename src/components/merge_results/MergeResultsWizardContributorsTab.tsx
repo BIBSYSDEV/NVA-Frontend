a3f69bd582ced6c6a2eca1264a76e6b8bf6428d3
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-10-16 12:24:28 +0200

    Show affiliation data for contributors to merge

--- src/components/merge_results/MergeResultsWizardContributorsTab.tsx
+++ src/components/merge_results/MergeResultsWizardContributorsTab.tsx
@@ -1,28 +1,15 @@
 import { useContext } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
-import { useTranslation } from 'react-i18next';
-import { Contributor } from '../../types/contributor.types';
 import { Registration } from '../../types/registration.types';
 import { MergeResultsWizardContext } from './MergeResultsWizardContext';
 import { CompareContributor } from './fields/CompareContributor';
 
 export const MergeResultsWizardContributorsTab = () => {
-  const { t } = useTranslation();
-
-  const { control, formState } = useFormContext<Registration>();
+  const { control } = useFormContext<Registration>();
   const { sourceResult } = useContext(MergeResultsWizardContext);
   const sourceContributors = sourceResult.entityDescription?.contributors ?? [];
 
   const targetContributors = useWatch({ name: 'entityDescription.contributors', control }) ?? [];
-  const initialTargetContributors = (formState.defaultValues?.entityDescription?.contributors ?? []) as Contributor[];
-
-  // const targetOnlyProjects = initialTargetContributors.filter(
-  //   (targetProject) => !commonProjects.some((sourceProject) => sourceProject.id === targetProject.id)
-  // );
-
-  // const addableSourceProjects = sourceProjects.filter(
-  //   (sourceProject) => !commonProjects.some((targetProject) => targetProject.id === sourceProject.id)
-  // );
 
   return (
     <>
