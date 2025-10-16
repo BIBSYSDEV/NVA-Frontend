c7bc8d39ff76baffddbb1aaeb17cd25b514d654b
Kjetil Møkkelgjerd <kjetil@mokkelgjerd.no>
2025-10-16 13:03:52 +0200

    Add headings on mobile screens

--- src/components/merge_results/fields/CompareContributor.tsx
+++ src/components/merge_results/fields/CompareContributor.tsx
@@ -1,4 +1,7 @@
+import { Divider, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import { Contributor } from '../../../types/contributor.types';
+import { isOnImportPage } from '../../../utils/urlPaths';
 import { ContributorBox } from './ContributorBox';
 
 interface CompareContributorProps {
@@ -7,10 +10,21 @@ interface CompareContributorProps {
 }
 
 export const CompareContributor = ({ sourceContributor, targetContributor }: CompareContributorProps) => {
+  const { t } = useTranslation();
+
   return (
     <>
+      <Typography variant="h3" sx={{ display: { xs: 'block', md: 'none' } }}>
+        {isOnImportPage() ? t('basic_data.central_import.import_candidate') : t('unpublished_result')}
+      </Typography>
       <ContributorBox sx={{ gridColumn: 1 }} contributor={sourceContributor} />
+
+      <Typography variant="h3" sx={{ display: { xs: 'block', md: 'none' } }}>
+        {t('published_result')}
+      </Typography>
       <ContributorBox sx={{ gridColumn: { xs: 1, md: 3 } }} contributor={targetContributor} />
+
+      <Divider sx={{ display: { xs: 'block', md: 'none' }, my: '0.5rem' }} />
     </>
   );
 };
