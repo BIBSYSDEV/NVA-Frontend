3af8075b1a5ff5c0e9f89a1e60c9901090e64067
Isar Buzza <53341294+iBuzza@users.noreply.github.com>
2025-10-21 13:29:48 +0200

    Revert "Test Lokalise webhook (#7905)"
    This reverts commit 9bcc048dee71584487352a46055e334b046d8942.

--- src/components/merge_results/fields/CompareFunding.tsx
+++ src/components/merge_results/fields/CompareFunding.tsx
@@ -1,8 +1,3 @@
-import { Typography } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-
 export const CompareFunding = () => {
-  const { t } = useTranslation();
-
-  return <Typography>{t('lokalise')}</Typography>;
+  return;
 };
