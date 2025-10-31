bec834e4d1578b9306979b423b43b5d8dcf85b3d
Isar Buzza <53341294+iBuzza@users.noreply.github.com>
2025-10-31 12:42:14 +0000

    NP-50070 Set default org on Correction List Page (#7924)
    * NP-50070 Set default org on Correction List Page
    
    * add topLevelOrg to correctionListConfig
    
    * remove unused import
    
    * move helpers functions and types
    
    * move function to helper files

--- src/pages/messages/components/NviCorrectionList.tsx
+++ src/pages/messages/components/NviCorrectionList.tsx
@@ -1,88 +1,18 @@
 import { Box, Divider, Typography } from '@mui/material';
-import { ParseKeys } from 'i18next';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router';
 import { useRegistrationSearch } from '../../../api/hooks/useRegistrationSearch';
-import { FetchResultsParams, ResultParam } from '../../../api/searchApi';
+import { ResultParam } from '../../../api/searchApi';
 import { CategorySearchFilter } from '../../../components/CategorySearchFilter';
 import { HeadTitle } from '../../../components/HeadTitle';
-import { BookType } from '../../../types/publicationFieldNames';
+import { CorrectionListId } from '../../../types/nvi.types';
+import { useCorrectionListConfig } from '../../../utils/hooks/useCorrectionListConfig';
 import { useRegistrationsQueryParams } from '../../../utils/hooks/useRegistrationSearchParams';
-import { nviApplicableTypes } from '../../../utils/registration-helpers';
 import { JournalFilter } from '../../search/advanced_search/JournalFilter';
 import { OrganizationFilters } from '../../search/advanced_search/OrganizationFilters';
 import { PublisherFilter } from '../../search/advanced_search/PublisherFilter';
-import { ScientificValueLevels } from '../../search/advanced_search/ScientificValueFilter';
 import { SeriesFilter } from '../../search/advanced_search/SeriesFilter';
 import { RegistrationSearch } from '../../search/registration_search/RegistrationSearch';
-
-export type CorrectionListId =
-  | 'ApplicableCategoriesWithNonApplicableChannel'
-  | 'NonApplicableCategoriesWithApplicableChannel'
-  | 'AnthologyWithoutChapter'
-  | 'AnthologyWithApplicableChapter'
-  | 'BooksWithLessThan50Pages'
-  | 'UnidentifiedContributorWithIdentifiedAffiliation';
-
-type CorrectionListSearchConfig = {
-  [key in CorrectionListId]: {
-    i18nKey: ParseKeys;
-    queryParams: FetchResultsParams;
-    disabledFilters: ResultParam[];
-  };
-};
-
-export const correctionListConfig: CorrectionListSearchConfig = {
-  ApplicableCategoriesWithNonApplicableChannel: {
-    i18nKey: 'tasks.nvi.correction_list_type.applicable_category_in_non_applicable_channel',
-    queryParams: {
-      categoryShould: nviApplicableTypes,
-      allScientificValues: ScientificValueLevels.LevelZero,
-    },
-    disabledFilters: [],
-  },
-  NonApplicableCategoriesWithApplicableChannel: {
-    i18nKey: 'tasks.nvi.correction_list_type.non_applicable_category_in_applicable_channel',
-    queryParams: {
-      categoryNot: nviApplicableTypes,
-      scientificValue: [ScientificValueLevels.LevelOne, ScientificValueLevels.LevelTwo].join(','),
-    },
-    disabledFilters: [ResultParam.CategoryShould],
-  },
-  AnthologyWithoutChapter: {
-    i18nKey: 'tasks.nvi.correction_list_type.anthology_without_chapter',
-    queryParams: {
-      categoryShould: [BookType.Anthology],
-      hasNoChildren: false,
-    },
-    disabledFilters: [],
-  },
-  BooksWithLessThan50Pages: {
-    i18nKey: 'tasks.nvi.correction_list_type.book_with_less_than_50_pages',
-    queryParams: {
-      categoryShould: Object.values(BookType),
-      publicationPages: '0,50',
-    },
-    disabledFilters: [],
-  },
-  AnthologyWithApplicableChapter: {
-    i18nKey: 'tasks.nvi.correction_list_type.anthology_with_applicable_chapter',
-    queryParams: {
-      categoryShould: [BookType.Anthology],
-      hasChildren: true,
-      scientificValue: [ScientificValueLevels.LevelOne, ScientificValueLevels.LevelTwo].join(','),
-    },
-    disabledFilters: [],
-  },
-  UnidentifiedContributorWithIdentifiedAffiliation: {
-    i18nKey: 'tasks.nvi.correction_list_type.unidentified_contributor_with_identified_affiliation',
-    queryParams: {
-      unidentifiedNorwegian: true,
-      categoryShould: nviApplicableTypes,
-    },
-    disabledFilters: [],
-  },
-};
 
 export const nviCorrectionListQueryKey = 'list';
 
@@ -91,6 +21,7 @@ export const NviCorrectionList = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const listId = searchParams.get(nviCorrectionListQueryKey) as CorrectionListId | null;
+  const correctionListConfig = useCorrectionListConfig();
   const listConfig = listId && correctionListConfig[listId];
 
   const registrationParams = useRegistrationsQueryParams();
