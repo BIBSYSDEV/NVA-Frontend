9ab5a789787324fc283ef86233ea751f19c4455b
nina.andersen <denektenina@gmail.com>
2025-11-19 10:12:12 +0100

    Added delay on NVI candidate fetch

--- src/pages/messages/components/NviDialoguePanel.tsx
+++ src/pages/messages/components/NviDialoguePanel.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Skeleton, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { NviStatusChip } from '../../../components/StatusChip';
@@ -11,9 +11,14 @@ import { NviCandidateActions } from './NviCandidateActions';
 interface NviDialoguePanelProps {
   nviCandidate: NviCandidate;
   nviCandidateQueryKey: string[];
+  isUpdatingNviCandidateInfo?: boolean;
 }
 
-export const NviDialoguePanel = ({ nviCandidate, nviCandidateQueryKey }: NviDialoguePanelProps) => {
+export const NviDialoguePanel = ({
+  nviCandidate,
+  nviCandidateQueryKey,
+  isUpdatingNviCandidateInfo = false,
+}: NviDialoguePanelProps) => {
   const { t } = useTranslation();
   const user = useSelector((state: RootState) => state.user);
 
@@ -39,32 +44,48 @@ export const NviDialoguePanel = ({ nviCandidate, nviCandidateQueryKey }: NviDial
         <Typography variant="h3" component="h2">
           {t('common.nvi_short')}
         </Typography>
-        {candidateStatus && <NviStatusChip status={candidateStatus} />}
+        {isUpdatingNviCandidateInfo ? (
+          <Skeleton width={100} height={40} />
+        ) : (
+          candidateStatus && <NviStatusChip status={candidateStatus} />
+        )}
       </Box>
-
-      <Box
-        sx={{
-          mx: '1rem',
-          display: 'grid',
-          gap: '1rem',
-          gridTemplateAreas: isPendingCandidate
-            ? hasProblem
-              ? "'curator' 'approvals' 'divider0' 'problem' 'divider1' 'actions' 'divider2' 'comment'"
-              : "'curator' 'approvals' 'divider1' 'actions' 'divider2' 'comment'"
-            : hasProblem
-              ? "'curator' 'approvals' 'divider0' 'problem' 'divider1' 'comment' 'divider2' 'actions'"
-              : "'curator' 'approvals' 'divider1' 'comment' 'divider2' 'actions'",
-        }}>
-        {periodStatus === 'OpenPeriod' ? (
-          <NviCandidateActions nviCandidate={nviCandidate} nviCandidateQueryKey={nviCandidateQueryKey} />
-        ) : periodStatus === 'ClosedPeriod' ? (
-          <Typography sx={{ p: '1rem', bgcolor: 'nvi.main' }}>{t('tasks.nvi.reporting_period_closed')}</Typography>
-        ) : periodStatus === 'NoPeriod' ? (
-          <Typography sx={{ p: '1rem', bgcolor: 'nvi.main' }}>{t('tasks.nvi.reporting_period_missing')}</Typography>
-        ) : null}
-
-        <NviApprovals approvals={nviCandidate?.approvals ?? []} />
-      </Box>
+      {!isUpdatingNviCandidateInfo ? (
+        <Box
+          sx={{
+            mx: '1rem',
+            display: 'grid',
+            gap: '1rem',
+            gridTemplateAreas: isPendingCandidate
+              ? hasProblem
+                ? "'curator' 'approvals' 'divider0' 'problem' 'divider1' 'actions' 'divider2' 'comment'"
+                : "'curator' 'approvals' 'divider1' 'actions' 'divider2' 'comment'"
+              : hasProblem
+                ? "'curator' 'approvals' 'divider0' 'problem' 'divider1' 'comment' 'divider2' 'actions'"
+                : "'curator' 'approvals' 'divider1' 'comment' 'divider2' 'actions'",
+          }}>
+          {periodStatus === 'OpenPeriod' ? (
+            <NviCandidateActions nviCandidate={nviCandidate} nviCandidateQueryKey={nviCandidateQueryKey} />
+          ) : periodStatus === 'ClosedPeriod' ? (
+            <Typography sx={{ p: '1rem', bgcolor: 'nvi.main' }}>{t('tasks.nvi.reporting_period_closed')}</Typography>
+          ) : periodStatus === 'NoPeriod' ? (
+            <Typography sx={{ p: '1rem', bgcolor: 'nvi.main' }}>{t('tasks.nvi.reporting_period_missing')}</Typography>
+          ) : null}
+          <NviApprovals approvals={nviCandidate?.approvals ?? []} />
+        </Box>
+      ) : (
+        <Box sx={{ mx: '1rem', display: 'flex', flexDirection: 'column', gap: 0 }}>
+          <Skeleton width="100%" height={80} />
+          <Skeleton width="100%" height={150} />
+          <Skeleton width="100%" height={60} />
+          <Skeleton width="100%" height={100} />
+          <Skeleton width="100%" height={40} />
+          <Skeleton width="100%" height={80} />
+          <Skeleton width="100%" height={40} />
+          <Skeleton width="100%" height={100} />
+          <Skeleton width="100%" height={150} />
+        </Box>
+      )}
     </>
   );
 };
