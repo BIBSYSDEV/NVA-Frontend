b2f99573091f9b39b3693cdbd492f273911d0fa7
Nina Andersen <denektenina@gmail.com>
2025-12-02 15:05:51 +0100

    Changed order on NVI-items in side bar  (#8002)
    * Changed order on NVI-items in side bar and created new disputes-button that leads to empty page for now

--- src/utils/dataTestIds.ts
+++ src/utils/dataTestIds.ts
@@ -786,6 +786,8 @@ export const dataTestId = {
       statusFilter: 'status-filter',
       showCandidateSearchButton: 'show-candidate-search-button',
       showReportingStatusButton: 'show-reporting-status-button',
+      showDisputesButton: 'show-disputes-button',
+      showPublicationPointsButton: 'show-publication-points-button',
       yearSelect: 'year-select',
       visibilitySelect: 'visibility-select',
     },
