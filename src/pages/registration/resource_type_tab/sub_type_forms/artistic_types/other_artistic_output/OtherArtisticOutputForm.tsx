fb552b8a0a9887076706dc6e6d9fe9920521abb0
Isar Buzza <53341294+iBuzza@users.noreply.github.com>
2026-01-19 14:36:03 +0100

    NP-50481 New Artistic type (#8137)
    * created new type and form for other artistic result
    
    * remove unused fields
    
    * update referenceValidation

---
+++ src/pages/registration/resource_type_tab/sub_type_forms/artistic_types/other_artistic_output/OtherArtisticOutputForm.tsx
@@ -0,0 +1,125 @@
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import {
+  Box,
+  Button,
+  FormHelperText,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { ErrorMessage, Field, FieldArray, FieldArrayRenderProps, FieldProps, useFormikContext } from 'formik';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { alternatingTableRowColor } from '../../../../../../themes/mainTheme';
+import { ResourceFieldNames } from '../../../../../../types/publicationFieldNames';
+import { ArtisticRegistration } from '../../../../../../types/publication_types/artisticRegistration.types';
+import { dataTestId } from '../../../../../../utils/dataTestIds';
+import { OutputRow } from '../OutputRow';
+import { VenueModal } from '../design/VenueModal';
+
+export const OtherArtisticOutputForm = () => {
+  const { t } = useTranslation();
+  const { values, touched, errors } = useFormikContext<ArtisticRegistration>();
+  const venues = values.entityDescription.reference.publicationInstance.venues ?? [];
+
+  const [openNewVenueModal, setOpenNewVenueModal] = useState(false);
+
+  return (
+    <>
+      <Field name={ResourceFieldNames.PublicationInstanceTypeDescription}>
+        {({ field, meta: { error, touched } }: FieldProps<string>) => (
+          <TextField
+            id={field.name}
+            data-testid={dataTestId.registrationWizard.resourceType.subtypeDescriptionField}
+            variant="filled"
+            fullWidth
+            {...field}
+            required
+            multiline
+            label={t('registration.resource_type.type_work_specified')}
+            error={!!error && touched}
+            helperText={<ErrorMessage name={field.name} />}
+          />
+        )}
+      </Field>
+
+      <Field name={ResourceFieldNames.PublicationInstanceDescription}>
+        {({ field, meta: { error, touched } }: FieldProps<string>) => (
+          <TextField
+            id={field.name}
+            data-testid={dataTestId.registrationWizard.resourceType.artisticDescriptionField}
+            variant="filled"
+            fullWidth
+            {...field}
+            multiline
+            label={t('registration.resource_type.more_info_about_work')}
+            error={!!error && touched}
+            helperText={<ErrorMessage name={field.name} />}
+          />
+        )}
+      </Field>
+
+      <div>
+        <Typography variant="h3" component="h2" gutterBottom>
+          {t('registration.resource_type.artistic.announcements')}
+        </Typography>
+        <FieldArray name={ResourceFieldNames.PublicationInstanceVenues}>
+          {({ push, replace, remove, move, name }: FieldArrayRenderProps) => (
+            <>
+              {venues.length > 0 && (
+                <Table sx={alternatingTableRowColor}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>{t('common.order')}</TableCell>
+                      <TableCell>{t('common.place')}</TableCell>
+                      <TableCell>{t('common.actions')}</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {venues.map((venue, index) => (
+                      <OutputRow
+                        key={index}
+                        item={venue}
+                        updateItem={(newVenue) => replace(index, newVenue)}
+                        removeItem={() => remove(index)}
+                        moveItem={(newIndex) => move(index, newIndex)}
+                        index={index}
+                        maxIndex={venues.length - 1}
+                      />
+                    ))}
+                  </TableBody>
+                </Table>
+              )}
+              {!!touched.entityDescription?.reference?.publicationInstance?.venues &&
+                typeof errors.entityDescription?.reference?.publicationInstance?.venues === 'string' && (
+                  <Box mt="1rem">
+                    <FormHelperText error>
+                      <ErrorMessage name={name} />
+                    </FormHelperText>
+                  </Box>
+                )}
+
+              <Button
+                data-testid={dataTestId.registrationWizard.resourceType.addVenueButton}
+                onClick={() => setOpenNewVenueModal(true)}
+                variant="outlined"
+                sx={{ mt: '1rem' }}
+                startIcon={<AddCircleOutlineIcon />}>
+                {t('registration.resource_type.artistic.add_announcement')}
+              </Button>
+              <VenueModal
+                onSubmit={(newVenue) => push({ ...newVenue, type: 'Venue' })}
+                open={openNewVenueModal}
+                closeModal={() => setOpenNewVenueModal(false)}
+              />
+            </>
+          )}
+        </FieldArray>
+      </div>
+    </>
+  );
+};
