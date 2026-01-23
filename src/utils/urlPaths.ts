adbfaf06902401eeb2359348ba7c00ae42d2d06f
nina.andersen <denektenina@gmail.com>
2026-01-23 15:56:04 +0100

    Reverting file

--- src/utils/urlPaths.ts
+++ src/utils/urlPaths.ts
@@ -1,5 +1,5 @@
 import { To } from 'react-router';
-import { NviCandidateGlobalStatus, NviCandidatesSearchParam, NviCandidateStatus } from '../api/searchApi';
+import { NviCandidateGlobalStatus, NviCandidateStatus } from '../api/searchApi';
 import { Registration, RegistrationStatus } from '../types/registration.types';
 import { getIdentifierFromId } from './general-helpers';
 
@@ -209,18 +209,4 @@ export const getNviCandidatesSearchPath = ({
     searchParams.set('excludeSubUnits', excludeSubUnits.toString());
   }
   return `${UrlPathTemplate.TasksNvi}?${searchParams.toString()}`;
-};
-
-export interface NviDisputesSearchParams {
-  affiliations?: string[];
-}
-
-export const getDisputesSearchPath = ({ affiliations }: NviDisputesSearchParams) => {
-  const searchParams = new URLSearchParams();
-
-  if (affiliations !== undefined) {
-    const value = Array.isArray(affiliations) ? affiliations.join(',') : affiliations;
-    searchParams.set(NviCandidatesSearchParam.Affiliations, value);
-  }
-  return `${UrlPathTemplate.TasksNviDisputes}?${searchParams.toString()}`;
 };
