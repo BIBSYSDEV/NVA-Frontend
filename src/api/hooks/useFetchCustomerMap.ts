b6a24ee083f2104576a1715bb1927a1251a8a25c
nina.andersen <denektenina@gmail.com>
2026-01-28 21:38:18 +0100

    usedMemo

--- src/api/hooks/useFetchCustomerMap.ts
+++ src/api/hooks/useFetchCustomerMap.ts
@@ -1,11 +1,12 @@
+import { useMemo } from 'react';
 import { useFetchCustomers } from './useFetchCustomers';
 
 export const useFetchCustomerMap = () => {
   const customersQuery = useFetchCustomers();
-  const customers = customersQuery.data?.customers ?? [];
+  const customers = useMemo(() => customersQuery.data?.customers ?? [], [customersQuery.data?.customers]);
 
   return {
-    nvaCustomers: new Map(customers.map((customer) => [customer.cristinId, customer])),
+    nvaCustomers: useMemo(() => new Map(customers.map((customer) => [customer.cristinId, customer])), [customers]),
     isFetchingCustomerMap: customersQuery.isLoading,
   };
 };
