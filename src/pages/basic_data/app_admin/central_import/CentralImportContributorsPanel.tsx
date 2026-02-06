99c681bd72c7946e5e7505603b7c29269c2aedf2
Isar Buzza <isar.buzza@sikt.no>
2026-02-06 09:52:04 +0100

    added filter to only show norwegian contributors

--- src/pages/basic_data/app_admin/central_import/CentralImportContributorsPanel.tsx
+++ src/pages/basic_data/app_admin/central_import/CentralImportContributorsPanel.tsx
@@ -1,9 +1,10 @@
-import { Box, Divider, Typography } from '@mui/material';
+import { Box, Checkbox, Divider, FormControlLabel, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { Fragment } from 'react/jsx-runtime';
 import { ImportContributor } from '../../../../types/importCandidate.types';
 import { CentralImportContributorBox } from './CentralImportContributorBox';
 import { CentralImportSearchForContributor } from './CentralImportSearchForContributor';
+import { useState } from 'react';
 
 interface CentralImportContributorsPanelProps {
   importCandidateContributors?: ImportContributor[];
@@ -13,25 +14,45 @@ export const CentralImportContributorsPanel = ({
   importCandidateContributors,
 }: CentralImportContributorsPanelProps) => {
   const { t } = useTranslation();
+  const [showOnlyNorwegianContributors, setShowOnlyNorwegianContributors] = useState(false);
+  const contributors = importCandidateContributors ?? [];
+
+  const norwegianContributors = contributors.filter((contributor) =>
+    contributor.affiliations?.some((aff) => aff.sourceOrganization?.country?.code?.toLowerCase() === 'nor')
+  );
+
+  const contributorsToDisplay = showOnlyNorwegianContributors ? norwegianContributors : contributors;
 
   return (
-    <Box sx={{ display: 'grid', gridTemplateColumns: 'auto 1fr 1fr', columnGap: '1rem', rowGap: '1rem' }}>
-      <Typography sx={{ gridColumn: '1', fontWeight: 'bold' }}>{t('common.order')}</Typography>
-      <Typography sx={{ gridColumn: '2', fontWeight: 'bold' }}>
-        {t('basic_data.central_import.import_candidate')}
-      </Typography>
-      <Typography sx={{ gridColumn: '3', fontWeight: 'bold' }}>{t('common.page_title')}</Typography>
+    <>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={showOnlyNorwegianContributors}
+            onChange={(event) => setShowOnlyNorwegianContributors(!showOnlyNorwegianContributors)}
+            color="secondary"
+          />
+        }
+        label={t('show_only_norwegian_contributors')}
+      />
+      <Box sx={{ display: 'grid', gridTemplateColumns: 'auto 1fr 1fr', columnGap: '1rem', rowGap: '1rem' }}>
+        <Typography sx={{ gridColumn: '1', fontWeight: 'bold' }}>{t('common.order')}</Typography>
+        <Typography sx={{ gridColumn: '2', fontWeight: 'bold' }}>
+          {t('basic_data.central_import.import_candidate')}
+        </Typography>
+        <Typography sx={{ gridColumn: '3', fontWeight: 'bold' }}>{t('common.page_title')}</Typography>
 
-      <Divider orientation="horizontal" sx={{ gridColumn: '1/-1' }} />
+        <Divider orientation="horizontal" sx={{ gridColumn: '1/-1' }} />
 
-      {!!importCandidateContributors &&
-        importCandidateContributors.map((importContributor) => (
-          <Fragment key={importContributor.sequence}>
-            <CentralImportContributorBox importContributor={importContributor} />
-            <CentralImportSearchForContributor importContributor={importContributor} />
-            <Divider orientation="horizontal" sx={{ gridColumn: '1/-1' }} />
-          </Fragment>
-        ))}
-    </Box>
+        {!!contributorsToDisplay &&
+          contributorsToDisplay.map((importContributor) => (
+            <Fragment key={importContributor.sequence}>
+              <CentralImportContributorBox importContributor={importContributor} />
+              <CentralImportSearchForContributor importContributor={importContributor} />
+              <Divider orientation="horizontal" sx={{ gridColumn: '1/-1' }} />
+            </Fragment>
+          ))}
+      </Box>
+    </>
   );
 };
