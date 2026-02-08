9ccff52e176760930743fa6dcd943f6df3cf5b85
Isar Buzza <isar@buzza.no>
2026-02-08 15:19:58 +0100

    search for contributor

---
+++ src/pages/basic_data/app_admin/central_import/CentralImportContributorSearchBar.tsx
@@ -0,0 +1,128 @@
+import SearchIcon from '@mui/icons-material/Search';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSearchForPerson } from '../../../../api/hooks/useSearchForPerson';
+import { ContributorName } from '../../../../components/ContributorName';
+import { OrganizationBox } from '../../../../components/institution/OrganizationBox';
+import { ListPagination } from '../../../../components/ListPagination';
+import { ListSkeleton } from '../../../../components/ListSkeleton';
+import { ImportContributor } from '../../../../types/importCandidate.types';
+import { dataTestId } from '../../../../utils/dataTestIds';
+import { getFullCristinName } from '../../../../utils/user-helpers';
+
+interface CentralImportContributorSearchBarProps {
+  importContributor: ImportContributor;
+  isExpanded: string | boolean;
+}
+
+export const CentralImportContributorSearchBar = ({
+  importContributor,
+  isExpanded,
+}: CentralImportContributorSearchBarProps) => {
+  const { t } = useTranslation();
+  const [searchTerm, setSearchTerm] = useState(importContributor.identity.name);
+  const [submittedSearchTerm, setSubmittedSearchTerm] = useState(searchTerm);
+  const [page, setPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const personQuery = useSearchForPerson({
+    enabled: !!isExpanded,
+    name: submittedSearchTerm,
+    results: rowsPerPage,
+    page,
+  });
+  const userSearch = personQuery.data?.hits ?? [];
+
+  const handleSearch = () => {
+    setSubmittedSearchTerm(searchTerm);
+    personQuery.refetch();
+  };
+
+  return (
+    <>
+      <TextField
+        type="search"
+        data-testid={dataTestId.registrationWizard.contributors.searchField}
+        variant="outlined"
+        fullWidth
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+          if (page !== 1) {
+            setPage(1);
+          }
+        }}
+        size="small"
+        defaultValue={searchTerm}
+      />
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Button
+          variant="contained"
+          color="secondary"
+          sx={{ padding: '0.1rem 0.75rem', width: 'fit-content' }}
+          data-testid={dataTestId.registrationWizard.contributors.verifyContributorButton(
+            importContributor.identity.name
+          )}
+          onClick={handleSearch}
+          startIcon={<SearchIcon />}>
+          {t('basic_data.add_employee.search_for_person')}
+        </Button>
+        <Button variant="contained" color="tertiary" sx={{ padding: '0.1rem 0.75rem', width: 'fit-content' }}>
+          {t('update_name')}
+        </Button>
+      </Box>
+
+      <Typography>{t('contributor_search_hits', { count: personQuery.data?.size ?? 0 })}</Typography>
+      <ListPagination
+        count={personQuery.data?.size ?? 0}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={setPage}
+        onRowsPerPageChange={(newRowsPerPage) => {
+          setRowsPerPage(newRowsPerPage);
+          setPage(1);
+        }}>
+        {personQuery.isFetching ? (
+          <ListSkeleton arrayLength={3} minWidth={100} height={80} />
+        ) : userSearch && userSearch.length > 0 ? (
+          <>
+            {userSearch.map((user, index) => (
+              <Box
+                key={index}
+                sx={{
+                  border: '1px solid',
+                  borderRadius: '4px',
+                  boxShadow: '0px 3px 3px 0px rgba(0, 0, 0, 0.30)',
+                  backgroundColor: 'white',
+                  height: 'fit-content',
+                  p: '0.5rem',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '0.5rem',
+                }}>
+                <ContributorName
+                  name={getFullCristinName(user.names)}
+                  hasVerifiedAffiliation={user.verified ? user.verified : false}
+                  id={user.id}
+                />
+                {user.affiliations.map((affiliation) => (
+                  <OrganizationBox key={affiliation.organization} unitUri={affiliation.organization} />
+                ))}
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  sx={{ padding: '0.1rem 0.75rem', width: 'fit-content', mt: '0.5rem' }}
+                  data-testid={dataTestId.registrationWizard.contributors.verifyContributorButton(
+                    importContributor.identity.name
+                  )}>
+                  {t('select_person_and_affiliation')}
+                </Button>
+              </Box>
+            ))}
+          </>
+        ) : (
+          submittedSearchTerm && <Typography>{t('common.no_hits')}</Typography>
+        )}
+      </ListPagination>
+    </>
+  );
+};
--- src/pages/basic_data/app_admin/central_import/CentralImportSearchForContributor.tsx
+++ src/pages/basic_data/app_admin/central_import/CentralImportSearchForContributor.tsx
@@ -1,6 +1,7 @@
+import AddIcon from '@mui/icons-material/AddCircleOutline';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import SearchIcon from '@mui/icons-material/Search';
-import { Accordion, AccordionDetails, AccordionSummary, Box, Button, TextField } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button } from '@mui/material';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ContributorName } from '../../../../components/ContributorName';
 import { OrganizationBox } from '../../../../components/institution/OrganizationBox';
@@ -10,6 +11,7 @@ import { UnconfirmedOrganization } from '../../../../types/common.types';
 import { ImportContributor } from '../../../../types/importCandidate.types';
 import { Organization } from '../../../../types/organization.types';
 import { dataTestId } from '../../../../utils/dataTestIds';
+import { CentralImportContributorSearchBar } from './CentralImportContributorSearchBar';
 
 interface CentralImportSearchForContributorProps {
   importContributor: ImportContributor;
@@ -17,6 +19,14 @@ interface CentralImportSearchForContributorProps {
 
 export const CentralImportSearchForContributor = ({ importContributor }: CentralImportSearchForContributorProps) => {
   const { t } = useTranslation();
+  const [expanded, setExpanded] = useState<string | boolean>(false);
+  const [openAffiliationModal, setOpenAffiliationModal] = useState(false);
+  const toggleAffiliationModal = () => setOpenAffiliationModal(!openAffiliationModal);
+
+  const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
   const isVerified = importContributor.identity.verificationStatus === 'Verified';
   const verifiedTargetOrganizations = importContributor.affiliations
     .map((a) => a.targetOrganization)
@@ -29,6 +39,8 @@ export const CentralImportSearchForContributor = ({ importContributor }: Central
   return (
     <Box sx={{ gridColumn: '3', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
       <Accordion
+        expanded={expanded === 'panel1'}
+        onChange={handleChange('panel1')}
         slotProps={{ heading: { component: 'p' } }}
         sx={{
           border: '1px solid',
@@ -57,29 +69,7 @@ export const CentralImportSearchForContributor = ({ importContributor }: Central
           </Box>
         </AccordionSummary>
         <AccordionDetails sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-          <TextField
-            type="search"
-            data-testid={dataTestId.registrationWizard.contributors.searchField}
-            variant="outlined"
-            fullWidth
-            size="small"
-            defaultValue={importContributor.identity.name}
-          />
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Button
-              variant="contained"
-              color="secondary"
-              sx={{ padding: '0.1rem 0.75rem', width: 'fit-content' }}
-              data-testid={dataTestId.registrationWizard.contributors.verifyContributorButton(
-                importContributor.identity.name
-              )}
-              startIcon={<SearchIcon />}>
-              {t('basic_data.add_employee.search_for_person')}
-            </Button>
-            <Button variant="contained" color="tertiary" sx={{ padding: '0.1rem 0.75rem', width: 'fit-content' }}>
-              {t('update_name')}
-            </Button>
-          </Box>
+          <CentralImportContributorSearchBar isExpanded={expanded} importContributor={importContributor} />
         </AccordionDetails>
       </Accordion>
       {verifiedTargetOrganizations.map((org) => {
@@ -88,6 +78,16 @@ export const CentralImportSearchForContributor = ({ importContributor }: Central
       {unverifiedTargetOrganizations.map((org, index) => {
         return <UnconfirmedOrganizationBox key={index} name={org.name} />;
       })}
+
+      <Button
+        color="tertiary"
+        variant="contained"
+        sx={{ padding: '0.1rem 0.75rem', width: 'fit-content' }}
+        data-testid={dataTestId.registrationWizard.contributors.addAffiliationButton}
+        startIcon={<AddIcon />}
+        onClick={toggleAffiliationModal}>
+        {t('registration.contributors.add_affiliation')}
+      </Button>
     </Box>
   );
 };
