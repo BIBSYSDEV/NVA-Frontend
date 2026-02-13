940d8d6c87823ed80ac68a944853ac7132ab6d46
Nina Andersen <denektenina@gmail.com>
2026-02-13 10:15:47 +0100

    Np 50743 pub points table (#8216)

--- src/pages/basic_data/app_admin/NviAdminPublicationPointsPage.tsx
+++ src/pages/basic_data/app_admin/NviAdminPublicationPointsPage.tsx
@@ -1,8 +1,22 @@
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Box, Button, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { PercentageWithIcon } from '../../../components/atoms/PercentageWithIcon';
+import { HorizontalBox } from '../../../components/styled/Wrappers';
+import { Sector } from '../../../types/customerInstitution.types';
 import { dataTestId } from '../../../utils/dataTestIds';
 import { NviStatusWrapper } from '../../messages/components/NviStatusWrapper';
 
@@ -10,6 +24,62 @@ export const NviAdminPublicationPointsPage = () => {
   const { t } = useTranslation();
   const [textExpanded, setTextExpanded] = useState(false);
   const detailsId = 'publication-points-details';
+
+  const mockData: Array<{
+    id: string;
+    institution: string;
+    sector: Sector;
+    approved: number;
+    total: number;
+    rejected: number;
+    publicationPoints: number;
+  }> = [
+    {
+      id: 'test1',
+      institution: 'Test institusjon 1',
+      sector: Sector.Uhi,
+      approved: 1,
+      total: 24,
+      rejected: 15,
+      publicationPoints: 31.45,
+    },
+    {
+      id: 'test2',
+      institution: 'Test institusjon 2',
+      sector: Sector.Uhi,
+      approved: 10,
+      total: 14,
+      rejected: 2,
+      publicationPoints: 67.31,
+    },
+    {
+      id: 'test3',
+      institution: 'Test institusjon 3',
+      sector: Sector.Health,
+      approved: 7,
+      total: 12,
+      rejected: 1,
+      publicationPoints: 13.57,
+    },
+    {
+      id: 'test4',
+      institution: 'Test institusjon 4',
+      sector: Sector.Uhi,
+      approved: 8,
+      total: 240,
+      rejected: 13,
+      publicationPoints: 12,
+    },
+    {
+      id: 'test5',
+      institution: 'Test institusjon 5',
+      sector: Sector.Uhi,
+      approved: 55,
+      total: 56,
+      rejected: 1,
+      publicationPoints: 98.49,
+    },
+  ];
 
   return (
     <NviStatusWrapper
@@ -33,7 +103,43 @@ export const NviAdminPublicationPointsPage = () => {
             </Typography>
           }
         </Box>
-      }
-    />
+      }>
+      <TableContainer component={Paper} variant="outlined">
+        <Table size="small">
+          <TableHead>
+            <TableRow sx={{ whiteSpace: 'nowrap', bgcolor: 'white' }}>
+              <TableCell>{t('common.institution')}</TableCell>
+              <TableCell>{t('sector')}</TableCell>
+              <TableCell align="center">{t('approved')}</TableCell>
+              <TableCell align="center">{t('publication_points')}</TableCell>
+              <TableCell align="center">{t('percentage_controlled')}</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {mockData.map((obj) => {
+              const controlled = obj.total > 0 ? (obj.approved + obj.rejected) / obj.total : 0;
+              return (
+                <TableRow key={obj.id} sx={{ height: '4rem' }}>
+                  <TableCell>{obj.institution}</TableCell>
+                  <TableCell>{t(`basic_data.institutions.sector_values.${obj.sector}`)}</TableCell>
+                  <TableCell align="center">{obj.approved}</TableCell>
+                  <TableCell align="center">{obj.publicationPoints}</TableCell>
+                  <TableCell align="center" sx={{ py: 0 }}>
+                    <HorizontalBox sx={{ justifyContent: 'center' }}>
+                      <PercentageWithIcon
+                        warningThresholdMinimum={30}
+                        successThresholdMinimum={100}
+                        displayPercentage={Math.round(controlled * 100)}
+                        alternativeIfZero={'-'}
+                      />
+                    </HorizontalBox>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </NviStatusWrapper>
   );
 };
