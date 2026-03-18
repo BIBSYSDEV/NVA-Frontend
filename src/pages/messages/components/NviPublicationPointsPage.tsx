858963f793a2bf89eeaf7a7cb01e6c3737f7d2df
Nina Andersen <denektenina@gmail.com>
2026-03-18 10:41:21 +0100

    New column for percentage approved in pub points (NP-50922) (#8301)
    * New column for percentage approved in pub points (NP-50922)
    
    * Hiding warning icon in publication points
    
    * Updated text
    
    * Added column for candidates approved by us (NP-50923)
    
    * Changed name for approved-column (NP-50925)
    
    * Removed global status for locally approved-link
    
    * Added column for candidates pending verification by others (NP-50924)
    
    * Added comment to make it clear why we have a visually hidden cell in the table header

--- src/pages/messages/components/NviPublicationPointsPage.tsx
+++ src/pages/messages/components/NviPublicationPointsPage.tsx
@@ -29,11 +29,15 @@ export const NviPublicationPointsPage = () => {
       <TableContainer component={Paper} variant="outlined">
         <Table size="small">
           <TableHead>
-            <TableRow sx={{ whiteSpace: 'nowrap', bgcolor: 'white' }}>
-              <TableCell sx={{ width: '80%' }}>{t('registration.contributors.department')}</TableCell>
-              <TableCell>{t('tasks.nvi.status.Approved')}</TableCell>
-              <TableCell>{t('tasks.nvi.publication_points')}</TableCell>
+            <TableRow sx={{ bgcolor: 'white' }}>
+              <TableCell sx={{ width: '30%' }}>{t('registration.contributors.department')}</TableCell>
+              <TableCell align="center">{t('candidates_we_have_approved')}</TableCell>
+              <TableCell align="center">{t('tasks.nvi.candidates_pending_verification_by_others')}</TableCell>
+              <TableCell align="center">{t('candidates_everyone_has_approved')}</TableCell>
+              <TableCell align="center">{t('tasks.nvi.publication_points')}</TableCell>
+              <TableCell align="center">{t('percentage_approved_by_all')}</TableCell>
               <TableCell>
+                {/* This cell is hidden to make the number of cells in the table header the same as in the table row, where we display an accordion-like arrow to expand or close rows that have subunits */}
                 <Box component="span" sx={visuallyHidden}>
                   {t('tasks.nvi.show_subunits')}
                 </Box>
--- src/pages/messages/components/NviStatusPage.tsx
+++ src/pages/messages/components/NviStatusPage.tsx
@@ -47,13 +47,14 @@ export const NviStatusPage = () => {
           <TableHead>
             <TableRow sx={{ whiteSpace: 'nowrap', bgcolor: 'white' }}>
               <TableCell sx={{ width: '60%' }}>{t('registration.contributors.department')}</TableCell>
-              <TableCell>{t('tasks.nvi.status.New')}</TableCell>
-              <TableCell>{t('tasks.nvi.status.Pending')}</TableCell>
-              <TableCell>{t('tasks.nvi.status.Approved')}</TableCell>
-              <TableCell>{t('tasks.nvi.status.Rejected')}</TableCell>
-              <TableCell>{t('common.total_number')}</TableCell>
-              <TableCell>{t('percentage_controlled')}</TableCell>
+              <TableCell align="center">{t('tasks.nvi.status.New')}</TableCell>
+              <TableCell align="center">{t('tasks.nvi.status.Pending')}</TableCell>
+              <TableCell align="center">{t('tasks.nvi.status.Approved')}</TableCell>
+              <TableCell align="center">{t('tasks.nvi.status.Rejected')}</TableCell>
+              <TableCell align="center">{t('common.total_number')}</TableCell>
+              <TableCell align="center">{t('percentage_controlled')}</TableCell>
               <TableCell>
+                {/* This cell is hidden to make the number of cells in the table header the same as in the table row, where we display an accordion-like arrow to expand or close rows that have subunits */}
                 <Box component="span" sx={visuallyHidden}>
                   {t('tasks.nvi.show_subunits')}
                 </Box>
