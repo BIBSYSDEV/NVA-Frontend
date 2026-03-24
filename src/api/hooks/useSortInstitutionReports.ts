1661c5431ca8a599b5818d3b3a3a16b890089415
nina.andersen <denektenina@gmail.com>
2026-03-24 13:37:32 +0100

    QA comments

--- src/api/hooks/useSortInstitutionReports.ts
+++ src/api/hooks/useSortInstitutionReports.ts
@@ -11,6 +11,10 @@ export const useSortInstitutionReports = (reports: InstitutionReport[]) => {
   const orderBy = searchParams.get('orderBy');
   const sort = searchParams.get('sort');
 
+  if (!orderBy || !isNviAdminOrderBy(orderBy) || (sort !== 'asc' && sort !== 'desc')) {
+    return reports;
+  }
+
   return reports
     .map((report, index) => ({ report, index }))
     .sort((a, b) => {
