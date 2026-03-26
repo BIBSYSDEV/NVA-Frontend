67f88876f49c3a366b8cf96f259d247fd1585564
Isar Buzza <isar.buzza@sikt.no>
2026-03-26 10:26:09 +0100

    add loading indicator

--- src/pages/messages/components/ExportNviStatusLink.tsx
+++ src/pages/messages/components/ExportNviStatusLink.tsx
@@ -1,5 +1,5 @@
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
-import { Link } from '@mui/material';
+import { CircularProgress, Link } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useFetchNviReportExport } from '../../../api/hooks/useFetchNviReportExport';
 import { dataTestId } from '../../../utils/dataTestIds';
@@ -12,6 +12,7 @@ export const ExportNviStatusLink = ({ acronym }: ExportNviStatusButtonProps) => 
   const { t } = useTranslation();
   const { year } = useNviCandidatesParams();
   const fetchNviApprovalReportQuery = useFetchNviReportExport(year, acronym);
+  const isFetching = fetchNviApprovalReportQuery.isFetching;
 
   const handleClick = () => {
     if (!fetchNviApprovalReportQuery.isFetching) {
@@ -26,15 +27,14 @@ export const ExportNviStatusLink = ({ acronym }: ExportNviStatusButtonProps) => 
       data-testid={dataTestId.common.exportLink}
       color="tertiary"
       onClick={handleClick}
-      disabled={fetchNviApprovalReportQuery.isFetching}
+      disabled={isFetching}
       sx={{
         display: 'inline-flex',
         alignItems: 'center',
         gap: 1,
-        opacity: fetchNviApprovalReportQuery.isFetching ? 0.7 : 1,
-        cursor: fetchNviApprovalReportQuery.isFetching ? 'default' : 'pointer',
+        cursor: isFetching ? 'default' : 'pointer',
       }}>
-      <FileDownloadOutlinedIcon fontSize="small" />
+      {isFetching ? <CircularProgress size={15} /> : <FileDownloadOutlinedIcon fontSize="small" />}
       {t('export_dataset_for_nvi_report')}
     </Link>
   );
