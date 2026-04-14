36027f69af042ce62b0db2066ce788d54e7610ab
Isar Buzza <isar.buzza@sikt.no>
2026-04-14 09:42:55 +0200

    add helper text modal

--- src/components/AdminNviPublicationPointsTexts.tsx
+++ src/components/AdminNviPublicationPointsTexts.tsx
@@ -1,12 +1,13 @@
 import { Skeleton, Typography } from '@mui/material';
 import { Trans, useTranslation } from 'react-i18next';
 import { useFetchNviPeriodReport } from '../api/hooks/useFetchNviPeriodReport';
+import { ExportNviStatusLink } from '../pages/messages/components/ExportNviStatusLink';
+import { HelperTextModal } from '../pages/registration/HelperTextModal';
 import { dataTestId } from '../utils/dataTestIds';
 import { formatLocaleNumber } from '../utils/general-helpers';
 import { getDefaultNviYear } from '../utils/hooks/useNviCandidatesParams';
 import { ExpandableNviTopView } from './ExpandableNviTopView';
-import { VerticalBox } from './styled/Wrappers';
-import { ExportNviStatusLink } from '../pages/messages/components/ExportNviStatusLink';
+import { HorizontalBox, VerticalBox } from './styled/Wrappers';
 
 export const AdminNviPublicationPointsTexts = () => {
   const { t } = useTranslation();
@@ -25,14 +26,19 @@ export const AdminNviPublicationPointsTexts = () => {
         {periodReport.isPending ? (
           <Skeleton sx={{ width: '50%' }} />
         ) : periodReport.isError || !periodTotals ? undefined : (
-          <Trans
-            i18nKey="x_results_are_ready_for_reporting_and_they_give_y_publication_points"
-            values={{
-              approvals: formatLocaleNumber(periodTotals.undisputedTotalCount),
-              publication_points: formatLocaleNumber(periodTotals.validPoints),
-            }}
-            components={{ p: <Typography />, b: <strong />, link: <ExportNviStatusLink isOnAdminPage /> }}
-          />
+          <HorizontalBox>
+            <Trans
+              i18nKey="x_results_are_ready_for_reporting_and_they_give_y_publication_points"
+              values={{
+                approvals: formatLocaleNumber(periodTotals.undisputedTotalCount),
+                publication_points: formatLocaleNumber(periodTotals.validPoints),
+              }}
+              components={{ p: <Typography />, b: <strong />, link: <ExportNviStatusLink isOnAdminPage /> }}
+            />
+            <HelperTextModal modalTitle={t('export_dataset_for_nvi_report')}>
+              <Trans i18nKey="export_dataset_for_nvi_report_description" components={{ p: <Typography /> }} />
+            </HelperTextModal>
+          </HorizontalBox>
         )}
         {periodReport.isPending || periodReportLastYear.isPending ? (
           <Skeleton sx={{ width: '40%' }} />
