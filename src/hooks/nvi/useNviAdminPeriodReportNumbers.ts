dca09893fc03b6a64a7c3414321adf0603e7bd0c
nina.andersen <denektenina@gmail.com>
2026-04-16 12:25:11 +0200

    Updated texts for admin publication points view

--- src/hooks/nvi/useNviAdminPeriodReportNumbers.ts
+++ src/hooks/nvi/useNviAdminPeriodReportNumbers.ts
@@ -4,6 +4,7 @@ export const useNviAdminPeriodReportNumbers = (year: number) => {
   const reportQuery = useFetchNviPeriodReport({ year: year, hideErrorMessage: true });
   const reportQueryLastYear = useFetchNviPeriodReport({ year: year - 1, hideErrorMessage: true });
   const totalCount = reportQuery.data?.totals?.undisputedTotalCount;
+  const validPoints = reportQuery.data?.totals?.validPoints;
   const totalCountLastYear = reportQueryLastYear.data?.totals?.undisputedTotalCount;
   const percentageComparedToYearBefore =
     totalCount && totalCountLastYear ? Math.round((totalCount / totalCountLastYear) * 100) : undefined;
@@ -11,6 +12,8 @@ export const useNviAdminPeriodReportNumbers = (year: number) => {
   return {
     totalCount,
     percentageComparedToYearBefore,
+    validPoints,
     isPending: reportQuery.isPending || reportQueryLastYear.isPending,
+    isError: reportQuery.isError || reportQueryLastYear.isError,
   };
 };
