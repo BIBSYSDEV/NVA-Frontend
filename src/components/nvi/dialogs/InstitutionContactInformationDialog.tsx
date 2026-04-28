9df3f99fd64d21d13bc8d66fd2184fde45743518
nina.andersen <denektenina@gmail.com>
2026-04-28 14:26:26 +0200

    Cleaned up

--- src/components/nvi/dialogs/InstitutionContactInformationDialog.tsx
+++ src/components/nvi/dialogs/InstitutionContactInformationDialog.tsx
@@ -1,12 +1,9 @@
 import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { dataTestId } from '../../../utils/dataTestIds';
-import { getFullName } from '../../../utils/user-helpers';
 import { BaseDialog } from '../../_molecules/BaseDialog';
 import { VerticalBox } from '../../styled/Wrappers';
 import { useInstitutionUsersByRole } from '../hooks/useInstitutionUsersByRole';
-import { ContactInformationLayout } from './ContactInformationLayout';
-import { CuratorName } from './CuratorName';
 import { RoleContactInformation } from './RoleContactInformation';
 
 interface InstitutionContactInformationDialogProps {
@@ -36,33 +33,12 @@ export const InstitutionContactInformationDialog = ({
         <Typography>{t('no_contact_information_for_institution')}</Typography>
       ) : (
         <VerticalBox sx={{ gap: '1rem' }}>
-          {editor && (
-            <RoleContactInformation
-              roleName={t('my_page.roles.editor')}
-              name={getFullName(editor.givenName, editor.familyName)}
-              cristinId={editor.cristinId}
-            />
-          )}
+          {editor && <RoleContactInformation roleName={t('my_page.roles.editor')} users={[editor]} />}
           {institutionAdmin && (
-            <RoleContactInformation
-              roleName={t('my_page.roles.institution_admin')}
-              name={getFullName(institutionAdmin.givenName, institutionAdmin.familyName)}
-              cristinId={institutionAdmin.cristinId}
-            />
+            <RoleContactInformation roleName={t('my_page.roles.institution_admin')} users={[institutionAdmin]} />
           )}
           {nviCurators && nviCurators.length > 0 && (
-            <ContactInformationLayout roleName={t('nvi_curators_at_main_unit')}>
-              <VerticalBox
-                sx={{ gap: '0.5rem', ...(nviCurators.length > 5 && { maxHeight: '9rem', overflowY: 'auto' }) }}>
-                {nviCurators.map((curator) => (
-                  <CuratorName
-                    key={curator.username}
-                    name={getFullName(curator.givenName, curator.familyName)}
-                    cristinId={curator.cristinId}
-                  />
-                ))}
-              </VerticalBox>
-            </ContactInformationLayout>
+            <RoleContactInformation roleName={t('nvi_curators_at_main_unit')} users={nviCurators} />
           )}
         </VerticalBox>
       )}
--- src/components/nvi/dialogs/RoleContactInformation.tsx
+++ src/components/nvi/dialogs/RoleContactInformation.tsx
@@ -1,16 +1,30 @@
+import { InstitutionUser } from '../../../types/user.types';
+import { getFullName } from '../../../utils/user-helpers';
+import { VerticalBox } from '../../styled/Wrappers';
 import { ContactInformationLayout } from './ContactInformationLayout';
 import { CuratorName } from './CuratorName';
 
 interface RoleContactInformationProps {
   roleName: string;
-  name: string;
-  cristinId?: string;
+  users: InstitutionUser[];
 }
 
-export const RoleContactInformation = ({ roleName, name, cristinId }: RoleContactInformationProps) => {
+export const RoleContactInformation = ({ roleName, users }: RoleContactInformationProps) => {
   return (
     <ContactInformationLayout roleName={roleName}>
-      <CuratorName name={name} cristinId={cristinId} />
+      {users.length === 1 ? (
+        <CuratorName name={getFullName(users[0].givenName, users[0].familyName)} cristinId={users[0].cristinId} />
+      ) : (
+        <VerticalBox sx={{ gap: '0.5rem', ...(users.length > 5 && { maxHeight: '9rem', overflowY: 'auto' }) }}>
+          {users.map((user) => (
+            <CuratorName
+              key={user.username}
+              name={getFullName(user.givenName, user.familyName)}
+              cristinId={user.cristinId}
+            />
+          ))}
+        </VerticalBox>
+      )}
     </ContactInformationLayout>
   );
 };
