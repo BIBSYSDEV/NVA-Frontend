3a6a50d05be703d2b88ee0ac22f5f1202bc1f57d
Isar Buzza <isar.buzza@sikt.no>
2026-05-13 09:42:10 +0200

    add error message to useBibtexExport

--- src/utils/bibtex/useBibtexExport.ts
+++ src/utils/bibtex/useBibtexExport.ts
@@ -1,13 +1,19 @@
 import { useMutation } from '@tanstack/react-query';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 import { SearchApiPath } from '../../api/apiPaths';
 import { apiRequest2 } from '../../api/apiRequest';
 import { buildRegistrationSearchParams, FetchResultsParams } from '../../api/searchApi';
+import { setNotification } from '../../redux/notificationSlice';
 import { formatDateStringToISO } from '../date-helpers';
 import { triggerFileDownload } from '../downloadFileHelpers';
 
 const maxNumberOfCitations = 500;
 
 export const useBibtexExport = (params: FetchResultsParams) => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+
   const mutation = useMutation({
     mutationFn: async () => {
       const searchParams = buildRegistrationSearchParams({
@@ -27,6 +33,7 @@ export const useBibtexExport = (params: FetchResultsParams) => {
       const currentDate = formatDateStringToISO(new Date());
       triggerFileDownload(blob, `registrations_${currentDate}.bib`);
     },
+    onError: () => dispatch(setNotification({ message: t('feedback.error.download_file'), variant: 'error' })),
   });
 
   return { exportBibTex: mutation.mutate, isFetchingBibtex: mutation.isPending };
