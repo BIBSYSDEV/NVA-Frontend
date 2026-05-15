de0956dba5a860c227fd874142f0bffc3c8a6fd0
nina.andersen <denektenina@gmail.com>
2026-05-15 09:08:31 +0200

    New StatusChip-component with reusable base statuses

---
+++ src/components/_molecules/status-chip/BaseStatusChip.tsx
@@ -0,0 +1,27 @@
+import { Box } from '@mui/material';
+import { ReactNode } from 'react';
+
+interface StatusChipProps {
+  children: ReactNode;
+  bgcolor?: string;
+  paddingY?: string | number;
+}
+
+export const BaseStatusChip = ({ children, bgcolor = 'info.light', paddingY }: StatusChipProps) => {
+  return (
+    <Box
+      sx={{
+        width: 'fit-content',
+        height: 'fit-content',
+        display: 'flex',
+        gap: '0.2rem',
+        alignItems: 'center',
+        p: '0.25rem 0.75rem 0.25rem 0.5rem',
+        borderRadius: '1rem',
+        bgcolor,
+        paddingY,
+      }}>
+      {children}
+    </Box>
+  );
+};
