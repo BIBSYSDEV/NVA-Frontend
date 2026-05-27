b896792fda026d7087e42411efa064e3a58f5ea7
Isar Buzza <isar.buzza@sikt.no>
2026-05-27 10:08:02 +0200

    added heading

--- src/pages/public_registration/details/CitationBox.tsx
+++ src/pages/public_registration/details/CitationBox.tsx
@@ -1,9 +1,10 @@
-import { TextField } from '@mui/material';
+import { TextField, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { JournalType } from '../../../types/publicationFieldNames';
 import { Registration } from '../../../types/registration.types';
 
 const supportedInstanceTypes: string[] = [JournalType.AcademicArticle, JournalType.AcademicLiteratureReview];
+const citationHeadingId = 'citation-box-heading';
 
 interface CitationBoxProps {
   registration: Registration;
@@ -17,19 +18,24 @@ export const CitationBox = ({ registration }: CitationBoxProps) => {
     return null;
   }
 
-  const citation = '';
+  const citation = `Otte, P. P., Adamsone-Fiskovica, A., Žabko, O., Kerge, K., Šūmane, S., Shvaichenko, S., Veveris, A., & Mincyte, D. (2025). A Qualitative Comparative Analysis of cross-sectoral bioresource residue flows from agriculture, forestry, and aquaculture: the crucial role of non-biobased sectors in the development of the circular bioeconomy. Frontiers in Sustainability, 6. https://doi.org/10.3389/frsus.2025.1490685`;
 
   return (
-    <TextField
-      fullWidth
-      multiline
-      minRows={3}
-      maxRows={6}
-      value={citation}
-      slotProps={{
-        input: { readOnly: true },
-        htmlInput: { 'aria-label': t('citation') },
-      }}
-    />
+    <>
+      <Typography id={citationHeadingId} variant="h3">
+        {t('citation')}
+      </Typography>
+      <TextField
+        fullWidth
+        multiline
+        minRows={3}
+        maxRows={6}
+        value={citation}
+        slotProps={{
+          input: { readOnly: true },
+          htmlInput: { 'aria-labelledby': citationHeadingId },
+        }}
+      />
+    </>
   );
 };
