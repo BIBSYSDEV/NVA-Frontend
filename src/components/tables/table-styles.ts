9cd35475c32996924c6ab238af5afb826b1b742e
nina.andersen <denektenina@gmail.com>
2026-05-28 17:15:55 +0200

    NP-51246: Added columns for number of candidates and points to NVI periods table

--- src/components/tables/table-styles.ts
+++ src/components/tables/table-styles.ts
@@ -4,6 +4,10 @@ export const CenteredTableCell = styled(TableCell)({
   textAlign: 'center',
 });
 
+export const RightAlignedTableCell = styled(TableCell)({
+  textAlign: 'right',
+});
+
 export const TableNumberSkeleton = styled(Skeleton)({
   width: '2ch',
   margin: 'auto',
