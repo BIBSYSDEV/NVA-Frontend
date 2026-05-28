8e07d195aaaa9ce0aea9c9ce8d91682a7ff68918
nina.andersen <denektenina@gmail.com>
2026-05-28 13:11:41 +0200

    NP-51268: Changed design of change date dialog

--- src/pages/basic_data/app_admin/UpsertNviPeriodDialog.tsx
+++ src/pages/basic_data/app_admin/UpsertNviPeriodDialog.tsx
@@ -1,11 +1,12 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material';
 import { DatePicker, DateTimePicker } from '@mui/x-date-pickers';
 import { useMutation } from '@tanstack/react-query';
 import { Field, FieldProps, Form, Formik } from 'formik';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router';
 import { createNviPeriod, updateNviPeriod } from '../../../api/scientificIndexApi';
+import { HorizontalBox, VerticalBox } from '../../../components/styled/Wrappers';
 import { setNotification } from '../../../redux/notificationSlice';
 import { NviPeriod } from '../../../types/nvi.types';
 import { dataTestId } from '../../../utils/dataTestIds';
@@ -65,9 +66,7 @@ export const UpsertNviPeriodDialog = ({
       open={location.pathname === UrlPathTemplate.BasicDataNviNew || !!nviPeriod}
       onClose={closeDialog}
       data-testid={dataTestId.basicData.nviPeriod.nviPeriodDialog}>
-      <DialogTitle>
-        {nviPeriod ? t('basic_data.nvi.update_reporting_period') : t('basic_data.nvi.add_reporting_period')}
-      </DialogTitle>
+      <DialogTitle>{nviPeriod ? t('edit_control_period') : t('add_control_period')}</DialogTitle>
       <Formik
         initialValues={nviPeriod ?? emptyNviPeriod}
         onSubmit={async (values) => {
@@ -77,85 +76,91 @@ export const UpsertNviPeriodDialog = ({
         }}>
         {({ values, setFieldValue, isSubmitting }) => (
           <Form>
-            <DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-              <Field name="publishingYear">
-                {({ field }: FieldProps<string>) => (
-                  <DatePicker
-                    label={t('basic_data.nvi.period_year')}
-                    slotProps={{
-                      textField: {
-                        required: true,
-                        inputProps: { 'data-testid': dataTestId.basicData.nviPeriod.nviPeriodYear },
-                      },
-                    }}
-                    disabled={!!nviPeriod}
-                    views={['year']}
-                    value={field.value ? new Date(field.value) : null}
-                    onChange={(newDate) => {
-                      const year = newDate ? newDate.getFullYear().toString() : '';
-                      setFieldValue(field.name, year);
-                      if (year) {
-                        setFieldValue('startDate', new Date(+year, 3, 1).toISOString());
-                        setFieldValue('reportingDate', new Date(+year + 1, 3, 1).toISOString());
-                      }
-                    }}
-                    shouldDisableYear={(date) => {
-                      const thisYear = date.getFullYear();
-                      return nviPeriod?.publishingYear !== thisYear.toString() && yearsWithPeriod.includes(thisYear);
-                    }}
-                    minDate={nviPeriod ? undefined : minNewNviDate}
-                    maxDate={nviPeriod ? undefined : maxNewNviDate}
-                  />
-                )}
-              </Field>
+            <DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+              <Trans t={t} i18nKey="edit_control_period_text" components={{ p: <Typography /> }} />
+              <VerticalBox sx={{ mt: '1rem', gap: '2rem' }}>
+                <Field name="publishingYear">
+                  {({ field }: FieldProps<string>) => (
+                    <DatePicker
+                      label={t('basic_data.nvi.period_year')}
+                      slotProps={{
+                        textField: {
+                          required: true,
+                          inputProps: { 'data-testid': dataTestId.basicData.nviPeriod.nviPeriodYear },
+                        },
+                      }}
+                      disabled={!!nviPeriod}
+                      views={['year']}
+                      value={field.value ? new Date(field.value) : null}
+                      onChange={(newDate) => {
+                        const year = newDate ? newDate.getFullYear().toString() : '';
+                        setFieldValue(field.name, year);
+                        if (year) {
+                          setFieldValue('startDate', new Date(+year, 3, 1).toISOString());
+                          setFieldValue('reportingDate', new Date(+year + 1, 3, 1).toISOString());
+                        }
+                      }}
+                      shouldDisableYear={(date) => {
+                        const thisYear = date.getFullYear();
+                        return nviPeriod?.publishingYear !== thisYear.toString() && yearsWithPeriod.includes(thisYear);
+                      }}
+                      minDate={nviPeriod ? undefined : minNewNviDate}
+                      maxDate={nviPeriod ? undefined : maxNewNviDate}
+                    />
+                  )}
+                </Field>
+                <HorizontalBox sx={{ gap: '1.5rem' }}>
+                  <Field name="startDate">
+                    {({ field }: FieldProps<string>) => (
+                      <DateTimePicker
+                        sx={{ flex: 1 }}
+                        label={t('common.start_date')}
+                        slotProps={{
+                          textField: {
+                            required: true,
+                            inputProps: { 'data-testid': dataTestId.basicData.nviPeriod.nviPeriodStartDate },
+                          },
+                        }}
+                        disabled={!values.publishingYear}
+                        value={field.value ? new Date(field.value) : null}
+                        minDate={values.publishingYear ? new Date(+values.publishingYear, 0, 1) : undefined}
+                        maxDate={values.publishingYear ? new Date(+values.publishingYear + 1, 0, 1) : undefined}
+                        onChange={(newDate, context) => {
+                          if (context.validationError !== 'invalidDate') {
+                            const dateString = newDate ? newDate.toISOString() : '';
+                            setFieldValue(field.name, dateString);
+                          }
+                        }}
+                      />
+                    )}
+                  </Field>
 
-              <Field name="startDate">
-                {({ field }: FieldProps<string>) => (
-                  <DateTimePicker
-                    label={t('common.start_date')}
-                    slotProps={{
-                      textField: {
-                        required: true,
-                        inputProps: { 'data-testid': dataTestId.basicData.nviPeriod.nviPeriodStartDate },
-                      },
-                    }}
-                    disabled={!values.publishingYear}
-                    value={field.value ? new Date(field.value) : null}
-                    minDate={values.publishingYear ? new Date(+values.publishingYear, 0, 1) : undefined}
-                    maxDate={values.publishingYear ? new Date(+values.publishingYear + 1, 0, 1) : undefined}
-                    onChange={(newDate, context) => {
-                      if (context.validationError !== 'invalidDate') {
-                        const dateString = newDate ? newDate.toISOString() : '';
-                        setFieldValue(field.name, dateString);
-                      }
-                    }}
-                  />
-                )}
-              </Field>
-
-              <Field name="reportingDate">
-                {({ field }: FieldProps<string>) => (
-                  <DateTimePicker
-                    label={t('common.end_date')}
-                    slotProps={{
-                      textField: {
-                        required: true,
-                        inputProps: { 'data-testid': dataTestId.basicData.nviPeriod.nviPeriodEndDate },
-                      },
-                    }}
-                    disabled={!values.publishingYear}
-                    value={field.value ? new Date(field.value) : null}
-                    minDate={values.publishingYear ? new Date(+values.publishingYear + 1, 0, 1) : undefined}
-                    maxDate={values.publishingYear ? new Date(+values.publishingYear + 1, 6, 31) : undefined}
-                    onChange={(newDate, context) => {
-                      if (context.validationError !== 'invalidDate') {
-                        const dateString = newDate ? newDate.toISOString() : '';
-                        setFieldValue(field.name, dateString);
-                      }
-                    }}
-                  />
-                )}
-              </Field>
+                  <Field name="reportingDate">
+                    {({ field }: FieldProps<string>) => (
+                      <DateTimePicker
+                        sx={{ flex: 1 }}
+                        label={t('common.end_date')}
+                        slotProps={{
+                          textField: {
+                            required: true,
+                            inputProps: { 'data-testid': dataTestId.basicData.nviPeriod.nviPeriodEndDate },
+                          },
+                        }}
+                        disabled={!values.publishingYear}
+                        value={field.value ? new Date(field.value) : null}
+                        minDate={values.publishingYear ? new Date(+values.publishingYear + 1, 0, 1) : undefined}
+                        maxDate={values.publishingYear ? new Date(+values.publishingYear + 1, 6, 31) : undefined}
+                        onChange={(newDate, context) => {
+                          if (context.validationError !== 'invalidDate') {
+                            const dateString = newDate ? newDate.toISOString() : '';
+                            setFieldValue(field.name, dateString);
+                          }
+                        }}
+                      />
+                    )}
+                  </Field>
+                </HorizontalBox>
+              </VerticalBox>
             </DialogContent>
             <DialogActions sx={{ gap: '0.5rem' }}>
               <Button onClick={closeDialog}>{t('common.cancel')}</Button>
