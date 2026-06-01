6943b2f0d75970f62692c9aab7d4cd8a646f329d
Isar Buzza <isar.buzza@sikt.no>
2026-06-01 15:15:45 +0200

    minor changes

--- src/pages/public_registration/details-panel/_components/citation-box/_utils/citation-helpers.ts
+++ src/pages/public_registration/details-panel/_components/citation-box/_utils/citation-helpers.ts
@@ -13,7 +13,6 @@ interface FormatAuthorListOptions {
 
 const joinNames = (names: string[]): string => {
   if (names.length === 1) return names[0];
-  if (names.length === 2) return `${names[0]}, & ${names[1]}`;
   if (names.length <= 20) return `${names.slice(0, -1).join(', ')}, & ${names[names.length - 1]}`;
   return `${names.slice(0, 19).join(', ')}, ... ${names[names.length - 1]}`;
 };
