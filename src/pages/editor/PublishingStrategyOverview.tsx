2d7b3b127bebf62f4d3e68cc6d6b15e02296a103
Jens Kilde Mjelva <jens.kilde.mjelva@sikt.no>
2026-09-14 11:13:30 +0200

    NP-51734 Hide RRS info page heading when there is no link

--- src/pages/editor/PublishingStrategyOverview.tsx
+++ src/pages/editor/PublishingStrategyOverview.tsx
@@ -101,12 +101,13 @@ export const PublishingStrategyOverview = () => {
                   components={[<Typography key="1" />]}
                 />
 
-                <Typography variant="h3">{t('editor.retention_strategy.rrs_info_page')}</Typography>
-
                 {customer.rightsRetentionStrategy.policyUri && (
-                  <Link href={customer.rightsRetentionStrategy.policyUri} target="_blank" rel="noopener noreferrer">
-                    {customer.rightsRetentionStrategy.policyUri}
-                  </Link>
+                  <>
+                    <Typography variant="h3">{t('editor.retention_strategy.rrs_info_page')}</Typography>
+                    <Link href={customer.rightsRetentionStrategy.policyUri} target="_blank" rel="noopener noreferrer">
+                      {customer.rightsRetentionStrategy.policyUri}
+                    </Link>
+                  </>
                 )}
 
                 <Typography variant="h3">{t('editor.retention_strategy.possible_not_to_follow_rrs')}</Typography>
